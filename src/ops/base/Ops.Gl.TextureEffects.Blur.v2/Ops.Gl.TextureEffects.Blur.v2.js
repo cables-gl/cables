9275fa97bcf8801cea7d3bd7198bf655c5c319e6
tom <tom@undev.de>
2016-04-09 19:21:46 +0200

    much faster blur

--- src/ops/base/Ops.Gl.TextureEffects.Blur.v2/Ops.Gl.TextureEffects.Blur.v2.js
+++ src/ops/base/Ops.Gl.TextureEffects.Blur.v2/Ops.Gl.TextureEffects.Blur.v2.js
@@ -1,49 +1,57 @@
-CABLES.Op.apply(this, arguments);
-var self=this;
 var cgl=this.patch.cgl;
 
 this.name='Blur';
-this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
-this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+var render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+var trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
 
-this.iterations=this.addInPort(new Port(this,"iterations",OP_PORT_TYPE_VALUE));
-this.iterations.val=10;
+var amount=this.addInPort(new Port(this,"amount",OP_PORT_TYPE_VALUE));
+amount.set(10);
 
 var shader=new CGL.Shader(cgl);
 this.onLoaded=shader.compile;
 
 var srcFrag=''
     .endl()+'precision highp float;'
-    .endl()+'#ifdef HAS_TEXTURES'
     .endl()+'  varying vec2 texCoord;'
     .endl()+'  uniform sampler2D tex;'
     .endl()+'  uniform float dirX;'
     .endl()+'  uniform float dirY;'
-    .endl()+'  uniform float width;'
-    .endl()+'  uniform float height;'
-    .endl()+'#endif'
+    .endl()+'  uniform float amount;'
+
+    .endl()+'uniform sampler2D texture;'
+    .endl()+'vec2 delta=vec2(dirX*amount*0.01,dirY*amount*0.01);'
     .endl()+''
-    .endl()+'vec4 blur9(sampler2D texture, vec2 uv, vec2 red, vec2 dir)'
+    .endl()+'float random(vec3 scale, float seed)'
     .endl()+'{'
-    .endl()+'   vec4 color = vec4(0.0);'
-    .endl()+'   vec2 offset1 = vec2(1.3846153846) * dir;'
-    .endl()+'   vec2 offset2 = vec2(3.2307692308) * dir;'
-    .endl()+'   color += texture2D(texture, uv) * 0.2270270270;'
-    .endl()+'   color += texture2D(texture, uv + (offset1 / red)) * 0.3162162162;'
-    .endl()+'   color += texture2D(texture, uv - (offset1 / red)) * 0.3162162162;'
-    .endl()+'   color += texture2D(texture, uv + (offset2 / red)) * 0.0702702703;'
-    .endl()+'   color += texture2D(texture, uv - (offset2 / red)) * 0.0702702703;'
-    .endl()+'   return color;'
+    .endl()+'    return fract(sin(dot(gl_FragCoord.xyz + seed, scale)) * 43758.5453 + seed);'
     .endl()+'}'
     .endl()+''
     .endl()+'void main()'
     .endl()+'{'
-    .endl()+'   vec4 col=vec4(1.0,0.0,0.0,1.0);'
-    .endl()+'   #ifdef HAS_TEXTURES'
-    .endl()+'       col=blur9(tex,texCoord,vec2(width,height),vec2(dirX,dirY));'
-    .endl()+'   #endif'
-    .endl()+'   gl_FragColor = col;'
-    .endl()+'}';
+    .endl()+'    vec4 color = vec4(0.0);'
+    .endl()+'    float total = 0.0;'
+    .endl()+'    '
+    // .endl()+'    /* randomize the lookup values to hide the fixed number of samples */'
+    .endl()+'    float offset = random(vec3(12.9898, 78.233, 151.7182), 0.0);'
+    .endl()+'    '
+    .endl()+'    for (float t = -30.0; t <= 30.0; t++) {'
+    .endl()+'        float percent = (t + offset - 0.5) / 30.0;'
+    .endl()+'        float weight = 1.0 - abs(percent);'
+    .endl()+'        vec4 sample = texture2D(texture, texCoord + delta * percent);'
+    .endl()+'        '
+    // .endl()+'        /* switch to pre-multiplied alpha to correctly blur transparent images */'
+    .endl()+'        sample.rgb *= sample.a;'
+    
+    .endl()+'        color += sample * weight;'
+    .endl()+'        total += weight;'
+    .endl()+'    }'
+    
+    .endl()+'    gl_FragColor = color / total;'
+    
+    // .endl()+'    /* switch back from pre-multiplied alpha */'
+    .endl()+'    gl_FragColor.rgb /= gl_FragColor.a + 0.00001;'
+    .endl()+'}'
+
 
 shader.setSource(shader.getDefaultVertexShader(),srcFrag);
 var textureUniform=new CGL.Uniform(shader,'t','tex',0);
@@ -52,6 +60,9 @@ var uniDirY=new CGL.Uniform(shader,'f','dirY',0);
 
 var uniWidth=new CGL.Uniform(shader,'f','width',0);
 var uniHeight=new CGL.Uniform(shader,'f','height',0);
+
+var uniAmount=new CGL.Uniform(shader,'f','amount',amount.get());
+amount.onValueChange(function(){ uniAmount.setValue(amount.get()); });
 
 var direction=this.addInPort(new Port(this,"direction",OP_PORT_TYPE_VALUE,{display:'dropdown',values:['both','vertical','horizontal']}));
 var dir=0;
@@ -63,7 +74,7 @@ direction.onValueChange(function()
     if(direction.get()=='vertical')dir=2;
 });
 
-this.render.onTriggered=function()
+render.onTriggered=function()
 {
     if(!cgl.currentTextureEffect)return;
     cgl.setShader(shader);
@@ -71,7 +82,7 @@ this.render.onTriggered=function()
     uniWidth.setValue(cgl.currentTextureEffect.getCurrentSourceTexture().width);
     uniHeight.setValue(cgl.currentTextureEffect.getCurrentSourceTexture().height);
 
-    for(var i =0;i<self.iterations.val;i++)
+    // for(var i =0;i<amount.val;i++)
     {
         // first pass
         if(dir===0 || dir==2)
@@ -103,5 +114,5 @@ this.render.onTriggered=function()
     }
 
     cgl.setPreviousShader();
-    self.trigger.trigger();
+    trigger.trigger();
 };
