23c02385d57e8b2ba226a00fda412e84404f6c2b
tom <tom@undev.de>
2016-04-12 14:29:00 +0200

    repeat2d randomarray

--- src/ops/base/Ops.Array.ArrayGetValue/Ops.Array.ArrayGetValue.js
+++ src/ops/base/Ops.Array.ArrayGetValue/Ops.Array.ArrayGetValue.js
@@ -3,10 +3,12 @@ this.name='ArrayGetValue';
 var array=this.addInPort(new Port(this, "array",OP_PORT_TYPE_ARRAY));
 var index=this.addInPort(new Port(this, "index",OP_PORT_TYPE_VALUE,{type:'int'}));
 var value=this.addOutPort(new Port(this, "value",OP_PORT_TYPE_VALUE));
+array.ignoreValueSerialize=true;
+
 
 function update()
 {
-    value.set( array.val[index.get()]);
+    if(array.get())value.set( array.get()[index.get()]);
 }
 
 index.onValueChanged=update;
--- src/ops/base/Ops.Gl.TextureEffects.DepthTexture/Ops.Gl.TextureEffects.DepthTexture.js
+++ src/ops/base/Ops.Gl.TextureEffects.DepthTexture/Ops.Gl.TextureEffects.DepthTexture.js
@@ -1,78 +1,71 @@
-    Op.apply(this, arguments);
-    var self=this;
-    var cgl=this.patch.cgl;
+var cgl=this.patch.cgl;
 
-    this.name='DepthTexture';
+this.name='DepthTexture';
 
-    this.render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
-    this.farPlane=this.addInPort(new Port(this,"farplane",OP_PORT_TYPE_VALUE));
-    this.nearPlane=this.addInPort(new Port(this,"nearplane",OP_PORT_TYPE_VALUE));
-    
-    this.image=this.addInPort(new Port(this,"image",OP_PORT_TYPE_TEXTURE));
-    this.trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
+var render=this.addInPort(new Port(this,"render",OP_PORT_TYPE_FUNCTION));
+var image=this.addInPort(new Port(this,"image",OP_PORT_TYPE_TEXTURE));
+var farPlane=this.addInPort(new Port(this,"farplane",OP_PORT_TYPE_VALUE));
+var nearPlane=this.addInPort(new Port(this,"nearplane",OP_PORT_TYPE_VALUE));
 
-    var shader=new CGL.Shader(cgl);
-    this.onLoaded=shader.compile;
+farPlane.set(100.0);
+nearPlane.set(0.1);
 
-    var srcFrag=''
-        .endl()+'precision highp float;'
-        .endl()+'#ifdef HAS_TEXTURES'
-        .endl()+'  varying vec2 texCoord;'
-        .endl()+'  uniform sampler2D image;'
-        .endl()+'#endif'
-        .endl()+'uniform float n;'
-        .endl()+'uniform float f;'
-        .endl()+''
-        .endl()+'void main()'
-        .endl()+'{'
-        .endl()+'   vec4 col=vec4(0.0,0.0,0.0,1.0);'
-        .endl()+'   #ifdef HAS_TEXTURES'
-        .endl()+'       col=texture2D(image,texCoord);'
-        .endl()+'       float z=col.r;'
-        .endl()+'       float c=(2.0*n)/(f+n-z*(f-n));'
+var trigger=this.addOutPort(new Port(this,"trigger",OP_PORT_TYPE_FUNCTION));
 
-        .endl()+'       col=vec4(c,c,c,1.0);'
+var shader=new CGL.Shader(cgl);
+this.onLoaded=shader.compile;
 
-        // .endl()+'       if(c>=0.999)col.a=0.0;'
-        // .endl()+'           else col.a=1.0;'
-        .endl()+'   #endif'
+var srcFrag=''
+    .endl()+'precision highp float;'
+    .endl()+'#ifdef HAS_TEXTURES'
+    .endl()+'  varying vec2 texCoord;'
+    .endl()+'  uniform sampler2D image;'
+    .endl()+'#endif'
+    .endl()+'uniform float n;'
+    .endl()+'uniform float f;'
+    .endl()+''
+    .endl()+'void main()'
+    .endl()+'{'
+    .endl()+'   vec4 col=vec4(0.0,0.0,0.0,1.0);'
+    .endl()+'   #ifdef HAS_TEXTURES'
+    .endl()+'       col=texture2D(image,texCoord);'
+    .endl()+'       float z=col.r;'
+    .endl()+'       float c=(2.0*n)/(f+n-z*(f-n));'
 
-        .endl()+'   gl_FragColor = col;'
-        .endl()+'}';
+    .endl()+'       col=vec4(c,c,c,1.0);'
 
-    shader.setSource(shader.getDefaultVertexShader(),srcFrag);
-    var textureUniform=new CGL.Uniform(shader,'t','image',0);
+    // .endl()+'       if(c>=0.999)col.a=0.0;'
+    // .endl()+'           else col.a=1.0;'
+    .endl()+'   #endif'
 
-    var uniFarplane=new CGL.Uniform(shader,'f','f',1.0);
-    var uniNearplane=new CGL.Uniform(shader,'f','n',1.0);
+    .endl()+'   gl_FragColor = col;'
+    .endl()+'}';
 
-    this.farPlane.onValueChanged=function()
+shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+var textureUniform=new CGL.Uniform(shader,'t','image',0);
+
+var uniFarplane=new CGL.Uniform(shader,'f','f',1.0);
+var uniNearplane=new CGL.Uniform(shader,'f','n',1.0);
+
+farPlane.onValueChanged=function(){ uniFarplane.setValue(farPlane.get()); };
+
+nearPlane.onValueChanged=function(){ uniNearplane.setValue(nearPlane.get()); };
+
+render.onTriggered=function()
+{
+    if(!cgl.currentTextureEffect)return;
+
+    if(image.val && image.val.tex)
     {
-        uniFarplane.setValue(self.farPlane.get());
-    };
-    self.farPlane.val=100.0;
+        cgl.setShader(shader);
+        cgl.currentTextureEffect.bind();
 
-    this.nearPlane.onValueChanged=function()
-    {
-        uniNearplane.setValue(self.nearPlane.get());
-    };
-    self.nearPlane.val=0.1;
+        cgl.gl.activeTexture(cgl.gl.TEXTURE0);
+        cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, image.get().tex );
 
-    this.render.onTriggered=function()
-    {
-        if(!cgl.currentTextureEffect)return;
+        cgl.currentTextureEffect.finish();
+        cgl.setPreviousShader();
+    }
 
-        if(self.image.val && self.image.val.tex)
-        {
-            cgl.setShader(shader);
-            cgl.currentTextureEffect.bind();
-
-            cgl.gl.activeTexture(cgl.gl.TEXTURE0);
-            cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, self.image.val.tex );
-
-            cgl.currentTextureEffect.finish();
-            cgl.setPreviousShader();
-        }
-
-        self.trigger.trigger();
-    };
+    trigger.trigger();
+};
