13ef1fc1bc9588d09d7a8124d99b2aa4970bde6d
tom <tom@undev.de>
2016-04-14 22:35:27 +0200

    torus texcoords

--- src/ops/base/Ops.Type.TypeOf/Ops.Type.TypeOf.js
+++ src/ops/base/Ops.Type.TypeOf/Ops.Type.TypeOf.js
@@ -1,12 +1,13 @@
 op.name="TypeOf";
 
-var number=op.addInPort(new Port(op,"number1"));
+var input=op.addInPort(new Port(op,"input"));
 var result=op.addOutPort(new Port(op,"result"));
 
 function update()
 {
-    result.set( typeof(number.get()) );
+    result.set( typeof(input.get()) );
 }
 
-number.onValueChanged=update;
+
+input.onValueChanged=update;
 
