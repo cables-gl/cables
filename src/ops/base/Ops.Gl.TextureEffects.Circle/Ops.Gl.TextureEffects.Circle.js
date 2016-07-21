fe5d1e0266116ecfe291fc255f4487ea34e15380
pandur <tom@undev.de>
2016-07-21 12:01:29 +0200

    circle textureeffect

--- src/ops/base/Ops.Gl.TextureEffects.Circle/Ops.Gl.TextureEffects.Circle.js
+++ src/ops/base/Ops.Gl.TextureEffects.Circle/Ops.Gl.TextureEffects.Circle.js
@@ -5,6 +5,8 @@ var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
 var inSize=op.addInPort(new Port(op,"size",OP_PORT_TYPE_VALUE,{display:'range'}));
 var inFadeOut=op.addInPort(new Port(op,"fade Out",OP_PORT_TYPE_VALUE,{display:'range'}));
 
+var warnOverflow=op.addInPort(new Port(op,"warn overflow",OP_PORT_TYPE_VALUE,{display:'bool'}));
+
 
 var r=op.addInPort(new Port(op,"r",OP_PORT_TYPE_VALUE,{ display:'range', colorPick:'true'}));
 var g=op.addInPort(new Port(op,"g",OP_PORT_TYPE_VALUE,{ display:'range' }));
@@ -28,22 +30,38 @@ var srcFrag=''
 
     .endl()+'void main()'
     .endl()+'{'
+    .endl()+'   vec4 texCol=texture2D(tex,texCoord);'
     .endl()+'   vec4 col=texture2D(tex,texCoord);'
     
     .endl()+'   float dist = distance(vec2(0.5,0.5),texCoord);'
+    
+    
+    .endl()+'   float sz=size*0.5;'
+    .endl()+'   float fade=fadeOut+0.002;'
+    .endl()+'   col.a=0.0;'
 
     .endl()+'   col.r=r;'
     .endl()+'   col.g=g;'
     .endl()+'   col.b=b;'
-    .endl()+'   col.a=0.0;'
-    
-    .endl()+'   float sz=size*0.5;'
-    .endl()+'   float fade=fadeOut+0.02;'
-    
-    .endl()+'   if(dist<sz)col.a=1.0;'
-    .endl()+'   if(dist>sz && dist<sz+fade)col.a=1.0-((dist-sz)/(fade));'
 
-    .endl()+'   gl_FragColor = col;'
+    .endl()+'   if(dist<sz)'
+    .endl()+'   {'
+    .endl()+'       col.a=1.0;'
+    .endl()+'   }'
+    .endl()+'   if(dist>sz && dist<sz+fade)col.a=1.0-((dist-sz)/(fade));'
+    
+    .endl()+'   col.a*=a;'
+    
+    .endl()+'   gl_FragColor.rgb = mix(texCol.rgb,vec3(r,g,b),col.a);'
+    .endl()+'   gl_FragColor.a=col.a;'
+
+    .endl()+'   #ifdef WARN_OVERFLOW'
+    .endl()+'       float width=0.02;'
+    .endl()+'       if( texCoord.x>1.0-width || texCoord.y>1.0-width || texCoord.y<width || texCoord.x<width )'
+    .endl()+'           if(col.a>0.0)gl_FragColor = vec4(1.0,0.0,0.0, 1.0);'
+    .endl()+'   #endif'
+
+
     .endl()+'}';
 
 
@@ -67,6 +85,16 @@ var uniformR=new CGL.Uniform(shader,'f','r',r);
 var uniformG=new CGL.Uniform(shader,'f','g',g);
 var uniformB=new CGL.Uniform(shader,'f','b',b);
 var uniformA=new CGL.Uniform(shader,'f','a',a);
+
+
+warnOverflow.onValueChanged=function()
+{
+    if(warnOverflow.get()) shader.define('WARN_OVERFLOW');
+        else shader.removeDefine('WARN_OVERFLOW');
+    shader.compile();
+
+};
+
 
 render.onTriggered=function()
 {
