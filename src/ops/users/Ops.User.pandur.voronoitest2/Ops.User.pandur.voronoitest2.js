91149970f3be02c10b54512d31e1b510c43e9582
pandur <tom@undev.de>
2016-08-02 10:26:44 +0200

    test

--- src/ops/users/Ops.User.pandur.voronoitest2/Ops.User.pandur.voronoitest2.js
+++ src/ops/users/Ops.User.pandur.voronoitest2/Ops.User.pandur.voronoitest2.js
@@ -1,13 +1,12 @@
 op.name="voronoitest2";
 
-var meshes=[];
 
 var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
 
 var voronoi = new Voronoi();
 var bbox = {xl: -140, xr: 140, yt: -80, yb: 80}; // xl is x-left, xr is x-right, yt is y-top, and yb is y-bottom
 var sites=[];
-for(var i=0;i<35;i++)
+for(var i=0;i<75;i++)
 {
     sites.push(
         {
@@ -38,162 +37,198 @@ function ip(x0,x1,x2,t)//Bezier
     var r =(x0 * (1-t) * (1-t) + 2 * x1 * (1 - t)* t + x2 * t * t);
     return r;
 }
+var meshes=[];
+var geoms=[];
+var checksums=[];
+
+    var points=[];
+    var points2=[];
+    
+    points2.length=1000;
+    points.length=1000;
+
 
 var firsttime=true;
 function updateGeom()
 {
+    
     diagram = voronoi.compute(sites, bbox);
-    meshes.length=1;
+    // meshes.length=1;
 
-    var geom=new CGL.Geometry();
+    // var geom=new CGL.Geometry();
     var vertices=[];
+    var texCoords=[];
+    var countMeshUpdates=0;
 
+// console.log(sites[0].voronoiId );
     
-for (var ic = 0; ic < sites.length; ic++) 
-    for(var i=0;i<diagram.cells.length;i++)
+    for (var ic = 0; ic < sites.length; ic++)
     {
-		var cell = diagram.cells[sites[i].voronoiId];
-        
-        var mX=0;
-        var mY=0;
-        for(var j=0;j<cell.halfedges.length;j++)
-        {
-            var edge=cell.halfedges[j].edge;
-            mX+=edge.va.x;
-            mY+=edge.va.y;
-            mX+=edge.vb.x;
-            mY+=edge.vb.y;
-        }
-        
-        mX/=(cell.halfedges.length*2);
-        mY/=(cell.halfedges.length*2);
-
-        var points=[];
-        for(var j=0;j<cell.halfedges.length;j++)
-        {
-            var lastedge=null;
-            var edge=cell.halfedges[j];
-            var smaller=1;//0.8;
-
-// if(firsttime)console.log(j,edge.va.x,edge.vb.x);
-var ep=edge.getEndpoint();
-if(firsttime)console.log( ep );
-
-                points.push( (mX + ((ep.x-mX)*smaller))/100 );
-                points.push( (mY + ((ep.y-mY)*smaller))/100 );
-                points.push( 0 );
-
-        }
-        
-        // if(diagram.cells[i].closeMe)
+        // for(var i=0;i<diagram.cells.length;i++)
         {
             
-            points.push(points[0]);
-            points.push(points[1]);
-            points.push(points[2]);
-    
-            points.push(points[3]);
-            points.push(points[4]);
-            points.push(points[5]);
-    
-            points.push(points[6]);
-            points.push(points[7]);
-            points.push(points[8]);
-
+            var vid=sites[ic].voronoiId;
+            vertices.length=0;
             
-        }
-firsttime=false;
-
-
-        var points2=[];
+            var cell = diagram.cells[vid];
+            // var cell = diagram.cells[i];
     
-        var subd=4;
-        var counter=0;
-        for(var m=3;m<points.length-6;m+=3)
-        {
-            for(j=0;j<subd;j++)
+            var mX=0;
+            var mY=0;
+            var check=0;
+            for(var j=0;j<cell.halfedges.length;j++)
             {
-                counter++;
-                if(counter%4===0)
+                var edge=cell.halfedges[j].edge;
+                mX+=edge.va.x;
+                mY+=edge.va.y;
+                mX+=edge.vb.x;
+                mY+=edge.vb.y;
+                check+=edge.vb.y+edge.vb.x+edge.va.y+edge.va.x;
+            }
+            
+            if(checksums[vid] && checksums[vid]==check)continue;
+
+            checksums[vid]=check;
+            
+            countMeshUpdates++;
+            
+            mX/=(cell.halfedges.length*2);
+            mY/=(cell.halfedges.length*2);
+    
+            var pointsLength=cell.halfedges.length*3+9;
+            var j=0;
+            for(j=0;j<cell.halfedges.length;j++)
+            {
+                var lastedge=null;
+                var edge=cell.halfedges[j];
+                var ep=edge.getEndpoint();
+
+                if(firsttime)console.log( ep );
+
+                points[j*3+0]=( ( (ep.x-mX))/100 );
+                points[j*3+1]=( ( (ep.y-mY))/100 );
+                points[j*3+2]=( 0 );
+            }
+            
+            points[j*3+0]=points[0];
+            points[j*3+1]=points[1];
+            points[j*3+2]=points[2];
+            points[j*3+3]=points[3];
+            points[j*3+4]=points[4];
+            points[j*3+5]=points[5];
+            points[j*3+6]=points[6];
+            points[j*3+7]=points[7];
+            points[j*3+8]=points[8];
+            
+            // points.push(points[0]);
+            // points.push(points[1]);
+            // points.push(points[2]);
+    
+            // points.push(points[3]);
+            // points.push(points[4]);
+            // points.push(points[5]);
+    
+            // points.push(points[6]);
+            // points.push(points[7]);
+            // points.push(points[8]);
+
+            firsttime=false;
+
+            
+//             var subd=2;
+//             // points2.length=points.length;
+//             var counter=0;
+//             var c=0;
+// // console.log('b  l ',points.length);
+//             for(var m=3;m<pointsLength-6;m+=3)
+//             {
+//                 for(j=0;j<subd;j++)
+//                 {
+//                     counter++;
+//                     if(counter%2===0)
+//                         for(k=0;k<3;k++)
+//                         {
+//                             var p=ip(
+//                                 (points[m+k-3]+points[m+k])/2,
+//                                 points[m+k+0],
+//                                 (points[m+k+3]+points[m+k+0])/2,
+//                                 j/subd
+//                                 );
+
+//                             if(k==0) points2[c]=p+mX/100;
+//                             if(k==1) points2[c]=p+mY/100;
+//                             c++;
+//                         }
+//                 }
+//             }
+            
+//             points2[c+0]=points2[0];
+//             points2[c+1]=points2[1];
+//             points2[c+2]=points2[2];
+//             points2[c+3]=points2[3];
+//             points2[c+4]=points2[4];
+//             points2[c+5]=points2[5];
+//             points2[c+6]=points2[6];
+//             points2[c+7]=points2[7];
+//             points2[c+8]=points2[8];
+
+            points2=points;
+
+            var subd=8;
+            vertices.length=pointsLength*(subd-1);
+
+            vertices[0]=(mX/100);
+            vertices[1]=(mY/100);
+            vertices[2]=0;
+
+// console.log('p2l',points2.length);
+
+            var c=0;
+            for(var m=3;m<pointsLength-3;m+=3)
+            {
+                for(j=0;j<subd;j++)
+                {
                     for(k=0;k<3;k++)
                     {
                         var p=ip(
-                            (points[m+k-3]+points[m+k])/2,
-                            points[m+k+0],
-                            (points[m+k+3]+points[m+k+0])/2,
+                            (points2[m+k-3]+points2[m+k])/2,
+                            points2[m+k+0],
+                            (points2[m+k+3]+points2[m+k+0])/2,
                             j/subd
                             );
-    
-                        points2.push(p);
+                            
+                        if(k==0) vertices[c]=p*0.86+mX/100;
+                            else if(k==1) vertices[c]=p*0.86+mY/100;
+                                else vertices[c]=p;
+                        c++;
+
                     }
-            }
-        }
-        
-        points2.push(points2[0]);
-        points2.push(points2[1]);
-        points2.push(points2[2]);
-        points2.push(points2[3]);
-        points2.push(points2[4]);
-        points2.push(points2[5]);
-        points2.push(points2[6]);
-        points2.push(points2[7]);
-        points2.push(points2[8]);
-    
-
-
-
-
-        var subd=6;
-        for(var m=3;m<points.length-6;m+=3)
-        {
-            for(j=0;j<subd;j++)
-            {
-                for(k=0;k<3;k++)
-                {
-                    var p=ip(
-                        (points2[m+k-3]+points2[m+k])/2,
-                        points2[m+k+0],
-                        (points2[m+k+3]+points2[m+k+0])/2,
-                        j/subd
-                        );
-
-                    vertices.push(p);
                 }
             }
+            // console.log('vl',vertices.length);
+    
+            
+            var rnd=Math.random();
+            texCoords.length=vertices.length/3*2;
+            for(var m=0;m<texCoords.length;m++)texCoords[m]=ic/sites.length;
+            
+            if(!geoms[vid])geoms[vid]=new CGL.Geometry();
+            geoms[vid].vertices=vertices;
+            geoms[vid].texCoords=texCoords;
+            // geom.setPointVertices(vertices);
+            if(!meshes[vid]) meshes[vid]=new CGL.Mesh(op.patch.cgl,geoms[vid]);
+                else meshes[vid].setGeom(geoms[vid]);
+            
+      
         }
-
-
-
-        // for(var j=0;j<diagram.cells[i].halfedges.length;j++)
-        // {
-        //     var edge=diagram.cells[i].halfedges[j].edge;
-        //     var smaller=0.9;
-
-        //     vertices.push( (mX+(edge.va.x-mX )*smaller)/100 );
-        //     vertices.push( (mY+(edge.va.y-mY )*smaller)/100 );
-        //     vertices.push( 0 );
-    
-        //     vertices.push( (mX+(edge.vb.x-mX )*smaller)/100 );
-        //     vertices.push( (mY+(edge.vb.y-mY )*smaller)/100 );
-        //     vertices.push( 0 );
-
-        //     vertices.push( (mX )/100 );
-        //     vertices.push( (mY )/100  );
-        //     vertices.push( 0 );
-        // }        
     }
-    
     // op.log('voro verts:'+vertices.length);
-    
-    geom.setPointVertices(vertices);
-    meshes[0]=new CGL.Mesh(op.patch.cgl,geom);
+
+// op.log('countMeshUpdates '+countMeshUpdates);
 
 }
 
-
 updateGeom();
-
-
 
 render.onTriggered=function()
 {
@@ -207,12 +242,14 @@ render.onTriggered=function()
     sites[20].y=-1*Math.sin(Date.now()*0.001)*40+0;
     sites[22].y=Math.sin(Date.now()*0.001)*40+0;
     sites[25].y=-1*Math.sin(Date.now()*0.001)*40+0;
-    voronoi.recycle(diagram);
+    // voronoi.recycle(diagram);
     updateGeom();
     
     shader=cgl.getShader();
     if(!shader)return;
     oldPrim=shader.glPrimitive;
+    
+    shader.glPrimitive=cgl.gl.TRIANGLE_FAN;
 
     for(var i in meshes)
     {
