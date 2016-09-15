bf0477dd0cf362ebc915888b9eb990d7b55ee5a1
pandur <tom@undev.de>
2016-09-15 22:02:40 +0200

    viewmatrix operations

---
+++ src/ops/base/Ops.Gl.Matrix.TranslateView/Ops.Gl.Matrix.TranslateView.js
@@ -0,0 +1,25 @@
+op.name="TranslateView";
+
+var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
+var trigger=op.addOutPort(new Port(op,"trigger",OP_PORT_TYPE_FUNCTION));
+
+var cgl=op.patch.cgl;
+
+var x=op.addInPort(new Port(op,"x"));
+var y=op.addInPort(new Port(op,"y"));
+var z=op.addInPort(new Port(op,"z"));
+
+x.set(0.0);
+y.set(0.0);
+z.set(0.0);
+
+var vec=vec3.create();
+
+render.onTriggered=function()
+{
+    vec3.set(vec, x.get(),y.get(),z.get());
+    cgl.pushViewMatrix();
+    mat4.translate(cgl.vMatrix,cgl.vMatrix, vec);
+    trigger.trigger();
+    cgl.popViewMatrix();
+};
