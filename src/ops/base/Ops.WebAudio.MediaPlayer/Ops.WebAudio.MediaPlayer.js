36be630979b9d4da2584fcd0271a549b627b8222
Tim Pulver <git@timpulver.de>
2016-10-04 14:58:44 +0200

    Add MediaPlayer-op

---
+++ src/ops/base/Ops.WebAudio.MediaPlayer/Ops.WebAudio.MediaPlayer.js
@@ -0,0 +1,93 @@
+op.name="MediaPlayer";
+
+/* ports in */
+var volume = op.addInPort(new Port(op,"Volume",OP_PORT_TYPE_VALUE,{ display:'range' }));
+var file = op.addInPort(new Port(op,"File",OP_PORT_TYPE_VALUE,{ display:'file',filter:'audio' }));
+var play = op.addInPort(new Port(op,"Play", OP_PORT_TYPE_FUNCTION,{ display:'button' }));
+var pause = op.addInPort(new Port(op,"Pause", OP_PORT_TYPE_FUNCTION,{ display:'button' }));
+var rewind = op.addInPort(new Port(op,"Rewind", OP_PORT_TYPE_FUNCTION,{ display:'button' }));
+var seekPosition = op.addInPort(new Port(op,"Seek Position (Seconds)",OP_PORT_TYPE_VALUE));
+var jumpToSeekPosition = op.addInPort(new Port(op,"Jump To Seek Position", OP_PORT_TYPE_FUNCTION,{ display:'button' }));
+
+/* ports out */
+var audioOut = op.addOutPort(new Port(op, "Audio Out",OP_PORT_TYPE_OBJECT));
+var duration = op.addOutPort(new Port(op,"Duration",OP_PORT_TYPE_VALUE));
+
+/* port default values*/
+volume.set(1.0);
+duration.set(0);
+
+/* audio context create */
+if(!window.audioContext) {
+    if('webkitAudioContext' in window) {
+        audioContext = new webkitAudioContext();
+    } else {
+        audioContext = new AudioContext();
+    }
+}
+/* browser check */
+if(!window.audioContext) {
+    if(op.patch.config.onError) op.patch.config.onError('sorry, could not initialize WebAudio. Please check if your Browser supports WebAudio');
+}
+
+/* vars */
+var audio = null;
+var media = null;
+var source = null;
+var isPlaying = false;
+
+function playbackEnded(){
+    isPlaying = false;
+}
+
+function rewindPlayback(){
+    if(audio) { audio.currentTime = 0; }
+}
+
+function handleSeek(){
+    if(audio) { audio.currentTime = seekPosition.get(); }
+}
+
+
+function startPlayback(){
+    if(audio) { audio.play(); }
+}
+
+function pausePlayback(){
+    if(audio && !audio.paused) { audio.pause(); }
+}
+
+function loadMediaFile(){
+    if(!file.get()){ return; }
+    audio = new Audio();
+    audio.crossOrigin = "anonymous";
+    audio.src = file.get();
+    media = audioContext.createMediaElementSource(audio);
+    audio.addEventListener('loadedmetadata', function(){
+        duration.set(audio.duration);
+    },false);
+    audioOut.set(media);
+}
+
+function handleFileChange(){
+    loadMediaFile();
+}
+
+op.onDelete=function() {
+    if(audio) audio.pause();
+    // TODO: We may need to delete this properly...
+};
+
+function handleVolumeChange() {
+    if(audio) {
+        audio.volume = volume.get();
+    }
+}
+
+play.onTriggered = startPlayback;
+pause.onTriggered = pausePlayback;
+rewind.onTriggered = rewindPlayback;
+seekPosition.onValueChanged = handleSeek;
+jumpToSeekPosition.onTriggered = handleSeek;
+file.onValueChanged = handleFileChange;
+volume.onValueChanged = handleVolumeChange;
