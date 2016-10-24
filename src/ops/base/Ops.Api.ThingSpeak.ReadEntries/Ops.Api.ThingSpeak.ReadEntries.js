84fee718d4cd5b1505bfae300567201aaccdfa93
Tim Pulver <git@timpulver.de>
2016-10-24 18:01:38 +0200

    Add ThingSpeak ops

---
+++ src/ops/base/Ops.Api.ThingSpeak.ReadEntries/Ops.Api.ThingSpeak.ReadEntries.js
@@ -0,0 +1,60 @@
+op.name="ReadEntries";
+
+// see https://de.mathworks.com/help/thingspeak/get-a-channel-feed.html
+
+var dummyKey = "12345";
+var dummyChannelId = "12345";
+
+var read = op.addInPort( new Port( this, "Read", OP_PORT_TYPE_FUNCTION, { "display": "button" } ));
+var channelId = op.inValue("Channel ID", dummyChannelId);
+var readApiKey = op.inValueString("Read API Key", dummyKey);
+
+// https://api.thingspeak.com/channels/12345/feeds.json?results=2
+var apiUrlPart1 = "https://api.thingspeak.com/channels/";
+var apiUrlPart2 = "/feeds.json";
+
+var finished = op.outFunction("When Finished");
+var entries = op.outArray("Entries");
+var channelInfos = op.outObject("Channel Infos");
+var success = op.outValue("Success", false);
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4) {
+            if(xmlHttp.status == 200){
+                callback(JSON.parse(xmlHttp.responseText));
+            } else {
+                op.log("ThingSpeak Error! Status: " + xmlHttp.status + ', ' + xmlHttp.statusText + ", see https://de.mathworks.com/help/thingspeak/error-codes.html");
+            }
+        }
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+function handleResponse(response) {
+    if(response == -1) {
+        success.set(false);
+        op.uiAttr( { 'error': 'Error: You do not have access to the channel!' } );
+    } else {
+        entry.set(response.feeds);
+        channelInfos.set(response.channel);
+        success.set(true);
+        // TODO: Remove UI-Error Attribute
+    }
+    finished.trigger();
+}
+
+read.onTriggered = function(){
+    success.set(false);
+    if(channelId.get() !== dummyChannelId) {
+        var requestUrl = apiUrlPart1 + channelId.get() + apiUrlPart2;
+        if(readApiKey.get() != dummyKey){
+            requestUrl += "?" + "api_key=" + readApiKey.get();
+        }
+        httpGetAsync(requestUrl, handleResponse);
+    } else {
+        op.uiAttr( { 'error': 'You need to enter a channel Id!' } );
+    }
+};
---
+++ src/ops/base/Ops.Api.ThingSpeak.Write/Ops.Api.ThingSpeak.Write.js
@@ -0,0 +1,59 @@
+op.name="ThingSpeakWrite";
+
+var dummyKey = "12345";
+
+var write = op.addInPort( new Port( this, "Write", OP_PORT_TYPE_FUNCTION, { "display": "button" } ));
+var field= op.inValueString("Field", "field1");
+var value = op.inValueString("Value", "");
+var writeApiKey = op.inValueString("Write API Key", dummyKey);
+
+var apiUrl = "https://api.thingspeak.com/update?";
+
+var finished = op.outFunction("When Finished");
+var success = op.outValue("Success", false);
+var rows = op.outValue("Rows", 0);
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4) {
+            if(xmlHttp.status == 200){
+                callback(xmlHttp.responseText);
+            } else {
+                op.log("ThingSpeak Error! Status: " + xmlHttp.status + ', ' + xmlHttp.statusText + ", see https://de.mathworks.com/help/thingspeak/error-codes.html");
+            }
+        }
+    }
+    xmlHttp.open("POST", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+function handleResponse(response) {
+    if(isNaN(response)) {
+        op.uiAttr( { 'error': 'Error: Write failed. Response: ' + response } );
+        success.set(false);
+        rows.set(0);
+    } else {
+        if(response == 0) {
+            success.set(false);
+            rows.set(0);
+            op.uiAttr( { 'error': 'Error: Write failed. Maybe you write too often!? Response: ' + response } );
+        } else {
+            rows.set(response);
+            success.set(true);
+            // TODO: Remove UI-Error Attribute
+        }
+    }
+    finished.trigger();
+}
+
+write.onTriggered = function(){
+    success.set(false);
+    if(writeApiKey.get() !== dummyKey) {
+        var requestUrl = apiUrl + "api_key=" + writeApiKey.get() + "&" + field.get() + "=" + value.get();
+        httpGetAsync(requestUrl, handleResponse);
+
+    } else {
+        op.uiAttr( { 'error': 'API Key or API URL invalid!' } );
+    }
+};
