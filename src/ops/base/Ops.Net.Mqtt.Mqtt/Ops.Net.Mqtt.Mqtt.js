f9e1a513af28b4c1d43b071d149ebc9e86e29e5e
Tim Pulver <git@timpulver.de>
2016-10-24 15:05:21 +0200

    Add MQTT ops (v1)

---
+++ src/ops/base/Ops.Net.Mqtt.Mqtt/Ops.Net.Mqtt.Mqtt.js
@@ -0,0 +1,147 @@
+op.name="Mqtt";
+
+var mqttClientName = op.inValueString("Client Name", "Carolin Cable");
+var mqttServer = op.inValueString("Broker URL", "broker.shiftr.io");
+var mqttUsername = op.inValueString("Username", "try");
+var mqttPassword = op.inValueString("Password", "try");
+var mqttPort = op.inValue("Port", 443);
+var useSsl = op.inValueBool("Use SSL", true);
+var reconnect = op.addInPort( new Port( this, "Reconnect", OP_PORT_TYPE_FUNCTION, { "display": "button" } ));
+
+var outObj = op.outObject("MQTT Object");
+var connectedPort = op.outValue("Connected", false);
+
+var TIMEOUT = 5;
+
+var client = null;
+var mqttCablesObj = null;
+var topicMap = {};
+
+op.onLoaded = function(){
+    outObj.set(null);
+    initLib();
+    connect();
+    reconnect.onTriggered = handleReconnect;
+};
+
+function initLib(){
+    console.log("Initiating MQTT lib");
+    client = new Paho.MQTT.Client(mqttServer.get(), mqttPort.get(), mqttClientName.get());
+    client.onMessageArrived = onMessageArrived;
+}
+
+// called when the client connects
+var onConnect = function() {
+    op.log("MQTT connection established");
+    constructMqttObject();
+    //outObj.set(undefined);
+    outObj.set(mqttCablesObj);
+    connectedPort.set(true);
+};
+
+var onFailure = function(res) {
+    op.log("MQTT Connection Failure, hit reconnect to try again...");
+    op.log(res.errorMessage);
+    mqttCablesObj.connected = false;
+    outObj.set(null);
+    connectedPort.set(false);
+};
+
+var onMessageArrived = function(message) {
+    constructMqttObject();
+    mqttCablesObj.message = message;
+    // inform others about change
+    outObj.set(null);
+    outObj.set(mqttCablesObj);
+};
+
+function constructMqttObject(){
+    mqttCablesObj = {};
+    mqttCablesObj.objectType = "mqtt";
+    //mqttCablesObj.client = client;
+    mqttCablesObj.requestSubscribe = requestSubscribe;
+    mqttCablesObj.requestUnsubscribe = requestUnsubscribe;
+    mqttCablesObj.requestSend = requestSend;
+    mqttCablesObj.connected = true;
+}
+
+function requestSend(message, topic){
+    var messageObj = new Paho.MQTT.Message(message);
+    messageObj.destinationName = topic;
+    try{
+        client.send(messageObj);
+    } catch(e) {
+        op.log("MQTTSend: Could not send message " + message + " to " + topic);
+    }
+}
+
+function requestSubscribe(topic){
+    console.log("Reqeust subscribe for: " + topic);
+    // already subscribed
+    if(topicMap.hasOwnProperty(topic)){
+        topicMap[topic]++;
+    } else {
+        topicMap[topic] = 1;
+        try{
+            client.subscribe(topic);
+        } catch(e) {
+            op.log("Could not subscribe: " + e);
+        }
+    }
+}
+
+function requestUnsubscribe(topic){
+    // check if topic is subscribed
+    if(topicMap.hasOwnProperty(topic)){
+        // only one op subscribed this topic, do unsubscribe
+        if(topicMap[topic] <= 1) {
+            try{
+                client.unsubscribe(topic);
+            } catch(e) {
+                op.log("Could not unsubscribe: " + e);
+            }
+            delete topicMap[topic];
+        } else { // another op subscribed this topic, do not unsubscribe
+            topicMap[topic]--;
+        }
+    } else {
+        // not subscribed, do nothing
+    }
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+    op.log("MQTT Connection Lost... :/");
+    op.log("Hit Reconnect-button to try again");
+  if (responseObject.errorCode !== 0) {
+    op.log("MQTT error message: " + responseObject.errorMessage);
+  }
+  mqttCablesObj.connected = false;
+  outObj.set(null);
+  //mqttCablesObj = null;
+  connectedPort.set(false);
+  connect(); // try again
+}
+
+/*
+ * Called once after op finished loading, can be triggered by pressing reconnect-button
+*/
+function connect(){
+    op.log("MQTT Trying to connect to broker...");
+    outObj.set(null);
+
+    // connect the client
+    client.connect({
+        onSuccess: onConnect,
+        onFailure: onFailure,
+        userName: mqttUsername.get(),
+        password: mqttPassword.get(),
+        useSSL: useSsl.get(),
+        timeout: TIMEOUT
+    });
+}
+
+function handleReconnect(){
+    console.log("Reconnecting...");
+    connect();
+}
---
+++ src/ops/base/Ops.Net.Mqtt.MqttSend/Ops.Net.Mqtt.MqttSend.js
@@ -0,0 +1,19 @@
+op.name="MqttSend";
+
+var mqttObj = op.inObject("MQTT Object");
+var message = op.inValue("Message", "Hello Cables");
+var channel = op.inValueString("Channel", "/test");
+var send = op.addInPort( new Port( this, "Send", OP_PORT_TYPE_FUNCTION, { "display": "button" } ));
+
+var mqttOutObj = op.outObject("MQTT Object");
+
+send.onTriggered = function(){
+    var o = mqttObj.get();
+    if(o && o.requestSend) {
+        o.requestSend(message.get(), channel.get());
+    } else {
+        op.log("MQTTSend Error: Could not send, MQTT object not defined / not complete");
+    }
+    // inform others on change
+    mqttOutObj.set(o);
+};
