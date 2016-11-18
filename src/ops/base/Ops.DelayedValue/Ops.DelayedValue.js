1b59df869f7eebbd5b5f5990c74c15edb1af7602
pandur <tom@undev.de>
2016-11-18 11:07:57 +0100

    new op: delayed value

---
+++ src/ops/base/Ops.DelayedValue/Ops.DelayedValue.js
@@ -0,0 +1,23 @@
+op.name="DelayedValue";
+
+var exe=op.inFunction("Update");
+var v=op.inValue("Value",0);
+var delay=op.inValue("Delay",0.5);
+var result=op.outValue("Result",0);
+
+var anim=new CABLES.TL.Anim();
+anim.createPort(op,"easing",function(){});
+
+exe.onTriggered=function()
+{
+    result.set(anim.getValue( op.patch.freeTimer.get() )||0);
+};
+
+v.onChange=function()
+{
+    var current=anim.getValue( op.patch.freeTimer.get() );
+    var t=op.patch.freeTimer.get();
+    anim.clear(t);
+    // anim.setValue(t,current);
+    anim.setValue(t+delay.get(),v.get());
+};
