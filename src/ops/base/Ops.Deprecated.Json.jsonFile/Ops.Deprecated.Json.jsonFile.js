c5c5b665ccf2c566f311836dcbc7fa340020371b
pandur <tom@undev.de>
2016-12-05 17:15:58 +0100

    jsonfile

---
+++ src/ops/base/Ops.Deprecated.Json.jsonFile/Ops.Deprecated.Json.jsonFile.js
@@ -0,0 +1,39 @@
+op.name='jsonFile';
+
+var filename=op.addInPort(new Port(op,"file",OP_PORT_TYPE_VALUE,{ display:'file',type:'string',filter:'json' } ));
+var result=op.addOutPort(new Port(op,"result",OP_PORT_TYPE_OBJECT));
+
+result.ignoreValueSerialize=true;
+var patch=op.patch;
+
+var loadingId=0;
+var reload=function()
+{
+    if(!filename.get())return;
+    
+    patch.loading.finished(loadingId);
+    
+    loadingId=patch.loading.start('jsonFile',''+filename.get());
+
+    CABLES.ajax(
+        patch.getFilePath(filename.get()),
+        function(err,_data,xhr)
+        {
+            try
+            {
+                var data=JSON.parse(_data);
+                if(result.get())result.set(null);
+                result.set(data);
+                op.uiAttr({'error':''});
+                patch.loading.finished(loadingId);
+            }
+            catch(e)
+            {
+                op.uiAttr({'error':'error loading json'});
+                patch.loading.finished(loadingId);
+            }
+        });
+    
+};
+
+filename.onValueChanged=reload;
