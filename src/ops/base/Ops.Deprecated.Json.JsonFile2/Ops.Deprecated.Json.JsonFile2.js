a9efce723215e4742bf9258ce6b480da3466de49
pandur <tom@undev.de>
2016-12-05 17:16:42 +0100

    rename

---
+++ src/ops/base/Ops.Deprecated.Json.JsonFile2/Ops.Deprecated.Json.JsonFile2.js
@@ -0,0 +1,62 @@
+op.name="JsonFile2";
+
+var filename=op.addInPort(new Port(op,"file",OP_PORT_TYPE_VALUE,{ display:'file',type:'string',filter:'json' } ));
+var outData=op.addOutPort(new Port(op,"data",OP_PORT_TYPE_OBJECT));
+var isLoading=op.outValue("Is Loading",false);
+
+var jsonp=op.inValueBool("JsonP",false);
+
+outData.ignoreValueSerialize=true;
+var patch=op.patch;
+
+
+filename.onChange=delayedReload;
+jsonp.onChange=delayedReload;
+var loadingId=0;
+
+
+var reloadTimeout=0;
+
+function delayedReload()
+{
+    clearTimeout(reloadTimeout);
+    reloadTimeout=setTimeout(reload,100);
+}
+
+function reload()
+{
+    if(!filename.get())return;
+    
+    patch.loading.finished(loadingId);
+    
+    loadingId=patch.loading.start('jsonFile',''+filename.get());
+    isLoading.set(true);
+
+    var f=CABLES.ajax;
+    if(jsonp.get())f=CABLES.jsonp;
+
+    f(
+        patch.getFilePath(filename.get()),
+        function(err,_data,xhr)
+        {
+            try
+            {
+                var data=_data;
+                if(typeof data === 'string') data=JSON.parse(_data);
+
+                if(outData.get())outData.set(null);
+                outData.set(data);
+                op.uiAttr({'error':''});
+                patch.loading.finished(loadingId);
+                isLoading.set(false);
+            }
+            catch(e)
+            {
+                op.uiAttr({'error':'error loading json'});
+                patch.loading.finished(loadingId);
+                isLoading.set(false);
+            }
+        });
+    
+};
+
