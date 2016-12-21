d36cd9eeaa0d0b52e7dfcde79b58cadd9bb7cdfb
pandur <tom@undev.de>
2016-12-21 13:50:23 +0100

    new ops

---
+++ src/ops/base/Ops.ObjectToJSON/Ops.ObjectToJSON.js
@@ -0,0 +1,23 @@
+op.name="ObjectToJson";
+
+var obj=op.inObject("Object");
+var result=op.outValue("Result");
+
+var pretty=op.inValueBool("Pretty Print",true);
+
+pretty.onChange=update;
+obj.onChange=update;
+
+function update()
+{
+    try
+    {
+        if(pretty.get()) result.set(JSON.stringify(obj.get(),null,4));
+            else result.set(JSON.stringify(obj.get()));
+    }
+    catch(e)
+    {
+        result.set("error");
+    }
+}
+
---
+++ src/ops/base/Ops.String.ParseInt/Ops.String.ParseInt.js
@@ -0,0 +1,12 @@
+op.name="ParseInt";
+
+var str=op.inValueString("String");
+var outNum=op.outValue("Number");
+
+str.onChange=function()
+{
+    var num=parseInt(str.get());
+    if(num!=num) num=0;
+    outNum.set(num);
+};
+
