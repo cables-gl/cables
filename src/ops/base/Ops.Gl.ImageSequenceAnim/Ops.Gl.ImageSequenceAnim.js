8d39c1dabdef28a6f4492f65c8b155e3eed88959
pandur <tom@undev.de>
2017-01-07 13:28:46 +0100

    keys / cursor keys

--- src/ops/base/Ops.Gl.ImageSequenceAnim/Ops.Gl.ImageSequenceAnim.js
+++ src/ops/base/Ops.Gl.ImageSequenceAnim/Ops.Gl.ImageSequenceAnim.js
@@ -3,9 +3,12 @@ op.name="ImageSequenceAnim";
 var exe=op.addInPort(new Port(op,"Exe",OP_PORT_TYPE_FUNCTION));
 var next=op.addOutPort(new Port(op,"Next",OP_PORT_TYPE_FUNCTION));
 var fps=op.addInPort(new Port(op,"FPS",OP_PORT_TYPE_VALUE));
+var inOffsetY=op.addInPort(new Port(op,"Row Offset Y",OP_PORT_TYPE_VALUE));
 var numFrames=op.addInPort(new Port(op,"Num Frames",OP_PORT_TYPE_VALUE));
 var numX=op.addInPort(new Port(op,"Num X",OP_PORT_TYPE_VALUE));
 var numY=op.addInPort(new Port(op,"Num Y",OP_PORT_TYPE_VALUE));
+
+var play=op.inValueBool("Play",true);
 
 var texRepeatX=op.addOutPort(new Port(op,"Repeat X",OP_PORT_TYPE_VALUE));
 var texRepeatY=op.addOutPort(new Port(op,"Repeat Y",OP_PORT_TYPE_VALUE));
@@ -25,14 +28,22 @@ var posY=0;
 var lastSwitch=0;
 var frame=0;
 
+
+
 function setRepeat()
 {
     texRepeatY.set(1.0/numY.get());
     texRepeatX.set(1.0/numX.get());
 }
 
+inOffsetY.onChange=function()
+{
+    posY=inOffsetY.get();
+};
+
 exe.onTriggered=function()
 {
+    if(play.get())
     if(op.patch.freeTimer.getMillis()-lastSwitch>1000/fps.get())
     {
         lastSwitch=op.patch.freeTimer.getMillis();
@@ -50,7 +61,7 @@ exe.onTriggered=function()
         {
             frame=0;
             posX=0;
-            posY=0;
+            posY=inOffsetY.get();
         }
 
         texU.set(texRepeatX.get()*posX);
