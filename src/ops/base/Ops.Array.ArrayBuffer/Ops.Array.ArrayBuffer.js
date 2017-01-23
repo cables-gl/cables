cd21af8680910384d70e99d6b9efa04508d31fac
pandur <tom@undev.de>
2017-01-23 19:46:11 +0100

    deprecated performance / new op arraybuffer

---
+++ src/ops/base/Ops.Array.ArrayBuffer/Ops.Array.ArrayBuffer.js
@@ -0,0 +1,33 @@
+op.name="ArrayStore";
+
+
+var exec=op.inFunction("exec");
+
+var val=op.inValue("Value");
+
+var arr=[];
+
+var arrOut=op.outArray("Result");
+
+var maxLength=op.inValue("Max Length",100);
+arrOut.set(arr);
+
+maxLength.onChange=reset;
+reset();
+
+function reset()
+{
+    arr.length=maxLength.get();
+    for(var i=0;i<arr.length;i++) arr[i]=0;
+}
+
+exec.onTriggered=function()
+{
+
+    for(var i=1;i<arr.length;i++)arr[i-1]=arr[i];
+
+    arr[arr.length-1]=val.get();
+    
+
+
+};
---
+++ src/ops/base/Ops.Deprecated.Gl.Performance/Ops.Deprecated.Gl.Performance.md
@@ -0,0 +1,12 @@
+# Performance
+
+*Ops.Gl.Performance*  
+
+Displays a small window on the bottom right side of the screen which shows the framerate over time. This can be handy for debugging purposes.
+
+## Input
+
+### Execute
+
+*Type: Function*  
+Connect e.g. to [MainLoop](../Ops.Gl.MainLoop/Ops.Gl.MainLoop.md)-op
