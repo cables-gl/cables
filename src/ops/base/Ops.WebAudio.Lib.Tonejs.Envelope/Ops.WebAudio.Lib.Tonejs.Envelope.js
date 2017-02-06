824c7dd7bedd18b0199822eb1ee7fa086f8ca68e
Tim Pulver <git@timpulver.de>
2017-02-06 18:24:26 +0100

    Make envelope great again

--- src/ops/base/Ops.WebAudio.Lib.Tonejs.Envelope/Ops.WebAudio.Lib.Tonejs.Envelope.js
+++ src/ops/base/Ops.WebAudio.Lib.Tonejs.Envelope/Ops.WebAudio.Lib.Tonejs.Envelope.js
@@ -8,6 +8,10 @@ var attackPort = op.addInPort( new Port( this, "Attack", OP_PORT_TYPE_VALUE, { '
 var decayPort = op.addInPort( new Port( this, "Decay", OP_PORT_TYPE_VALUE, { 'display': 'range', 'min': 0, 'max': 1 } ));
 var sustainPort = op.addInPort( new Port( this, "Sustain", OP_PORT_TYPE_VALUE, { 'display': 'range', 'min': 0, 'max': 1 } ));
 var releasePort = op.addInPort( new Port( this, "Release", OP_PORT_TYPE_VALUE, { 'display': 'range', 'min': 0, 'max': 1 } ));
+var triggerAttackPort = op.addInPort( new Port( this, "Trigger Attack", OP_PORT_TYPE_FUNCTION, { "display": "button" } ));
+var triggerReleasePort = op.addInPort( new Port( this, "Trigger Release", OP_PORT_TYPE_FUNCTION, { "display": "button" } ));
+var timePort = op.inValueString("Time");
+var velocityPort = op.addInPort( new Port( this, "Velocity", OP_PORT_TYPE_VALUE, { 'display': 'range', 'min': 0, 'max': 1 } ));
 
 // output port
 var signalPort = op.outObject("Signal");
@@ -31,6 +35,17 @@ releasePort.onChange = function() {
     updateEnvObjPort();
 };
 
+triggerAttackPort.onTriggered = function(){
+    var velocity = velocityPort.get();
+    var time = timePort.get();
+    env.triggerAttack(time, velocity);
+};
+
+triggerReleasePort.onTriggered = function(){
+    var time = timePort.get();
+    env.triggerRelease(time);
+};
+
 // default values
 var DEFAULT_ENVELOPE_OBJ = {
     attack: 0.1,
@@ -38,6 +53,8 @@ var DEFAULT_ENVELOPE_OBJ = {
     sustain: 1,
     release: 0.8
 };
+var DEFAULT_TIME = "+0";
+var DEFAULT_VELOCITY = 1.0;
 
 // vars
 var env = new Tone.Envelope();
@@ -58,6 +75,8 @@ attackPort.set(DEFAULT_ENVELOPE_OBJ.attack);
 decayPort.set(DEFAULT_ENVELOPE_OBJ.decay);
 sustainPort.set(DEFAULT_ENVELOPE_OBJ.sustain);
 releasePort.set(DEFAULT_ENVELOPE_OBJ.release);
+timePort.set(DEFAULT_TIME);
+velocityPort.set(DEFAULT_VELOCITY);
 
 // set defaults (output)
 signalPort.set(env);
