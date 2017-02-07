c8d25714d5c2606759efbb7ab265997ae4d4d7c3
Tim Pulver <git@timpulver.de>
2017-02-07 14:26:47 +0100

    Update Biquad Filter

--- src/ops/base/Ops.WebAudio.BiquadFilter/Ops.WebAudio.BiquadFilter.js
+++ src/ops/base/Ops.WebAudio.BiquadFilter/Ops.WebAudio.BiquadFilter.js
@@ -1,7 +1,8 @@
 this.name="Ops.WebAudio.BiquadFilter";
 
-if(!window.audioContext){ audioContext = new AudioContext(); }
+var audioContext = CABLES.WebAudio.createAudioContext(op);
 
+// default values + min and max
 var Q_MIN = 0.0001;
 var Q_MAX = 1000;
 var Q_DEF = 1;
@@ -18,15 +19,16 @@ var GAIN_MAX = 40;
 var GAIN_DEF = 0;
 var TYPE_DEF = "allpass";
 
-var audioIn=this.addInPort(new Port(this,"audio in",OP_PORT_TYPE_OBJECT));
+var biquadFilter = audioContext.createBiquadFilter();
+
+var audioInPort = CABLES.WebAudio.createAudioInPort(op, "Audio In", biquadFilter);
+var audioOutPort = CABLES.WebAudio.createAudioOutPort(op, "Audio Out", biquadFilter);
 
 var type = this.addInPort(new Port(this,"type",OP_PORT_TYPE_VALUE,{display:'dropdown',values:['allpass','lowpass','highpass','bandpass','lowshelf','highshelf','peaking','notch']}));
 var frequency = this.addInPort(new Port(this, "frequency", OP_PORT_TYPE_VALUE, {"display": "range", "min": FREQUENCY_MIN, "max": FREQUENCY_MAX}));
 var detune = this.addInPort(new Port(this, "detune", OP_PORT_TYPE_VALUE, {"display": "range", "min": DETUNE_MIN, "max": DETUNE_MAX}));
 var q = this.addInPort(new Port(this, "q", OP_PORT_TYPE_VALUE, {"display": "range", "min": Q_MIN, "max": Q_MAX}));
 var gain = this.addInPort(new Port(this, "gain", OP_PORT_TYPE_VALUE, {"display": "range", "min": GAIN_MIN, "max": GAIN_MAX}));
-
-var biquadFilter = audioContext.createBiquadFilter();
 
 var updateType = function(){
     biquadFilter.type = type.get();
@@ -54,39 +56,9 @@ detune.onValueChange(updateDetune);
 q.onValueChange(updateQ);
 gain.onValueChange(updateGain);
 
-var audioOut=this.addOutPort(new Port(this,"audio out",OP_PORT_TYPE_OBJECT));
-
-var oldAudioIn = null;
-
-audioIn.onValueChanged = function() {
-    if (!audioIn.get()) {
-        if (oldAudioIn !== null) {
-            try{
-                oldAudioIn.disconnect(biquadFilter);
-            } catch(e) {
-                console.log(e);
-            }
-        }
-    } else {
-        audioIn.val.connect(biquadFilter);
-    }
-    oldAudioIn=audioIn.val;
-};
 
 type.set(TYPE_DEF);
 frequency.set( FREQUENCY_DEF );
 detune.set( DETUNE_DEF );
 q.set( Q_DEF );
 gain.set( GAIN_DEF );
-
-audioOut.set(biquadFilter);
-
-/*
-
-                    attribute BiquadFilterType type;
-    readonly        attribute AudioParam       frequency;
-    readonly        attribute AudioParam       detune;
-    readonly        attribute AudioParam       Q;
-    readonly        attribute AudioParam       gain;
-    void getFrequencyResponse (Float32Array frequencyHz, Float32Array magResponse, Float32Array phaseResponse);
-*/
