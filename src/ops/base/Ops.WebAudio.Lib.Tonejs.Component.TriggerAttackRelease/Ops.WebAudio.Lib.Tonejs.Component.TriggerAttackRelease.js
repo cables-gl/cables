6ff32ec3630cc6007df94f551b4e642d3cc6c00a
Tim Pulver <git@timpulver.de>
2017-02-09 16:08:26 +0100

    Add op: TriggerAttackRelease

---
+++ src/ops/base/Ops.WebAudio.Lib.Tonejs.Component.TriggerAttackRelease/Ops.WebAudio.Lib.Tonejs.Component.TriggerAttackRelease.js
@@ -0,0 +1,28 @@
+op.name="TriggerAttackRelease";
+
+CABLES.WebAudio.createAudioContext(op);
+
+// default values
+var DURATION_DEFAULT = "4n"
+var TIME_DEFAULT = "+0";
+var VELOCITY_DEFAULT = 1;
+
+// in ports
+var audioIn = op.inObject("Audio In");
+var durationPort = op.inValueString("Duration", DURATION_DEFAULT);
+durationPort.set(DURATION_DEFAULT);
+var timePort = op.inValueString("Time", TIME_DEFAULT);
+timePort.set(TIME_DEFAULT);
+var velocityPort = op.inValueSlider("Velocity", VELOCITY_DEFAULT);
+var triggerAttackReleasePort = op.addInPort( new Port( this, "Trigger Attack Release", OP_PORT_TYPE_FUNCTION, { "display": "button" } ));
+
+// listeners
+triggerAttackReleasePort.onTriggered = function() {
+    var node = audioIn.get();
+    if(node && node.triggerAttackRelease) {
+        var duration = durationPort.get() || DURATION_DEFAULT;
+        var time = timePort.get() || TIME_DEFAULT;
+        var velocity = velocityPort.get();
+        node.triggerAttackRelease(duration, time, velocity);
+    }    
+};
