96693423dafd681d30fd84517725a1be37a8380d
Tim Pulver <git@timpulver.de>
2017-02-09 18:45:19 +0100

    Op Add: Frequency Envelope

---
+++ src/ops/base/Ops.WebAudio.Lib.Tonejs.Component.FrequencyEnvelope/Ops.WebAudio.Lib.Tonejs.Component.FrequencyEnvelope.js
@@ -0,0 +1,66 @@
+op.name="FrequencyEnvelope";
+
+CABLES.WebAudio.createAudioContext(op);
+
+// TODO:
+// - baseFrequency can also be in the form "C3", should this be supported?
+//   Maybe a conversion op NoteToFrequency is enough
+
+var CURVE_VALUES = [
+    "linear",
+    "exponential",
+    "sine",
+    "cosine",
+    "bounce",
+    "ripple",
+    "step"
+];
+
+// default values
+var ATTACK_DEFAULT = 0.1;
+var DECAY_DEFAULT = 0.2;
+var SUSTAIN_DEFAULT = 1.0;
+var RELEASE_DEFAULT = 0.8;
+var ATTACK_CURVE_DEFAULT = "linear";
+var RELEASE_CURVE_DEFAULT = "linear";
+var EXPONENT_DEFAULT = 2; // ?
+var EXPONENT_MIN = 1; // ?
+var EXPONENT_MAX = 8; // ?
+var BASE_FREQUENCY_DEFAULT = 200;
+var BASE_FREQUENCY_MIN = 1;
+var BASE_FREQUENCY_MAX = 20000; // ?
+var OCTAVES_DEFAULT = 4;
+var OCTAVES_MIN = 1;
+var OCTAVES_MIN = 12; // ?
+
+// vars
+var node = new Tone.FrequencyEnvelope();
+
+// in ports
+var audioInPort = CABLES.WebAudio.createAudioInPort(op, "Audio In", node);
+var attackPort = op.inValueString("Attack", ATTACK_DEFAULT);
+var decayPort = op.inValue("Decay", DECAY_DEFAULT);
+var sustainPort = op.inValueSlider("Sustain", SUSTAIN_DEFAULT);
+var releasePort = op.inValueString("Release", RELEASE_DEFAULT);
+var attackCurvePort = this.addInPort( new Port( this, "Attack Curve", OP_PORT_TYPE_VALUE, { display: 'dropdown', values: CURVE_VALUES } ) );
+attackCurvePort.set(ATTACK_CURVE_DEFAULT);
+var releaseCurvePort = this.addInPort( new Port( this, "Release Curve", OP_PORT_TYPE_VALUE, { display: 'dropdown', values: CURVE_VALUES } ) );
+releaseCurvePort.set(ATTACK_CURVE_DEFAULT);
+var exponentPort = op.inValue("Exponent", EXPONENT_DEFAULT);
+
+// value change listeners
+attackPort.onChange = setNodeValue("attack", attackPort.get());
+decayPort.onChange = setNodeValue("decay", decayPort.get());
+sustainPort.onChange = setNodeValue("sustain", sustainPort.get());
+releasePort.onChange = setNodeValue("release", releasePort.get());
+attackCurvePort.onChange = setNodeValue("attackCurve", attackCurvePort.get());
+releaseCurvePort.onChange = setNodeValue("releaseCurve", releaseCurvePort.get());
+
+exponentPort.onChange = setNodeValue("exponent", releasePort.get());
+
+function setNodeValue(key, value) {
+    node.set(key, value);
+}
+
+// output ports
+var audioOutPort = CABLES.WebAudio.createAudioOutPort(op, "Audio Out", node);
