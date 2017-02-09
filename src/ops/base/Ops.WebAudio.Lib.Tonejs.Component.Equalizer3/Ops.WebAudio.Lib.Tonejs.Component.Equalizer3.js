1013b7a286fc9709da8cf17aca96d9252d76f278
Tim Pulver <git@timpulver.de>
2017-02-09 16:07:37 +0100

    Add Op: Equalizer3

---
+++ src/ops/base/Ops.WebAudio.Lib.Tonejs.Component.Equalizer3/Ops.WebAudio.Lib.Tonejs.Component.Equalizer3.js
@@ -0,0 +1,33 @@
+op.name="Equalizer3";
+
+CABLES.WebAudio.createAudioContext(op);
+
+// defaults
+var DECIBELS_MIN = -100;
+var DECIBELS_MAX = 0;
+var FREQUENCY_MIN = 1;
+var FREQUENCY_MAX = 20000;
+
+var LOW_DEFAULT = 0;
+var MID_DEFAULT = 0;
+var HIGH_DEFAULT = 0;
+var LOW_FREQUENCY_DEFAULT = 400;
+var HIGH_FREQUENCY_DEFAULT = 2500;
+var Q_DEFAULT = 1; // ?
+var Q_MIN = 1; // ?
+var Q_MAX = 45; // ?
+
+// vars
+var node = new Tone.EQ3();
+
+// input ports
+var audioInPort = CABLES.WebAudio.createAudioInPort(op, "Audio In", node);
+var lowPort = CABLES.WebAudio.createAudioParamInPort(op, "Low", node.low, {"display": "range", "min": DECIBELS_MIN, "max": DECIBELS_MAX}, LOW_DEFAULT);
+var midPort = CABLES.WebAudio.createAudioParamInPort(op, "Mid", node.mid, {"display": "range", "min": DECIBELS_MIN, "max": DECIBELS_MAX}, MID_DEFAULT);
+var highPort = CABLES.WebAudio.createAudioParamInPort(op, "High", node.high, {"display": "range", "min": DECIBELS_MIN, "max": DECIBELS_MAX}, HIGH_DEFAULT);
+var qPort = CABLES.WebAudio.createAudioParamInPort(op, "Q", node.Q, {"display": "range", "min": Q_MIN, "max": Q_MAX}, Q_DEFAULT);
+var lowFrequencyPort = CABLES.WebAudio.createAudioParamInPort(op, "Low Frequency", node.lowFrequency, {"display": "range", "min": FREQUENCY_MIN, "max": FREQUENCY_MAX}, LOW_FREQUENCY_DEFAULT);
+var highFrequencyPort = CABLES.WebAudio.createAudioParamInPort(op, "High Frequency", node.highFrequency, {"display": "range", "min": FREQUENCY_MIN, "max": FREQUENCY_MAX}, HIGH_FREQUENCY_DEFAULT);
+
+// output ports
+var audioOutPort = CABLES.WebAudio.createAudioOutPort(op, "Audio Out", node);
