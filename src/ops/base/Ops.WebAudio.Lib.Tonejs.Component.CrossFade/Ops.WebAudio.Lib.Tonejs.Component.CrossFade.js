602a1a3f6345cf18a90cb5c2d68dee4579cdfbb6
Tim Pulver <git@timpulver.de>
2017-02-09 16:07:27 +0100

    Add op: Crossfade

---
+++ src/ops/base/Ops.WebAudio.Lib.Tonejs.Component.CrossFade/Ops.WebAudio.Lib.Tonejs.Component.CrossFade.js
@@ -0,0 +1,22 @@
+op.name="CrossFade";
+
+CABLES.WebAudio.createAudioContext(op);
+
+// TODO: Should this have two internal gain nodes, so users 
+// so not add two gain before?
+
+// defaults
+var FADE_DEFAULT = 0.5;
+var FADE_MIN = 0;
+var FADE_MAX = 1;
+
+// vars
+var node = new Tone.CrossFade();
+
+// input ports
+var audioInPort1 = CABLES.WebAudio.createAudioInPort(op, "Audio In 1", node.input[0]);
+var audioInPort2 = CABLES.WebAudio.createAudioInPort(op, "Audio In 2", node.input[1]);
+var fadePort = CABLES.WebAudio.createAudioParamInPort(op, "Fade", node.fade, {"display": "range", "min": FADE_MIN, "max": FADE_MAX}, FADE_DEFAULT);
+
+// output ports
+var audioOutPort = CABLES.WebAudio.createAudioOutPort(op, "Audio Out", node);
