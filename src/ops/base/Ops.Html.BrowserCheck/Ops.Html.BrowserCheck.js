beece244f11a0d6581a6761d69fd21dd7a3f15c6
tom <tom@undev.de>
2017-02-17 16:23:17 +0100

    browser/os check

--- src/ops/base/Ops.Html.BrowserCheck/Ops.Html.BrowserCheck.js
+++ src/ops/base/Ops.Html.BrowserCheck/Ops.Html.BrowserCheck.js
@@ -1,4 +1,4 @@
-op.name="BrowserCheck";
+op.name="BrowserInfo";
 
 var isIe=op.outValue("Is IE",false);
 var isIe10Plus=op.outValue("Is IE 10+",false);
@@ -6,8 +6,13 @@ var isIe11=op.outValue("Is IE 11",false);
 var isEdge=op.outValue("Is Edge",false);
 var isChrome=op.outValue("Is Chrome",false);
 var isFirefox=op.outValue("Is Firefox",false);
-var isIos=op.outValue("Is iOS",false);
 var isMobile=op.outValue("Is Mobile",false);
+var isWindows=op.outValue("Is Windows",false);
+var isLinux=op.outValue("Is Linux",false);
+var isMac=op.outValue("Is Mac",false);
+var isIos=op.outValue("Is iOS",false);
+var isAndroid=op.outValue("Is Android",false);
+
 var outNav=op.outValue("Language");
 
 outNav.set(navigator.language || navigator.userLanguage);
@@ -44,6 +49,7 @@ var isChromium = window.chrome,
     isIEedge = winNav.userAgent.indexOf("Edge") > -1,
     isIOSChrome = winNav.userAgent.match("CriOS");
 
+
 if(isIOSChrome || (isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera === false && isIEedge === false))
 {
    // is Google Chrome
@@ -56,7 +62,31 @@ else
 }
 
 
+
+if(window.navigator.userAgent.indexOf("Windows") != -1)
+{
+    isWindows.set(true);
+}
+
+if(window.navigator.userAgent.indexOf("Linux") != -1)
+{
+    isWindows.set(false);
+    isLinux.set(true);
+}
+
+if(window.navigator.userAgent.indexOf("Mac") != -1)
+{
+    isWindows.set(false);
+    isMac.set(true);
+}
+
 isIos.set( /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
+
+if(window.navigator.userAgent.toLowerCase().indexOf("android") != -1) 
+    isAndroid.set(true);
+
+
+
 
 isMobile.set(false);
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
