cb19e4691b9ebb517b138d0c02b7bc4389e50f58
Tim Pulver <git@timpulver.de>
2017-02-20 13:20:07 +0100

    Add op: Freeverb

---
+++ src/ops/base/Ops.WebAudio.Lib.Tonejs.Effect.Freeverb/Ops.WebAudio.Lib.Tonejs.Effect.Freeverb.js
@@ -0,0 +1,27 @@
+op.name="Freeverb";
+
+CABLES.WebAudio.createAudioContext(op);
+
+// vars
+var node = new Tone.Freeverb();
+
+// default values
+var ROOM_SIZE_DEFAULT = 0.7;
+var ROOM_SIZE_MIN = 0.0;
+var ROOM_SIZE_MAX = 1.0;
+var DAMPENING_DEFAULT = 3000;
+var DAMPENING_MIN = 1;
+var DAMPENING_MAX = 20000;
+var WET_DEFAULT = 1.0;
+var WET_MIN = 0.0;
+var WET_MAX = 1.0;
+
+// input ports
+var audioInPort = CABLES.WebAudio.createAudioInPort(op, "Audio In", node);
+var roomSizePort = CABLES.WebAudio.createAudioParamInPort(op, "Room Size", node.roomSize, {"display": "range", "min": ROOM_SIZE_MIN, "max": ROOM_SIZE_MAX}, ROOM_SIZE_DEFAULT);
+var dampeningPort = CABLES.WebAudio.createAudioParamInPort(op, "Dampening", node.dampening, {"display": "range", "min": DAMPENING_MIN, "max": DAMPENING_MAX}, DAMPENING_DEFAULT);
+var wetPort = CABLES.WebAudio.createAudioParamInPort(op, "Wet", node.wet, {"display": "range", "min": WET_MIN, "max": WET_MAX}, WET_DEFAULT);
+
+// output ports
+var audioOutPort = CABLES.WebAudio.createAudioOutPort(op, "Audio Out", node);
+
