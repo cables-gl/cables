fb88496c351e5c6a1cd6618b20e5ba8b75953dc9
Tim Pulver <git@timpulver.de>
2017-02-20 17:46:42 +0100

    Add op: Limiter

---
+++ src/ops/base/Ops.WebAudio.Lib.Tonejs.Component.Limiter/Ops.WebAudio.Lib.Tonejs.Component.Limiter.js
@@ -0,0 +1,18 @@
+op.name="Limiter";
+
+CABLES.WebAudio.createAudioContext(op);
+
+// default
+var THRESHOLD_DEFAULT = -2;
+var THRESHOLD_MIN = -100;
+var THRESHOLD_MAX = 0;
+
+// vars
+var node = new Tone.Limiter(THRESHOLD_DEFAULT);
+
+// input ports
+var audioInPort = CABLES.WebAudio.createAudioInPort(op, "Audio In", node);
+var thresholdPort = CABLES.WebAudio.createAudioParamInPort(op, "Threshold", node.threshold, {"display": "range", "min": THRESHOLD_MIN, "max": THRESHOLD_MAX}, THRESHOLD_DEFAULT);
+
+// output ports
+var audioOutPort = CABLES.WebAudio.createAudioOutPort(op, "Audio Out", node);
