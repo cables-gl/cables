59f11b1646b0bd0028bd2c6b9e9b029a5577f4de
pandur <tom@undev.de>
2017-02-22 15:07:59 +0100

    math vectorlength

---
+++ src/ops/base/Ops.Math.VectorLength/Ops.Math.VectorLength.js
@@ -0,0 +1,21 @@
+op.name="VectorLength";
+
+var x=op.inValue("X");
+var y=op.inValue("Y");
+var z=op.inValue("Z");
+
+var l=op.outValue("Length");
+
+x.onChange=update;
+y.onChange=update;
+z.onChange=update;
+
+var vec=vec3.create();
+
+function update()
+{
+    vec3.set(vec,x.get(),y.get(),z.get());
+    l.set(vec3.len(vec));
+}
+
+
