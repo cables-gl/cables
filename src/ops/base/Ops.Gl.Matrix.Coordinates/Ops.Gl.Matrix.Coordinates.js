e5236701397c63e7590d129f39883544ee9b34c1
pandur <tom@undev.de>
2017-02-28 15:15:54 +0100

    new op: coordinates

---
+++ src/ops/base/Ops.Gl.Matrix.Coordinates/Ops.Gl.Matrix.Coordinates.js
@@ -0,0 +1,23 @@
+op.name="Coordinates";
+
+var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
+var trigger=op.addOutPort(new Port(op,"trigger",OP_PORT_TYPE_FUNCTION));
+
+var outX=op.outValue("X");
+var outY=op.outValue("Y");
+var outZ=op.outValue("Z");
+
+
+var cgl=op.patch.cgl;
+var pos=[0,0,0];
+
+render.onTriggered=function()
+{
+    vec3.transformMat4(pos, [0,0,0], cgl.mvMatrix);
+
+    outX.set(pos[0]);
+    outY.set(pos[1]);
+    outZ.set(pos[2]);
+
+    trigger.trigger();
+};
