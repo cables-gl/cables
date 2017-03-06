bedebb40428fea2f8caf7ae6e3b5569b844f6acc
tom <tom@undev.de>
2017-03-06 18:09:54 +0100

    new op posterize

---
+++ src/ops/base/Ops.Gl.TextureEffects.Posterize/Ops.Gl.TextureEffects.Posterize.js
@@ -0,0 +1,41 @@
+op.name="Posterize";
+
+var render=op.inFunction("Render");
+var trigger=op.outFunction("Trigger");
+
+var levels=op.inValue("levels",2);
+
+var cgl=op.patch.cgl;
+var shader=new CGL.Shader(cgl);
+op.onLoaded=shader.compile;
+
+
+
+shader.setSource(shader.getDefaultVertexShader(),attachments.posterize_frag);
+var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+var levelsUniform=new CGL.Uniform(shader,'f','levels',levels);
+
+var uniWidth=new CGL.Uniform(shader,'f','texWidth',128);
+var uniHeight=new CGL.Uniform(shader,'f','texHeight',128);
+
+
+render.onTriggered=function()
+{
+    if(!cgl.currentTextureEffect)return;
+
+    cgl.setShader(shader);
+    cgl.currentTextureEffect.bind();
+
+
+uniWidth.setValue(cgl.currentTextureEffect.getCurrentSourceTexture().width);
+uniHeight.setValue(cgl.currentTextureEffect.getCurrentSourceTexture().height);
+
+
+    cgl.gl.activeTexture(cgl.gl.TEXTURE0);
+    cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+    cgl.currentTextureEffect.finish();
+    cgl.setPreviousShader();
+
+    trigger.trigger();
+};
