c9689a592090eb47f4a163d0b75da0149173541d
pandur <tom@undev.de>
2017-03-14 11:27:33 +0100

    op: arraybuffer 3x

---
+++ src/ops/base/Ops.Array.ArrayBuffer3x/Ops.Array.ArrayBuffer3x.js
@@ -0,0 +1,37 @@
+op.name="ArrayBuffer3x";
+
+var exec=op.inFunction("exec");
+
+var valX=op.inValue("Value X");
+var valY=op.inValue("Value Y");
+var valZ=op.inValue("Value Z");
+
+var arr=[];
+
+var arrOut=op.outArray("Result");
+
+var maxLength=op.inValue("Max Num Elements",100);
+arrOut.set(arr);
+
+maxLength.onChange=reset;
+reset();
+
+function reset()
+{
+    arr.length=Math.abs(Math.floor(maxLength.get()*3))||0;
+    for(var i=0;i<arr.length;i++) arr[i]=0;
+}
+
+exec.onTriggered=function()
+{
+    // if(op.instanced(exec))return;
+
+    for(var i=3;i<arr.length;i++)arr[i-3]=arr[i];
+
+    arr[arr.length-3]=valX.get();
+    arr[arr.length-2]=valY.get();
+    arr[arr.length-1]=valZ.get();
+    
+
+
+};
