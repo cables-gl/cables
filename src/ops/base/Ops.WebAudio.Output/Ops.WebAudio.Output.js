5553046c452ea79b57a401ccc0395b30db012cfc
pandur <tom@undev.de>
2017-03-14 12:52:49 +0100

    Edit op: Output: Add Mute Button / Volume Slider

--- src/ops/base/Ops.WebAudio.Output/Ops.WebAudio.Output.js
+++ src/ops/base/Ops.WebAudio.Output/Ops.WebAudio.Output.js
@@ -1,8 +1,42 @@
 op.name='audioOutput';
 
+// constants
+var VOLUME_DEFAULT = 1;
+var VOLUME_MIN = 0;
+var VOLUME_MAX = 1;
+
+// vars
 var audioContext = CABLES.WebAudio.createAudioContext(op);
+var gainNode = audioContext.createGain();
+var destinationNode = audioContext.destination;
+gainNode.connect(destinationNode);
+var oldVolume = 1;
 
-var audioNode = audioContext.destination;
+// inputs
+var audioInPort = CABLES.WebAudio.createAudioInPort(op, "Audio In", gainNode);
+var volumePort = op.inValueSlider("Volume", VOLUME_DEFAULT);
+var mutePort = op.inValueBool("Mute", false);
 
-var audioInPort = CABLES.WebAudio.createAudioInPort(op, "Audio In", audioNode);
+// change listeners
+mutePort.onChange = function() {
+    if(mutePort.get()) {
+        gainNode.gain.value = 0;
+        var volume = volumePort.get();
+        if(volume >= VOLUME_MIN && volume <= VOLUME_MAX) {
+            oldVolume = volume;    
+        } else {
+            oldVolume = 1;
+        }
+        oldVolume = volume;
+    } else {
+        gainNode.gain.value = oldVolume;
+    }
+};
+volumePort.onChange = function() {
+    var volume = volumePort.get();
+    if(volume >= VOLUME_MIN && volume <= VOLUME_MAX) {
+        gainNode.gain.value = volume;
+    }
+};
+
 
