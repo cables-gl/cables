2be0cb4c41aa6e86b4fd4ade5d7b1640b49e1d21
pandur <tom@undev.de>
2017-03-14 13:00:24 +0100

    Edit op: Output volume fix

--- src/ops/base/Ops.WebAudio.Output/Ops.WebAudio.Output.js
+++ src/ops/base/Ops.WebAudio.Output/Ops.WebAudio.Output.js
@@ -10,33 +10,35 @@ var audioContext = CABLES.WebAudio.createAudioContext(op);
 var gainNode = audioContext.createGain();
 var destinationNode = audioContext.destination;
 gainNode.connect(destinationNode);
-var oldVolume = 1;
 
 // inputs
 var audioInPort = CABLES.WebAudio.createAudioInPort(op, "Audio In", gainNode);
 var volumePort = op.inValueSlider("Volume", VOLUME_DEFAULT);
 var mutePort = op.inValueBool("Mute", false);
 
+// functions
+function setVolume() {
+    var volume = volumePort.get();
+    if(volume >= VOLUME_MIN && volume <= VOLUME_MAX) {
+        gainNode.gain.value = volume;
+    } else {
+        gainNode.gain.value = VOLUME_DEFAULT;
+    }
+}
+
 // change listeners
 mutePort.onChange = function() {
     if(mutePort.get()) {
         gainNode.gain.value = 0;
-        var volume = volumePort.get();
-        if(volume >= VOLUME_MIN && volume <= VOLUME_MAX) {
-            oldVolume = volume;    
-        } else {
-            oldVolume = 1;
-        }
-        oldVolume = volume;
     } else {
-        gainNode.gain.value = oldVolume;
+        setVolume();
     }
 };
 volumePort.onChange = function() {
-    var volume = volumePort.get();
-    if(volume >= VOLUME_MIN && volume <= VOLUME_MAX) {
-        gainNode.gain.value = volume;
+    if(mutePort.get()) {
+        return;
     }
+    setVolume();
 };
 
 
