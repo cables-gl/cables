cb636183fbbe4d560b4f7c94611fe681d50c37d3
pandur <tom@undev.de>
2017-03-14 11:30:29 +0100

    missing docs / deprecated stuff

---
+++ src/ops/base/Ops.Deprecated.ValueSet/Ops.Deprecated.ValueSet.js
@@ -0,0 +1,13 @@
+op.name="ValueSet";
+
+var exe=op.addInPort(new Port(op,"exe",OP_PORT_TYPE_FUNCTION));
+var v=op.addInPort(new Port(op,"value",OP_PORT_TYPE_VALUE));
+
+var result=op.addOutPort(new Port(op,"result"));
+
+var exec=function()
+{
+    result.set(v.get());
+};
+
+exe.onTriggered=exec;
