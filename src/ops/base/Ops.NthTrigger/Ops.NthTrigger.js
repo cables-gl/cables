3e0dd45dc370c53529e8d6e3856f04ce44b30436
pandur <tom@undev.de>
2017-03-14 11:31:00 +0100

    ops

---
+++ src/ops/base/Ops.NthTrigger/Ops.NthTrigger.js
@@ -0,0 +1,32 @@
+op.name="NthTrigger";
+
+var exe=op.inFunction("Exe");
+
+var nth=op.inValue("Nth");
+
+var next=op.outFunction("next");
+
+var count=0;
+var lastFrame=-1;
+
+nth.onChange=reset;
+
+function reset()
+{
+    count=0;
+}
+
+
+exe.onTriggered=function()
+{
+    if(op.patch._frameNum!=lastFrame) count=0;
+    lastFrame=op.patch._frameNum;
+
+    if(count==nth.get())
+    {
+        next.trigger();
+        count=0;
+    }
+    count++;
+    
+};
