1967aa94dc7676226123d040e8b46e77417880c6
tom <tom@undev.de>
2017-03-22 12:48:02 +0100

    tweaks

--- src/ops/base/Ops.Net.Websocket/Ops.Net.Websocket.js
+++ src/ops/base/Ops.Net.Websocket/Ops.Net.Websocket.js
@@ -1,77 +1,83 @@
-    var self=this;
-    Op.apply(this, arguments);
+var self=this;
+Op.apply(this, arguments);
 
-    this.name='Websocket';
-    this.url=this.addInPort(new Port(this,"url",OP_PORT_TYPE_VALUE,{type:'string'}));
-    this.result=this.addOutPort(new Port(this,"result", OP_PORT_TYPE_OBJECT));
-    this.connected=this.addOutPort(new Port(this,"connected"));
+this.name='Websocket';
+this.url=this.addInPort(new Port(this,"url",OP_PORT_TYPE_VALUE,{type:'string'}));
+this.result=this.addOutPort(new Port(this,"result", OP_PORT_TYPE_OBJECT));
+this.connected=this.addOutPort(new Port(this,"connected"));
 
-    var connection=null;
-    var timeout=null;
-    var connectedTo='';
+var outSocket=op.outObject("Socket");
 
-    function checkConnection()
+
+var connection=null;
+var timeout=null;
+var connectedTo='';
+
+function checkConnection()
+{
+    if(self.connected.val===false)
     {
-        if(self.connected.val===false)
-        {
-            connect();
-        }
-        timeout=setTimeout(checkConnection,1000);
+        connect();
     }
+    timeout=setTimeout(checkConnection,1000);
+}
 
-    function connect()
+function connect()
+{
+    if(self.connected.val===true && connectedTo==self.url.val) return;
+    if(self.connected.val===true)connection.close();
+
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+ 
+     if (!window.WebSocket)
+        console.error('Sorry, but your browser doesn\'t support WebSockets.');
+
+    try
     {
-        if(self.connected.val===true && connectedTo==self.url.val) return;
-        if(self.connected.val===true)connection.close();
+        if(connection!=null)connection.close();
+        connection = new WebSocket(self.url.val);
+    }catch (e)
+    {
+        console.log('could not connect to',self.url.val);
+    }
+    
+    connection.onerror = function (message)
+    {
+        self.connected.val=false;
+        outSocket.set(null);
+    };
 
-        window.WebSocket = window.WebSocket || window.MozWebSocket;
-     
-         if (!window.WebSocket)
-            console.error('Sorry, but your browser doesn\'t support WebSockets.');
+    connection.onclose = function (message)
+    {
+        self.connected.val=false;
+        outSocket.set(null);
+    };
 
+    connection.onopen = function (message)
+    {
+        self.connected.val=true;
+        connectedTo=self.url.val;
+        outSocket.set(connection);
+    };
+
+    connection.onmessage = function (message)
+    {
         try
         {
-            if(connection!=null)connection.close();
-            connection = new WebSocket(self.url.val);
-        }catch (e)
-        {
-            console.log('could not connect to',self.url.val);
+            var json = JSON.parse(message.data);
+            self.result.val=json;
+                    
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
         }
-        
-        connection.onerror = function (message)
-        {
-            self.connected.val=false;
-        };
+    };
 
-        connection.onclose = function (message)
-        {
-            self.connected.val=false;
-        };
+    
+    
+}
 
-        connection.onopen = function (message)
-        {
-            self.connected.val=true;
-            connectedTo=self.url.val;
-        };
+this.url.onValueChanged=connect;
+timeout=setTimeout(checkConnection,1000);
 
-        connection.onmessage = function (message)
-        {
-            try
-            {
-                var json = JSON.parse(message.data);
-                self.result.val=json;
-                        
-            } catch (e) {
-                console.log('This doesn\'t look like a valid JSON: ', message.data);
-                return;
-            }
-        };
-
-        
-        
-    }
-
-    this.url.onValueChanged=connect;
-    timeout=setTimeout(checkConnection,1000);
-
-    this.url.val='ws://127.0.0.1:1337';
+this.url.val='ws://127.0.0.1:1337';
