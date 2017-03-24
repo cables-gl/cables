7bfcf02f07fd982636f62b55d02c2517d6cb499a
pandur <tom@undev.de>
2017-03-24 12:24:49 +0100

    object stringify

---
+++ src/ops/base/Ops.Json.ObjectStringify/Ops.Json.ObjectStringify.js
@@ -0,0 +1,19 @@
+op.name="JsonStringify";
+
+var inObj=op.inObject("Object");
+
+var inBeautify=op.inValueBool("beautify");
+
+var outString=op.outValue("Result");
+
+
+inBeautify.onChange=update;
+inObj.onChange=update;
+
+function update()
+{
+    if(inBeautify.get())outString.set(JSON.stringify(inObj.get()));
+        else outString.set(JSON.stringify(inObj.get(),false,4));
+}
+
+
