0a1a9f5fafe3402b5a0c5d8e3883d3cdeba22d93
Tim Pulver <git@timpulver.de>
2017-03-25 10:25:14 +0100

    Op edit: AudioMixer: Add solo-feature + some improvements + doc

--- src/ops/base/Ops.WebAudio.Lib.Tonejs.Component.AudioMixer/Ops.WebAudio.Lib.Tonejs.Component.AudioMixer.js
+++ src/ops/base/Ops.WebAudio.Lib.Tonejs.Component.AudioMixer/Ops.WebAudio.Lib.Tonejs.Component.AudioMixer.js
@@ -6,8 +6,10 @@ CABLES.WebAudio.createAudioContext(op);
 var VOLUME_DEFAULT = 0;
 var VOLUME_MIN = -96;
 var VOLUME_MAX = 0;
+var NONE = -1;
 
 // vars
+// every in-port has a data attribute "index": index in the array: port.data.index
 var volumeNodes = [];
 var labelPorts = [];
 var audioInPorts = [];
@@ -16,22 +18,49 @@ var mutePorts = [];
 var soloPorts = [];
 
 var gainNode = new Tone.Gain();
-var nPorts = 8;
-
-// inputs
-//var numberOfPortsPort = op.inValue("Number Of Ports");
+var nPorts = 10;
+var soloChannel = NONE; // index of the current solo channel
 
 // functions
 function muteOnChange() {
     var i = this.data.index;
-    op.log("Mute. Index: ", i);
     var mute = mutePorts[i].get();
     var volume = volumePorts[i].get();
     volumeNodes[i].set("mute", mute ? true : false);
-    if(!mute) {
-        //op.log("setting volume: ", volume);
-        //op.log("... on node: ", volumeNodes[i]);
+    if(!mute && soloChannel === i || soloChannel === NONE) {
         volumeNodes[i].set("volume", volume);
+    }
+}
+
+function soloOnChange() {
+    var i = this.data.index;
+    var solo = soloPorts[i].get();
+    // if another channel was soloed before
+    if(i !== soloChannel) {
+        if(soloChannel !== NONE) {
+            // uncheck previous solo UI toggle
+            soloPorts[soloChannel].set(false);    
+            if(gui) gui.patch().showOpParams(op);
+        }
+        soloChannel = i;
+    }
+    if(solo) {
+        // set all volumes to VOLUME_MIN
+        for(var j=0; j<nPorts; j++) {
+            volumeNodes[j].set("volume", VOLUME_MIN);
+        }
+        // set volume of soloed channel if not muted
+        if(!mutePorts[soloChannel].get()) {
+            volumeNodes[soloChannel].set("volume", volumePorts[soloChannel].get()); // TODO: Add min, max check        
+        }
+    } else { // unsolo
+        // set all channels back to original volume if not muted   
+        for(var k=0; k<nPorts; k++) {
+            // set volume if not muted
+            if(!mutePorts[k].get()) {
+                volumeNodes[k].set("volume", volumePorts[k].get());    
+            }
+        }
     }
 }
 
@@ -39,7 +68,7 @@ function volumeOnChange() {
     var i = this.data.index;
     var mute = mutePorts[i].get();
     var volume = volumePorts[i].get();
-    if(!mute) {
+    if(!mute && soloChannel === i || soloChannel === NONE) {
         volumeNodes[i].set("volume", volume);
     }
 }
@@ -59,7 +88,6 @@ function createInputPorts() {
         audioInPort.data.index = i;
         audioInPorts.push(audioInPort);
         // volume port
-        //var volumePort = CABLES.WebAudio.createAudioParamInPort(op, "Channel " + (i+1) + " Volume", volumeNode.volume, {"display": "range", "min": VOLUME_MIN, "max": VOLUME_MAX}, VOLUME_DEFAULT);
         var volumePort = op.addInPort( new Port( op, "Channel " + (i+1) + " Volume", OP_PORT_TYPE_VALUE, { 'display': 'range', 'min': VOLUME_MIN, 'max': VOLUME_MAX } ));
         volumePort.onChange = volumeOnChange.bind(volumePort);
         volumePort.data.index = i;
@@ -67,9 +95,13 @@ function createInputPorts() {
         // mute port
         var mutePort = op.inValueBool("Channel " + (i+1) + " Mute", false);
         mutePort.data.index = i;
-        mutePort.data.node = volumeNode;
         mutePort.onChange = muteOnChange.bind(mutePort);
         mutePorts.push(mutePort);
+        // solo port
+        var soloPort = op.inValueBool("Channel " + (i+1) + " Solo", false);
+        soloPort.data.index = i;
+        soloPort.onChange = soloOnChange.bind(soloPort);
+        soloPorts.push(soloPort);
     }
 }
 
