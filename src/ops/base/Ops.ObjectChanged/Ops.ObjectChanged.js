82da9929cc4134518fbff4c1e7acc7308b5a76db
tom <tom@undev.de>
2017-03-26 14:46:34 +0200

    new op objectchanged

---
+++ src/ops/base/Ops.ObjectChanged/Ops.ObjectChanged.js
@@ -0,0 +1,16 @@
+op.name="ObjectChanged";
+
+var inObj=op.inObject("Object");
+
+var next=op.outFunction("Next");
+
+var obj=null;
+
+inObj.onChange=function()
+{
+    if(inObj.get()!=obj)
+    {
+        obj=inObj.get();
+        next.trigger();
+    }
+};
