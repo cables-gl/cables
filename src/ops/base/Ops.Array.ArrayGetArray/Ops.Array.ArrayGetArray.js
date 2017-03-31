ae2ed1a40380bd420c8811cf94de382c35f1eb3c
tom <tom@undev.de>
2017-03-31 15:43:48 +0200

    new op array get array

---
+++ src/ops/base/Ops.Array.ArrayGetArray/Ops.Array.ArrayGetArray.js
@@ -0,0 +1,28 @@
+op.name="ArrayGetArray";
+
+var inArrays=op.inArray("Array of Arrays");
+var index=op.inValueInt("Index");
+
+var result=op.outArray("Result Array");
+
+inArrays.onChange=update;
+index.onChange=update;
+
+function update()
+{
+    var theArray=inArrays.get();
+    if(!theArray)
+    {
+        op.log('no array');
+        return;
+    }
+    
+    var ind=Math.floor(index.get());
+    if(ind<0 || ind>theArray.length-1)
+    {
+        op.log("index wrong");
+        return;
+    }
+    
+    result.set(theArray[ind]);
+}
