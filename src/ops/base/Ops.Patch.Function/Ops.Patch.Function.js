dae15cb234df3ffa1d81071cd2ba411b8064052a
pandur <tom@undev.de>
2017-04-04 15:02:49 +0200

    hurz

--- src/ops/base/Ops.Patch.Function/Ops.Patch.Function.js
+++ src/ops/base/Ops.Patch.Function/Ops.Patch.Function.js
@@ -3,7 +3,12 @@ op.name='ExternalFunction';
 // todo: rename to externalFunction ?
 
 var funcName=op.addInPort(new Port(op,"Function Name",OP_PORT_TYPE_VALUE,{type:'string'}));
+
+var triggerButton=op.inFunctionButton("trigger");
+
 var trigger=op.addOutPort(new Port(op,"Trigger",OP_PORT_TYPE_FUNCTION));
+
+triggerButton.onTriggered=triggered;
 
 funcName.onValueChanged=function()
 {
