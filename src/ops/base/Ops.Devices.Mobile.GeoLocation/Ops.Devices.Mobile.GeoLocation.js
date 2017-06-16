838a17db3ba4084eb9894a8e23170800cfac916e
pandur <tom@undev.de>
2017-06-16 14:19:56 +0200

    geolocation op

---
+++ src/ops/base/Ops.Devices.Mobile.GeoLocation/Ops.Devices.Mobile.GeoLocation.js
@@ -0,0 +1,21 @@
+op.name="GeoLocation";
+
+var outSupported=op.outValue("Browser Support",navigator.geolocation!=false);
+
+var outLat=op.outValue("Latitude");
+var outLon=op.outValue("Longitude");
+
+if(navigator.geolocation && navigator.geolocation.watchPosition)
+    navigator.geolocation.watchPosition(updatePos);
+
+function updatePos(position)
+{
+    outLat.set(position.coords.latitude);
+    outLon.set(position.coords.longitude);
+}
+
+
+op.onDelete=function()
+{
+    
+};
