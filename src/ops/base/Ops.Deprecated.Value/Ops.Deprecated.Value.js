158a9e6c45888cf7f87757c9512dee6c48593084
pandur <tom@undev.de>
2017-07-28 14:59:04 +0200

    renames / cleanup

---
+++ src/ops/base/Ops.Deprecated.Value/Ops.Deprecated.Value.js
@@ -0,0 +1,24 @@
+
+// remove trigger - see valuetrigger...
+
+
+op.name='Value';
+var exe=op.addInPort(new Port(op,"exe",OP_PORT_TYPE_FUNCTION));
+var v=op.addInPort(new Port(op,"value",OP_PORT_TYPE_VALUE));
+
+var result=op.addOutPort(new Port(op,"result"));
+
+// function frame(time)
+// {
+//     op.exec();
+// }
+
+var exec=function()
+{
+    // var va=v.get();
+    // if(result.get()!=va) 
+    result.set(v.get());
+};
+
+exe.onTriggered=exec;
+v.onValueChanged=exec;
---
+++ src/ops/base/Ops.String.Split/Ops.String.Split.js
@@ -0,0 +1,21 @@
+op.name="Split";
+
+var inString = op.inValueString("Input String","1,2,3");
+var separator = op.inValueString("Separator", ",");
+
+var outArray=op.outArray("Result");
+
+separator.onChange = exec;
+inString.onChange = exec;
+exec();
+
+function exec()
+{
+    var s = inString.get();
+    if(s)
+    {
+        var arr = s.split(separator.get());
+        outArray.set(arr);    
+    }
+
+}
