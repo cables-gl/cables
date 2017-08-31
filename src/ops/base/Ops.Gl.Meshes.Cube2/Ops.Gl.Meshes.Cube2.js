2aef6eb0a26e612a9b3d4da7ad68d1c3134e1c3d
pandur <tom@undev.de>
2017-08-31 09:56:30 +0200

    correct sized cube

---
+++ src/ops/base/Ops.Gl.Meshes.Cube2/Ops.Gl.Meshes.Cube2.js
@@ -0,0 +1,182 @@
+op.name='Cube';
+
+var render=op.inFunction('render');
+var width=op.inValue('width');
+var height=op.inValue('height');
+var lengt=op.inValue('length');
+var center=op.inValueBool('center');
+
+var active=op.inValueBool('Active',true);
+
+var trigger=op.outFunction('trigger');
+var geomOut=op.outObject("geometry");
+
+var cgl=op.patch.cgl;
+var geom=null;
+var mesh=null;
+width.set(1.0);
+height.set(1.0);
+lengt.set(1.0);
+center.set(true);
+
+render.onTriggered=function()
+{
+    if(active.get() && mesh) mesh.render(cgl.getShader());
+    trigger.trigger();
+};
+
+function buildMesh()
+{
+    if(!geom)geom=new CGL.Geometry("cube");
+    geom.clear();
+
+    var x=width.get();
+    var nx=-1*width.get();
+    var y=lengt.get();
+    var ny=-1*lengt.get();
+    var z=height.get();
+    var nz=-1*height.get();
+
+    if(!center.get())
+    {
+        nx=0;
+        ny=0;
+        nz=0;
+    }
+    else
+    {
+        x*=0.5;
+        nx*=0.5;
+        y*=0.5;
+        ny*=0.5;
+        z*=0.5;
+        nz*=0.5;
+    }
+
+    geom.vertices = [
+        // Front face
+        nx, ny,  z,
+        x, ny,  z,
+        x,  y,  z,
+        nx,  y,  z,
+        // Back face
+        nx, ny, nz,
+        nx,  y, nz,
+        x,  y, nz,
+        x, ny, nz,
+        // Top face
+        nx,  y, nz,
+        nx,  y,  z,
+        x,  y,  z,
+        x,  y, nz,
+        // Bottom face
+        nx, ny, nz,
+        x, ny, nz,
+        x, ny,  z,
+        nx, ny,  z,
+        // Right face
+        x, ny, nz,
+        x,  y, nz,
+        x,  y,  z,
+        x, ny,  z,
+        // zeft face
+        nx, ny, nz,
+        nx, ny,  z,
+        nx,  y,  z,
+        nx,  y, nz
+        ];
+
+    geom.texCoords = [
+          // Front face
+          0.0, 1.0,
+          1.0, 1.0,
+          1.0, 0.0,
+          0.0, 0.0,
+          // Back face
+          1.0, 1.0,
+          1.0, 0.0,
+          0.0, 0.0,
+          0.0, 1.0,
+          // Top face
+          0.0, 0.0,
+          0.0, 1.0,
+          1.0, 1.0,
+          1.0, 0.0,
+          // Bottom face
+          1.0, 0.0,
+          0.0, 0.0,
+          0.0, 1.0,
+          1.0, 1.0,
+          // Right face
+          1.0, 1.0,
+          1.0, 0.0,
+          0.0, 0.0,
+          0.0, 1.0,
+          // Left face
+          0.0, 1.0,
+          1.0, 1.0,
+          1.0, 0.0,
+          0.0, 0.0,
+        ];
+
+    geom.vertexNormals = [
+        // Front face
+         0.0,  0.0,  1.0,
+         0.0,  0.0,  1.0,
+         0.0,  0.0,  1.0,
+         0.0,  0.0,  1.0,
+
+        // Back face
+         0.0,  0.0, -1.0,
+         0.0,  0.0, -1.0,
+         0.0,  0.0, -1.0,
+         0.0,  0.0, -1.0,
+
+        // Top face
+         0.0,  1.0,  0.0,
+         0.0,  1.0,  0.0,
+         0.0,  1.0,  0.0,
+         0.0,  1.0,  0.0,
+
+        // Bottom face
+         0.0, -1.0,  0.0,
+         0.0, -1.0,  0.0,
+         0.0, -1.0,  0.0,
+         0.0, -1.0,  0.0,
+
+        // Right face
+         1.0,  0.0,  0.0,
+         1.0,  0.0,  0.0,
+         1.0,  0.0,  0.0,
+         1.0,  0.0,  0.0,
+
+        // Left face
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0,
+        -1.0,  0.0,  0.0
+    ];
+
+
+    geom.verticesIndices = [
+        0, 1, 2,      0, 2, 3,    // Front face
+        4, 5, 6,      4, 6, 7,    // Back face
+        8, 9, 10,     8, 10, 11,  // Top face
+        12, 13, 14,   12, 14, 15, // Bottom face
+        16, 17, 18,   16, 18, 19, // Right face
+        20, 21, 22,   20, 22, 23  // Left face
+    ];
+
+    mesh=new CGL.Mesh(cgl,geom);
+    geomOut.set(null);
+    geomOut.set(geom);
+
+}
+
+width.onValueChanged=buildMesh;
+height.onValueChanged=buildMesh;
+lengt.onValueChanged=buildMesh;
+center.onValueChanged=buildMesh;
+buildMesh();
+
+
