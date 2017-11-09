b8f868b306c2f5561b2d8249115d2657eb744144
tom <tom@hackymac.lan>
2017-11-09 19:24:48 +0100

    cleaned up drawimage

---
+++ src/ops/base/Ops.Gl.TextureEffects.DrawImageNew/Ops.Gl.TextureEffects.DrawImageNew.js
@@ -0,0 +1,151 @@
+op.name='DrawImage';
+
+var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
+
+var blendMode=CGL.TextureEffect.AddBlendSelect(op,"blendMode");
+var amount=op.inValueSlider("amount",1);
+
+var image=op.addInPort(new Port(op,"image",OP_PORT_TYPE_TEXTURE,{preview:true }));
+var imageAlpha=op.addInPort(new Port(op,"imageAlpha",OP_PORT_TYPE_TEXTURE,{preview:true }));
+
+var alphaSrc=op.inValueSelect("alphaSrc",['alpha channel','luminance']);
+var removeAlphaSrc=op.addInPort(new Port(op,"removeAlphaSrc",OP_PORT_TYPE_VALUE,{ display:'bool' }));
+var invAlphaChannel=op.addInPort(new Port(op,"invert alpha channel",OP_PORT_TYPE_VALUE,{ display:'bool' }));
+
+var trigger=op.addOutPort(new Port(op,"trigger",OP_PORT_TYPE_FUNCTION));
+
+blendMode.set('normal');
+var cgl=op.patch.cgl;
+var shader=new CGL.Shader(cgl,'drawimage');
+
+
+var srcFrag=attachments.drawimage_frag.replace('{{BLENDCODE}}',CGL.TextureEffect.getBlendCode());
+
+
+    
+
+shader.setSource(attachments.drawimage_vert,srcFrag);
+var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+var textureImaghe=new CGL.Uniform(shader,'t','image',1);
+var textureAlpha=new CGL.Uniform(shader,'t','imageAlpha',2);
+
+invAlphaChannel.onValueChanged=function()
+{
+    if(invAlphaChannel.get()) shader.define('INVERT_ALPHA');
+        else shader.removeDefine('INVERT_ALPHA');
+};
+
+removeAlphaSrc.onValueChanged=function()
+{
+    if(removeAlphaSrc.get()) shader.define('REMOVE_ALPHA_SRC');
+        else shader.removeDefine('REMOVE_ALPHA_SRC');
+};
+removeAlphaSrc.set(true);
+
+alphaSrc.onValueChanged=function()
+{
+    if(alphaSrc.get()=='luminance') shader.define('ALPHA_FROM_LUMINANCE');
+        else shader.removeDefine('ALPHA_FROM_LUMINANCE');
+};
+
+alphaSrc.set("alpha channel");
+
+
+{
+    //
+    // texture flip
+    //
+    var flipX=op.addInPort(new Port(op,"flip x",OP_PORT_TYPE_VALUE,{ display:'bool' }));
+    var flipY=op.addInPort(new Port(op,"flip y",OP_PORT_TYPE_VALUE,{ display:'bool' }));
+
+    flipX.onValueChanged=function()
+    {
+        if(flipX.get()) shader.define('TEX_FLIP_X');
+            else shader.removeDefine('TEX_FLIP_X');
+    };
+
+    flipY.onValueChanged=function()
+    {
+        if(flipY.get()) shader.define('TEX_FLIP_Y');
+            else shader.removeDefine('TEX_FLIP_Y');
+    };
+}
+
+{
+    //
+    // texture transform
+    //
+    
+    var doTransform=op.inValueBool("Transform",false);
+    
+    var scaleX=op.inValueSlider("Scale X",1);
+    var scaleY=op.inValueSlider("Scale Y",1);
+
+    var posX=op.inValue("Position X",0);
+    var posY=op.inValue("Position Y",0);
+    
+    var rotate=op.inValue("Rotation",0);
+
+    var uniScaleX=new CGL.Uniform(shader,'f','scaleX',scaleX);
+    var uniScaleY=new CGL.Uniform(shader,'f','scaleY',scaleY);
+    
+    var uniPosX=new CGL.Uniform(shader,'f','posX',posX);
+    var uniPosY=new CGL.Uniform(shader,'f','posY',posY);
+    var uniRotate=new CGL.Uniform(shader,'f','rotate',rotate);
+
+    doTransform.onChange=function()
+    {
+        if(doTransform.get())shader.define('TEX_TRANSFORM');
+        else shader.removeDefine('TEX_TRANSFORM');
+    };
+
+}
+
+blendMode.onValueChanged=function()
+{
+    CGL.TextureEffect.onChangeBlendSelect(shader,blendMode.get());
+};
+
+
+var amountUniform=new CGL.Uniform(shader,'f','amount',amount);
+
+
+imageAlpha.onValueChanged=function()
+{
+    if(imageAlpha.get() && imageAlpha.get().tex) shader.define('HAS_TEXTUREALPHA');
+        else shader.removeDefine('HAS_TEXTUREALPHA');
+};
+
+
+
+function doRender()
+{
+    if(!CGL.TextureEffect.checkOpInEffect(op)) return;
+
+    if(image.get() && image.get().tex && amount.get()>0.0)
+    {
+        cgl.setShader(shader);
+        cgl.currentTextureEffect.bind();
+
+        cgl.gl.activeTexture(cgl.gl.TEXTURE0);
+        cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
+
+        cgl.gl.activeTexture(cgl.gl.TEXTURE1);
+        cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, image.get().tex );
+        // else cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, null);
+
+        
+        if(imageAlpha.get() && imageAlpha.get().tex)
+        {
+            cgl.gl.activeTexture(cgl.gl.TEXTURE2);
+            cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, imageAlpha.get().tex );
+        }
+
+        cgl.currentTextureEffect.finish();
+        cgl.setPreviousShader();
+    }
+
+    trigger.trigger();
+}
+
+render.onTriggered=doRender;
---
+++ src/ops/base/Ops.Gl.TextureEffects.DrawImageNew/att_drawimage.frag
@@ -0,0 +1,67 @@
+#ifdef HAS_TEXTURES
+  IN vec2 texCoord;
+  UNI sampler2D tex;
+  UNI sampler2D image;
+#endif
+
+IN mat3 transform;
+UNI float rotate;
+{{BLENDCODE}}
+
+#ifdef HAS_TEXTUREALPHA
+   UNI sampler2D imageAlpha;
+#endif
+
+UNI float amount;
+
+void main()
+{
+   vec4 blendRGBA=vec4(0.0,0.0,0.0,1.0);
+   #ifdef HAS_TEXTURES
+       vec2 tc=texCoord;
+
+       #ifdef TEX_FLIP_X
+           tc.x=1.0-tc.x;
+       #endif
+       #ifdef TEX_FLIP_Y
+           tc.y=1.0-tc.y;
+       #endif
+
+       #ifdef TEX_TRANSFORM
+           vec3 coordinates=vec3(tc.x, tc.y,1.0);
+           tc=(transform * coordinates ).xy;
+       #endif
+    
+       blendRGBA=texture2D(image,tc);
+
+       vec3 blend=blendRGBA.rgb;
+       vec4 baseRGBA=texture2D(tex,texCoord);
+       vec3 base=baseRGBA.rgb;
+
+       vec3 colNew=_blend(base,blend);
+
+       #ifdef REMOVE_ALPHA_SRC
+           blendRGBA.a=1.0;
+       #endif
+
+       #ifdef HAS_TEXTUREALPHA
+           vec4 colImgAlpha=texture2D(imageAlpha,texCoord);
+           float colImgAlphaAlpha=colImgAlpha.a;
+
+           #ifdef ALPHA_FROM_LUMINANCE
+               vec3 gray = vec3(dot(vec3(0.2126,0.7152,0.0722), colImgAlpha.rgb ));
+               colImgAlphaAlpha=(gray.r+gray.g+gray.b)/3.0;
+           #endif
+
+           blendRGBA.a=colImgAlphaAlpha*blendRGBA.a;
+       #endif
+
+
+   #endif
+
+   blendRGBA.rgb=mix( colNew, base ,1.0-blendRGBA.a*amount);
+   blendRGBA.a=1.0;
+
+
+   gl_FragColor = blendRGBA;
+}
