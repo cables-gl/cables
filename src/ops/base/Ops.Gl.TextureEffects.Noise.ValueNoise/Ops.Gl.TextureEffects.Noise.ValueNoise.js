f282351adc5c90126388c9779e2c7fd4741eb7db
tom <tom@hackymac.lan>
2017-11-09 18:20:54 +0100

    valuenoise blendmodes

--- src/ops/base/Ops.Gl.TextureEffects.Noise.ValueNoise/Ops.Gl.TextureEffects.Noise.ValueNoise.js
+++ src/ops/base/Ops.Gl.TextureEffects.Noise.ValueNoise/Ops.Gl.TextureEffects.Noise.ValueNoise.js
@@ -3,6 +3,12 @@ op.name="ValueNoise";
 var cgl=op.patch.cgl;
 
 var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
+
+var blendMode=CGL.TextureEffect.AddBlendSelect(op,"Blend Mode","normal");
+var amount=op.inValueSlider("Amount",1);
+
+
+
 var x=op.inValue("X",0);
 var y=op.inValue("Y",0);
 var z=op.inValue("Z",0);
@@ -11,13 +17,23 @@ var trigger=op.addOutPort(new Port(op,"trigger",OP_PORT_TYPE_FUNCTION));
 
 var shader=new CGL.Shader(cgl);
 
-shader.setSource(shader.getDefaultVertexShader(),attachments.valuenoise3d_frag);
+var srcFrag=attachments.valuenoise3d_frag.replace('{{BLENDCODE}}',CGL.TextureEffect.getBlendCode());
+
+shader.setSource(shader.getDefaultVertexShader(),srcFrag);
 var textureUniform=new CGL.Uniform(shader,'t','tex',0);
 
 var uniZ=new CGL.Uniform(shader,'f','z',z);
 var uniX=new CGL.Uniform(shader,'f','x',x);
 var uniY=new CGL.Uniform(shader,'f','y',y);
 var uniScale=new CGL.Uniform(shader,'f','scale',scale);
+
+var amountUniform=new CGL.Uniform(shader,'f','amount',amount);
+
+blendMode.onChange=function()
+{
+    CGL.TextureEffect.onChangeBlendSelect(shader,blendMode.get());
+};
+
 
 render.onTriggered=function()
 {
