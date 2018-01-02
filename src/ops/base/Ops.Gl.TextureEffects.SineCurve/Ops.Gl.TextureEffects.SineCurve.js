8d83baa0598f9331d803c0e0ccc630021dc930a3
pandur <tom@undev.de>
2018-01-02 23:22:09 +0100

    sinuscurve textureeffect

--- src/ops/base/Ops.Gl.TextureEffects.SineCurve/Ops.Gl.TextureEffects.SineCurve.js
+++ src/ops/base/Ops.Gl.TextureEffects.SineCurve/Ops.Gl.TextureEffects.SineCurve.js
@@ -1,8 +1,14 @@
 
 var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
+
+var blendMode=CGL.TextureEffect.AddBlendSelect(op,"Blend Mode","normal");
+var amount=op.inValueSlider("Amount",1);
+
+var fill=op.inValueBool("Fill",false);
 var offset=op.inValue("offset",0);
 var frequency=op.inValue("frequency",10);
 var amplitude=op.inValueSlider("amplitude",1);
+var thick=op.inValueSlider("Thickness",0.1);
 
 var flip=op.inValueBool("Flip",false);
 
@@ -11,7 +17,10 @@ var trigger=op.addOutPort(new Port(op,"trigger",OP_PORT_TYPE_FUNCTION));
 var cgl=op.patch.cgl;
 var shader=new CGL.Shader(cgl);
 var tsize=[128,128];
+
 var srcFrag=attachments.sinecurve_frag;
+srcFrag=srcFrag.replace('{{BLENDCODE}}',CGL.TextureEffect.getBlendCode());
+
 
 shader.setSource(shader.getDefaultVertexShader(),srcFrag );
 var textureUniform=new CGL.Uniform(shader,'t','tex',0);
@@ -19,15 +28,30 @@ var textureUniform=new CGL.Uniform(shader,'t','tex',0);
 var offsetUniform=new CGL.Uniform(shader,'f','offset',offset);
 var frequencyUniform=new CGL.Uniform(shader,'f','frequency',frequency);
 var amplitudeUniform=new CGL.Uniform(shader,'f','amplitude',amplitude);
+var thickUniform=new CGL.Uniform(shader,'f','thick',thick);
 var texSizeUniform=new CGL.Uniform(shader,'2f','texSize',tsize);
+var amountUniform=new CGL.Uniform(shader,'f','amount',amount);
 
 flip.onChange=updateFlip;
+fill.onChange=updateFill;
 
 function updateFlip()
 {
     if(flip.get()) shader.define('FLIP');
         else shader.removeDefine('FLIP');
 }
+
+function updateFill()
+{
+    if(fill.get()) shader.define('FILL');
+        else shader.removeDefine('FILL');
+}
+
+blendMode.onChange=function()
+{
+    CGL.TextureEffect.onChangeBlendSelect(shader,blendMode.get());
+};
+
 
 render.onTriggered=function()
 {
--- src/ops/base/Ops.Gl.TextureEffects.SineCurve/att_sinecurve.frag
+++ src/ops/base/Ops.Gl.TextureEffects.SineCurve/att_sinecurve.frag
@@ -1,34 +1,47 @@
 
 IN vec2 texCoord;
 UNI sampler2D tex;
-
+UNI float amount;
 UNI float offset;
 UNI float frequency;
 UNI float amplitude;
+UNI float thick;
 
 #define M_PI2 1.5707963267948966
 #define M_PI 3.1415926535897932384626433832795
 
+{{BLENDCODE}}
+
 void main()
 {
-    vec4 col=texture2D(tex,texCoord);
+    vec4 base=texture2D(tex,texCoord);
 
-    float s=sin((texCoord.x*M_PI*frequency)+offset)+1.0;
-    s*=amplitude;
-    s/=2.0;
+    float s=sin((texCoord.x*M_PI*frequency)+offset)*(amplitude*0.5);
+    s+=texCoord.y;
+
+    #ifndef FILL
+        s+=(thick/2.0);
+        s-=0.5;
+        s*=2.0;
+        s-=(thick/2.0);
+        s+=texCoord.y;
+        s=thick/s;
+        s=smoothstep(0.75,1.0,s);
+    #endif
     
+    #ifdef FILL
+        if(s>0.5) s=1.0;
+            else s=0.0;
+    #endif
+
     #ifdef FLIP
-        if(texCoord.y>s) s=1.0;
-            else s=0.0;
-    #endif
+        s=1.0-s;
+    #endif    
 
-    #ifndef FLIP
-        if(texCoord.y<s) s=1.0;
-            else s=0.0;
-    #endif
-
-
-    col*=vec4(s,s,s,1.0);
+    vec4 col=vec4(s,s,s,1.0);
+    // col.a=1.0;
+    col=vec4( _blend(base.rgb,col.rgb) ,1.0);
+    col=vec4( mix( col.rgb, base.rgb ,1.0-base.a*amount),1.0);
 
     gl_FragColor=col;
 }
