095ee83b55e0adf1d7362c8cd063ea7df3c517a2
pandrr <tom@drastic.net>
2018-01-18 18:44:25 +0100

    twist axis select

--- src/ops/base/Ops.Gl.ShaderEffects.Twist2/Ops.Gl.ShaderEffects.Twist2.js
+++ src/ops/base/Ops.Gl.ShaderEffects.Twist2/Ops.Gl.ShaderEffects.Twist2.js
@@ -4,6 +4,8 @@ var trigger=op.outFunction("Trigger");
 var amount=op.inValue("Amount",300);
 var height=op.inValue("Height",2);
 
+var axis=op.inValueSelect("Axis",["X","Y","Z"],"Y");
+
 
 var uniAmount=null;
 var uniHeight=null;
@@ -18,6 +20,20 @@ function removeModule()
         shader.removeModule(mod);
         shader=null;
     }
+}
+
+axis.onChange=updateAxis;
+
+function updateAxis()
+{
+    if(!shader)return;
+    shader.removeDefine(mod.prefix+"AXIS_X");
+    shader.removeDefine(mod.prefix+"AXIS_Y");
+    shader.removeDefine(mod.prefix+"AXIS_Z");
+    
+    if(axis.get()=="X")shader.define(mod.prefix+"AXIS_X");
+    if(axis.get()=="Y")shader.define(mod.prefix+"AXIS_Y");
+    if(axis.get()=="Z")shader.define(mod.prefix+"AXIS_Z");
 }
 
 render.onLinkChanged=removeModule;
@@ -36,7 +52,7 @@ render.onTriggered=function()
 
         uniAmount=new CGL.Uniform(shader,'f',mod.prefix+'amount',amount);
         uniHeight=new CGL.Uniform(shader,'f',mod.prefix+'height',height);
-        
+        updateAxis();
     }
 
     trigger.trigger();
