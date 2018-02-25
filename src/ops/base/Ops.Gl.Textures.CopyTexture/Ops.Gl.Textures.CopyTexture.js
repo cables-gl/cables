21c97f069de34e1f485db45b6bb60384a1b98f8d
pandur <tom@undev.de>
2018-02-25 18:24:53 +0100

    copytexture

---
+++ src/ops/base/Ops.Gl.Textures.CopyTexture/Ops.Gl.Textures.CopyTexture.js
@@ -0,0 +1,214 @@
+
+var inTexture=op.inTexture("Texture");
+var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
+var useVPSize=op.addInPort(new Port(op,"use original size",OP_PORT_TYPE_VALUE,{ display:'bool' }));
+var width=op.inValueInt("width");
+var height=op.inValueInt("height");
+
+var tfilter=op.inValueSelect("filter",['nearest','linear','mipmap']);
+var twrap=op.inValueSelect("wrap",['clamp to edge','repeat','mirrored repeat']);
+var fpTexture=op.inValueBool("HDR");
+
+var trigger=op.addOutPort(new Port(op,"trigger",OP_PORT_TYPE_FUNCTION));
+var texOut=op.outTexture("texture_out");
+
+var outRatio=op.outValue("Aspect Ratio");
+
+texOut.set(null);
+var cgl=op.patch.cgl;
+var effect=null;
+var tex=null;
+
+var w=8,h=8;
+var prevViewPort=[0,0,0,0];
+var reInitEffect=true;
+
+var bgFrag=''
+    .endl()+'UNI float a;'
+    .endl()+'UNI sampler2D tex;'
+    .endl()+'IN vec2 texCoord;'
+    .endl()+'void main()'
+    .endl()+'{'
+    .endl()+'   vec4 col=texture2D(tex,texCoord);'
+    .endl()+'   gl_FragColor = col;'
+    .endl()+'}';
+var bgShader=new CGL.Shader(cgl,'imgcompose bg');
+bgShader.setSource(bgShader.getDefaultVertexShader(),bgFrag);
+var textureUniform=new CGL.Uniform(bgShader,'t','tex',0);
+
+var selectedFilter=CGL.Texture.FILTER_LINEAR;
+var selectedWrap=CGL.Texture.WRAP_CLAMP_TO_EDGE;
+
+
+function initEffect()
+{
+    if(effect)effect.delete();
+    if(tex)tex.delete();
+
+    effect=new CGL.TextureEffect(cgl,{"isFloatingPointTexture":fpTexture.get()});
+
+    tex=new CGL.Texture(cgl,
+        {
+            "isFloatingPointTexture":fpTexture.get(),
+            "filter":selectedFilter,
+            "wrap":selectedWrap,
+            "width": Math.floor(width.get()),
+            "height": Math.floor(height.get()),
+        });
+
+    effect.setSourceTexture(tex);
+    texOut.set(null);
+    reInitEffect=false;
+}
+
+fpTexture.onChange=function()
+{
+    reInitEffect=true;
+};
+
+function updateResolution()
+{
+    if(!effect)initEffect();
+    if(!inTexture.get())return;
+
+    if(useVPSize.get())
+    {
+        w=inTexture.get().width;
+        h=inTexture.get().height;
+    }
+    else
+    {
+        w=Math.floor(width.get());
+        h=Math.floor(height.get());
+    }
+
+    if((w!=tex.width || h!= tex.height) && (w!==0 && h!==0))
+    {
+        height.set(h);
+        width.set(w);
+        tex.filter=CGL.Texture.FILTER_LINEAR;
+        tex.setSize(w,h);
+        outRatio.set(w/h);
+        effect.setSourceTexture(tex);
+    }
+
+    if(texOut.get())
+        if(!texOut.get().isPowerOfTwo() )
+        {
+            if(!op.uiAttribs.hint)
+                op.uiAttr(
+                    {
+                        hint:'texture dimensions not power of two! - texture filtering will not work.',
+                        warning:null
+                    });
+        }
+        else
+        if(op.uiAttribs.hint)
+        {
+            op.uiAttr({hint:null,warning:null}); //todo only when needed...
+        }
+
+}
+
+
+function updateSizePorts()
+{
+    if(useVPSize.get())
+    {
+        width.setUiAttribs({hidePort:true,greyout:true});
+        height.setUiAttribs({hidePort:true,greyout:true});
+    }
+    else
+    {
+        width.setUiAttribs({hidePort:false,greyout:false});
+        height.setUiAttribs({hidePort:false,greyout:false});
+    }
+}
+
+useVPSize.onValueChanged=function()
+{
+    updateSizePorts();
+    if(useVPSize.get())
+    {
+        width.onValueChanged=null;
+        height.onValueChanged=null;
+    }
+    else
+    {
+        width.onValueChanged=updateResolution;
+        height.onValueChanged=updateResolution;
+    }
+    updateResolution();
+};
+
+
+var doRender=function()
+{
+    if(!effect || reInitEffect)
+    {
+        initEffect();
+    }
+    var vp=cgl.getViewPort();
+    prevViewPort[0]=vp[0];
+    prevViewPort[1]=vp[1];
+    prevViewPort[2]=vp[2];
+    prevViewPort[3]=vp[3];
+
+    updateResolution();
+
+    cgl.currentTextureEffect=effect;
+    effect.setSourceTexture(tex);
+
+    effect.startEffect();
+
+    // render background color...
+    cgl.setShader(bgShader);
+    cgl.currentTextureEffect.bind();
+    cgl.gl.activeTexture(cgl.gl.TEXTURE0);
+    cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, inTexture.get().tex );
+    cgl.currentTextureEffect.finish();
+    cgl.setPreviousShader();
+
+    texOut.set(effect.getCurrentSourceTexture());
+
+    effect.endEffect();
+
+    cgl.setViewPort(prevViewPort[0],prevViewPort[1],prevViewPort[2],prevViewPort[3]);
+
+    cgl.currentTextureEffect=null;
+    
+    trigger.trigger();
+};
+
+
+function onWrapChange()
+{
+    if(twrap.get()=='repeat') selectedWrap=CGL.Texture.WRAP_REPEAT;
+    if(twrap.get()=='mirrored repeat') selectedWrap=CGL.Texture.WRAP_MIRRORED_REPEAT;
+    if(twrap.get()=='clamp to edge') selectedWrap=CGL.Texture.WRAP_CLAMP_TO_EDGE;
+
+    reInitEffect=true;
+    updateResolution();
+}
+
+twrap.set('clamp to edge');
+twrap.onValueChanged=onWrapChange;
+
+function onFilterChange()
+{
+    if(tfilter.get()=='nearest') selectedFilter=CGL.Texture.FILTER_NEAREST;
+    if(tfilter.get()=='linear')  selectedFilter=CGL.Texture.FILTER_LINEAR;
+
+    reInitEffect=true;
+    updateResolution();
+}
+
+tfilter.set('linear');
+tfilter.onValueChanged=onFilterChange;
+
+useVPSize.set(true);
+render.onTriggered=doRender;
+
+width.set(640);
+height.set(360);
+updateSizePorts();
