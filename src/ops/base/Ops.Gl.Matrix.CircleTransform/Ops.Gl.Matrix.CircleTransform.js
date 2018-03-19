00285df7489bc66d0e3112491405679196698f4e
tom <tom@undev.de>
2018-03-19 08:54:30 +0100

    circletransform rotate

--- src/ops/base/Ops.Gl.Matrix.CircleTransform/Ops.Gl.Matrix.CircleTransform.js
+++ src/ops/base/Ops.Gl.Matrix.CircleTransform/Ops.Gl.Matrix.CircleTransform.js
@@ -1,11 +1,8 @@
-
-
 var render=op.addInPort(new Port(op,"render",OP_PORT_TYPE_FUNCTION));
-
 var segments=op.addInPort(new Port(op,"segments"));
 var radius=op.addInPort(new Port(op,"radius"));
 var percent=op.addInPort(new Port(op,"percent",OP_PORT_TYPE_VALUE,{display:'range'}));
-
+var inRotate=op.inValueBool("Rotate");
 var trigger=op.addOutPort(new Port(op,"trigger",OP_PORT_TYPE_FUNCTION));
 var index=op.addOutPort(new Port(op,"index"));
 
@@ -19,23 +16,23 @@ var pos=[];
 segments.onChange=calcLater;
 radius.onChange=calcLater;
 percent.onChange=calcLater;
-
 var needsCalc=true;
-// calc();
 
 render.onTriggered=doRender;
 
 function doRender()
 {
     if(needsCalc)calc();
+    var doRot=inRotate.get();
     for(var i=0;i<pos.length;i++)
     {
         cgl.pushModelMatrix();
 
         mat4.translate(cgl.mvMatrix,cgl.mvMatrix, pos[i] );
+        if(doRot)mat4.rotateZ(cgl.mvMatrix,cgl.mvMatrix, i/pos.length*CGL.DEG2RAD*-360);
+
         index.set(i);
         trigger.trigger();
-
 
         cgl.popModelMatrix();
     }
