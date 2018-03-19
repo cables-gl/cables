8ca4e095e4a0b0930583d75f2b0f6f035ae4c82e
tom <tom@undev.de>
2018-03-19 08:45:28 +0100

    twirl new

---
+++ src/ops/base/Ops.Gl.TextureEffects.Twirl2/Ops.Gl.TextureEffects.Twirl2.js
@@ -0,0 +1,36 @@
+var render=op.inFunction("Render");
+var amount=op.inValue("Amount");
+var radius=op.inValue("Radius");
+var centerX=op.inValue("Center X",0.5);
+var centerY=op.inValue("Center Y",0.5);
+var trigger=op.outFunction("Next");
+
+var cgl=op.patch.cgl;
+var shader=new CGL.Shader(cgl);
+shader.setSource(shader.getDefaultVertexShader(),attachments.twirl_frag);
+
+var textureUniform=new CGL.Uniform(shader,'t','tex',0);
+var uniamount=new CGL.Uniform(shader,'f','amount',0);
+var uniRadius=new CGL.Uniform(shader,'f','radius',radius);
+var unicenterX=new CGL.Uniform(shader,'f','centerX',centerX);
+var unicenterY=new CGL.Uniform(shader,'f','centerY',centerY);
+
+render.onTriggered=function()
+{
+    if(!cgl.currentTextureEffect)return;
+
+    var texture=cgl.currentTextureEffect.getCurrentSourceTexture();
+
+    uniamount.setValue(amount.get()*(1/texture.width));
+
+    cgl.setShader(shader);
+    cgl.currentTextureEffect.bind();
+
+    cgl.gl.activeTexture(cgl.gl.TEXTURE0);
+    cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, texture.tex );
+
+    cgl.currentTextureEffect.finish();
+    cgl.setPreviousShader();
+
+    trigger.trigger();
+};
