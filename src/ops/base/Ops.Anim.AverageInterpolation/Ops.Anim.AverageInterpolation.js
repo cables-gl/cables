901195aff850d7f7c625fe3b25c92de603a153b5
pandur <tom@undev.de>
2018-04-06 17:28:02 +0200

    averageinterpolation new op

---
+++ src/ops/base/Ops.Anim.AverageInterpolation/Ops.Anim.AverageInterpolation.js
@@ -0,0 +1,49 @@
+var divisor=5;
+
+var exec=op.inFunction("Update");
+var inVal=op.inValue("Value");
+
+var next=op.outFunction("Next");
+var inDivisor=op.inValue("Divisor",divisor);
+var result=op.outValue("Result",0);
+
+var val=0;
+var goal=0;
+
+var oldVal=0;
+var lastTrigger=0;
+
+inVal.onChange=function()
+{
+    goal=inVal.get();
+};
+
+inDivisor.onChange=function()
+{
+    divisor=inDivisor.get();
+    if(divisor<=0)divisor=5;
+};
+
+
+exec.onTriggered=function()
+{
+    var tm=1;
+    if(CABLES.now()-lastTrigger>500)val=inVal.get();
+    else tm=(CABLES.now()-lastTrigger)/16;
+
+    lastTrigger=CABLES.now();
+
+    if(divisor<=0)divisor=0.0001;
+    val=val+(goal-val)/(divisor*tm);
+
+    if(val>0 && val<0.000000001)val=0;
+    if(divisor!=divisor)val=0;
+    
+    if(oldVal!=val)
+    {
+        result.set(val);
+        oldVal=val;
+    }
+    
+    next.trigger();
+};
