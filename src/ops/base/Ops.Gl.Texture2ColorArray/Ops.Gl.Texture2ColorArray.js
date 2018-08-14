8044d6b7a098b8a9fb032e18215a6347051dd2a3
pandur <tom@undev.de>
2018-08-14 13:06:03 +0200

    missing files

---
+++ src/ops/base/Ops.Gl.Texture2ColorArray/Ops.Gl.Texture2ColorArray.js
@@ -0,0 +1,50 @@
+const
+    cgl = op.patch.cgl,
+    pUpdate = op.inFunction("update"),
+    tex = op.inObject("texture"),
+    outTrigger = op.outFunction("trigger"),
+    outColors = op.outArray("Colors")
+;
+
+var
+    fb = null,
+    pixelData = null,
+    texChanged = false
+;
+
+tex.onChange = function (){ texChanged=true; };
+pUpdate.onTriggered = function () {
+    var realTexture = tex.get(), gl = cgl.gl;
+    if (!realTexture) return;
+    if (!fb) fb = gl.createFramebuffer();
+    
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+    
+    if (texChanged) {
+        gl.framebufferTexture2D(
+           gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0,
+           gl.TEXTURE_2D, realTexture.tex, 0
+        );
+        pixelData = new Uint8Array(realTexture.width*realTexture.height*4);
+        texChanged = false;
+    }
+    
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+    
+    gl.readPixels(
+       0, 0,
+       realTexture.width,
+       realTexture.height,
+       gl.RGBA,
+       gl.UNSIGNED_BYTE,
+       pixelData
+    );        
+    
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    
+    outColors.set(null);
+    outColors.set(pixelData);
+    
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    outTrigger.trigger();
+}
