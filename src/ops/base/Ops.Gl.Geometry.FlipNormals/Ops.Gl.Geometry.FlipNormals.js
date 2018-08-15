eea6afb49ebc9a510913766cba920b5ae1f2c16e
pandur <tom@undev.de>
2018-08-15 18:29:22 +0200

    flip normals op

---
+++ src/ops/base/Ops.Gl.Geometry.FlipNormals/Ops.Gl.Geometry.FlipNormals.js
@@ -0,0 +1,57 @@
+
+const geometry=op.addInPort(new Port(op,"Geometry",OP_PORT_TYPE_OBJECT));
+const outGeom=op.outObject("Result");
+
+const doFlip=op.inValueBool("Flip",true);
+const doNormalize=op.inValueBool("Normalize",true);
+
+doFlip.onChange=
+doNormalize.onChange=
+geometry.onChange=flip;
+
+function flip()
+{
+    var oldGeom=geometry.get();
+
+    if(!oldGeom)
+    {
+        outGeom.set(null);
+        return;
+    }
+
+
+    var geom=oldGeom.copy();
+    
+
+    if(doFlip.get())
+    {
+
+        for(var i=0;i<geom.vertexNormals.length;i++)
+        {
+            geom.vertexNormals[i]*=-1;
+        }
+        
+        if(doNormalize.get())
+        {
+            var vec=vec3.create();
+    
+            for(var i=0;i<geom.vertexNormals.length;i+=3)
+            {
+                vec3.set(vec,
+                    geom.vertexNormals[i+0],
+                    geom.vertexNormals[i+1],
+                    geom.vertexNormals[i+2]);
+                vec3.normalize(vec,vec);
+                
+                geom.vertexNormals[i+0]=vec[0];
+                geom.vertexNormals[i+1]=vec[1];
+                geom.vertexNormals[i+2]=vec[2];
+    
+            }
+            
+        }
+    }
+
+    outGeom.set(geom);
+
+}
