fb564d4f9cd1750128517b8904ac39cb080d3763
pandrr <tom@drastic.net>
2018-08-30 07:49:01 +0200

    directionaltranslate

--- src/ops/base/Ops.Gl.DirectionalTranslate/Ops.Gl.DirectionalTranslate.js
+++ src/ops/base/Ops.Gl.DirectionalTranslate/Ops.Gl.DirectionalTranslate.js
@@ -1,8 +1,6 @@
-
 const exec=op.inFunction("Exec");
-const inMatrix=op.inArray("Model Matrix");
+const inMatrix=op.inArray("Center Model Matrix");
 const inAmount=op.inValue("Amount");
-
 const next=op.outFunction("Next");
 
 const cgl=op.patch.cgl;
@@ -10,18 +8,15 @@ const cgl=op.patch.cgl;
 var va=vec3.create();
 var vb=vec3.create();
 var diff=vec3.create();
-    var mm=mat3.create();
+var mm=mat3.create();
 
 exec.onTriggered=function()
 {
     var matbefore=inMatrix.get();
     if(!matbefore)return;
 
-    cgl.modelMatrix();
-    
     mat4.getTranslation(va,matbefore);
     mat4.getTranslation(vb,cgl.modelMatrix());
-
 
     mat3.fromMat4(mm,cgl.modelMatrix());
     mat3.invert(mm,mm);
@@ -29,26 +24,14 @@ exec.onTriggered=function()
     vec3.sub(diff,vb,va);
     vec3.normalize(diff,diff);
     vec3.transformMat3(diff,diff,mm );    
-    
     vec3.normalize(diff,diff);
 
-// vec3.transformMat4(diff,diff,cgl.modelMatrix() );    
-    
-    
     diff[0]*=inAmount.get();
     diff[1]*=inAmount.get();
     diff[2]*=inAmount.get();
 
-// console.log('diff',diff);
-// console.log('va',va);
-// console.log('vb',vb);
-
-    
     cgl.pushModelMatrix();
     mat4.translate(cgl.mMatrix,cgl.mMatrix, diff);
     next.trigger();
     cgl.popModelMatrix();
-
 };
-
-
