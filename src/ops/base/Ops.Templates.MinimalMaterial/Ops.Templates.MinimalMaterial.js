192b46b9d962c1117cbff4688beda3d48b30cd9c
pandur <tom@undev.de>
2018-09-21 08:48:02 +0200

    missing ops/renamed ops

---
+++ src/ops/base/Ops.Templates.MinimalMaterial/Ops.Templates.MinimalMaterial.js
@@ -0,0 +1,20 @@
+const render=op.inFunction("render");
+const trigger=op.outFunction("trigger");
+const inRed=op.inValueSlider("Red");
+
+const cgl=op.patch.cgl;
+
+function doRender()
+{
+    cgl.setShader(shader);
+    trigger.trigger();
+    cgl.setPreviousShader();
+}
+
+var shader=new CGL.Shader(cgl,'MinimalMaterial');
+shader.setModules(['MODULE_VERTEX_POSITION','MODULE_COLOR','MODULE_BEGIN_FRAG']);
+shader.setSource(attachments.shader_vert,attachments.shader_frag);
+const uni=new CGL.Uniform(shader,'f','red',inRed);
+
+
+render.onTriggered=doRender;
---
+++ src/ops/base/Ops.Templates.MinimalMaterial/Ops.Templates.MinimalMaterial.md
@@ -0,0 +1,1 @@
+Colors all subsequent objects yellow, can be useful to quickly highlight and identify objects (debugging).
