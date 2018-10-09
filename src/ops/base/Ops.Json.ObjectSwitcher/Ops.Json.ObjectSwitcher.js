6bd7be7a363fc474f5e9b130169803e72b226ab6
Andro <androvisuals@gmail.com>
2018-10-09 10:27:43 +0200

    Fixes and improvements based upon code review
    Worked together on this with Pandur. All points should be addressed now.

--- src/ops/base/Ops.Json.ObjectSwitcher/Ops.Json.ObjectSwitcher.js
+++ src/ops/base/Ops.Json.ObjectSwitcher/Ops.Json.ObjectSwitcher.js
@@ -1,36 +1,36 @@
 const NUM_PORTS = 8;
 
 const inIndex = op.inValueInt("Object Index");
-
 const objectPorts = [];
-const obj = null;
-
 const outObject = op.outObject("object out");
+
+inIndex.onChange = indexChanged;
+
 for(var i = 0; i < NUM_PORTS; i++)
 {
-    objectPorts[i] = op.inObject("object port " + i);
-    objectPorts[i].inputNum = i;
-    objectPorts[i].onChange = function()
-    {
-        if(this.inputNum == inIndex.get())
-        {
-            outObject.set(null);
-            outObject.set(this.get());
-        }
-    };
+    var port = op.inObject("object port " + i);
+    port.data.inputNum = i;
+    port.onChange = onPortChange;
+    objectPorts[i]=port;
 }
-
-
 
 function indexChanged()
 {
     var index = inIndex.get();
     if(index < 0) index = 0;
-    if(index > NUM_PORTS-1) index = NUM_PORTS-1;
+        else if(index > NUM_PORTS-1) index = NUM_PORTS-1;
+
     outObject.set(null);
     outObject.set(objectPorts[index].get());
 }
 
-inIndex.onChange = indexChanged;
+function onPortChange()
+{
+    if(this.data.inputNum != inIndex.get()) return;
+
+    outObject.set(null);
+    outObject.set(this.get());
+}
+
 
 
