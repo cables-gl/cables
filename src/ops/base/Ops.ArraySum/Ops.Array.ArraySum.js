fb69c79bd10aedcddadf885ebc0f9c94eb94160d
Andro <androvisuals@gmail.com>
2018-10-25 11:40:55 +0200

    Added new op ArraySum due to the old one now being renamed to ArraySumUp

---
+++ src/ops/base/Ops.ArraySum/Ops.Array.ArraySum.js
@@ -0,0 +1,23 @@
+var inArray=op.inArray("In");
+var inValue=op.inValue("Value",1.0);
+var outArray=op.outArray("Result");
+
+var newArr=[];
+outArray.set(newArr);
+inArray.onChange=
+inValue.onChange=inArray.onChange=function()
+{
+    var arr=inArray.get();
+    if(!arr)return;
+    
+    var add=inValue.get();
+    
+    if(newArr.length!=arr.length)newArr.length=arr.length;
+    
+    for(var i=0;i<arr.length;i++)
+    {
+        newArr[i]=arr[i]+add;
+    }
+    outArray.set(null);
+    outArray.set(newArr);
+};
