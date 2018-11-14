c492f81db739b8d81530d13cc425838df92c4e71
pandrr <tom@undev.de>
2018-11-14 12:14:20 +0100

    repeat

--- src/ops/base/Ops.Gl.TextureEffects.Repeat/Ops.Gl.TextureEffects.Repeat.js
+++ src/ops/base/Ops.Gl.TextureEffects.Repeat/Ops.Gl.TextureEffects.Repeat.js
@@ -1,4 +1,6 @@
 const render=op.inTrigger("render");
+const blendMode=CGL.TextureEffect.AddBlendSelect(op,"Blend Mode","normal");
+const amount=op.inValueSlider("Amount",1);
 const amountX=op.inValue("x",3);
 const amountY=op.inValue("y",3);
 const trigger=op.outTrigger("trigger");
@@ -6,10 +8,18 @@ const trigger=op.outTrigger("trigger");
 const cgl=op.patch.cgl;
 const shader=new CGL.Shader(cgl);
 
-shader.setSource(shader.getDefaultVertexShader(),attachments.repeat_frag);
+const srcFrag=(attachments.repeat_frag||'').replace("{{BLENDCODE}}",CGL.TextureEffect.getBlendCode());
+shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+
 const textureUniform=new CGL.Uniform(shader,'t','tex',0);
+const amountUniform=new CGL.Uniform(shader,'f','amount',amount);
 const amountXUniform=new CGL.Uniform(shader,'f','amountX',amountX);
 const amountYUniform=new CGL.Uniform(shader,'f','amountY',amountY);
+
+blendMode.onChange=function()
+{
+    CGL.TextureEffect.onChangeBlendSelect(shader,blendMode.get());
+};
 
 render.onTriggered=function()
 {
