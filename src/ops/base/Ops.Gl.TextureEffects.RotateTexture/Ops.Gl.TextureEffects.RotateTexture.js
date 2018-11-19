053f80db3a8eb7f0189923d3f84851f0715ece86
Mark andro Smith <androvisuals@gmail.com>
2018-11-19 15:13:22 +0100

    Added crop so that theres a transparent surrounding edge
    Default image has crop on and is rotated to make clear to the user
    that the effect is there

--- src/ops/base/Ops.Gl.TextureEffects.RotateTexture/Ops.Gl.TextureEffects.RotateTexture.js
+++ src/ops/base/Ops.Gl.TextureEffects.RotateTexture/Ops.Gl.TextureEffects.RotateTexture.js
@@ -1,10 +1,8 @@
 const render=op.inTrigger('render');
 const blendMode=CGL.TextureEffect.AddBlendSelect(op,"Blend Mode","normal");
 const amount=op.inValueSlider("Amount",1);
-const inRotate=op.inValueSlider("Rotate",0.0);
-const inResX=op.inValue("Resolution X",512);
-const inResY=op.inValue("Resolution Y",512);
-
+const inRotate=op.inValueSlider("Rotate",0.125);
+const crop=op.inValueBool("Crop",true);
 const trigger=op.outTrigger('trigger');
 
 const cgl=op.patch.cgl;
@@ -13,22 +11,23 @@ const shader=new CGL.Shader(cgl);
 const srcFrag=(attachments.rotate_frag||'').replace("{{BLENDCODE}}",CGL.TextureEffect.getBlendCode());
 shader.setSource(shader.getDefaultVertexShader(),srcFrag);
 
-
-//inResX = cgl.currentTextureEffect.getCurrentSourceTexture().width;
-//inResY = cgl.currentTextureEffect.getCurrentSourceTexture().height;
-
 const textureUniform=new CGL.Uniform(shader,'t','tex',0);
 const amountUniform=new CGL.Uniform(shader,'f','amount',amount);
-// const uniWidth=new CGL.Uniform(shader,'f','resX',inResX);
-// const uniHeight=new CGL.Uniform(shader,'f','resY',inResY);
-
 const rotateUniform=new CGL.Uniform(shader,'f','rotate',inRotate);
+
+crop.onChange=updateCrop;
+updateCrop();
 
 blendMode.onChange=function()
 {
     CGL.TextureEffect.onChangeBlendSelect(shader,blendMode.get());
 };
 
+function updateCrop()
+{
+    if(crop.get()==true) shader.define('CROP_IMAGE');
+        else shader.removeDefine('CROP_IMAGE');
+}
 render.onTriggered=function()
 {
     if(!CGL.TextureEffect.checkOpInEffect(op)) return;
@@ -40,9 +39,6 @@ render.onTriggered=function()
 
     cgl.currentTextureEffect.finish();
     cgl.setPreviousShader();
-
-    // uniWidth.setValue(cgl.currentTextureEffect.getCurrentSourceTexture().width);
-    // uniHeight.setValue(cgl.currentTextureEffect.getCurrentSourceTexture().height);
 
     trigger.trigger();
 };
--- src/ops/base/Ops.Gl.TextureEffects.RotateTexture/att_rotate.frag
+++ src/ops/base/Ops.Gl.TextureEffects.RotateTexture/att_rotate.frag
@@ -12,7 +12,7 @@ UNI float rotate;
 
 void pR(inout vec2 p, float a)
 {
-	p = cos(a) * p + sin(a) * vec2(p.y, -p.x);
+	p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
 }
 
 void main()
@@ -23,12 +23,20 @@ void main()
     pR(uv.xy,rotate * (TAU));
     uv += 0.5;
 
+    #ifdef CROP_IMAGE
+    if(uv.x>1.0 ||uv.x<0.0  || uv.y>1.0 ||uv.y<0.0 )
+    {
+        discard;
+        return;
+    }
+    #endif
+
     //blend section
     vec4 col=texture2D(tex,uv);
     //original texture
     vec4 base=texture2D(tex,texCoord);
     //blend stuff
     col=vec4( _blend(base.rgb,col.rgb) ,1.0);
-    col=vec4( mix( col.rgb, base.rgb ,1.0 - base.a * amount),1.0);
+    col=vec4( mix( col.rgb, base.rgb ,1.0-base.a*amount),1.0);
     outColor= col;
 }
