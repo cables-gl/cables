c91b89eb56db79126b6faf527b56b3c4f936b58d
tom <tom@undev.de>
2018-12-03 19:21:01 +0100

    css variable ops

---
+++ src/ops/base/Ops.Html.SetCssVariableColor/Ops.Html.SetCssVariableColor.js
@@ -0,0 +1,27 @@
+const
+    varname=op.inString("Var Name"),
+    r=op.addInPort(new CABLES.Port(op,"r",CABLES.OP_PORT_TYPE_VALUE,{ display:'range', colorPick:'true'})),
+    g=op.addInPort(new CABLES.Port(op,"g",CABLES.OP_PORT_TYPE_VALUE,{ display:'range' })),
+    b=op.addInPort(new CABLES.Port(op,"b",CABLES.OP_PORT_TYPE_VALUE,{ display:'range' }));
+
+
+var root = document.documentElement;
+
+r.onChange=
+    g.onChange=
+    b.onChange=
+    varname.onChange=update;
+
+function toHex(c)
+{
+    c=(Math.floor(c*255));
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function update()
+{
+    const hex='#'+toHex(r.get())+toHex(g.get())+toHex(b.get());
+    console.log(hex);
+    root.style.setProperty('--'+varname.get(), hex);
+}
