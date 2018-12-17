739e3ed04af7c2f7a4977dd3237f0f98907ca978
Mark andro Smith <androvisuals@gmail.com>
2018-12-17 12:59:55 +0100

    Added groups to the op ui

--- src/ops/base/Ops.Gl.TextureEffects.GridTexture/Ops.Gl.TextureEffects.GridTexture.js
+++ src/ops/base/Ops.Gl.TextureEffects.GridTexture/Ops.Gl.TextureEffects.GridTexture.js
@@ -2,7 +2,6 @@ const render=op.inTrigger('render');
 const blendMode=CGL.TextureEffect.AddBlendSelect(op,"Blend Mode","normal");
 const amount=op.inValueSlider("Amount",1);
 
-const invertColor=op.inValueBool("Invert color",false);
 const lineThicknessX=op.inValueSlider("Line thickness X",0.4);
 const lineThicknessY=op.inValueSlider("Line thickness Y",0.4);
 const cellsX=op.inValueFloat("Cells X",10);
@@ -11,11 +10,16 @@ const inRotate=op.inValueSlider("Rotate",0.0);
 const offsetX=op.inValue("Offset X",0.0);
 const offsetY=op.inValue("Offset Y",0.0);
 
+const invertColor=op.inValueBool("Invert color",false);
 const r=op.inValueSlider("Line red",Math.random());
 const g=op.inValueSlider("Line green",Math.random());
 const b=op.inValueSlider("Line Blue",Math.random());
 
 r.setUiAttribs({colorPick:true});
+
+op.setPortGroup('LineThickness',[lineThicknessX,lineThicknessY]);
+op.setPortGroup('Cells',[cellsX,cellsY]);
+op.setPortGroup('Position',[inRotate,offsetX,offsetY]);
 
 const trigger=op.outTrigger('trigger');
 
