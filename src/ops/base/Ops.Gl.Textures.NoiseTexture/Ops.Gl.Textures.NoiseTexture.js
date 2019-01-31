803d31438a5f0122f10a196b6f6f334bab3712ad
pandur <tom@undev.de>
2019-01-31 13:45:01 +0100

    remove log

--- src/ops/base/Ops.Gl.Textures.NoiseTexture/Ops.Gl.Textures.NoiseTexture.js
+++ src/ops/base/Ops.Gl.Textures.NoiseTexture/Ops.Gl.Textures.NoiseTexture.js
@@ -1,19 +1,54 @@
-const outTex=op.outTexture("Texture");
+var inWidth=op.inValueInt("Width",256);
+var inHeight=op.inValueInt("Height",256);
+var inColor=op.inValueBool("Color",false);
+var outTex=op.outTexture("Texture");
 
-const cgl=op.patch.cgl;
-const size=256;
+var cgl=op.patch.cgl;
 
-const temptex=new CGL.Texture(cgl);
-const data = new Uint8Array(size*size*4);
+inWidth.onChange=
+    inHeight.onChange=
+    inColor.onChange=update;
 
-for(var x=0;x<size*size;x++)
+update();
+
+function update()
 {
-    data[ x*4+0]=Math.random()*255;
-    data[ x*4+1]=Math.random()*255;
-    data[ x*4+2]=Math.random()*255;
-    data[ x*4+3]=255;
+    var width=Math.ceil(inWidth.get());
+    var height=Math.ceil(inHeight.get());
+
+    if(width<1)width=1;
+    if(height<1)height=1;
+
+    const num =width*4*height;
+    const pixels=new Uint8Array(num);
+    var i=0;
+
+    if(inColor.get())
+    {
+        for(i=0;i<num;i+=4)
+        {
+            pixels[i+0]=Math.random()*255;
+            pixels[i+1]=Math.random()*255;
+            pixels[i+2]=Math.random()*255;
+            pixels[i+3]=255;
+        }
+    }
+    else
+    {
+        for(i=0;i<num;i+=4)
+        {
+            var c=Math.random()*255;
+            pixels[i+0]=c;
+            pixels[i+1]=c;
+            pixels[i+2]=c;
+            pixels[i+3]=255;
+        }
+    }
+
+    var tex=new CGL.Texture(cgl,{wrap:CGL.Texture.WRAP_REPEAT});
+
+    tex.initFromData(pixels,width,height);
+
+    outTex.set(null);
+    outTex.set(tex);
 }
-
-temptex.initFromData(data,size,size,CGL.Texture.FILTER_NEAREST,CGL.Texture.WRAP_REPEAT);
-
-outTex.set(temptex);
