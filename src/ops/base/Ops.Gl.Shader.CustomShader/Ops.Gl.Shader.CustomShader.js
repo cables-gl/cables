765e4fc407c08622d145e89c2298bed8a2896df7
pandrr <tom@drastic.net>
2019-02-01 12:21:33 +0100

    fix customshader uniform values not remembered

--- src/ops/base/Ops.Gl.Shader.CustomShader/Ops.Gl.Shader.CustomShader.js
+++ src/ops/base/Ops.Gl.Shader.CustomShader/Ops.Gl.Shader.CustomShader.js
@@ -23,12 +23,30 @@ vertexShader.onChange=updateLater;
 render.onTriggered=doRender;
 
 var needsUpdate=true;
-op.onLoadedValueSet=updateShader;
+op.onLoadedValueSet=initDataOnLoad;
+
+function initDataOnLoad(data)
+{
+    updateShader();
+
+    // set uniform values AFTER shader has been compiled and uniforms are extracted and uniform ports are created.
+
+    for(var i=0;i<uniformInputs.length;i++)
+    {
+        for(var j=0;j<data.portsIn.length;j++)
+        {
+            if(uniformInputs[i].name==data.portsIn[j].name)
+            {
+                uniformInputs[i].set(data.portsIn[j].value);
+            }
+        }
+    }
+
+}
 
 function updateLater()
 {
     needsUpdate=true;
-    // updateShader();
 }
 
 op.init=function()
