205aca94eb1517f7363a93d6fabec4fc5fe85f4e
pandrr <tom@drastic.net>
2019-04-15 14:25:24 +0200

    tweak

--- src/core/core_port.js
+++ src/core/core_port.js
@@ -204,24 +204,24 @@ CABLES.Port=function(__parent,name,type,uiAttribs)
         }
     };
 
+    CABLES.Port.args=function(func) { 
+        return (func + '')
+            .replace(/[/][/].*$/mg,'') // strip single-line comments
+            .replace(/\s+/g, '') // strip white space
+            .replace(/[/][*][^/*]*[*][/]/g, '') // strip multi-line comments  
+            .split('){', 1)[0].replace(/^[^(]*[(]/, '') // extract the parameters  
+            .replace(/=[^,]+/g, '') // strip any ES6 defaults  
+            .split(',').filter(Boolean); // split & filter [""]
+    }  
+
+
     CABLES.Port.prototype.forceChange=function()
     {
         if(this.onValueChanged || this.onChange)
         {
-            // very temporary!!!!!!!!!
-            function args(func) {  
-                return (func + '')
-                    .replace(/[/][/].*$/mg,'') // strip single-line comments
-                    .replace(/\s+/g, '') // strip white space
-                    .replace(/[/][*][^/*]*[*][/]/g, '') // strip multi-line comments  
-                    .split('){', 1)[0].replace(/^[^(]*[(]/, '') // extract the parameters  
-                    .replace(/=[^,]+/g, '') // strip any ES6 defaults  
-                    .split(',').filter(Boolean); // split & filter [""]
-            }  
-
-            var params=args(this.onValueChanged||this.onChange)
-
-            if(params.length>0) console.warn('TOM: port has onchange params!',this.parent.objName,this.name);
+            // very temporary: deprecated warning!!!!!!!!!
+            // var params=CABLES.Port.args(this.onValueChanged||this.onChange)
+            // if(params.length>0) console.warn('TOM: port has onchange params!',this.parent.objName,this.name);
         }
     
         if(this.onChange) this.onChange(this,this.value);
