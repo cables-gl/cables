bccad5aeed7cde6a2a3031d0c2524371a6720e20
pandrr <tom@undev.de>
2019-05-17 16:28:59 +0200

    rename

--- src/ops/base/Ops.Gl.Matrix.SetProjectionMatrix/Ops.Gl.Matrix.SetProjectionMatrix.js
+++ src/ops/base/Ops.Gl.Matrix.SetProjectionMatrix/Ops.Gl.Matrix.SetProjectionMatrix.js
@@ -1,20 +1,19 @@
-
-var inExe=op.inTrigger("exe");
-var inMatrix=op.inArray('Matrix');
-var next=op.outTrigger("next");
-var cgl=op.patch.cgl;
+const inExe=op.inTrigger("exe");
+const inMatrix=op.inArray('Matrix');
+const next=op.outTrigger("next");
+const cgl=op.patch.cgl;
 
 inExe.onTriggered=function()
 {
     if(inMatrix.get())
     {
         cgl.pushPMatrix();
-    
+
         mat4.copy(cgl.pMatrix,inMatrix.get() );
-    
+
         next.trigger();
         cgl.popPMatrix();
-        
+
     }
 
 };
