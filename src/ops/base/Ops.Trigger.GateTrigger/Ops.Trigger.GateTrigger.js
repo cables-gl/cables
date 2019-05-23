949a28bbed9923307fe62e9142565c518430a829
AndroLaptop <androvisuals@gmail.com>
2019-05-23 15:14:29 +0200

    Removed extra port that wasn't being used. Now works as expected.

--- src/ops/base/Ops.Trigger.GateTrigger/Ops.Trigger.GateTrigger.js
+++ src/ops/base/Ops.Trigger.GateTrigger/Ops.Trigger.GateTrigger.js
@@ -1,5 +1,4 @@
 const passThrough = op.inValueBool('Pass Through',true),
-    triggerIn = op.inTrigger('Trigger to pass'),
     exe = op.inTrigger('Execute'),
     triggerOut = op.outTrigger('Trigger out');
 
