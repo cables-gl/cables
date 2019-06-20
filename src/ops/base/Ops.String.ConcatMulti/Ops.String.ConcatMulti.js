8a057f711db7c2667bf61aa30ea9a47b2ed39059
androvisuals <androvisuals@gmail.com>
2019-06-20 10:23:30 +0200

    new op concat multi , allows up to 8 strings to be joined together. has options to add a space at the ned of each string and line breaks

---
+++ src/ops/base/Ops.String.ConcatMulti/Ops.String.ConcatMulti.js
@@ -0,0 +1,33 @@
+const addSpacesCheckBox = op.inBool("add spaces",false),
+        newLinesCheckBox = op.inBool("new lines",false),
+        stringPorts = [],
+        result = op.outString("concat string");
+
+stringPorts.onChange = addSpacesCheckBox.onChange =
+newLinesCheckBox.onChange = update;
+
+addSpacesCheckBox.hidePort(true);
+newLinesCheckBox.hidePort(true);
+
+for(var i=0; i<8; i++)
+{
+    var p=op.inString("string " + i);
+    stringPorts.push(p);
+    p.onChange = update;
+}
+
+function update()
+{
+    var str = "";
+    var nl = "";
+    var space = addSpacesCheckBox.get();
+
+    for(var i=0; i<stringPorts.length; i++)
+    {
+        if(space && stringPorts[i].get())  str += " ";
+        if(newLinesCheckBox.get()) nl = '\n';
+            str += nl;
+            str += stringPorts[i].get();
+    }
+    result.set(str);
+}
