719897e08d3758bcab438d6943b81b99b50f4710
androvisuals <androvisuals@gmail.com>
2019-06-25 08:52:24 +0200

    added normalize option to array op

--- src/ops/base/Ops.Array.Array_v2/Ops.Array.Array_v2.js
+++ src/ops/base/Ops.Array.Array_v2/Ops.Array.Array_v2.js
@@ -1,12 +1,21 @@
-const modeSelect = op.inSwitch("Mode select",['Number','1,2,3,4'],'Number'),
+const modeSelect = op.inSwitch("Mode select",['Number','1,2,3,4',"0-1"],'Number'),
     inLength=op.inValueInt("Array length",10),
     inDefaultValue=op.inValueFloat("Default Value"),
     outArr=op.outArray("Array");
 
 var arr=[];
 
-modeSelect.onChange = function()
+var selectIndex = 0;
+const MODE_NUMBER = 0;
+const MODE_1_TO_4 = 1;
+const MODE_0_TO_1 = 2;
+
+function onFilterChange()
 {
+    var selectedMode = modeSelect.get();
+    if(selectedMode === 'Number') selectIndex = MODE_NUMBER;
+    else if(selectedMode === '1,2,3,4') selectIndex = MODE_1_TO_4;
+    else if(selectedMode === '0-1') selectIndex = MODE_0_TO_1;
     //used to grey out parameter
     if( modeSelect.get() === 'Number')
     {
@@ -16,13 +25,11 @@ modeSelect.onChange = function()
     {
         inDefaultValue.setUiAttribs({greyout:true});
     }
-    reset();
+    else if(modeSelect.get() === '0-1')
+    {
+        inDefaultValue.setUiAttribs({greyout:true});
+    }
     op.setUiAttrib({"extendTitle":modeSelect.get()});
-}
-
-inDefaultValue.onChange = inLength.onChange = function ()
-{
-    reset();
 }
 
 function reset()
@@ -41,16 +48,30 @@ function reset()
             arr[i]=valueForArray;
         }
     }
-
     //mode 1 Continuous number array - increments up to array length
-    else
+    else if(modeSelect.get() === "1,2,3,4")
     {
         for(i = 0;i < arrLength; i++)
         {
             arr[i] = i;
         }
     }
+    //mode 2 Normalized array
+    else if(modeSelect.get() === "0-1")
+    {
+        var length = arrLength;
+        for(i = 0;i < arrLength; i++)
+        {
+            arr[i] = i / length;
+        }
+    }
     outArr.set(null);
     outArr.set(arr);
 }
+modeSelect.onChange = inDefaultValue.onChange = inLength.onChange = function ()
+{
+    onFilterChange();
+    reset();
+}
+onFilterChange();
 reset();
