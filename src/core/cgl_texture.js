739fc8654dfb293c6134dec70df1965950c9e8b4
pandrr <tom@drastic.net>
2019-07-02 17:45:03 +0200

    half float for webgl1

--- src/core/cgl_texture.js
+++ src/core/cgl_texture.js
@@ -154,8 +154,8 @@ CGL.Texture.prototype.setSize=function(w,h)
 
     if(this.textureType==CGL.Texture.TYPE_FLOAT)
     {
-        // if(this._cgl.glVersion==1 && !this._cgl.gl.getExtension('OES_texture_float')) throw "no floating point texture extension";
-        if(this._cgl.glVersion==1 && !this._cgl.gl.getExtension('OES_texture_halfloat')) throw "no floating point texture extension";
+        // if(this._cgl.glVersion==1 && !this._cgl.gl.getExtension('OES_texture_float')) throw "no float texture extension";
+        if(this._cgl.glVersion==1 && !this._cgl.gl.getExtension('OES_texture_half_loat')) throw "no half float texture extension";
         
         // should also check for HALF_FLOAT and use this if this is available, but no float... (some ios devices)
 
