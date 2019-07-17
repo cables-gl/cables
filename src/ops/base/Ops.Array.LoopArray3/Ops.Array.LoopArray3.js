24133b663ef791eeafffb9071d85aa0039f9fbaf
androvisuals <androvisuals@gmail.com>
2019-07-17 18:17:17 +0200

    Implemented feedback on op

--- src/ops/base/Ops.Array.LoopArray3/Ops.Array.LoopArray3.js
+++ src/ops/base/Ops.Array.LoopArray3/Ops.Array.LoopArray3.js
@@ -13,22 +13,18 @@ inArray.onChange = function()
     }
 
     if(newArray.length !== arr.length) newArray.length = arr.length;
-
-    for (var i = 0; i < newArray.length; i+=3)
+    var i ;
+    for (i = 0; i < newArray.length; i+=3)
     {
-        if(i < newArray.length-3 )
-        {
-            newArray[i]   = arr[i];
-            newArray[i+1] = arr[i+1];
-            newArray[i+2] = arr[i+2];
-        }
-        else if(i === newArray.length-3)
-        {
-            newArray[arr.length-3] = arr[0];
-            newArray[arr.length-3+1] = arr[1];
-            newArray[arr.length-3+2] = arr[2];
-        }
+        newArray[i+0] = arr[i+0];
+        newArray[i+1] = arr[i+1];
+        newArray[i+2] = arr[i+2];
     }
+
+    newArray[i+0] = arr[0];
+    newArray[i+1] = arr[1];
+    newArray[i+2] = arr[2];
+
     outArray.set(null);
     outArray.set(newArray);
 }
