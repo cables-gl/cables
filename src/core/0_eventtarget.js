11f5e9cbcc1756362b8d9db6aaded5bcda5e4664
pandrr <tom@drastic.net>
2019-08-02 12:00:52 +0200

    debug

--- src/core/0_eventtarget.js
+++ src/core/0_eventtarget.js
@@ -7,7 +7,6 @@ CABLES.EventTarget=function()
 
     this.addEventListener=function(which,cb)
     {
-        console.log(which);
         if(!this._eventCallbacks[which]) this._eventCallbacks[which]=[cb];
             else this._eventCallbacks[which].push(cb);
     }
@@ -43,8 +42,6 @@ CABLES.EventTarget=function()
     {
         if(this._eventCallbacks[which])
         {
-            console.log("xxx",this._eventCallbacks[which].length,which);
-
             for(var i=0;i<this._eventCallbacks[which].length;i++)
                 if(this._eventCallbacks[which][i])this._eventCallbacks[which][i](param1,param2,param3,param4,param5,param6);
         }
