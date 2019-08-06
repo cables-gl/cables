0e3f3b423a17f27751d3041116a76155aa201d81
pandrr <tom@drastic.net>
2019-08-06 15:36:45 +0200

    profiledata singleton pattern

---
+++ src/core/cgl/cgl_profiledata.js
@@ -0,0 +1,14 @@
+
+
+var ProfileData=function()
+{
+    this.profileUniformCount= 0;
+    this.profileShaderBinds= 0;
+    this.profileUniformCount= 0;
+    this.profileShaderCompiles= 0;
+    this.profileVideosPlaying= 0;
+    this.profileMVPMatrixCount= 0;
+};
+
+
+export const profileData = new ProfileData();
