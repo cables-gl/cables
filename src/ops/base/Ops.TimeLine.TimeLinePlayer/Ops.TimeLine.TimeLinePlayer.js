d3c65195baf9fba9cad02dbca4e08228e91ae2f6
androvisuals <androvisuals@gmail.com>
2019-08-20 14:37:03 +0200

    New op time line player

---
+++ src/ops/base/Ops.TimeLine.TimeLinePlayer/Ops.TimeLine.TimeLinePlayer.js
@@ -0,0 +1,43 @@
+const
+    play = op.inTriggerButton("Play"),
+    pause = op.inTriggerButton("Pause"),
+    rewind = op.inTriggerButton("rewind"),
+    setTime = op.inFloat("Set current time",0),
+
+    outPlayTrigger = op.outTrigger("play trigger"),
+    outPauseTrigger= op.outTrigger("pause trigger"),
+    outrewindTrigger = op.outTrigger("rewind trigger"),
+    isPlaying = op.outBool("is Playing"),
+    outSetTimeTrigger = op.outNumber("set time (seconds)"),
+    currentTime = op.outValue("current time"),
+    currentFrame = op.outValue("current frame");
+
+play.onTriggered=function()
+{
+    op.patch.timer.play();
+    isPlaying.set(true);
+    op.patch.timer.setTime(setTime.get());
+    outSetTimeTrigger.set(setTime.get());
+    outPlayTrigger.trigger();
+};
+
+pause.onTriggered=function()
+{
+    op.patch.timer.pause();
+    isPlaying.set(false);
+    outPauseTrigger.trigger();
+};
+
+
+op.onAnimFrame=function(time)
+{
+    currentFrame.set(Math.round(time*30.0));
+    currentTime.set(time);
+};
+
+rewind.onTriggered=function()
+{
+    op.patch.timer.setTime(0);
+    outrewindTrigger.trigger();
+};
+
