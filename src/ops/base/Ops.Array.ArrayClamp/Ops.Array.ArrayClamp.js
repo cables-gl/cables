b70ed73b6da198b700c91a89dece69b49968a8a8
androvisuals <androvisuals@gmail.com>
2019-08-23 12:37:35 +0200

    New op array clamp

---
+++ src/ops/base/Ops.Array.ArrayClamp/Ops.Array.ArrayClamp.js
@@ -0,0 +1,26 @@
+const
+    inArray = op.inArray("Array In"),
+    inMinValue = op.inFloat("Min",0.0),
+    inMaxValue = op.inFloat("Max",1.0),
+    outArray = op.outArray("Array Out");
+
+var newArr = [];
+outArray.set(newArr);
+inArray.onChange = inMinValue.onChange = inMaxValue.onChange = inArray.onChange = function()
+{
+    var arr = inArray.get();
+    if(!arr)return;
+
+    var inMin = inMinValue.get();
+    var inMax = inMaxValue.get();
+
+    if(newArr.length != arr.length)newArr.length = arr.length;
+
+    var i = 0;
+    for(i = 0;i < arr.length;i++)
+    {
+        newArr[i] = Math.min(Math.max(arr[i],inMin),inMax);
+    }
+    outArray.set(null);
+    outArray.set(newArr);
+};
