9479eca0c8d06b52fad5ca3983bb10a8b9aa0e5b
androvisuals <androvisuals@gmail.com>
2019-08-23 12:41:01 +0200

    removed duplicate code from array min and max ops

--- src/ops/base/Ops.Array.ArrayMax/Ops.Array.ArrayMax.js
+++ src/ops/base/Ops.Array.ArrayMax/Ops.Array.ArrayMax.js
@@ -4,16 +4,16 @@ const outArray = op.outArray("Array Out");
 
 var newArr = [];
 outArray.set(newArr);
-inArray.onChange =
+
 inValue.onChange = inArray.onChange = function()
 {
     var arr = inArray.get();
     if(!arr)return;
-    
+
     var inMax = inValue.get();
-    
+
     if(newArr.length != arr.length)newArr.length = arr.length;
-    
+
     var i = 0;
     for(i = 0;i < arr.length;i++)
     {
