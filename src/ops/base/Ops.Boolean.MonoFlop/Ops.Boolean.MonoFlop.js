3f9c07b60772df6f08cb8efa78e945edc499c2fd
pandur <tom@undev.de>
2019-09-06 11:01:15 +0200

    new op objectgetstring

--- src/ops/base/Ops.Boolean.MonoFlop/Ops.Boolean.MonoFlop.js
+++ src/ops/base/Ops.Boolean.MonoFlop/Ops.Boolean.MonoFlop.js
@@ -3,12 +3,14 @@ const
     duration=op.inValue("Duration",1),
     valueTrue=op.inValue("Value True",1),
     valueFalse=op.inValue("Value False",0),
+    outAct=op.outTrigger("Activated"),
     result=op.outValue("Result",false);
 
 var lastTimeout=-1;
 
 trigger.onTriggered=function()
 {
+    if(result.get()==valueFalse.get())outAct.trigger();
     result.set(valueTrue.get());
 
     clearTimeout(lastTimeout);
