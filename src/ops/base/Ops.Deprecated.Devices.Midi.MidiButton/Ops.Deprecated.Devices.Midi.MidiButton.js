e14c7b70ab27569a55db7088d31dd1beefff3952
pandur <tom@undev.de>
2019-09-06 11:01:59 +0200

    depre

---
+++ src/ops/base/Ops.Deprecated.Devices.Midi.MidiButton/Ops.Deprecated.Devices.Midi.MidiButton.js
@@ -0,0 +1,54 @@
+
+var eventIn=op.addInPort(new CABLES.Port(op,"Event Input",CABLES.OP_PORT_TYPE_OBJECT));
+var note=op.addInPort(new CABLES.Port(op,"note"));
+var channel=op.inValueInt("Channel",0);
+var learn=op.addInPort(new CABLES.Port(op,"learn",CABLES.OP_PORT_TYPE_FUNCTION,{display:'button'}));
+
+var eventOut=op.addOutPort(new CABLES.Port(op,"Event Output",CABLES.OP_PORT_TYPE_OBJECT));
+var outPressed=op.addOutPort(new CABLES.Port(op,"pressed"));
+
+var trigger=op.outTrigger('trigger');
+
+var lights=op.addInPort(new CABLES.Port(op,"Light",CABLES.OP_PORT_TYPE_VALUE,{display:'bool'}));
+
+note.set(1);
+var learning=false;
+var lastValue=-1;
+learn.onTriggered=function(){learning=true;};
+
+eventIn.onChange=function()
+{
+    var event=eventIn.get();
+    if(!event)return;
+    if(learning)
+    {
+        channel.set(event.channel);
+        note.set(event.note);
+        learning=false;
+        
+        if(CABLES.UI)
+        {
+            op.uiAttr({info:'bound to note: ' + note.get()});
+            gui.patch().showOpParams(op);
+        }
+    }
+
+    if(note.get()==event.note && event.channel==channel.get())
+    {
+        var v=event.velocity;
+        if(v===0)
+        {
+            if(lights.get())event.output.send( [0x90, note.get(), 0] );
+            outPressed.set(false);
+        }
+        if(v==1)
+        {
+            if(lights.get())event.output.send( [0x90, note.get(), 120] );
+            outPressed.set(true);
+            trigger.trigger();
+        }
+        
+    }
+    eventOut.set(event);
+};
+
---
+++ src/ops/base/Ops.Deprecated.Devices.Midi.MidiValue/Ops.Deprecated.Devices.Midi.MidiValue.js
@@ -0,0 +1,60 @@
+var eventIn=op.addInPort(new CABLES.Port(this,"Event Input",CABLES.OP_PORT_TYPE_OBJECT));
+
+var note=op.addInPort(new CABLES.Port(this,"note"));
+var channel=op.inValueInt("Channel",0);
+var learn=op.addInPort(new CABLES.Port(this,"learn",CABLES.OP_PORT_TYPE_FUNCTION,{display:'button'}));
+
+var eventOut=op.addOutPort(new CABLES.Port(this,"Event Output",CABLES.OP_PORT_TYPE_OBJECT));
+var value=op.addOutPort(new CABLES.Port(this,"value"));
+
+note.set(60);
+value.set(0);
+
+var learning=false;
+learn.onTriggered=function(){ learning=true; };
+op.onLoaded=initMidiValue;
+
+var lastEvent=null;
+
+function setMidiValue()
+{
+    if(lastEvent && lastEvent.output)
+    {
+        var noteOnMessage = [0xB0, note.get(), parseInt(value.get()*127,10)];
+        lastEvent.output.send( noteOnMessage );
+        return true;
+    }
+    return false;
+}
+
+function initMidiValue()
+{
+    if(!setMidiValue()) setTimeout(initMidiValue,300);
+}
+
+eventIn.onChange=function()
+{
+    var event=eventIn.get();
+    if(!event)return;
+    if(learning)
+    {
+        note.set(event.note);
+        channel.set(event.channel);
+        learning=false;
+        
+        if(CABLES.UI)
+        {
+            op.uiAttr({info:'bound to note: ' + note.get()});
+            gui.patch().showOpParams(op);
+        }
+    }
+
+    if(event.note==note.get() && event.channel==channel.get())
+    {
+        value.set(event.velocity);
+    }
+    
+    eventOut.set(event);
+    lastEvent=event;
+};
+
