9505832c89a12a38baf9a39cf0b4e99d3d4cca2e
pandrr <tom@drastic.net>
2019-09-10 10:24:13 +0200

    renderanim size input

--- src/ops/base/Ops.Exp.RenderAnim/Ops.Exp.RenderAnim.js
+++ src/ops/base/Ops.Exp.RenderAnim/Ops.Exp.RenderAnim.js
@@ -1,16 +1,47 @@
+const
+    inFilename=op.inValueString("Filename","cables"),
+    inFormat=op.inValueSelect("Fileformat",["webm","png"],"png"),
+    inExec=op.inTriggerButton("Render"),
 
-var inFilename=op.inValueString("Filename","cables");
-var inStart=op.inValue("Start Time",0);
-var inEnd=op.inValue("End Time",1);
-var inFps=op.inValue("FPS",30);
-var inFormat=op.inValueSelect("Fileformat",["webm","png"],"png");
-var inExec=op.inTriggerButton("Render");
+    inStart=op.inValue("Start Time",0),
+    inEnd=op.inValue("End Time",1),
+    inFps=op.inValue("FPS",30),
 
-var outProgress=op.outValue("Progress");
-var outFinished=op.outTrigger("Finished");
+    inVpSize=op.inBool("Use Viewport Size",true),
+    inWidth=op.inInt("Width",1280),
+    inHeight=op.inInt("Height",720),
+
+    outProgress=op.outValue("Progress"),
+    outFinished=op.outTrigger("Finished");
+
+op.setPortGroup("Size",[inVpSize,inWidth,inHeight]);
+op.setPortGroup("Timing",[inStart,inEnd,inFps]);
+
+const cgl=gui.patch().scene.cgl;
+
+inVpSize.onChange=updateSizeInputs;
+updateSizeInputs();
+
+function updateSizeInputs()
+{
+    inHeight.setUiAttribs({greyout:inVpSize.get()});
+    inWidth.setUiAttribs({greyout:inVpSize.get()});
+}
 
 inExec.onTriggered=function()
 {
+    if(!inVpSize.get())
+    {
+        if(window.gui)
+        {
+            window.gui.rendererWidth=Math.floor(inWidth.get());
+            window.gui.rendererHeight=Math.floor(inHeight.get());
+            window.gui.setLayout();
+        }
+
+        cgl.setSize(inWidth.get(),inHeight.get());
+    }
+
     var seq=CABLES.UI.ImageSequenceExport(
         inFilename.get(),
         inStart.get(),
@@ -30,5 +61,5 @@ function progress(v)
         op.patch.resume();
         outFinished.trigger();
     }
-
 }
+
