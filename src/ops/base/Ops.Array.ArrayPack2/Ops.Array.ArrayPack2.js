f0eb8c42cc60d40de1996947421de3ed88444b9d
andro <androvisuals@gmail.com>
2019-09-13 12:17:25 +0200

    array pack ops and TransformArray3 op only calculates array when one has changed

--- src/ops/base/Ops.Array.ArrayPack2/Ops.Array.ArrayPack2.js
+++ src/ops/base/Ops.Array.ArrayPack2/Ops.Array.ArrayPack2.js
@@ -10,9 +10,15 @@ var showingError = false;
 
 var arr=[];
 var emptyArray=[];
+var needsCalc = true;
 
 exe.onTriggered = update;
 
+inArr1.onChange=inArr2.onChange=calcLater;
+function calcLater()
+{
+    needsCalc=true;
+}
 
 function update()
 {
@@ -24,47 +30,50 @@ function update()
         outArr.set(null);
         return;
     }
-
-    var arrlen=0;
-
-    if(!array1 || !array2)
+    if(needsCalc)
     {
-        if(array1) arrlen=array1.length;
-            else if(array2) arrlen=array2.length;
+        var arrlen=0;
 
-        if(emptyArray.length!=arrlen)
-            for(var i=0;i<arrlen;i++) emptyArray[i]=0;
-
-        if(!array1)array1=emptyArray;
-        if(!array2)array2=emptyArray;
-    }
-
-    if(array1.length !== array2.length)
-    {
-        if(!showingError)
+        if(!array1 || !array2)
         {
-            op.uiAttr({error:"Arrays do not have the same length !"});
-            showingError = true;
+            if(array1) arrlen=array1.length;
+                else if(array2) arrlen=array2.length;
+
+            if(emptyArray.length!=arrlen)
+                for(var i=0;i<arrlen;i++) emptyArray[i]=0;
+
+            if(!array1)array1=emptyArray;
+            if(!array2)array2=emptyArray;
         }
-        return;
-    }
 
-    if(showingError)
-    {
-        showingError = false;
-        op.uiAttr({error:null});
-    }
+        if(array1.length !== array2.length)
+        {
+            if(!showingError)
+            {
+                op.uiAttr({error:"Arrays do not have the same length !"});
+                showingError = true;
+            }
+            return;
+        }
 
-    arr.length = array1.length;
-    for(var i=0;i<array1.length;i++)
-    {
-        arr[i*2+0] = array1[i];
-        arr[i*2+1] = array2[i];
-    }
+        if(showingError)
+        {
+            showingError = false;
+            op.uiAttr({error:null});
+        }
 
-    outArr.set(null);
-    outArr.set(arr);
-    outArrayLength.set(arr.length);
+        arr.length = array1.length;
+        for(var i=0;i<array1.length;i++)
+        {
+            arr[i*2+0] = array1[i];
+            arr[i*2+1] = array2[i];
+        }
+
+        needsCalc = false;
+        outArr.set(null);
+        outArr.set(arr);
+        outArrayLength.set(arr.length);
+    }
 
     exeOut.trigger();
 }
--- src/ops/base/Ops.Array.TransformArray3/Ops.Array.TransformArray3.js
+++ src/ops/base/Ops.Array.TransformArray3/Ops.Array.TransformArray3.js
@@ -14,14 +14,24 @@ const
     outArr=op.outArray("Result");
 
 var resultArr=[];
-
-inExec.onTriggered=doTransform;
+var needsCalc = true;
 
 var rotVec=vec3.create();
 var emptyVec=vec3.create();
 var transVec=vec3.create();
 var centerVec=vec3.create();
 
+inExec.onTriggered=doTransform;
+
+inArr.onChange=
+transX.onChange=transY.onChange=transZ.onChange=
+scaleX.onChange=scaleY.onChange=scaleZ.onChange=
+rotX.onChange=rotY.onChange=rotZ.onChange=calcLater;
+
+function calcLater()
+{
+    needsCalc=true;
+}
 
 function doTransform()
 {
@@ -31,37 +41,40 @@ function doTransform()
         outArr.set(null);
         return;
     }
-
-    resultArr.length=arr.length;
-
-    for(var i=0;i<arr.length;i+=3)
+    if(needsCalc)
     {
-        resultArr[i+0]=arr[i+0]*scaleX.get();
-        resultArr[i+1]=arr[i+1]*scaleY.get();
-        resultArr[i+2]=arr[i+2]*scaleZ.get();
+        resultArr.length=arr.length;
 
-        resultArr[i+0]=resultArr[i+0]+transX.get();
-        resultArr[i+1]=resultArr[i+1]+transY.get();
-        resultArr[i+2]=resultArr[i+2]+transZ.get();
+        for(var i=0;i<arr.length;i+=3)
+        {
+            resultArr[i+0]=arr[i+0]*scaleX.get();
+            resultArr[i+1]=arr[i+1]*scaleY.get();
+            resultArr[i+2]=arr[i+2]*scaleZ.get();
+
+            resultArr[i+0]=resultArr[i+0]+transX.get();
+            resultArr[i+1]=resultArr[i+1]+transY.get();
+            resultArr[i+2]=resultArr[i+2]+transZ.get();
+        }
+
+        for(var i=0;i<arr.length;i+=3)
+        {
+            vec3.set(rotVec,
+                resultArr[i+0],
+                resultArr[i+1],
+                resultArr[i+2]);
+
+            vec3.rotateX(rotVec,rotVec,transVec,rotX.get()*CGL.DEG2RAD);
+            vec3.rotateY(rotVec,rotVec,transVec,rotY.get()*CGL.DEG2RAD);
+            vec3.rotateZ(rotVec,rotVec,transVec,rotZ.get()*CGL.DEG2RAD);
+
+            resultArr[i+0]=rotVec[0];
+            resultArr[i+1]=rotVec[1];
+            resultArr[i+2]=rotVec[2];
+        }
+
+        needsCalc=false;
+        outArr.set(null);
+        outArr.set(resultArr);
+        next.trigger();
     }
-
-    for(var i=0;i<arr.length;i+=3)
-    {
-        vec3.set(rotVec,
-            resultArr[i+0],
-            resultArr[i+1],
-            resultArr[i+2]);
-
-        vec3.rotateX(rotVec,rotVec,transVec,rotX.get()*CGL.DEG2RAD);
-        vec3.rotateY(rotVec,rotVec,transVec,rotY.get()*CGL.DEG2RAD);
-        vec3.rotateZ(rotVec,rotVec,transVec,rotZ.get()*CGL.DEG2RAD);
-
-        resultArr[i+0]=rotVec[0];
-        resultArr[i+1]=rotVec[1];
-        resultArr[i+2]=rotVec[2];
-    }
-
-    outArr.set(null);
-    outArr.set(resultArr);
-    next.trigger();
 }
