e5d2a2595eb1ff52d6e79c482f45f95923d27673
andro <androvisuals@gmail.com>
2019-10-01 14:59:03 +0200

    Cleaned up biquadfilter code.Params now work as expected

--- src/ops/base/Ops.WebAudio.BiquadFilter/Ops.WebAudio.BiquadFilter.js
+++ src/ops/base/Ops.WebAudio.BiquadFilter/Ops.WebAudio.BiquadFilter.js
@@ -3,20 +3,8 @@ this.name="BiquadFilter";
 var audioContext = CABLES.WEBAUDIO.createAudioContext(op);
 
 // default values + min and max
-var Q_MIN = 0.0001;
-var Q_MAX = 1000;
-var Q_DEF = 1;
-var Q_RESTRICT = true;
-var DETUNE_MIN = -3600;
-var DETUNE_MAX = 3600;
-var DETUNE_DEF = 0;
-var DETUNE_RESTRICT = false;
 var FREQUENCY_MIN = 10;
 var FREQUENCY_MAX = audioContext.sampleRate / 2; // Nyquist frequency.
-var FREQUENCY_DEF = 350;
-var GAIN_MIN = -40;
-var GAIN_MAX = 40;
-var GAIN_DEF = 0;
 var TYPE_DEF = "allpass";
 
 var biquadFilter = audioContext.createBiquadFilter();
@@ -24,11 +12,18 @@ var biquadFilter = audioContext.createBiquadFilter();
 var audioInPort = CABLES.WEBAUDIO.createAudioInPort(op, "Audio In", biquadFilter);
 var audioOutPort = CABLES.WEBAUDIO.createAudioOutPort(op, "Audio Out", biquadFilter);
 
-var type = this.addInPort(new CABLES.Port(this,"type",CABLES.OP_PORT_TYPE_VALUE,{display:'dropdown',values:['allpass','lowpass','highpass','bandpass','lowshelf','highshelf','peaking','notch']}));
-var frequency = this.addInPort(new CABLES.Port(this, "frequency", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": FREQUENCY_MIN, "max": FREQUENCY_MAX}));
-var detune = this.addInPort(new CABLES.Port(this, "detune", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": DETUNE_MIN, "max": DETUNE_MAX}));
-var q = this.addInPort(new CABLES.Port(this, "q", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": Q_MIN, "max": Q_MAX}));
-var gain = this.addInPort(new CABLES.Port(this, "gain", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": GAIN_MIN, "max": GAIN_MAX}));
+// var type = this.addInPort(new CABLES.Port(this,"type",CABLES.OP_PORT_TYPE_VALUE,{display:'dropdown',values:['allpass','lowpass','highpass','bandpass','lowshelf','highshelf','peaking','notch']}));
+
+var type = op.inValueSelect ("type",['allpass','lowpass','highpass','bandpass','lowshelf','highshelf','peaking','notch'],'allpass');
+
+// type.set('allpass');
+// var frequency = this.addInPort(new CABLES.Port(this, "frequency", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": FREQUENCY_MIN, "max": FREQUENCY_MAX}));
+var frequency = op.inFloat("frequency",1000);
+
+// var detune = this.addInPort(new CABLES.Port(this, "detune", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": DETUNE_MIN, "max": DETUNE_MAX}));
+var detune = op.inFloatSlider("detune",0);// (new CABLES.Port(this, "detune", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": DETUNE_MIN, "max": DETUNE_MAX}));
+var q = op.inFloatSlider("q",0);//this.addInPort(new CABLES.Port(this, "q", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": Q_MIN, "max": Q_MAX}));
+var gain = op.inFloatSlider("gain",0.5);//this.addInPort(new CABLES.Port(this, "gain", CABLES.OP_PORT_TYPE_VALUE, {"display": "range", "min": GAIN_MIN, "max": GAIN_MAX}));
 
 var updateType = function(){
     biquadFilter.type = type.get();
@@ -63,9 +58,5 @@ detune.onChange=updateDetune;
 q.onChange=updateQ;
 gain.onChange=updateGain;
 
+updateType();
 
-type.set(TYPE_DEF);
-frequency.set( FREQUENCY_DEF );
-detune.set( DETUNE_DEF );
-q.set( Q_DEF );
-gain.set( GAIN_DEF );
