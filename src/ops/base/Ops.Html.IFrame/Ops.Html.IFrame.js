941024f68fb401c623aa0f049b9dbf0673807eb2
Alex <gitlab@rocken.org>
2019-11-20 16:36:47 +0100

    #1065 iframe v2

--- src/ops/base/Ops.Html.IFrame/Ops.Html.IFrame.js
+++ src/ops/base/Ops.Html.IFrame/Ops.Html.IFrame.js
@@ -3,6 +3,7 @@ var width=op.inValue("width",400);
 var height=op.inValue("height",400);
 var src=op.inValueString("URL",'https://undev.studio');
 var elId=op.inValueString("ID");
+var inBorder=op.inBool("Show Border");
 const outEle=op.outObject("Element");
 
 function updateSize()
@@ -21,9 +22,19 @@ function updateID()
     element.setAttribute('id',elId.get());
 }
 
+function updateBorder()
+{
+    if (inBorder.get()){
+        element.style.border=null;
+    }
+    else{
+        element.style.border="0";
+    }
+}
 
 var element = document.createElement('iframe');
 element.style.position="absolute";
+
 element.style.width="40px";
 element.style.height="40px";
 element.style["z-index"]="9999";
@@ -37,6 +48,7 @@ canvas.appendChild(element);
 width.onChange=updateSize;
 src.onChange=updateURL;
 elId.onChange=updateID;
+inBorder.onChange=updateBorder;
 updateSize();
 updateURL();
 
