8fbcd089b95c0ab562e6ae30eee51249fbbdeafd
Siamend Darwesh <simo@undev.de>
2019-12-12 15:34:38 +0100

    Make LambertMaterial use Old & New PointLight

--- src/ops/base/Ops.Gl.Phong.LambertMaterial/Ops.Gl.Phong.LambertMaterial.js
+++ src/ops/base/Ops.Gl.Phong.LambertMaterial/Ops.Gl.Phong.LambertMaterial.js
@@ -1,3 +1,6 @@
+const cgl = op.patch.cgl;
+
+
 const execute=op.inTrigger("execute");
 const r = op.inValueSlider("diffuse r", Math.random());
 const g = op.inValueSlider("diffuse g", Math.random());
@@ -14,7 +17,7 @@ const next=op.outTrigger("next");
 
 r.setUiAttribs({ colorPick: true });
 
-const cgl=op.patch.cgl;
+
 const shader=new CGL.Shader(cgl,"LambertMaterial");
 shader.define('NUM_LIGHTS','1');
 
@@ -49,15 +52,25 @@ var updateLights=function()
     var count=0;
     var i=0;
     var num=0;
-    if(!cgl.frameStore.phong || !cgl.frameStore.phong.lights)
+    if(
+        (!cgl.frameStore.phong || !cgl.frameStore.phong.lights)
+        && !cgl.lightStack
+    )
     {
         num=0;
     }
     else
     {
-        for(i in cgl.frameStore.phong.lights)
-        {
-            num++;
+        if (cgl.frameStore.phong) {
+            if (cgl.frameStore.phong.lights) {
+                for(i in cgl.frameStore.phong.lights) num++;
+            }
+        }
+
+        for (let light in cgl.lightStack) {
+            if (cgl.lightStack[light].type === "point") {
+                num++;
+            }
         }
     }
 
@@ -67,40 +80,68 @@ var updateLights=function()
         shader.define('NUM_LIGHTS',''+Math.max(numLights,1));
     }
 
-    if(!cgl.frameStore.phong || !cgl.frameStore.phong.lights)
+    if(
+        (!cgl.frameStore.phong || !cgl.frameStore.phong.lights)
+        && !cgl.lightStack
+    )
     {
-        lights[count].pos.setValue([5,5,5]);
-        lights[count].color.setValue([1,1,1]);
-        lights[count].ambient.setValue([0.1,0.1,0.1]);
-        lights[count].mul.setValue(1);
-        lights[count].fallOff.setValue(0.5);
+            lights[count].pos.setValue([5,5,5]);
+            lights[count].color.setValue([1,1,1]);
+            lights[count].ambient.setValue([0.1,0.1,0.1]);
+            lights[count].mul.setValue(1);
+            lights[count].fallOff.setValue(0.5);
+
     }
     else
     {
         count=0;
-        if(shader)
-            for(i in cgl.frameStore.phong.lights)
-            {
-                lights[count].pos.setValue(cgl.frameStore.phong.lights[i].pos);
-                // if(cgl.frameStore.phong.lights[i].changed)
-                {
-                    cgl.frameStore.phong.lights[i].changed=false;
-                    if(cgl.frameStore.phong.lights[i].target) lights[count].target.setValue(cgl.frameStore.phong.lights[i].target);
+        if(shader) {
 
-                    lights[count].fallOff.setValue(cgl.frameStore.phong.lights[i].fallOff);
-                    lights[count].radius.setValue(cgl.frameStore.phong.lights[i].radius);
-                    lights[count].color.setValue(cgl.frameStore.phong.lights[i].color);
-                    lights[count].ambient.setValue(cgl.frameStore.phong.lights[i].ambient);
-                    lights[count].attenuation.setValue(cgl.frameStore.phong.lights[i].attenuation);
-                    lights[count].type.setValue(cgl.frameStore.phong.lights[i].type);
-                    if(cgl.frameStore.phong.lights[i].cone) lights[count].cone.setValue(cgl.frameStore.phong.lights[i].cone);
-                    if(cgl.frameStore.phong.lights[i].depthTex) lights[count].texDepthTex=cgl.frameStore.phong.lights[i].depthTex;
+            if (cgl.frameStore.phong) {
+                if (cgl.frameStore.phong.lights) {
+                    let length = cgl.frameStore.phong.lights.length;
 
-                    lights[count].mul.setValue(cgl.frameStore.phong.lights[i].mul||1);
+                    for(let i = 0; i < length; i +=1 )//in cgl.frameStore.phong.lights)
+                    {
+                        lights[count].pos.setValue(cgl.frameStore.phong.lights[i].pos);
+                        // if(cgl.frameStore.phong.lights[i].changed)
+                        {
+                            cgl.frameStore.phong.lights[i].changed=false;
+                            if(cgl.frameStore.phong.lights[i].target) lights[count].target.setValue(cgl.frameStore.phong.lights[i].target);
+
+                            lights[count].fallOff.setValue(cgl.frameStore.phong.lights[i].fallOff);
+                            lights[count].radius.setValue(cgl.frameStore.phong.lights[i].radius);
+                            lights[count].color.setValue(cgl.frameStore.phong.lights[i].color);
+                            lights[count].ambient.setValue(cgl.frameStore.phong.lights[i].ambient);
+                            lights[count].attenuation.setValue(cgl.frameStore.phong.lights[i].attenuation);
+                            lights[count].type.setValue(cgl.frameStore.phong.lights[i].type);
+                            if(cgl.frameStore.phong.lights[i].cone) lights[count].cone.setValue(cgl.frameStore.phong.lights[i].cone);
+                            if(cgl.frameStore.phong.lights[i].depthTex) lights[count].texDepthTex=cgl.frameStore.phong.lights[i].depthTex;
+
+                            lights[count].mul.setValue(cgl.frameStore.phong.lights[i].mul||1);
+                        }
+
+                        count++;
+                    }
                 }
-
-                count++;
+                if (cgl.lightStack) {
+                    if (cgl.lightStack.length) {
+                        for (let j = 0; j < cgl.lightStack.length; j += 1) {
+                            const light = cgl.lightStack[j];
+                                 if (light.type === "point") { // POINT LIGHT
+                                    lights[count].pos.setValue(light.position);
+                                    lights[count].fallOff.setValue(light.falloff);
+                                    lights[count].radius.setValue(light.radius);
+                                    lights[count].color.setValue(light.color);
+                                    lights[count].ambient.setValue([0, 0, 0]);
+                                    lights[count].type.setValue(0); // old point light type index
+                                    count++;
+                                 }
+                        }
+                    }
+                }
             }
+        }
     }
 };
 
