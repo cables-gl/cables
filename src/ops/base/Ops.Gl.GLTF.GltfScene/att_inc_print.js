494bd2421d7cbf90d6f9dce512a9c46ee980bb21
pandrr <tom@undev.de>
2019-12-14 16:45:20 +0100

    gltf tree

--- src/ops/base/Ops.Gl.GLTF.GltfScene/att_inc_print.js
+++ src/ops/base/Ops.Gl.GLTF.GltfScene/att_inc_print.js
@@ -2,33 +2,49 @@
 
 function printNode(html,node,level)
 {
-    var tt=""
+    html+='<tr class="row">';
+    var i=0;
+    var ident="";
+
+    for(i=0;i<level;i++)
+    {
+        ident+='<td style="width:20px;height:20px;padding:0px;border-left:1px solid #333;border-bottom:1px solid #333;"><div style=""></div></td>';
+    }
+    var id=CABLES.uuid();
+    html+=ident;
+    html+='<td colspan="'+(20-level)+'">';
+    if(node.mesh && node.mesh.meshes.length)html+='<span class="icon icon-cube"></span>&nbsp;';
+    else html+='<span class="icon corner-down-right"></span> &nbsp;';
+    // if(node.mesh && node.mesh.meshes.length>1)html+=" ["+node.mesh.meshes.length+" meshes]";
+
+
+    html+='<a>'+node.name+'</a>';
+    html+='</td>';
+    html+='<td>';
+    html+='<a onclick="" class="">expose node</a>';
+    html+='</td>';
+
+    html+='<td>';
+
+    // if(node.mesh && node.mesh.meshes.length==1)html+=" [mesh]";
+    // if(node.mesh && node.mesh.meshes.length>1)html+=" ["+node.mesh.meshes.length+" meshes]";
+
+
     if(node.mesh)
     {
         for(i=0;i<node.mesh.meshes.length;i++)
         {
-            tt+="- mesh: "+node.mesh.meshes[i].name+'('+node.mesh.meshes[i].geom.vertices.length/3+' verts) <br/>';
+            html+='<span class="icon icon-cube"></span> '+node.mesh.meshes[i].name+' ('+node.mesh.meshes[i].geom.vertices.length/3+' verts)</a>';
         }
-
     }
 
+    html+='</td>';
 
+    html+='<td>';
+    html+='<span class="icon icon-eye"></span>';
+    html+='</td>';
 
-
-    var i=0;
-    var ident="";
-    for(i=0;i<level;i++)
-    {
-        ident+="| ";
-    }
-    var id=CABLES.uuid();
-    html+=ident;
-    html+='<a class="tt" data-tt="'+tt+'">'+node.name+'</a>';
-
-    if(node.mesh && node.mesh.meshes.length==1)html+=" [mesh]";
-    if(node.mesh && node.mesh.meshes.length>1)html+=" ["+node.mesh.meshes.length+" meshes]";
-
-    html+='<br/>';
+    html+="</tr>";
 
 
     if(node.children)
@@ -36,7 +52,6 @@ function printNode(html,node,level)
         {
             html=printNode(html,gltf.nodes[node.children[i]],level+1);
         }
-
 
     return html;
 }
@@ -49,16 +64,16 @@ function printMaterial()
 
 function printInfo()
 {
-    var html='<div style="font-family:monospace;">';
-
+    var html='<div style="">';
 
     html+='Nodes<br/>';
+    html+='<table class="treetable table">';
     for(var i=0;i<gltf.nodes.length;i++)
     {
         html=printNode(html,gltf.nodes[i],1);
     }
 
-
+    html+='</table>';
     html+='</div>';
 
     CABLES.UI.MODAL.show(html);
