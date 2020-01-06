0e641ad53ff6c573ffd31485eeb052ea44bd288a
andro <androvisuals@gmail.com>
2020-01-06 10:53:13 +0100

    added cosine mode option to sineAnim

--- src/ops/base/Ops.Anim.SineAnim/Ops.Anim.SineAnim.js
+++ src/ops/base/Ops.Anim.SineAnim/Ops.Anim.SineAnim.js
@@ -2,17 +2,35 @@ const
     exe=op.inTrigger("exe"),
     trigOut = op.outTrigger("Trigger out"),
     result=op.outValue("result"),
+    mode =op.inSwitch("Mode",['Sine','Cosine'],'Sine'),
     phase=op.inValueFloat("phase",0),
     mul=op.inValueFloat("frequency",1),
     amplitude=op.inValueFloat("amplitude",1);
 
+var selectIndex = 0;
+const SINE = 0;
+const COSINE = 1;
+
 op.toWorkPortsNeedToBeLinked(exe);
 
-exe.onTriggered=exec;
 exec();
+onModeChange();
 
+function onModeChange()
+{
+    var modeSelectValue = mode.get();
+
+    if(modeSelectValue === 'Sine') selectIndex = SINE;
+        else if(modeSelectValue === 'Cosine') selectIndex = COSINE;
+
+    op.setUiAttrib({"extendTitle":modeSelectValue});
+    exec();
+}
 function exec()
 {
-    result.set( amplitude.get() * Math.sin( (op.patch.freeTimer.get()*mul.get()) + phase.get() ));
+    if(selectIndex == SINE) result.set( amplitude.get() * Math.sin( (op.patch.freeTimer.get()*mul.get()) + phase.get() ));
+        else result.set( amplitude.get() * Math.cos( (op.patch.freeTimer.get()*mul.get()) + phase.get() ));
     trigOut.trigger();
 }
+exe.onTriggered=exec;
+mode.onChange=onModeChange;
