d5a7236c6e514fa7d8be108261cee953910b2e8c
pandrr <tom@undev.de>
2020-01-07 14:44:13 +0100

    clean

--- doc/build_docs.js
+++ doc/build_docs.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
  * Generates a gitbook documentation to "_book"
  */
 
-function generateGitbook() {
+// function generateGitbook() {
   exec('gitbook build', (error, stdout, stderr) => {
     console.log('stdout:', stdout);
 
@@ -18,31 +18,31 @@ function generateGitbook() {
       console.log('Gitbook generation finished.');
     }
   });
-}
+// }
 
-const generateJSDoc = () => exec('node generate-jsdoc.js', (err, stdout, stderr) => {
-  console.log('stdout:', stdout);
-  if (stderr.toString() !== '') {
-    console.error('stderr:', stderr);
-  }
-  if (err !== null) {
-    throw new Error(err);
-  } else {
-    console.log('JSDoc generation finished. Starting gitbook generation. This may take a few minutes...');
-    generateGitbook();
-  }
-});
+// const generateJSDoc = () => exec('node generate-jsdoc.js', (err, stdout, stderr) => {
+//   console.log('stdout:', stdout);
+//   if (stderr.toString() !== '') {
+//     console.error('stderr:', stderr);
+//   }
+//   if (err !== null) {
+//     throw new Error(err);
+//   } else {
+//     console.log('JSDoc generation finished. Starting gitbook generation. This may take a few minutes...');
+//     generateGitbook();
+//   }
+// });
 
-exec('node add_ops.js', (error, stdout, stderr) => {
-  console.log('stdout:', stdout);
-  if (stderr.toString() !== '') {
-    console.log('stderr:', stderr);
-  }
+// exec('node add_ops.js', (error, stdout, stderr) => {
+//   console.log('stdout:', stdout);
+//   if (stderr.toString() !== '') {
+//     console.log('stderr:', stderr);
+//   }
 
-  if (error !== null) throw new Error(error);
-  // success
-  else {
-    console.log( 'Summary generation / op-markdown copy finished. Now generating jsdoc-Documentation from comments.');
-    generateJSDoc();
-  }
-});
+//   if (error !== null) throw new Error(error);
+//   // success
+//   else {
+//     console.log( 'Summary generation / op-markdown copy finished. Now generating jsdoc-Documentation from comments.');
+//     generateJSDoc();
+//   }
+// });
