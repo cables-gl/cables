f25dae59b95cb4baf23008267c8f5914be356732
andro <androvisuals@gmail.com>
2020-01-23 11:18:27 +0100

    fixed all random array ops integer max value never reached

---
+++ src/ops/base/Ops.Array.RandomNumbersArray4_v2/Ops.Array.RandomNumbersArray4_v2.js
@@ -0,0 +1,75 @@
+const
+    numValues=op.inValueInt("numValues",100),
+    min=op.inValueFloat("Min",0),
+    max=op.inValueFloat("Max",1),
+    seed=op.inValueFloat("random seed"),
+    closed=op.inValueBool("Last == First"),
+    inInteger=op.inValueBool("Integer",false),
+    values=op.outArray("values"),
+    outTotalPoints = op.outNumber("Tuple Amount"),
+    outArrayLength = op.outNumber("Array length");
+
+op.setPortGroup("Value Range",[min,max]);
+op.setPortGroup("",[seed,closed]);
+
+values.ignoreValueSerialize=true;
+
+closed.onChange=max.onChange=
+    min.onChange=
+    numValues.onChange=
+    seed.onChange=
+    values.onLinkChanged=
+    inInteger.onChange=init;
+
+var arr=[];
+init();
+
+function init()
+{
+    Math.randomSeed=seed.get();
+
+    var isInteger=inInteger.get();
+
+    var arrLength = arr.length=Math.floor(Math.abs(numValues.get()*4));
+    if(arrLength === 0)
+    {
+        values.set(null);
+        outTotalPoints.set(0);
+        outArrayLength.set(0);
+        return
+    }
+
+    var minIn = min.get();
+    var maxIn = max.get();
+
+    for(let i = 0; i < arrLength; i += 4)
+    {
+        if(!isInteger)
+        {
+            arr[i+0]=Math.seededRandom() * ( maxIn - minIn ) + minIn ;
+            arr[i+1]=Math.seededRandom() * ( maxIn - minIn ) + minIn ;
+            arr[i+2]=Math.seededRandom() * ( maxIn - minIn ) + minIn ;
+            arr[i+3]=Math.seededRandom() * ( maxIn - minIn ) + minIn ;
+        }
+        else
+        {
+            arr[i+0]=Math.floor(Math.seededRandom() * ( (maxIn - minIn)+1 ) + minIn) ;
+            arr[i+1]=Math.floor(Math.seededRandom() * ( (maxIn - minIn)+1 ) + minIn) ;
+            arr[i+2]=Math.floor(Math.seededRandom() * ( (maxIn - minIn)+1 ) + minIn) ;
+            arr[i+3]=Math.floor(Math.seededRandom() * ( (maxIn - minIn)+1 ) + minIn) ;
+        }
+    }
+
+    if(closed.get() && arrLength>4)
+    {
+        arr[arrLength-4+0]=arr[0];
+        arr[arrLength-4+1]=arr[1];
+        arr[arrLength-4+2]=arr[2];
+        arr[arrLength-4+3]=arr[2];
+    }
+
+    values.set(null);
+    values.set(arr);
+    outTotalPoints.set(arrLength/4);
+    outArrayLength.set(arrLength);
+};
