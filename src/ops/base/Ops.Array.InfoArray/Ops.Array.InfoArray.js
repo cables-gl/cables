560ee2cf4f49867869f5162e8246167c8cdc054a
Stephan Maihoefer <stephan@undev.de>
2020-01-30 11:53:19 +0100

    Revert "add unique items information to InfoArray"
    This reverts commit 4afea5e3e2946a7f9f721260e431f13b89a78ef0.

--- src/ops/base/Ops.Array.InfoArray/Ops.Array.InfoArray.js
+++ src/ops/base/Ops.Array.InfoArray/Ops.Array.InfoArray.js
@@ -1,40 +1,28 @@
 const
-    inArr = op.inArray("Array"),
-    outMin = op.outValue("Min"),
-    outMax = op.outValue("Max"),
-    outAvg = op.outValue("Average"),
-    outUnique = op.outObject("Unique Items");
+    inArr=op.inArray("Array"),
+    outMin=op.outValue("Min"),
+    outMax=op.outValue("Max"),
+    outAvg=op.outValue("Average");
 
-inArr.onChange = function ()
+inArr.onChange=function()
 {
-    var arr = inArr.get();
+    var arr=inArr.get();
 
-    var min = 999999999;
-    var max = -999999999;
-    var avg = 0;
-    const counts = {};
+    var min=999999999;
+    var max=-999999999;
+    var avg=0;
 
-    if (arr)
+    if(arr)
     {
-        for (let i = 0; i < arr.length; i++)
+        for(var i=0;i<arr.length;i++)
         {
-            const value = arr[i];
-            avg += value;
-            min = Math.min(min, value);
-            max = Math.max(max, value);
-            if (counts.hasOwnProperty(value))
-            {
-                counts[value]++;
-            }
-            else
-            {
-                counts[value] = 1;
-            }
+            avg+=arr[i];
+            min=Math.min(min,arr[i]);
+            max=Math.max(max,arr[i]);
         }
-        avg /= arr.length;
+        avg/=arr.length;
     }
     outMin.set(min);
     outMax.set(max);
     outAvg.set(avg);
-    outUnique.set(counts);
 };
