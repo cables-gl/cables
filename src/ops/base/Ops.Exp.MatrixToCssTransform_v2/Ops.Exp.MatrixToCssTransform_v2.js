013fc73d732a80e975dc44ccea235cbf9f2b1d99
andro <androvisuals@gmail.com>
2020-02-19 16:52:41 +0100

    missed an op with the string port update

---
+++ src/ops/base/Ops.Exp.MatrixToCssTransform_v2/Ops.Exp.MatrixToCssTransform_v2.js
@@ -0,0 +1,63 @@
+const update=op.inTrigger("Update");
+const elId=op.inString("Html Id");
+const persp=op.inValue("Perspective",600);
+const next=op.outTrigger("Next");
+
+function generateCSSString(matrix ){
+   var str = '';
+   str += matrix[0].toFixed(20) + ',';
+   str += matrix[1].toFixed(20) + ',';
+   str += matrix[2].toFixed(20) + ',';
+   str += matrix[3].toFixed(20) + ',';
+   str += matrix[4].toFixed(20) + ',';
+   str += matrix[5].toFixed(20) + ',';
+   str += matrix[6].toFixed(20) + ',';
+   str += matrix[7].toFixed(20) + ',';
+   str += matrix[8].toFixed(20) + ',';
+   str += matrix[9].toFixed(20) + ',';
+   str += matrix[10].toFixed(20) + ',';
+   str += matrix[11].toFixed(20) + ',';
+   str += matrix[12].toFixed(20) + ',';
+   str += matrix[13].toFixed(20) + ',';
+   str += matrix[14].toFixed(20) + ',';
+   str += matrix[15].toFixed(20);
+
+   return 'matrix3d(' + str + ')';
+}
+
+var ele=null;
+
+elId.onCHange=function()
+{
+    ele=null;
+};
+
+function updatePerspective()
+{
+    if(ele) ele.style.perspective=persp.get()+'px';
+}
+
+update.onTriggered=function()
+{
+    if(!ele)
+    {
+        ele=document.getElementById(elId.get());
+    }
+
+    if(ele)
+    {
+        try
+        {
+            ele.style.transform= generateCSSString(op.patch.cgl.modelMatrix());
+            updatePerspective();
+            ele.parentNode.style.perspective=persp.get()+'px';
+            ele.style['perspective-origin']='50% 50%';
+        }
+        catch(e)
+        {
+            ele=null;
+        }
+    }
+
+
+};
