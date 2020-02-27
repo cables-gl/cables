c3783987fded2e01be175f2379f343a1a4e5f21b
Stephan Maihoefer <stephan@undev.de>
2020-02-27 17:33:29 +0100

    cors proxy op should urlencode urls that are fed to it #1232

--- src/ops/base/Ops.Net.CorsProxy/Ops.Net.CorsProxy.js
+++ src/ops/base/Ops.Net.CorsProxy/Ops.Net.CorsProxy.js
@@ -1,12 +1,30 @@
-var a=op.inValueString('url','');
-var result=op.outValue("Result");
-var CORS_CABLES_PROXY = 'https://cors.cables.gl/';
+var a = op.inString("URL", "");
+var result = op.outString("CORS URL");
+var CORS_CABLES_PROXY = "https://cors.cables.gl/";
 
-a.onChange=update;
+a.onChange = update;
 
 update();
 
 function update()
 {
-    result.set(CORS_CABLES_PROXY + a.get());
+    // for backwards compatibility, decode url as long as neccessary, then
+    // encode it to let it work with the actual proxy properly given url variables
+    const encoded = encodeURIComponent(fullyDecodeURI(a.get()));
+    result.set(CORS_CABLES_PROXY + encoded);
+}
+
+function isEncoded(uri)
+{
+    uri = uri || "";
+    return uri !== decodeURIComponent(uri);
+}
+
+function fullyDecodeURI(uri)
+{
+    while (isEncoded(uri))
+    {
+        uri = decodeURIComponent(uri);
+    }
+    return uri;
 }
