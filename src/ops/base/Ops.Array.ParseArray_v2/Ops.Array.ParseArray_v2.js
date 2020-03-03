a6d80426794091e1ae05c94c79da7f51bde4aab8
Stephan Maihoefer <stephan@undev.de>
2020-03-03 13:22:29 +0100

    ParseArray error "Parse Error: / Not all values numerical!" does not disappear when unchecking #1221

--- src/ops/base/Ops.Array.ParseArray_v2/Ops.Array.ParseArray_v2.js
+++ src/ops/base/Ops.Array.ParseArray_v2/Ops.Array.ParseArray_v2.js
@@ -1,37 +1,43 @@
-const
-    text=op.inStringEditor("text",'1,2,3'),
-    separator=op.inValueString("separator",","),
-    toNumber=op.inValueBool("Numbers",true),
-    parsed=op.outTrigger("Parsed"),
-    arr=op.outArray("array"),
-    len=op.outValue("length");
+const text = op.inStringEditor("text", "1,2,3"),
+    separator = op.inValueString("separator", ","),
+    toNumber = op.inValueBool("Numbers", true),
+    parsed = op.outTrigger("Parsed"),
+    arr = op.outArray("array"),
+    len = op.outValue("length");
 
-text.onChange=
-    separator.onChange=
-    toNumber.onChange=parse;
+text.onChange = separator.onChange = toNumber.onChange = parse;
 
 parse();
 
 function parse()
 {
-    if(!text.get())return;
+    if (!text.get()) return;
 
-    var r=text.get().split(separator.get());
+    var r = text.get().split(separator.get());
 
-    if(r[r.length-1]==="") r.length-=1;
+    if (r[r.length - 1] === "") r.length -= 1;
 
     len.set(r.length);
 
-    if(toNumber.get())
-        for(var i=0;i<r.length;i++)
+    op.setUiError("notnum", null);
+    if (toNumber.get())
+    {
+        var hasStrings = false;
+        for (var i = 0; i < r.length; i++)
         {
-            r[i]=Number(r[i]);
-            if(!CABLES.UTILS.isNumeric(r[i]))op.setUiError("notnum","Parse Error / Not all values numerical!");
-            else op.setUiError("notnum",null);
+            r[i] = Number(r[i]);
+            if (!CABLES.UTILS.isNumeric(r[i]))
+            {
+                hasStrings = true;
+            }
         }
+        if (hasStrings)
+        {
+            op.setUiError("notnum", "Parse Error / Not all values numerical!");
+        }
+    }
 
     arr.set(null);
     arr.set(r);
     parsed.trigger();
 }
-
