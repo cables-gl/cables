d1756bfb38cd62f6a832c3105d7694dec2e1454f
pandrr <tom@undev.de>
2020-03-05 18:50:53 +0100

    basicmatetial clean up

--- src/ops/base/Ops.Gl.Shader.BasicMaterial_v3/Ops.Gl.Shader.BasicMaterial_v3.js
+++ src/ops/base/Ops.Gl.Shader.BasicMaterial_v3/Ops.Gl.Shader.BasicMaterial_v3.js
@@ -1,6 +1,9 @@
 const render=op.inTrigger("render");
+
+
 const trigger=op.outTrigger('trigger');
 const shaderOut=op.outObject("shader");
+
 shaderOut.ignoreValueSerialize=true;
 
 const cgl=op.patch.cgl;
@@ -9,17 +12,72 @@ op.toWorkPortsNeedToBeLinked(render);
 
 const shader=new CGL.Shader(cgl,"basicmaterialnew");
 shader.setModules(['MODULE_VERTEX_POSITION','MODULE_COLOR','MODULE_BEGIN_FRAG']);
-// shader.bindTextures=bindTextures;
 shader.setSource(attachments.basicmaterial_vert,attachments.basicmaterial_frag);
 shaderOut.set(shader);
 
 render.onTriggered=doRender;
 
-// function bindTextures()
-// {
-//     // if(diffuseTexture.get()) cgl.setTexture(0, diffuseTexture.get().tex);
-//     // if(textureOpacity.get()) cgl.setTexture(1, textureOpacity.get().tex);
-// }
+
+// rgba colors
+const r=op.inValueSlider("r",Math.random());
+const g=op.inValueSlider("g",Math.random());
+const b=op.inValueSlider("b",Math.random());
+const a=op.inValueSlider("a",1);
+r.setUiAttribs({"colorPick":true});
+
+const uniColor=new CGL.Uniform(shader,'4f','color',r,g,b,a);
+
+// diffuse outTexture
+
+const diffuseTexture=op.inTexture("texture");
+var diffuseTextureUniform=null;
+diffuseTexture.onChange=updateDiffuseTexture;
+
+const colorizeTexture=op.inValueBool("colorizeTexture",false);
+
+// opacity texture
+const textureOpacity=op.inTexture("textureOpacity");
+var textureOpacityUniform=null;
+
+const alphaMaskSource=op.inSwitch("Alpha Mask Source",["Luminance","R","G","B","A"],"Luminance");
+alphaMaskSource.setUiAttribs({greyout:true});
+textureOpacity.onChange=updateOpacity;
+
+const texCoordAlpha=op.inValueBool("Opacity TexCoords Transform",false);
+const discardTransPxl=op.inValueBool("Discard Transparent Pixels");
+
+
+// texture coords
+
+const diffuseRepeatX=op.inValue("diffuseRepeatX",1);
+const diffuseRepeatY=op.inValue("diffuseRepeatY",1);
+const diffuseOffsetX=op.inValue("Tex Offset X",0);
+const diffuseOffsetY=op.inValue("Tex Offset Y",0);
+
+const diffuseRepeatXUniform=new CGL.Uniform(shader,'f','diffuseRepeatX',diffuseRepeatX);
+const diffuseRepeatYUniform=new CGL.Uniform(shader,'f','diffuseRepeatY',diffuseRepeatY);
+const diffuseOffsetXUniform=new CGL.Uniform(shader,'f','texOffsetX',diffuseOffsetX);
+const diffuseOffsetYUniform=new CGL.Uniform(shader,'f','texOffsetY',diffuseOffsetY);
+
+const doBillboard=op.inValueBool("billboard",false);
+
+alphaMaskSource.onChange=
+    doBillboard.onChange=
+    discardTransPxl.onChange=
+    texCoordAlpha.onChange=
+    colorizeTexture.onChange=updateDefines;
+
+
+op.setPortGroup("Color",[r,g,b,a]);
+op.setPortGroup("Color Texture",[diffuseTexture,colorizeTexture]);
+op.setPortGroup("Opacity",[textureOpacity,alphaMaskSource,discardTransPxl,texCoordAlpha]);
+op.setPortGroup("Texture Transform",[diffuseRepeatX,diffuseRepeatY,diffuseOffsetX,diffuseOffsetY]);
+
+
+updateOpacity();
+updateDiffuseTexture();
+
+
 
 op.preRender=function()
 {
@@ -32,35 +90,41 @@ function doRender()
     if(!shader)return;
 
     cgl.pushShader(shader);
-    // shader.bindTextures();
     shader.popTextures();
 
     if(diffuseTextureUniform && diffuseTexture.get()) shader.pushTexture(diffuseTextureUniform,diffuseTexture.get().tex);
     if(textureOpacityUniform && textureOpacity.get()) shader.pushTexture(textureOpacityUniform,textureOpacity.get().tex);
     trigger.trigger();
 
-
     cgl.popShader();
 }
 
-// rgba colors
-const r=op.inValueSlider("r",Math.random());
-const g=op.inValueSlider("g",Math.random());
-const b=op.inValueSlider("b",Math.random());
-const a=op.inValueSlider("a",1);
-r.setUiAttribs({"colorPick":true});
+function updateOpacity()
+{
+    if(textureOpacity.get())
+    {
+        if(textureOpacityUniform!==null)return;
+        shader.removeUniform('texOpacity');
+        shader.define('HAS_TEXTURE_OPACITY');
+        if(!textureOpacityUniform)textureOpacityUniform=new CGL.Uniform(shader,'t','texOpacity',1);
 
-const uniColor=new CGL.Uniform(shader,'4f','color',r,g,b,a);
+        alphaMaskSource.setUiAttribs({greyout:false});
+        discardTransPxl.setUiAttribs({greyout:false});
+        texCoordAlpha.setUiAttribs({greyout:false});
+    }
+    else
+    {
+        shader.removeUniform('texOpacity');
+        shader.removeDefine('HAS_TEXTURE_OPACITY');
+        textureOpacityUniform=null;
 
-op.setPortGroup("Color",[r,g,b,a]);
+        alphaMaskSource.setUiAttribs({greyout:true});
+        discardTransPxl.setUiAttribs({greyout:true});
+        texCoordAlpha.setUiAttribs({greyout:true});
+    }
 
-// diffuse outTexture
-
-var diffuseTexture=op.inTexture("texture");
-var diffuseTextureUniform=null;
-// shader.bindTextures=bindTextures;
-
-diffuseTexture.onChange=updateDiffuseTexture;
+    updateDefines();
+}
 
 function updateDiffuseTexture()
 {
@@ -89,117 +153,18 @@ function updateDiffuseTexture()
     }
 }
 
-const colorizeTexture=op.inValueBool("colorizeTexture",false);
-
-op.setPortGroup("Color Texture",[diffuseTexture,colorizeTexture]);
-
-// opacity texture
-var textureOpacity=op.inTexture("textureOpacity");
-var textureOpacityUniform=null;
-
-op.alphaMaskSource=op.inSwitch("Alpha Mask Source",["Luminance","R","G","B","A"],"Luminance");
-op.alphaMaskSource.onChange=updateAlphaMaskMethod;
-op.alphaMaskSource.setUiAttribs({greyout:true});
-
-function updateAlphaMaskMethod()
+function updateDefines()
 {
-    if(op.alphaMaskSource.get()=='Alpha Channel') shader.define('ALPHA_MASK_ALPHA');
-        else shader.removeDefine('ALPHA_MASK_ALPHA');
+    shader.toggleDefine('COLORIZE_TEXTURE', colorizeTexture.get());
+    shader.toggleDefine('TRANSFORMALPHATEXCOORDS', texCoordAlpha.get());
+    shader.toggleDefine('DISCARDTRANS', discardTransPxl.get());
+    shader.toggleDefine('BILLBOARD', doBillboard.get());
 
-    if(op.alphaMaskSource.get()=='Luminance') shader.define('ALPHA_MASK_LUMI');
-        else shader.removeDefine('ALPHA_MASK_LUMI');
-
-    if(op.alphaMaskSource.get()=='R') shader.define('ALPHA_MASK_R');
-        else shader.removeDefine('ALPHA_MASK_R');
-
-    if(op.alphaMaskSource.get()=='G') shader.define("ALPHA_MASK_G");
-        else shader.removeDefine('ALPHA_MASK_G');
-
-    if(op.alphaMaskSource.get()=='B') shader.define('ALPHA_MASK_B');
-        else shader.removeDefine('ALPHA_MASK_B');
+    shader.toggleDefine('ALPHA_MASK_ALPHA', alphaMaskSource.get()=='Alpha Channel');
+    shader.toggleDefine('ALPHA_MASK_LUMI', alphaMaskSource.get()=='Luminance');
+    shader.toggleDefine('ALPHA_MASK_R', alphaMaskSource.get()=='R');
+    shader.toggleDefine("ALPHA_MASK_G", alphaMaskSource.get()=='G');
+    shader.toggleDefine('ALPHA_MASK_B', alphaMaskSource.get()=='B');
 }
 
-textureOpacity.onChange=updateOpacity;
-function updateOpacity()
-{
 
-    if(textureOpacity.get())
-    {
-        if(textureOpacityUniform!==null)return;
-        shader.removeUniform('texOpacity');
-        shader.define('HAS_TEXTURE_OPACITY');
-        if(!textureOpacityUniform)textureOpacityUniform=new CGL.Uniform(shader,'t','texOpacity',1);
-
-        op.alphaMaskSource.setUiAttribs({greyout:false});
-        discardTransPxl.setUiAttribs({greyout:false});
-        texCoordAlpha.setUiAttribs({greyout:false});
-
-    }
-    else
-    {
-        shader.removeUniform('texOpacity');
-        shader.removeDefine('HAS_TEXTURE_OPACITY');
-        textureOpacityUniform=null;
-
-        op.alphaMaskSource.setUiAttribs({greyout:true});
-        discardTransPxl.setUiAttribs({greyout:true});
-        texCoordAlpha.setUiAttribs({greyout:true});
-    }
-    updateAlphaMaskMethod();
-};
-
-
-var texCoordAlpha=op.inValueBool("Opacity TexCoords Transform",false);
-const discardTransPxl=op.inValueBool("Discard Transparent Pixels");
-
-discardTransPxl.onChange=function()
-{
-    if(discardTransPxl.get()) shader.define('DISCARDTRANS');
-        else shader.removeDefine('DISCARDTRANS');
-};
-
-
-texCoordAlpha.onChange=function()
-{
-    if(texCoordAlpha.get()) shader.define('TRANSFORMALPHATEXCOORDS');
-        else shader.removeDefine('TRANSFORMALPHATEXCOORDS');
-};
-
-op.setPortGroup("Opacity",[textureOpacity,op.alphaMaskSource,discardTransPxl,texCoordAlpha]);
-
-
-colorizeTexture.onChange=function()
-{
-    if(colorizeTexture.get()) shader.define('COLORIZE_TEXTURE');
-        else shader.removeDefine('COLORIZE_TEXTURE');
-};
-
-
-
-
-// texture coords
-
-const diffuseRepeatX=op.inValue("diffuseRepeatX",1);
-const diffuseRepeatY=op.inValue("diffuseRepeatY",1);
-const diffuseOffsetX=op.inValue("Tex Offset X",0);
-const diffuseOffsetY=op.inValue("Tex Offset Y",0);
-
-const diffuseRepeatXUniform=new CGL.Uniform(shader,'f','diffuseRepeatX',diffuseRepeatX);
-const diffuseRepeatYUniform=new CGL.Uniform(shader,'f','diffuseRepeatY',diffuseRepeatY);
-const diffuseOffsetXUniform=new CGL.Uniform(shader,'f','texOffsetX',diffuseOffsetX);
-const diffuseOffsetYUniform=new CGL.Uniform(shader,'f','texOffsetY',diffuseOffsetY);
-
-op.setPortGroup("Texture Transform",[diffuseRepeatX,diffuseRepeatY,diffuseOffsetX,diffuseOffsetY]);
-
-
-
-const doBillboard=op.inValueBool("billboard",false);
-
-doBillboard.onChange=function()
-{
-    if(doBillboard.get()) shader.define('BILLBOARD');
-        else shader.removeDefine('BILLBOARD');
-};
-
-updateOpacity();
-updateDiffuseTexture();
