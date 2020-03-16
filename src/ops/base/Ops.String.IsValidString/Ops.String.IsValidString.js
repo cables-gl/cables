0d6986e8b477b0d421d4b121a030fe733946f537
pandrr <tom@undev.de>
2020-03-16 14:06:36 +0100

    new string ops

---
+++ src/ops/base/Ops.String.IsValidString/Ops.String.IsValidString.js
@@ -0,0 +1,24 @@
+
+const
+    inStr=op.inString("String",""),
+    checkNull=op.inBool("Invalid if null",true),
+    checkUndefined=op.inBool("Invalid if undefined",true),
+    checkEmpty=op.inBool("Invalid if empty",false),
+    result=op.outBool("Is Valid");
+
+inStr.onChange=
+checkNull.onChange=
+checkUndefined.onChange=
+checkEmpty.onChange=
+function()
+{
+    const str=inStr.get();
+    var r=true;
+
+    if(r && checkNull.get() && str===null) r=false;
+    if(r && checkUndefined.get() && str===undefined) r=false;
+    if(r && checkEmpty.get() && str==="") r=false;
+
+    result.set(r);
+
+};
