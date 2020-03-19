ebc0562cfd15f210ce98cdfb134fa46663e3c028
Stephan Maihoefer <stephan@undev.de>
2020-03-19 19:10:26 +0100

    add clientid to receive

--- src/ops/base/Ops.Admin.SocketClusterRecieve/Ops.Admin.SocketClusterRecieve.js
+++ src/ops/base/Ops.Admin.SocketClusterRecieve/Ops.Admin.SocketClusterRecieve.js
@@ -1,6 +1,7 @@
 const inSocket = op.inObject("socket");
 const inTopic = op.inString("topic", "main");
 const outData = op.outNumber("data");
+const clientIdOut = op.outString("client id");
 
 const init = () =>
 {
@@ -15,6 +16,7 @@ const init = () =>
                 if (obj.clientId != socket.clientId && obj.topic == inTopic.get())
                 {
                     outData.set(obj.payload);
+                    clientIdOut.set(obj.clientId);
                 }
             }
         })();
