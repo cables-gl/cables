c45cd4a9034d8fcc9c587e07d0f672b6da298b19
andro <androvisuals@gmail.com>
2020-04-08 14:33:54 +0200

    Ops.Math.Math now uses function references so should be much faster #1217 and #1296

--- src/ops/base/Ops.Math.Math/Ops.Math.Math.js
+++ src/ops/base/Ops.Math.Math/Ops.Math.Math.js
@@ -1,18 +1,9 @@
-
 const num0 = op.inFloat("number 0",0),
     num1 = op.inFloat("number 1",0),
     mathDropDown = op.inSwitch("math mode",['+','-','*','/','%','min','max'], "+"),
     result = op.outNumber("result");
 
-var selectIndex = 0;
-
-const MATH_FUNC_ADD = 0;
-const MATH_FUNC_SUB = 1;
-const MATH_FUNC_MUL = 2;
-const MATH_FUNC_DIV = 3;
-const MATH_FUNC_MOD = 4;
-const MATH_FUNC_MIN = 5;
-const MATH_FUNC_MAX = 6;
+var mathFunc;
 
 num0.onChange = num1.onChange = update;
 mathDropDown.onChange = onFilterChange;
@@ -20,13 +11,15 @@ mathDropDown.onChange = onFilterChange;
 function onFilterChange()
 {
     var mathSelectValue = mathDropDown.get();
-    if(mathSelectValue === '+') selectIndex = MATH_FUNC_ADD;
-    else if(mathSelectValue === '-') selectIndex = MATH_FUNC_SUB;
-    else if(mathSelectValue === '*') selectIndex = MATH_FUNC_MUL;
-    else if(mathSelectValue === '/') selectIndex = MATH_FUNC_DIV;
-    else if(mathSelectValue === '%') selectIndex = MATH_FUNC_MOD;
-    else if(mathSelectValue === 'min') selectIndex = MATH_FUNC_MIN;
-    else if(mathSelectValue === 'max') selectIndex = MATH_FUNC_MAX;
+
+    if(mathSelectValue === '+')         mathFunc = function(a,b){return a+b};
+    else if(mathSelectValue === '-')    mathFunc = function(a,b){return a-b};
+    else if(mathSelectValue === '*')    mathFunc = function(a,b){return a*b};
+    else if(mathSelectValue === '/')    mathFunc = function(a,b){return a/b};
+    else if(mathSelectValue === '%')    mathFunc = function(a,b){return a%b};
+    else if(mathSelectValue === 'min')  mathFunc = function(a,b){return Math.min(a,b)};
+    else if(mathSelectValue === 'max')  mathFunc = function(a,b){return Math.max(a,b)};
+
     update();
     op.setUiAttrib({"extendTitle":mathSelectValue});
 }
@@ -36,32 +29,7 @@ function update()
     var n0 = num0.get();
     var n1 = num1.get();
 
-    if(selectIndex === MATH_FUNC_ADD)
-    {
-       result.set(n0 + n1);
-    }
-    else if(selectIndex === MATH_FUNC_SUB)
-    {
-       result.set(n0 - n1);
-    }
-    else if(selectIndex === MATH_FUNC_MUL)
-    {
-       result.set(n0 * n1);
-    }
-    else if(selectIndex === MATH_FUNC_DIV)
-    {
-       result.set(n0 / n1);
-    }
-    else if(selectIndex === MATH_FUNC_MOD)
-    {
-       result.set(n0 % n1);
-    }
-    else if(selectIndex === MATH_FUNC_MIN)
-    {
-       result.set( Math.min(n0,n1));
-    }
-    else if(selectIndex === MATH_FUNC_MAX)
-    {
-       result.set(Math.max(n0,n1));
-    }
+    result.set(mathFunc(n0,n1));
 }
+
+onFilterChange();
