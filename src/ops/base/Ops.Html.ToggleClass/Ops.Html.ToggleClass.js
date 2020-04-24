27b66790bdc5a48808c9b299bccf09c74aac99db
pandrr <tom@undev.de>
2020-04-24 08:19:09 +0200

    #1326 toggle class fix

--- src/ops/base/Ops.Html.ToggleClass/Ops.Html.ToggleClass.js
+++ src/ops/base/Ops.Html.ToggleClass/Ops.Html.ToggleClass.js
@@ -7,18 +7,30 @@ inActive.onChange=
     inClassName.onChange=
     inEle.onChange=update;
 
+op.onDelete=remove;
+
 var oldEle=null;
+var oldName=null;
+
+function remove(ele)
+{
+    if(ele && ele.classList) ele.classList.remove(oldName);
+}
 
 function update()
 {
     var ele=inEle.get();
     var cn=inClassName.get();
 
+    if(!inEle.isLinked()) remove(oldEle);
+
     if(!cn || !ele || !ele.classList)return;
     if(oldEle && ele!=oldEle)oldEle.classList.remove(cn);
+    remove(ele);
 
     if(!inActive.get()) ele.classList.remove(cn);
-        else ele.classList.add(cn);
+    else ele.classList.add(cn);
 
+    oldName=cn;
     oldEle=ele;
 }
