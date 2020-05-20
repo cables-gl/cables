c29d2a3b8714e07fa011265071633331e38864f7
Stephan Maihoefer <stephan@undev.de>
2020-05-20 12:09:32 +0200

    fix opconfig for min/max

--- src/core/patchConnection.js
+++ src/core/patchConnection.js
@@ -45,10 +45,6 @@ PatchConnectionReceiver.prototype._receive = function (ev)
         Log.log("PACO load patch.....");
         this._patch.clear();
         this._patch.deSerialize(data.vars.patch);
-        if (gui)
-        {
-            gui.patch().updateSubPatches();
-        }
     }
     else if (data.event == CONSTANTS.PACO.PACO_CLEAR)
     {
