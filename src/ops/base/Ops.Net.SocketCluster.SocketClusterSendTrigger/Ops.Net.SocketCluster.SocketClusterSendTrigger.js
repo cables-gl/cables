6eaa0f5d3568999cd9dc0c914f1583bfe8a0f2d7
Stephan Maihoefer <stephan@undev.de>
2020-06-09 10:50:08 +0200

    allow for delay in socketcluster ops

--- src/ops/base/Ops.Net.SocketCluster.SocketClusterSendTrigger/Ops.Net.SocketCluster.SocketClusterSendTrigger.js
+++ src/ops/base/Ops.Net.SocketCluster.SocketClusterSendTrigger/Ops.Net.SocketCluster.SocketClusterSendTrigger.js
@@ -1,14 +1,28 @@
 const inData = op.inTrigger("data");
 const inSocket = op.inObject("socket");
 const inTopic = op.inString("topic", "main");
+const inDelay = op.inInt("delay (ms)", 0);
 
 const send = () =>
 {
     const socket = inSocket.get();
     if (socket && socket.channelName && socket.allowSend)
     {
-        const payload = { topic: inTopic.get(), clientId: socket.clientId, payload: inData.get() };
-        socket.transmitPublish(socket.channelName + "/triggers", payload);
+        const payload = { "topic": inTopic.get(), "clientId": socket.clientId, "payload": inData.get() };
+        let delay = 0;
+        const localDelay = inDelay.get();
+        if (inDelay.get() > 0 || socket.globalDelay > 0)
+        {
+            delay = socket.globalDelay;
+            if (localDelay)
+            {
+                delay = localDelay;
+            }
+        }
+        setTimeout(() =>
+        {
+            socket.transmitPublish(socket.channelName + "/triggers", payload);
+        }, delay);
     }
 };
 
