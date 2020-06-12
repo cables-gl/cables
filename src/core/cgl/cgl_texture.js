2f0a982a89125f2e2f3cedad130938d0072e5496
Siamend Darwesh <simo@undev.de>
2020-06-12 16:00:28 +0200

    Remove unused code

--- src/core/cgl/cgl_texture.js
+++ src/core/cgl/cgl_texture.js
@@ -126,17 +126,6 @@ Texture.prototype.clone = function ()
  * @param {Number} width
  * @param {Number} height
  */
-
-function a()
-{
-    if (1)x();
-}
-
-function x()
-{
-    if (1) return 1;
-}
-
 Texture.prototype.setSize = function (w, h)
 {
     if (w != w || w <= 0 || !w) w = DEFAULT_TEXTURE_SIZE;
