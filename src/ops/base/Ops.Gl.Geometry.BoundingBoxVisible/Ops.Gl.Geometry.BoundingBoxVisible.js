39f82b97413ec0359d8e013c4d1176507c4daa0d
pandrr <tom@undev.de>
2020-07-06 15:19:12 +0200

    better collision bb to screen

--- src/ops/base/Ops.Gl.Geometry.BoundingBoxVisible/Ops.Gl.Geometry.BoundingBoxVisible.js
+++ src/ops/base/Ops.Gl.Geometry.BoundingBoxVisible/Ops.Gl.Geometry.BoundingBoxVisible.js
@@ -1,10 +1,10 @@
 const
-    exec=op.inTrigger("Exec"),
-    inBB=op.inObject("Boundings"),
-    inActive=op.inBool("Active",true),
-    inDraw=op.inBool("Draw",true),
-    next=op.outTrigger("Next"),
-    result=op.outBool("Visible"),
+    exec = op.inTrigger("Exec"),
+    inBB = op.inObject("Boundings"),
+    inActive = op.inBool("Active", true),
+    inDraw = op.inBool("Draw", true),
+    next = op.outTrigger("Next"),
+    result = op.outBool("Visible"),
 
     cgl = op.patch.cgl,
     trans = vec3.create(),
@@ -13,8 +13,9 @@ const
     identVec = vec3.create();
 
 let
-    geom=null,
-    mesh=null;
+    vp = null,
+    geom = null,
+    mesh = null;
 
 
 function isVisible(posi)
@@ -24,7 +25,6 @@ function isVisible(posi)
 
     vec3.transformMat4(trans, pos, cgl.pMatrix);
 
-    const vp = cgl.getViewPort();
 
     const xp = (trans[0] * vp[2] / 2) + vp[2] / 2;
     const yp = (trans[1] * vp[3] / 2) + vp[3] / 2;
@@ -32,97 +32,165 @@ function isVisible(posi)
     return pos[2] < 0.0 && xp > 0 && xp < vp[2] && yp > 0 && yp < vp[3];
 }
 
-inBB.onChange=function()
+inBB.onChange = function ()
 {
-    mesh=null;
+    mesh = null;
 };
 
 function buildMesh()
 {
-    const bb=inBB.get();
-    if(!bb)return;
-    geom=new CGL.Geometry();
-    geom.vertices=
+    const bb = inBB.get();
+    if (!bb) return;
+    geom = new CGL.Geometry();
+    geom.vertices =
     [
-        bb._max[0],bb._max[1],bb._max[2],
-        bb._min[0],bb._max[1],bb._max[2],
+        bb._max[0], bb._max[1], bb._max[2],
+        bb._min[0], bb._max[1], bb._max[2],
 
-        bb._min[0],bb._max[1],bb._max[2],
-        bb._min[0],bb._min[1],bb._max[2],
+        bb._min[0], bb._max[1], bb._max[2],
+        bb._min[0], bb._min[1], bb._max[2],
 
-        bb._min[0],bb._min[1],bb._max[2],
-        bb._max[0],bb._min[1],bb._max[2],
+        bb._min[0], bb._min[1], bb._max[2],
+        bb._max[0], bb._min[1], bb._max[2],
 
-        bb._max[0],bb._min[1],bb._max[2],
-        bb._max[0],bb._max[1],bb._max[2],
-
-        //
-
-        bb._max[0],bb._max[1],bb._min[2],
-        bb._min[0],bb._max[1],bb._min[2],
-
-        bb._min[0],bb._max[1],bb._min[2],
-        bb._min[0],bb._min[1],bb._min[2],
-
-        bb._min[0],bb._min[1],bb._min[2],
-        bb._max[0],bb._min[1],bb._min[2],
-
-        bb._max[0],bb._min[1],bb._min[2],
-        bb._max[0],bb._max[1],bb._min[2],
+        bb._max[0], bb._min[1], bb._max[2],
+        bb._max[0], bb._max[1], bb._max[2],
 
         //
 
-        bb._max[0],bb._max[1],bb._min[2],
-        bb._max[0],bb._max[1],bb._max[2],
+        bb._max[0], bb._max[1], bb._min[2],
+        bb._min[0], bb._max[1], bb._min[2],
 
-        bb._min[0],bb._max[1],bb._min[2],
-        bb._min[0],bb._max[1],bb._max[2],
+        bb._min[0], bb._max[1], bb._min[2],
+        bb._min[0], bb._min[1], bb._min[2],
+
+        bb._min[0], bb._min[1], bb._min[2],
+        bb._max[0], bb._min[1], bb._min[2],
+
+        bb._max[0], bb._min[1], bb._min[2],
+        bb._max[0], bb._max[1], bb._min[2],
+
+        //
+
+        bb._max[0], bb._max[1], bb._min[2],
+        bb._max[0], bb._max[1], bb._max[2],
+
+        bb._min[0], bb._max[1], bb._min[2],
+        bb._min[0], bb._max[1], bb._max[2],
 
 
-        bb._max[0],bb._min[1],bb._min[2],
-        bb._max[0],bb._min[1],bb._max[2],
+        bb._max[0], bb._min[1], bb._min[2],
+        bb._max[0], bb._min[1], bb._max[2],
 
-        bb._min[0],bb._min[1],bb._min[2],
-        bb._min[0],bb._min[1],bb._max[2],
+        bb._min[0], bb._min[1], bb._min[2],
+        bb._min[0], bb._min[1], bb._max[2],
+
+
+        //
+
+        bb._min[0], bb._min[1], bb._min[2],
+        bb._max[0], bb._max[1], bb._max[2],
+
+        bb._max[0], bb._min[1], bb._min[2],
+        bb._min[0], bb._max[1], bb._max[2],
 
     ];
 
-    mesh=new CGL.Mesh(cgl,geom,cgl.gl.LINES);
+    mesh = new CGL.Mesh(cgl, geom, cgl.gl.LINES);
 }
 
 
-
-exec.onTriggered=()=>
+// LINE/LINE
+function lineToScreen(posA, posB)
 {
-    if(!inActive.get())return;
-    const bb=inBB.get();
-    if(!bb)
+    vec3.transformMat4(pos, posA, m);
+    vec3.transformMat4(trans, pos, cgl.pMatrix);
+
+    const x1 = (trans[0] * vp[2] / 2) + vp[2] / 2;
+    const y1 = (trans[1] * vp[3] / 2) + vp[3] / 2;
+
+    vec3.transformMat4(pos, posB, m);
+    vec3.transformMat4(trans, pos, cgl.pMatrix);
+
+    const x2 = (trans[0] * vp[2] / 2) + vp[2] / 2;
+    const y2 = (trans[1] * vp[3] / 2) + vp[3] / 2;
+
+
+    const w = vp[2];
+    const h = vp[3];
+
+    return lineline(x1, y1, x2, y2, 0, 0, w, h) ||
+
+        lineline(x1, y1, x2, y2, 0, 0, w, 0) ||
+        lineline(x1, y1, x2, y2, 0, h, w, h) ||
+
+        lineline(x1, y1, x2, y2, 0, 0, 0, h) ||
+        lineline(x1, y1, x2, y2, w, 0, w, h);
+}
+
+function lineline(x1, y1, x2, y2, x3, y3, x4, y4)
+{
+    // calculate the direction of the lines
+    const uA = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+    const uB = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / ((y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1));
+
+    // if uA and uB are between 0-1, lines are colliding
+    if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1)
+    {
+        // const intersectionX = x1 + (uA * (x2 - x1));
+        // const intersectionY = y1 + (uA * (y2 - y1));
+
+        return true;
+    }
+    return false;
+}
+
+
+exec.onTriggered = () =>
+{
+    if (!inActive.get()) return;
+    const bb = inBB.get();
+    if (!bb)
     {
         result.set(false);
         return;
     }
 
-    if(inDraw.get())
+    if (inDraw.get())
     {
-        if(!geom || !mesh) buildMesh();
+        if (!geom || !mesh) buildMesh();
         mesh.render(cgl.getShader());
     }
 
+    vp = cgl.getViewPort();
+
     mat4.multiply(m, cgl.vMatrix, cgl.mMatrix);
 
-    const isVis=
-    // isVisible(bb._center) ||
+    const isVis =
 
-        isVisible([bb._max[0],bb._max[1],bb._max[2]]) ||
-        isVisible([bb._max[0],bb._min[1],bb._max[2]]) ||
-        isVisible([bb._min[0],bb._min[1],bb._max[2]]) ||
-        isVisible([bb._min[0],bb._max[1],bb._max[2]]) ||
+        isVisible(bb._center) ||
 
-        isVisible([bb._max[0],bb._max[1],bb._min[2]]) ||
-        isVisible([bb._max[0],bb._min[1],bb._min[2]]) ||
-        isVisible([bb._min[0],bb._min[1],bb._min[2]]) ||
-        isVisible([bb._min[0],bb._max[1],bb._min[2]])
-        ;
+        lineToScreen(bb._max, bb._min) ||
+
+        lineToScreen([bb._max[0], bb._max[1], bb._max[2]], [bb._min[0], bb._max[1], bb._max[2]]) ||
+        lineToScreen([bb._min[0], bb._max[1], bb._max[2]], [bb._min[0], bb._min[1], bb._max[2]]) ||
+        lineToScreen([bb._min[0], bb._min[1], bb._max[2]], [bb._max[0], bb._min[1], bb._max[2]]) ||
+        lineToScreen([bb._max[0], bb._min[1], bb._max[2]], [bb._max[0], bb._max[1], bb._max[2]]) ||
+
+        //
+
+        lineToScreen([bb._max[0], bb._max[1], bb._min[2]], [bb._min[0], bb._max[1], bb._min[2]]) ||
+        lineToScreen([bb._min[0], bb._max[1], bb._min[2]], [bb._min[0], bb._min[1], bb._min[2]]) ||
+        lineToScreen([bb._min[0], bb._min[1], bb._min[2]], [bb._max[0], bb._min[1], bb._min[2]]) ||
+        lineToScreen([bb._max[0], bb._min[1], bb._min[2]], [bb._max[0], bb._max[1], bb._min[2]]) ||
+
+        //
+
+        lineToScreen([bb._max[0], bb._max[1], bb._min[2]], [bb._max[0], bb._max[1], bb._max[2]]) ||
+        lineToScreen([bb._min[0], bb._max[1], bb._min[2]], [bb._min[0], bb._max[1], bb._max[2]]) ||
+        lineToScreen([bb._max[0], bb._min[1], bb._min[2]], [bb._max[0], bb._min[1], bb._max[2]]) ||
+        lineToScreen([bb._min[0], bb._min[1], bb._min[2]], [bb._min[0], bb._min[1], bb._max[2]]);
+
 
     result.set(isVis);
     next.trigger();
