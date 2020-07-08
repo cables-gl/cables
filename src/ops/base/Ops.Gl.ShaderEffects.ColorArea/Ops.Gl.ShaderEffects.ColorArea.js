76541664b01498c0cc3ecea55d98660cbe2beeaf
pandrr <tom@undev.de>
2020-07-08 16:46:53 +0200

    cleanup

--- src/ops/base/Ops.Gl.ShaderEffects.ColorArea/Ops.Gl.ShaderEffects.ColorArea.js
+++ src/ops/base/Ops.Gl.ShaderEffects.ColorArea/Ops.Gl.ShaderEffects.ColorArea.js
@@ -1,175 +1,174 @@
-const cgl=op.patch.cgl;
+const cgl = op.patch.cgl;
 
-op.render=op.inTrigger("render");
-op.trigger=op.outTrigger("trigger");
+op.render = op.inTrigger("render");
+op.trigger = op.outTrigger("trigger");
 
-const inArea=op.inValueSelect("Area",["Sphere","Axis X","Axis Y","Axis Z","Axis X Infinite","Axis Y Infinite","Axis Z Infinite"],"Sphere");
+const inArea = op.inValueSelect("Area", ["Sphere", "Axis X", "Axis Y", "Axis Z", "Axis X Infinite", "Axis Y Infinite", "Axis Z Infinite"], "Sphere");
 
-const inSize=op.inValue("Size",1);
-const inAmount=op.inValueSlider("Amount",0.5);
+const inSize = op.inValue("Size", 1);
+const inAmount = op.inValueSlider("Amount", 0.5);
 
-const inFalloff=op.inValueSlider("Falloff",0);
-const inInvert=op.inValueBool("Invert");
-var inBlend=op.inSwitch("Blend ",["Normal","Multiply"],"Normal");
+const inFalloff = op.inValueSlider("Falloff", 0);
+const inInvert = op.inValueBool("Invert");
+let inBlend = op.inSwitch("Blend ", ["Normal", "Multiply"], "Normal");
 
 const r = op.inValueSlider("r", Math.random());
 const g = op.inValueSlider("g", Math.random());
 const b = op.inValueSlider("b", Math.random());
-r.setUiAttribs({ colorPick: true });
+r.setUiAttribs({ "colorPick": true });
 
-const x=op.inValue("x");
-const y=op.inValue("y");
-const z=op.inValue("z");
+const x = op.inValue("x");
+const y = op.inValue("y");
+const z = op.inValue("z");
 
-const sizeX=op.inValueSlider("Size X",1);
+const sizeX = op.inValueSlider("Size X", 1);
 
-op.setPortGroup("Position",[x,y,z]);
-op.setPortGroup("Color",[inBlend,r,g,b]);
+op.setPortGroup("Position", [x, y, z]);
+op.setPortGroup("Color", [inBlend, r, g, b]);
 
-const inWorldSpace=op.inValueBool("WorldSpace",true);
+const inWorldSpace = op.inValueBool("WorldSpace", true);
 
-var shader=null;
+let shader = null;
 
-var srcHeadVert=''
-    .endl()+'OUT vec4 MOD_areaPos;'
+let srcHeadVert = ""
+    .endl() + "OUT vec4 MOD_areaPos;"
     .endl();
 
-var srcBodyVert=''
-    .endl()+'#ifndef MOD_WORLDSPACE'
-    .endl()+'   MOD_areaPos=pos;'
-    .endl()+'#endif'
-    .endl()+'#ifdef MOD_WORLDSPACE'
-    .endl()+'   MOD_areaPos=mMatrix*pos;'
-    .endl()+'#endif'
+let srcBodyVert = ""
+    .endl() + "#ifndef MOD_WORLDSPACE"
+    .endl() + "   MOD_areaPos=pos;"
+    .endl() + "#endif"
+    .endl() + "#ifdef MOD_WORLDSPACE"
+    .endl() + "   MOD_areaPos=mMatrix*pos;"
+    .endl() + "#endif"
     .endl();
 
-var moduleFrag=null;
-var moduleVert=null;
+let moduleFrag = null;
+let moduleVert = null;
 
-op.render.onLinkChanged=removeModule;
-inWorldSpace.onChange=updateWorldspace;
-inArea.onChange=updateArea;
-inInvert.onChange=updateInvert;
-inBlend.onChange=updateBlend;
+op.render.onLinkChanged = removeModule;
+inWorldSpace.onChange = updateWorldspace;
+inArea.onChange = updateArea;
+inInvert.onChange = updateInvert;
+inBlend.onChange = updateBlend;
 
 function updateBlend()
 {
-    if(!shader)return;
+    if (!shader) return;
 
-    shader.removeDefine(moduleVert.prefix+"BLEND_NORMAL");
-    shader.removeDefine(moduleVert.prefix+"BLEND_MULTIPLY");
+    shader.removeDefine(moduleVert.prefix + "BLEND_NORMAL");
+    shader.removeDefine(moduleVert.prefix + "BLEND_MULTIPLY");
 
-    if(inBlend.get()=="Normal") shader.define(moduleVert.prefix+"BLEND_NORMAL");
-        else shader.define(moduleVert.prefix+"BLEND_MULTIPLY");
+    if (inBlend.get() == "Normal") shader.define(moduleVert.prefix + "BLEND_NORMAL");
+    else shader.define(moduleVert.prefix + "BLEND_MULTIPLY");
 }
 
 
 function updateInvert()
 {
-    if(!shader)return;
-    if(inInvert.get()) shader.define(moduleVert.prefix+"AREA_INVERT");
-        else shader.removeDefine(moduleVert.prefix+"AREA_INVERT");
+    if (!shader) return;
+    if (inInvert.get()) shader.define(moduleVert.prefix + "AREA_INVERT");
+    else shader.removeDefine(moduleVert.prefix + "AREA_INVERT");
 }
 
 function updateArea()
 {
-    if(!shader)return;
+    if (!shader) return;
 
-    shader.removeDefine(moduleVert.prefix+"AREA_AXIS_X");
-    shader.removeDefine(moduleVert.prefix+"AREA_AXIS_Y");
-    shader.removeDefine(moduleVert.prefix+"AREA_AXIS_Z");
-    shader.removeDefine(moduleVert.prefix+"AREA_AXIS_X_INFINITE");
-    shader.removeDefine(moduleVert.prefix+"AREA_AXIS_Y_INFINITE");
-    shader.removeDefine(moduleVert.prefix+"AREA_AXIS_Z_INFINITE");
-    shader.removeDefine(moduleVert.prefix+"AREA_SPHERE");
-    if(inArea.get()=="Axis X")shader.define(moduleVert.prefix+"AREA_AXIS_X");
-    else if(inArea.get()=="Axis Y")shader.define(moduleVert.prefix+"AREA_AXIS_Y");
-    else if(inArea.get()=="Axis Z")shader.define(moduleVert.prefix+"AREA_AXIS_Z");
+    shader.removeDefine(moduleVert.prefix + "AREA_AXIS_X");
+    shader.removeDefine(moduleVert.prefix + "AREA_AXIS_Y");
+    shader.removeDefine(moduleVert.prefix + "AREA_AXIS_Z");
+    shader.removeDefine(moduleVert.prefix + "AREA_AXIS_X_INFINITE");
+    shader.removeDefine(moduleVert.prefix + "AREA_AXIS_Y_INFINITE");
+    shader.removeDefine(moduleVert.prefix + "AREA_AXIS_Z_INFINITE");
+    shader.removeDefine(moduleVert.prefix + "AREA_SPHERE");
+    if (inArea.get() == "Axis X")shader.define(moduleVert.prefix + "AREA_AXIS_X");
+    else if (inArea.get() == "Axis Y")shader.define(moduleVert.prefix + "AREA_AXIS_Y");
+    else if (inArea.get() == "Axis Z")shader.define(moduleVert.prefix + "AREA_AXIS_Z");
 
-    else if(inArea.get()=="Axis X Infinite")shader.define(moduleVert.prefix+"AREA_AXIS_X_INFINITE");
-    else if(inArea.get()=="Axis Y Infinite")shader.define(moduleVert.prefix+"AREA_AXIS_Y_INFINITE");
-    else if(inArea.get()=="Axis Z Infinite")shader.define(moduleVert.prefix+"AREA_AXIS_Z_INFINITE");
-    else shader.define(moduleVert.prefix+"AREA_SPHERE");
+    else if (inArea.get() == "Axis X Infinite")shader.define(moduleVert.prefix + "AREA_AXIS_X_INFINITE");
+    else if (inArea.get() == "Axis Y Infinite")shader.define(moduleVert.prefix + "AREA_AXIS_Y_INFINITE");
+    else if (inArea.get() == "Axis Z Infinite")shader.define(moduleVert.prefix + "AREA_AXIS_Z_INFINITE");
+    else shader.define(moduleVert.prefix + "AREA_SPHERE");
 }
 
 function updateWorldspace()
 {
-    if(!shader)return;
-    if(inWorldSpace.get()) shader.define(moduleVert.prefix+"WORLDSPACE");
-        else shader.removeDefine(moduleVert.prefix+"WORLDSPACE");
+    if (!shader) return;
+    if (inWorldSpace.get()) shader.define(moduleVert.prefix + "WORLDSPACE");
+    else shader.removeDefine(moduleVert.prefix + "WORLDSPACE");
 }
 
 function removeModule()
 {
-    if(shader && moduleFrag) shader.removeModule(moduleFrag);
-    if(shader && moduleVert) shader.removeModule(moduleVert);
-    shader=null;
+    if (shader && moduleFrag) shader.removeModule(moduleFrag);
+    if (shader && moduleVert) shader.removeModule(moduleVert);
+    shader = null;
 }
 
-op.render.onTriggered=function()
+op.render.onTriggered = function ()
 {
-
-    if(CABLES.UI)
+    if (CABLES.UI)
     {
         cgl.pushModelMatrix();
         mat4.identity(cgl.mMatrix);
-        if(op.isCurrentUiOp())
+        if (op.isCurrentUiOp())
             gui.setTransformGizmo(
                 {
-                    posX:x,
-                    posY:y,
-                    posZ:z
+                    "posX": x,
+                    "posY": y,
+                    "posZ": z
                 });
 
-        if(CABLES.UI.renderHelper ||op.isCurrentUiOp())
+        if (CABLES.UI.renderHelper || op.isCurrentUiOp())
         {
-            mat4.translate(cgl.mMatrix,cgl.mMatrix,[x.get(),y.get(),z.get()]);
-            CABLES.GL_MARKER.drawSphere(op,inSize.get());
+            mat4.translate(cgl.mMatrix, cgl.mMatrix, [x.get(), y.get(), z.get()]);
+            CABLES.GL_MARKER.drawSphere(op, inSize.get());
         }
         cgl.popModelMatrix();
     }
 
-    if(!cgl.getShader())
+    if (!cgl.getShader())
     {
-         op.trigger.trigger();
-         return;
+        op.trigger.trigger();
+        return;
     }
 
-    if(cgl.getShader()!=shader)
+    if (cgl.getShader() != shader)
     {
-        if(shader) removeModule();
-        shader=cgl.getShader();
+        if (shader) removeModule();
+        shader = cgl.getShader();
 
-        moduleVert=shader.addModule(
+        moduleVert = shader.addModule(
             {
-                priority:2,
-                title:op.objName,
-                name:'MODULE_VERTEX_POSITION',
-                srcHeadVert:srcHeadVert,
-                srcBodyVert:srcBodyVert
+                "priority": 2,
+                "title": op.objName,
+                "name": "MODULE_VERTEX_POSITION",
+                "srcHeadVert": srcHeadVert,
+                "srcBodyVert": srcBodyVert
             });
 
-        moduleFrag=shader.addModule(
+        moduleFrag = shader.addModule(
             {
-                title:op.objName,
-                name:'MODULE_COLOR',
-                srcHeadFrag:attachments.colorarea_head_frag,
-                srcBodyFrag:attachments.colorarea_frag
-            },moduleVert);
+                "title": op.objName,
+                "name": "MODULE_COLOR",
+                "srcHeadFrag": attachments.colorarea_head_frag,
+                "srcBodyFrag": attachments.colorarea_frag
+            }, moduleVert);
 
-        inSize.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'size',inSize);
-        inAmount.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'amount',inAmount);
+        inSize.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "size", inSize);
+        inAmount.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "amount", inAmount);
 
-        r.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'r',r);
-        g.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'g',g);
-        b.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'b',b);
+        r.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "r", r);
+        g.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "g", g);
+        b.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "b", b);
 
-        x.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'x',x);
-        y.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'y',y);
-        z.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'z',z);
-        sizeX.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'sizeX',sizeX);
+        x.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "x", x);
+        y.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "y", y);
+        z.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "z", z);
+        sizeX.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "sizeX", sizeX);
 
-        inFalloff.uniform=new CGL.Uniform(shader,'f',moduleFrag.prefix+'falloff',inFalloff);
+        inFalloff.uniform = new CGL.Uniform(shader, "f", moduleFrag.prefix + "falloff", inFalloff);
 
         updateWorldspace();
         updateArea();
@@ -177,21 +176,8 @@ op.render.onTriggered=function()
         updateBlend();
     }
 
-    if(!shader)return;
-    var texSlot=moduleVert.num+5;
+    if (!shader) return;
+    let texSlot = moduleVert.num + 5;
 
     op.trigger.trigger();
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
