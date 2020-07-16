281ccdd61d3c4b66a860f5b6efa88c334a873b6f
Siamend Darwesh <simo@undev.de>
2020-07-16 15:43:24 +0200

    WIP: add seperate initTex & initFramebuffer methods

--- src/libs/cgl/cubemapframebuffer.js
+++ src/libs/cgl/cubemapframebuffer.js
@@ -3,11 +3,51 @@ class CubemapFramebuffer
     constructor(cgl, width, height, options)
     {
         this._cgl = cgl;
+        this._cubemapProperties = [
+            // targets for use in some gl functions for working with cubemaps
+            {
+                "face": this._cgl.gl.TEXTURE_CUBE_MAP_POSITIVE_X,
+                "lookAt": vec3.fromValues(1.0, 0.0, 0.0),
+                "up": vec3.fromValues(0.0, -1.0, 0.0),
+            },
+            {
+                "face": this._cgl.gl.TEXTURE_CUBE_MAP_NEGATIVE_X,
+                "lookAt": vec3.fromValues(-1.0, 0.0, 0.0),
+                "up": vec3.fromValues(0.0, -1.0, 0.0),
+            },
+            {
+                "face": this._cgl.gl.TEXTURE_CUBE_MAP_POSITIVE_Y,
+                "lookAt": vec3.fromValues(0.0, 1.0, 0.0),
+                "up": vec3.fromValues(0.0, 0.0, 1.0),
+            },
+            {
+                "lookAt": vec3.fromValues(0.0, -1.0, 0.0),
+                "up": vec3.fromValues(0.0, 0.0, -1.0),
+                "face": this._cgl.gl.TEXTURE_CUBE_MAP_NEGATIVE_Y,
+            },
+            {
+                "face": this._cgl.gl.TEXTURE_CUBE_MAP_POSITIVE_Z,
+                "lookAt": vec3.fromValues(0.0, 0.0, 1.0),
+                "up": vec3.fromValues(0.0, -1.0, 0.0),
+            },
+            {
+                "face": this._cgl.gl.TEXTURE_CUBE_MAP_NEGATIVE_Z,
+                "lookAt": vec3.fromValues(0.0, 0.0, -1.0),
+                "up": vec3.fromValues(0.0, -1.0, 0.0),
+            },
+        ];
 
         this._depthRenderbuffer = null;
         this._framebuffer = null;
+        this._depthbuffer = null;
         this._textureFrameBuffer = null;
         this._textureDepth = null;
+
+        // * TEST PARAMETERS
+        this.texture = null;
+        this.glFramebuffer = null;
+        this.glRenderbuffer = null;
+
 
         this._options = options || {
             "isFloatingPointTexture": false
@@ -32,6 +72,61 @@ class CubemapFramebuffer
         if (!this._options.hasOwnProperty("filter")) this._options.filter = CGL.Texture.FILTER_LINEAR;
 
         this._colorTexture = this._cgl.gl.createTexture();
+    }
+
+    initializeRenderbuffers()
+    {
+        this._framebuffer = this._cgl.gl.createFramebuffer(); // crate the framebuffer that will draw to the reflection map
+        this._depthbuffer = this._cgl.gl.createRenderbuffer(); // renderbuffer for depth buffer in framebuffer
+
+        this._cgl.gl.bindFramebuffer(this._cgl.gl.FRAMEBUFFER, this._framebuffer); // select the framebuffer, so we can attach the depth buffer to it
+        this._cgl.gl.bindRenderbuffer(this._cgl.gl.RENDERBUFFER, this._depthbuffer); // so we can create storage for the depthBuffer
+
+        this._cgl.gl.renderbufferStorage(this._cgl.gl.RENDERBUFFER, this._cgl.gl.DEPTH_COMPONENT16, this._width, this._height);
+        this._cgl.gl.framebufferRenderbuffer(this._cgl.gl.FRAMEBUFFER, this._cgl.gl.DEPTH_ATTACHMENT, this._cgl.gl.RENDERBUFFER, this._depthbuffer);
+
+        this._cgl.gl.bindRenderbuffer(this._cgl.gl.RENDERBUFFER, null);
+        this._cgl.gl.bindFramebuffer(this._cgl.gl.FRAMEBUFFER, null);
+    }
+
+    initializeTexture()
+    {
+        this.texture = this._cgl.gl.createTexture(); // Create the texture object for the reflection map
+        this._cgl.gl.bindTexture(this._cgl.gl.TEXTURE_CUBE_MAP, this.cubemap); // create storage for the reflection map images
+
+        // * TODO: add filter & wrap type switch
+        this._cgl.gl.texParameteri(this._cgl.gl.TEXTURE_CUBE_MAP, this._cgl.gl.TEXTURE_MIN_FILTER, this._cgl.gl.LINEAR);
+        this._cgl.gl.texParameteri(this._cgl.gl.TEXTURE_CUBE_MAP, this._cgl.gl.TEXTURE_MAG_FILTER, this._cgl.gl.LINEAR);
+        this._cgl.gl.texParameteri(this._cgl.gl.TEXTURE_CUBE_MAP, this._cgl.gl.TEXTURE_WRAP_S, this._cgl.gl.CLAMP_TO_EDGE);
+        this._cgl.gl.texParameteri(this._cgl.gl.TEXTURE_CUBE_MAP, this._cgl.gl.TEXTURE_WRAP_T, this._cgl.gl.CLAMP_TO_EDGE);
+
+        for (let i = 0; i < 6; i++)
+        {
+            if (this._cgl.glVersion == 1)
+            {
+                if (this._cgl.glUseHalfFloatTex)
+                {
+                    const ext = this._cgl.gl.getExtension("OES_texture_half_float");
+                    if (this._cgl.glVersion == 1 && !ext) throw new Error("no half float texture extension");
+
+                    this._cgl.gl.texImage2D(this._cubemapProperties[i].face, 0, this._cgl.gl.RGBA, this._width, this._height, 0, this._cgl.gl.RGBA, ext.HALF_FLOAT_OES, null);
+                }
+                else
+                {
+                    const ext = this._cgl.gl.getExtension("OES_texture_float");
+
+                    this._cgl.gl.texImage2D(this._cubemapProperties[i].face, 0, this._cgl.gl.RGBA, this._width, this._height, 0, this._cgl.gl.RGBA, this._cgl.gl.FLOAT, null);
+                }
+            }
+            else this._cgl.gl.texImage2D(this._cubemapProperties[i].face, 0, this._cgl.gl.RGBA, this._width, this._height, 0, this._cgl.gl.RGBA, this._cgl.gl.UNSIGNED_BYTE, null);
+            // * NOTE: was gl.RGBA32F && gl.FLOAT instead of gl.RGBA && gl.UNSIGNED_BYTE
+        }
+
+        this._cgl.gl.bindTexture(this._cgl.gl.TEXTURE_CUBE_MAP, null);
+        this._cgl.gl.bindRenderbuffer(this._cgl.gl.RENDERBUFFER, null);
+        this._cgl.gl.bindFramebuffer(this._cgl.gl.FRAMEBUFFER, null);
+
+        this.isInitialized = true;
     }
 
     getWidth()
@@ -111,15 +206,13 @@ class CubemapFramebuffer
     renderStart()
     {
         this._cgl.pushModelMatrix();
+        this._cgl.pushPMatrix();
+
         this._cgl.gl.bindFramebuffer(this._cgl.gl.FRAMEBUFFER, this._framebuffer);
+        this._cgl.gl.bindRenderbuffer(this._cgl.gl.RENDERBUFFER, this._depthbuffer);
+        this._cgl.gl.viewport(0, 0, this._width, this._height);
         this._cgl.pushGlFrameBuffer(this._framebuffer);
         this._cgl.pushFrameBuffer(this);
-
-        this._cgl.pushPMatrix();
-        this._cgl.gl.viewport(0, 0, this._width, this._height);
-
-        // TODO: draw code for cubemap here?
-        this._cgl.gl.drawBuffers(this._drawTargetArray);
 
         if (this._options.clear)
         {
@@ -137,7 +230,9 @@ class CubemapFramebuffer
         this._cgl.gl.bindFramebuffer(this._cgl.gl.DRAW_FRAMEBUFFER, this._textureFrameBuffer);
 
         this._cgl.gl.clearBufferfv(this._cgl.gl.COLOR, 0, [0.0, 0.0, 0.0, 1.0]);
-        this._cgl.gl.blitFramebuffer(0, 0, this._width, this._height, 0, 0, this._width, this._height, this._cgl.gl.COLOR_BUFFER_BIT | this._cgl.gl.DEPTH_BUFFER_BIT, this._cgl.gl.NEAREST);
+
+        // * NOTE: webgl2 only
+        // this._cgl.gl.blitFramebuffer(0, 0, this._width, this._height, 0, 0, this._width, this._height, this._cgl.gl.COLOR_BUFFER_BIT | this._cgl.gl.DEPTH_BUFFER_BIT, this._cgl.gl.NEAREST);
 
         this._cgl.gl.bindFramebuffer(this._cgl.gl.FRAMEBUFFER, this._cgl.popGlFrameBuffer());
         this._cgl.popFrameBuffer();
