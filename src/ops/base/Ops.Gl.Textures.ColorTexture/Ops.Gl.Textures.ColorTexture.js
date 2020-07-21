12195d9a4b7a8b834f1f362d63cb00c7be91fde4
pandrr <tom@undev.de>
2020-07-21 14:47:29 +0200

    dev changes / previewtexture aspect ratio fixes

--- src/ops/base/Ops.Gl.Textures.ColorTexture/Ops.Gl.Textures.ColorTexture.js
+++ src/ops/base/Ops.Gl.Textures.ColorTexture/Ops.Gl.Textures.ColorTexture.js
@@ -2,31 +2,31 @@ const r = op.inValueSlider("r", Math.random());
 const g = op.inValueSlider("g", Math.random());
 const b = op.inValueSlider("b", Math.random());
 const a = op.inValueSlider("a", 1.0);
-const texOut=op.outTexture("texture_out");
+const texOut = op.outTexture("texture_out");
 
-r.setUiAttribs({ colorPick: true });
-const cgl=op.patch.cgl;
+r.setUiAttribs({ "colorPick": true });
+const cgl = op.patch.cgl;
 
-var fb=null;
+let fb = null;
 
-r.onChange=
-    g.onChange=
-    b.onChange=
-    a.onChange=render;
+r.onChange =
+    g.onChange =
+    b.onChange =
+    a.onChange = cgl.addNextFrameOnceCallback(render);
 
-render();
+cgl.addNextFrameOnceCallback(render);
 
 function render()
 {
-    if(!fb)
+    if (!fb)
     {
-        if(cgl.glVersion==1) fb=new CGL.Framebuffer(cgl,4,4);
-            else fb=new CGL.Framebuffer2(cgl,4,4);
+        if (cgl.glVersion == 1) fb = new CGL.Framebuffer(cgl, 4, 4);
+        else fb = new CGL.Framebuffer2(cgl, 4, 4);
         fb.setFilter(CGL.Texture.FILTER_MIPMAP);
     }
 
     fb.renderStart();
-    cgl.gl.clearColor(r.get(),g.get(),b.get(),a.get());
+    cgl.gl.clearColor(r.get(), g.get(), b.get(), a.get());
     cgl.gl.clear(cgl.gl.COLOR_BUFFER_BIT);
     fb.renderEnd();
 
