bfa982da0a46976c7fb0c3f8a62a5a1ac68b7fd1
pandrr <tom@undev.de>
2020-07-28 13:04:28 +0200

    all cameras dont work on viewmatrix in shadowpass

--- src/ops/base/Ops.Gl.Matrix.LookatCamera/Ops.Gl.Matrix.LookatCamera.js
+++ src/ops/base/Ops.Gl.Matrix.LookatCamera/Ops.Gl.Matrix.LookatCamera.js
@@ -1,19 +1,19 @@
-var render=op.inTrigger('render');
-var trigger=op.outTrigger('trigger');
+const render = op.inTrigger("render");
+const trigger = op.outTrigger("trigger");
 
-var eyeX=op.inValueFloat("eyeX");
-var eyeY=op.inValueFloat("eyeY");
-var eyeZ=op.inValueFloat("eyeZ");
+const eyeX = op.inValueFloat("eyeX");
+const eyeY = op.inValueFloat("eyeY");
+const eyeZ = op.inValueFloat("eyeZ");
 
-var centerX=op.inValueFloat("centerX");
-var centerY=op.inValueFloat("centerY");
-var centerZ=op.inValueFloat("centerZ");
+const centerX = op.inValueFloat("centerX");
+const centerY = op.inValueFloat("centerY");
+const centerZ = op.inValueFloat("centerZ");
 
-var vecUpX=op.inValueFloat("upX");
-var vecUpY=op.inValueFloat("upY");
-var vecUpZ=op.inValueFloat("upZ");
+const vecUpX = op.inValueFloat("upX");
+const vecUpY = op.inValueFloat("upY");
+const vecUpZ = op.inValueFloat("upZ");
 
-var outArr=op.outArray("Array");
+const outArr = op.outArray("Array");
 
 centerX.set(0);
 centerY.set(0);
@@ -27,51 +27,53 @@ vecUpX.set(0);
 vecUpY.set(1);
 vecUpZ.set(0);
 
-var cgl=op.patch.cgl;
-var vUp=vec3.create();
-var vEye=vec3.create();
-var vCenter=vec3.create();
-var transMatrix = mat4.create();
+const cgl = op.patch.cgl;
+const vUp = vec3.create();
+const vEye = vec3.create();
+const vCenter = vec3.create();
+const transMatrix = mat4.create();
 mat4.identity(transMatrix);
 
-var arr=[];
+const arr = [];
 
 
-render.onTriggered=function()
+render.onTriggered = function ()
 {
+    if (cgl.frameStore.shadowPass) return trigger.trigger();
 
-    if(op.isCurrentUiOp())
+
+    if (op.isCurrentUiOp())
         gui.setTransformGizmo(
             {
-                posX:eyeX,
-                posY:eyeY,
-                posZ:eyeZ
+                "posX": eyeX,
+                "posY": eyeY,
+                "posZ": eyeZ
             });
 
 
     cgl.pushViewMatrix();
 
-    arr[0]=eyeX.get();
-    arr[1]=eyeY.get();
-    arr[2]=eyeZ.get();
+    arr[0] = eyeX.get();
+    arr[1] = eyeY.get();
+    arr[2] = eyeZ.get();
 
-    arr[3]=centerX.get();
-    arr[4]=centerY.get();
-    arr[5]=centerZ.get();
+    arr[3] = centerX.get();
+    arr[4] = centerY.get();
+    arr[5] = centerZ.get();
 
-    arr[6]=vecUpX.get();
-    arr[7]=vecUpY.get();
-    arr[8]=vecUpZ.get();
+    arr[6] = vecUpX.get();
+    arr[7] = vecUpY.get();
+    arr[8] = vecUpZ.get();
     outArr.set(null);
     outArr.set(arr);
 
-    vec3.set(vUp, vecUpX.get(),vecUpY.get(),vecUpZ.get());
-    vec3.set(vEye, eyeX.get(),eyeY.get(),eyeZ.get());
-    vec3.set(vCenter, centerX.get(),centerY.get(),centerZ.get());
+    vec3.set(vUp, vecUpX.get(), vecUpY.get(), vecUpZ.get());
+    vec3.set(vEye, eyeX.get(), eyeY.get(), eyeZ.get());
+    vec3.set(vCenter, centerX.get(), centerY.get(), centerZ.get());
 
     mat4.lookAt(transMatrix, vEye, vCenter, vUp);
 
-    mat4.multiply(cgl.vMatrix,cgl.vMatrix,transMatrix);
+    mat4.multiply(cgl.vMatrix, cgl.vMatrix, transMatrix);
 
     trigger.trigger();
     cgl.popViewMatrix();
