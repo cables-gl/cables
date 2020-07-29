66a3a34bba04361ad968054a965967bb2d712269
pandrr <tom@undev.de>
2020-07-29 11:40:31 +0200

    fix physicsbodies scaling

--- src/ops/base/Ops.Array.ArrayMathExpression/Ops.Array.ArrayMathExpression.js
+++ src/ops/base/Ops.Array.ArrayMathExpression/Ops.Array.ArrayMathExpression.js
@@ -5,11 +5,15 @@ const inC = op.inArray("C");
 const inX = op.inFloat("X", 1);
 const inY = op.inFloat("Y", 1);
 const inZ = op.inFloat("Z", 1);
+
 op.setPortGroup("Parameters", [inA, inB, inC, inX, inY, inZ]);
 const inEquation = op.inString("Equation", "a*(b+c+d)");
 op.setPortGroup("Equation", [inEquation]);
-const outResult = op.outArray("Result");
+const outResultFloat = op.outNumber("Result Number");
+const outResultArray = op.outArray("Result Array");
 const outLength = op.outNumber("Array Length");
+const outCurrentIndex = op.outNumber("Current Index");
+const outResultPerIndex = op.outNumber("Result per Index");
 const outEquationIsValid = op.outBool("Equation Valid");
 const outEquation = op.outString("Equation");
 
@@ -37,33 +41,37 @@ const createFunction = () =>
 
 const resultArray = [];
 let showingError = false;
-let showingErrorLength = false;
+const showingErrorLength = false;
 
 const evaluateFunction = () =>
 {
     const arrayA = inA.get();
     const arrayB = inB.get();
     const arrayC = inC.get();
-
     const arrays = [arrayA, arrayB, arrayC];
 
+    const x = inX.get();
+    const y = inY.get();
+    const z = inZ.get();
+
     // * check if we have at least 2 arrays that are valid
-    if (arrays.filter(Boolean).length < 2)
+    if (arrays.filter(Boolean).length === 0)
     {
-        outResult.set(null);
+        outResultArray.set(null);
         outLength.set(0);
-        if (!showingErrorLength) op.uiAttr({ "error": "Cannot calculate with less than two arrays !" });
-        showingErrorLength = true;
+
+
+        outResultArray.set(null);
+        outCurrentIndex.set(0);
+        if (functionValid)
+            outResultFloat.set(
+                currentFunction(Math, null, null, null, x, y, z)
+            );
+
         return;
     }
     else
     {
-        if (showingErrorLength)
-        {
-            op.uiAttr({ "error": null });
-            showingErrorLength = false;
-        }
-
         const arrayLengths = arrays.map((arr) => (arr ? arr.length : undefined));
         const firstValidArrayLength = arrayLengths.find(Boolean);
         const sameLength = arrayLengths.filter(Boolean).every((length) => length === firstValidArrayLength);
@@ -83,11 +91,10 @@ const evaluateFunction = () =>
 
             validArrays = arrays.map((arr, index) =>
             {
+                // * map all undefined arrays to 0 values
                 if (!arr)
-                {
-                    // * map all undefined arrays to 0 values
                     arr = arrays.find(Boolean).map((x) => 0);
-                }
+
                 return arr;
             });
 
@@ -97,17 +104,29 @@ const evaluateFunction = () =>
             {
                 for (let i = 0; i < firstValidArray.length; i += 1)
                 {
-                    resultArray[i] = currentFunction(Math, validArrays[0][i], validArrays[1][i], validArrays[2][i], validArrays[3][i]);
+                    resultArray[i] = currentFunction(
+                        Math,
+                        validArrays[0][i],
+                        validArrays[1][i],
+                        validArrays[2][i],
+                        x,
+                        y,
+                        z
+                    );
+
+                    outCurrentIndex.set(i);
+                    outResultPerIndex.set(resultArray[i]);
                 }
-                outResult.set(null);
-                outResult.set(resultArray);
+                outResultArray.set(null);
+                outResultArray.set(resultArray);
                 outLength.set(resultArray.length);
             }
         }
         else
         {
-            outResult.set(null);
+            outResultArray.set(null);
             outLength.set(0);
+            outCurrentIndex.set(0);
             op.uiAttr({ "error": "Arrays do not have the same length !" });
             showingError = true;
         }
@@ -117,5 +136,6 @@ const evaluateFunction = () =>
 };
 
 
-inA.onChange = inB.onChange = inC.onChange = evaluateFunction;
+inA.onChange = inB.onChange = inC.onChange
+= inX.onChange = inY.onChange = inZ.onChange = evaluateFunction;
 inEquation.onChange = createFunction;
