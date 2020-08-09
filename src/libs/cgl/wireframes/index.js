ab8c5d012d2e58e0745aa7b75d933406e9353bbd
Stephan Maihoefer <stephan@undev.de>
2020-08-09 17:03:38 +0200

    case sensitive

--- src/libs/cgl/wireframes/index.js
+++ src/libs/cgl/wireframes/index.js
@@ -1,5 +1,5 @@
 import { WireframeCube } from "./cgl_wireframecube";
-import { WireframeRect } from "./cgl_wireframerect";
+import { WireframeRect } from "./cgl_wireframeRect";
 
 CGL.WireframeCube = WireframeCube;
 CGL.WireframeRect = WireframeRect;
