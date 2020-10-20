7e811d3f13b315046c162825729deabf74941c83
pandrr <tom@undev.de>
2020-10-20 08:38:35 +0200

    new op: VertexPositionFromTextur_v2

---
+++ src/ops/base/Ops.Gl.ShaderEffects.VertexPositionFromTexture_v2/Ops.Gl.ShaderEffects.VertexPositionFromTexture_v2.js
@@ -0,0 +1,42 @@
+const
+    render = op.inTrigger("render"),
+    inTex = op.inTexture("Texture"),
+    inMode = op.inSwitch("Mode", ["Absolute", "Add"], "Absolute"),
+    trigger = op.outTrigger("Trigger");
+
+const cgl = op.patch.cgl;
+// let inTexUniform = null;
+
+const mod = new CGL.ShaderModifier(cgl, op.name);
+mod.addModule({
+    "priority": 2,
+    "title": op.name,
+    "name": "MODULE_VERTEX_POSITION",
+    srcHeadVert:"",
+    srcBodyVert:attachments.vertposbody_vert
+});
+
+mod.addUniformVert("t", "MOD_tex");
+
+
+inMode.onChange = updateDefines;
+
+render.onTriggered = doRender;
+
+function updateDefines()
+{
+    mod.toggleDefine("MOD_ADD", inMode.get() == "Add");
+    mod.toggleDefine("MOD_ABS", inMode.get() == "Absolute");
+
+}
+
+
+function doRender()
+{
+
+    mod.bind();
+    if(inTex.get())mod.pushTexture("MOD_tex", inTex.get().tex);
+
+    trigger.trigger();
+    mod.unbind();
+}
