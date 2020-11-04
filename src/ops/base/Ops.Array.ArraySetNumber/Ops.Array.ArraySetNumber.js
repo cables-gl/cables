76470b586583ca3bc9ed896e359e63416e866d96
pandur <tom@undev.de>
2020-11-04 11:29:38 +0100

    remove log

--- src/ops/base/Ops.Array.ArraySetNumber/Ops.Array.ArraySetNumber.js
+++ src/ops/base/Ops.Array.ArraySetNumber/Ops.Array.ArraySetNumber.js
@@ -1,57 +1,51 @@
 const
-    inTrigger=op.inTriggerButton("Execute"),
-    inArray=op.inArray("Array"),
-    inIndex=op.inInt("Index",0),
-    inValue=op.inFloat("Number",1),
-    inReset=op.inTriggerButton("Reset"),
-    outArray=op.outArray("Result");
+    inTrigger = op.inTriggerButton("Execute"),
+    inArray = op.inArray("Array"),
+    inIndex = op.inInt("Index", 0),
+    inValue = op.inFloat("Number", 1),
+    inReset = op.inTriggerButton("Reset"),
+    outArray = op.outArray("Result");
 
-var newArr=[];
+const newArr = [];
 
-inReset.onTriggered=function(){
+inReset.onTriggered = function ()
+{
     copyArray(true);
 };
-inArray.onChange=copyArray;
+inArray.onChange = copyArray;
 
-inTrigger.onTriggered=
-function()
+inTrigger.onTriggered =
+function ()
 {
+    const arr = inArray.get();
 
-    const arr=inArray.get();
+    if (!arr) return;
+    if (newArr.length != arr.length)newArr.length = arr.length;
 
-    console.log({arr});
-    if(!arr)return;
-    if(newArr.length!=arr.length)newArr.length=arr.length;
+    const idx = Math.floor(inIndex.get());
 
-    const idx=Math.floor(inIndex.get());
-
-    if(idx>=0)
+    if (idx >= 0)
     {
-        newArr[idx]=inValue.get();
+        newArr[idx] = inValue.get();
     }
 
-    inArray.onChange=null;
+    inArray.onChange = null;
     outArray.set(null);
     outArray.set(newArr);
-
 };
 
 function copyArray(force)
 {
+    const arr = inArray.get();
 
-    const arr=inArray.get();
+    if (!arr) return;
 
-    if(!arr) return;
-
-    if(force===true || (arr && !outArray.get()))
+    if (force === true || (arr && !outArray.get()))
     {
-
-        if(newArr.length!=arr.length) newArr.length=arr.length;
-        for(var i=0;i<arr.length;i++) newArr[i]=arr[i];
+        if (newArr.length != arr.length) newArr.length = arr.length;
+        for (let i = 0; i < arr.length; i++) newArr[i] = arr[i];
 
         outArray.set(null);
         outArray.set(newArr);
-
     }
-
 }
