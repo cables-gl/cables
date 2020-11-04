30770d91ba645b781176968296f436425e5dd5e3
pandrr <tom@undev.de>
2020-11-04 11:29:44 +0100

    performance data

--- src/core/cgl/cgl_profiledata.js
+++ src/core/cgl/cgl_profiledata.js
@@ -1,27 +1,58 @@
-const ProfileData = function ()
+class ProfileData
 {
-    this.pause = false;
-    this.profileUniformCount = 0;
-    this.profileShaderBinds = 0;
-    this.profileUniformCount = 0;
-    this.profileShaderCompiles = 0;
-    this.profileVideosPlaying = 0;
-    this.profileMVPMatrixCount = 0;
-    this.profileEffectBuffercreate = 0;
-    this.profileShaderGetUniform = 0;
-    this.profileFrameBuffercreate = 0;
-    this.profileMeshSetGeom = 0;
-    this.profileTextureNew = 0;
-    this.profileGenMipMap = 0;
-    this.profileOnAnimFrameOps = 0;
+    constructor()
+    {
+        this._lastTime = 0;
+        this.pause = false;
+        this.profileUniformCount = 0;
+        this.profileShaderBinds = 0;
+        this.profileUniformCount = 0;
+        this.profileShaderCompiles = 0;
+        this.profileVideosPlaying = 0;
+        this.profileMVPMatrixCount = 0;
+        this.profileEffectBuffercreate = 0;
+        this.profileShaderGetUniform = 0;
+        this.profileFrameBuffercreate = 0;
+        this.profileMeshSetGeom = 0;
+        this.profileTextureNew = 0;
+        this.profileGenMipMap = 0;
+        this.profileOnAnimFrameOps = 0;
 
-    this.profileMainloopMs = 0;
-    this.profileMeshDraw = 0;
-    this.profileTextureEffect = 0;
-    this.profileTexPreviews = 0;
-    this.shaderCompileTime = 0;
-    this.profileMeshNumElements = 0;
-};
+        this.profileMainloopMs = 0;
+        this.profileMeshDraw = 0;
+        this.profileTextureEffect = 0;
+        this.profileTexPreviews = 0;
+        this.shaderCompileTime = 0;
+        this.profileMeshNumElements = 0;
+        this.profileMeshAttributes = 0;
+    }
+
+
+    clear()
+    {
+        this.profileMeshAttributes = 0;
+        this.profileUniformCount = 0;
+        this.profileShaderGetUniform = 0;
+        this.profileShaderCompiles = 0;
+        this.profileShaderBinds = 0;
+        this.profileTextureResize = 0;
+        this.profileFrameBuffercreate = 0;
+        this.profileEffectBuffercreate = 0;
+        this.profileTextureDelete = 0;
+        this.profileMeshSetGeom = 0;
+        this.profileVideosPlaying = 0;
+        this.profileMVPMatrixCount = 0;
+        this.profileNonTypedAttrib = 0;
+        this.profileNonTypedAttribNames = "";
+        this.profileTextureNew = 0;
+        this.profileGenMipMap = 0;
+        this.profileFramebuffer = 0;
+        this.profileMeshDraw = 0;
+        this.profileTextureEffect = 0;
+        this.profileTexPreviews = 0;
+        this.profileMeshNumElements = 0;
+    }
+}
 
 
 export const profileData = new ProfileData();
