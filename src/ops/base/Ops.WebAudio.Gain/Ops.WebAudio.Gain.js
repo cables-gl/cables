5998d3dc6422290812a49d674a46ca67c581bcfd
Siamend Darwesh <simo@undev.de>
2020-11-10 17:39:32 +0100

    Fix Gain node audio connect disconnect errors, update to new op port funcs

--- src/ops/base/Ops.WebAudio.Gain/Ops.WebAudio.Gain.js
+++ src/ops/base/Ops.WebAudio.Gain/Ops.WebAudio.Gain.js
@@ -1,37 +1,42 @@
-var audioIn=this.addInPort(new CABLES.Port(this,"audio in",CABLES.OP_PORT_TYPE_OBJECT));
-var gain=this.addInPort(new CABLES.Port(this,"gain",CABLES.OP_PORT_TYPE_VALUE));
-gain.onChange=updateGain;
-var audioOut=this.addOutPort(new CABLES.Port(this,"audio out",CABLES.OP_PORT_TYPE_OBJECT));
+const audioIn = op.inObject("audio in");
+const gain = op.inFloatSlider("Gain", 1);
+gain.onChange = updateGain;
+const audioOut = op.outObject("audio out");
 
-if(!window.audioContext){ audioContext = new AudioContext(); }
+if (!window.audioContext) { audioContext = new AudioContext(); }
 
-var gainNode = audioContext.createGain();
+const gainNode = audioContext.createGain();
 
-function updateGain(){
+function updateGain()
+{
     // gainNode.gain.value = parseFloat( gain.get() )||0;
     gainNode.gain.setValueAtTime(parseFloat(gain.get()) || 0, window.audioContext.currentTime);
 }
 
+let oldAudioIn = null;
 
-var oldAudioIn = null;
-
-audioIn.onChange=function()
+audioIn.onChange = function ()
 {
-    if (!audioIn.get()) {
-        if (oldAudioIn) {
-            try{
-                oldAudioIn.disconnect(gainNode);
-            } catch(e) {
-
-
+    if (!audioIn.get())
+    {
+        if (oldAudioIn)
+        {
+            try
+            {
+                if (oldAudioIn.disconnect) oldAudioIn.disconnect(gainNode);
+            }
+            catch (e)
+            {
                 console.log(e);
             }
         }
-    } else {
-        audioIn.val.connect(gainNode);
     }
-    oldAudioIn=audioIn.get();
+    else
+    {
+        if (audioIn.val.connect) audioIn.val.connect(gainNode);
+    }
+    oldAudioIn = audioIn.get();
 };
 
-gain.set( 1.0 );
-audioOut.set( gainNode );
+gain.set(1.0);
+audioOut.set(gainNode);
