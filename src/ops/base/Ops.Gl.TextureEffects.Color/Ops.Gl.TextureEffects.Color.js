04ce9053fa24cd4cb6deb4d7cc83f7cb52293966
pandrr <tom@undev.de>
2020-11-17 13:31:08 +0100

    new op alpha

--- src/ops/base/Ops.Gl.TextureEffects.Color/Ops.Gl.TextureEffects.Color.js
+++ src/ops/base/Ops.Gl.TextureEffects.Color/Ops.Gl.TextureEffects.Color.js
@@ -1,58 +1,57 @@
 const
-    render=op.inTrigger("render"),
-    blendMode=CGL.TextureEffect.AddBlendSelect(op,"Blend Mode","normal"),
-    amount=op.inValueSlider("Amount",1),
-    inMask=op.inTexture("Mask"),
-    inMaskInvert=op.inValueBool("Mask Invert"),
-    r=op.inValueSlider("r",Math.random()),
-    g=op.inValueSlider("g",Math.random()),
-    b=op.inValueSlider("b",Math.random()),
-    trigger=op.outTrigger("trigger");
+    render = op.inTrigger("render"),
+    blendMode = CGL.TextureEffect.AddBlendSelect(op, "Blend Mode", "normal"),
+    amount = op.inValueSlider("Amount", 1),
+    inMask = op.inTexture("Mask"),
+    inMaskInvert = op.inValueBool("Mask Invert"),
+    r = op.inValueSlider("r", Math.random()),
+    g = op.inValueSlider("g", Math.random()),
+    b = op.inValueSlider("b", Math.random()),
+    trigger = op.outTrigger("trigger");
 
-r.setUiAttribs({colorPick:true});
+r.setUiAttribs({ "colorPick": true });
 
-op.setPortGroup('Color',[r,g,b]);
+op.setPortGroup("Color", [r, g, b]);
 
-const TEX_SLOT=0;
-const cgl=op.patch.cgl;
-const shader=new CGL.Shader(cgl,'textureeffect color');
+const TEX_SLOT = 0;
+const cgl = op.patch.cgl;
+const shader = new CGL.Shader(cgl, "textureeffect color");
 
-var srcFrag=attachments.color_frag||'';
+const srcFrag = attachments.color_frag || "";
 
-shader.setSource(shader.getDefaultVertexShader(),srcFrag);
+shader.setSource(shader.getDefaultVertexShader(), srcFrag);
 
 const
-    textureUniform=new CGL.Uniform(shader,'t','tex',TEX_SLOT),
-    makstextureUniform=new CGL.Uniform(shader,'t','mask',1),
-    uniformR=new CGL.Uniform(shader,'f','r',r),
-    uniformG=new CGL.Uniform(shader,'f','g',g),
-    uniformB=new CGL.Uniform(shader,'f','b',b),
-    uniformAmount=new CGL.Uniform(shader,'f','amount',amount);
+    textureUniform = new CGL.Uniform(shader, "t", "tex", TEX_SLOT),
+    makstextureUniform = new CGL.Uniform(shader, "t", "mask", 1),
+    uniformR = new CGL.Uniform(shader, "f", "r", r),
+    uniformG = new CGL.Uniform(shader, "f", "g", g),
+    uniformB = new CGL.Uniform(shader, "f", "b", b),
+    uniformAmount = new CGL.Uniform(shader, "f", "amount", amount);
 
-inMask.onChange=function()
+inMask.onChange = function ()
 {
-    if(inMask.get())shader.define("MASK");
-        else shader.removeDefine("MASK");
+    if (inMask.get())shader.define("MASK");
+    else shader.removeDefine("MASK");
 };
 
-inMaskInvert.onChange=function()
+inMaskInvert.onChange = function ()
 {
-    if(inMaskInvert.get())shader.define("INVERTMASK");
-        else shader.removeDefine("INVERTMASK");
+    if (inMaskInvert.get())shader.define("INVERTMASK");
+    else shader.removeDefine("INVERTMASK");
 };
 
+CGL.TextureEffect.setupBlending(op, shader, blendMode, amount);
 
-CGL.TextureEffect.setupBlending(op,shader,blendMode,amount);
-
-render.onTriggered=function()
+render.onTriggered = function ()
 {
-    if(!CGL.TextureEffect.checkOpInEffect(op)) return;
+    if (!CGL.TextureEffect.checkOpInEffect(op)) return;
 
     cgl.pushShader(shader);
     cgl.currentTextureEffect.bind();
 
-    cgl.setTexture(TEX_SLOT, cgl.currentTextureEffect.getCurrentSourceTexture().tex );
-    if(inMask.get()) cgl.setTexture(1, inMask.get().tex );
+    cgl.setTexture(TEX_SLOT, cgl.currentTextureEffect.getCurrentSourceTexture().tex);
+    if (inMask.get()) cgl.setTexture(1, inMask.get().tex);
 
     cgl.currentTextureEffect.finish();
     cgl.popShader();
