a3bbeb6c64779e63aef5d0825e44ffc016c3c3f3
tom <tom@undev.de>
2020-11-19 09:43:48 +0100

    fixed meshmorph

--- src/ops/base/Ops.Gl.MeshMorph/Ops.Gl.MeshMorph.js
+++ src/ops/base/Ops.Gl.MeshMorph/Ops.Gl.MeshMorph.js
@@ -1,77 +1,92 @@
-var cgl=op.patch.cgl;
+const cgl = op.patch.cgl;
 
+const render = op.inTrigger("render");
 
-var render=op.inTrigger("render");
+const nextGeom = op.inValueInt("Geometry");
+const duration = op.inValue("Duration", 1.0);
 
-var nextGeom=op.inValueInt("Geometry");
-var duration=op.inValue("Duration",1.0);
+const finished = op.outValue("Finished");
 
-var finished=op.outValue("Finished");
+let mesh = null;
+const inGeoms = [];
+nextGeom.onChange = updateGeom;
 
-var mesh=null;
-var inGeoms=[];
-nextGeom.onChange=updateGeom;
-
-var oldGeom=0;
-var anim=new CABLES.Anim();
+let oldGeom = 0;
+const anim = new CABLES.Anim();
 anim.clear();
 
-var trigger=op.outTrigger("trigger");
+const trigger = op.outTrigger("trigger");
 
-var calcVertexNormals=op.inValueBool("smooth");
+const calcVertexNormals = op.inValueBool("smooth");
 calcVertexNormals.set(true);
 
-var geoms=[];
-//var mesh=null;
-window.meshsequencecounter=window.meshsequencecounter||1;
+const geoms = [];
+// var mesh=null;
+window.meshsequencecounter = window.meshsequencecounter || 1;
 window.meshsequencecounter++;
-var prfx=String.fromCharCode(97 + window.meshsequencecounter);
-var needsUpdateFrame=false;
-render.onTriggered=doRender;
+const prfx = String.fromCharCode(97 + window.meshsequencecounter);
+const needsUpdateFrame = false;
+render.onTriggered = doRender;
 
-var srcHeadVert=''
-    .endl()+'IN vec3 '+prfx+'_attrMorphTargetA;'
-    .endl()+'IN vec3 '+prfx+'_attrMorphTargetB;'
-    .endl()+'UNI float {{mod}}_fade;'
-    .endl()+'UNI float {{mod}}_doMorph;'
+const srcHeadVert = ""
+    .endl() + "IN vec3 " + prfx + "attrMorphTargetA;"
+    .endl() + "IN vec3 " + prfx + "attrMorphTargetB;"
+    .endl() + "UNI float {{mod}}fade;"
+    .endl() + "UNI float {{mod}}doMorph;"
     .endl();
 
-var srcBodyVert=''
+const srcBodyVert = ""
     // .endl()+'   pos =vec4(vPosition,1.0);'
-    .endl()+'if({{mod}}_doMorph==1.0){'
-    .endl()+'  pos = vec4( '+prfx+'_attrMorphTargetA * {{mod}}_fade + '+prfx+'_attrMorphTargetB * (1.0 - {{mod}}_fade ), 1. );'
-    .endl()+'}'
+    .endl() + "if({{mod}}doMorph==1.0){"
+    .endl() + "  pos = vec4( " + prfx + "attrMorphTargetA * {{mod}}fade + " + prfx + "attrMorphTargetB * (1.0 - {{mod}}fade ), 1. );"
+    .endl() + "}"
     .endl();
 
-var uniFade=null;
-var module=null;
-var shader=null;
-var uniDoMorph=null;
+let uniFade = null;
+let module = null;
+let shader = null;
+let uniDoMorph = null;
 
-for(var i=0;i<8;i++)
+for (let i = 0; i < 8; i++)
 {
-    var inGeom=op.inObject("Geometry "+(i));
-    inGeom.onChange=updateMeshes;
+    const inGeom = op.inObject("Geometry " + (i));
+    inGeom.onChange = updateMeshes;
     inGeoms.push(inGeom);
+}
+
+function checkLength()
+{
+    op.setUiError("nosamelength", null);
+    let oldGeomLength = 0;
+
+    for (let i = 0; i < inGeoms.length; i++)
+    {
+        const geom = inGeoms[i].get();
+        if (geom && geom._vertices)
+        {
+            if (oldGeomLength != 0 && oldGeomLength != geom._vertices.length) op.setUiError("nosamelength", "Geometries must have the same number of vertices!", 1);
+            oldGeomLength = geom._vertices.length;
+        }
+    }
 }
 
 function updateMeshes()
 {
-    if(mesh)return;
-    for(var i=0;i<inGeoms.length;i++)
+    checkLength();
+    if (mesh) return;
+
+    for (let i = 0; i < inGeoms.length; i++)
     {
-        var geom=inGeoms[i].get();
-        if(geom && geom._vertices)
+        const geom = inGeoms[i].get();
+        if (geom && geom._vertices)
         {
-            //op.log(i+" verts",geom._vertices.length);
-
-            if(i===0)
+            if (i === 0)
             {
-                mesh=new CGL.Mesh(cgl,geom);
+                mesh = new CGL.Mesh(cgl, geom);
 
-                mesh.addAttribute(prfx+'_attrMorphTargetA',geom._vertices,3);
-                mesh.addAttribute(prfx+'_attrMorphTargetB',geom._vertices,3);
-                //op.log("MESH BUILD");
+                mesh.addAttribute(prfx + "attrMorphTargetA", geom._vertices, 3);
+                mesh.addAttribute(prfx + "attrMorphTargetB", geom._vertices, 3);
+                // op.log("MESH BUILD");
                 updateGeom();
             }
         }
@@ -80,74 +95,71 @@ function updateMeshes()
 
 function updateGeom()
 {
-    var getGeom = nextGeom.get();
-    if (getGeom < 0) getGeom =0 ;
-    else if(getGeom >= 7) getGeom = 7;
-    var temp = 0;//= 0
+    let getGeom = nextGeom.get();
+    if (getGeom < 0) getGeom = 0;
+    else if (getGeom >= 7) getGeom = 7;
+    let temp = 0;//= 0
 
-    if(oldGeom === getGeom)return;
+    if (oldGeom === getGeom) return;
 
     anim.clear();
     anim.setValue(op.patch.freeTimer.get(), 0);
-    anim.setValue(op.patch.freeTimer.get()+duration.get(), 1,
-        function()
+    anim.setValue(op.patch.freeTimer.get() + duration.get(), 1,
+        function ()
         {
-            //op.log("finished");
-            oldGeom=getGeom;
+            // op.log("finished");
+            oldGeom = getGeom;
             finished.set(true);
         });
     finished.set(false);
 
-    var geom1=inGeoms[oldGeom].get();
+    const geom1 = inGeoms[oldGeom].get();
 
     temp = getGeom;
 
-    var geom2=inGeoms[temp].get();
+    const geom2 = inGeoms[temp].get();
 
-    if(mesh && geom1 && geom2 && geom1._vertices && geom2._vertices)
+    if (mesh && geom1 && geom2 && geom1._vertices && geom2._vertices)
     {
-        mesh.updateAttribute(prfx+'_attrMorphTargetB',geom1._vertices);
-        mesh.updateAttribute(prfx+'_attrMorphTargetA',geom2._vertices);
+        mesh.updateAttribute(prfx + "attrMorphTargetB", geom1._vertices);
+        mesh.updateAttribute(prfx + "attrMorphTargetA", geom2._vertices);
     }
-
 }
 
 function removeModule()
 {
-    if(shader && module)
+    if (shader && module)
     {
         shader.removeModule(module);
-        shader=null;
+        shader = null;
     }
 }
 
 function doRender()
 {
-    if(cgl.getShader() && cgl.getShader()!=shader)
+    if (cgl.getShader() && cgl.getShader() != shader)
     {
-        if(shader) removeModule();
+        if (shader) removeModule();
 
-        shader=cgl.getShader();
-        module=shader.addModule(
+        shader = cgl.getShader();
+        module = shader.addModule(
             {
-                name: 'MODULE_VERTEX_POSITION',
-                srcHeadVert: srcHeadVert,
-                srcBodyVert: srcBodyVert
+                "name": "MODULE_VERTEX_POSITION",
+                "srcHeadVert": srcHeadVert,
+                "srcBodyVert": srcBodyVert
             });
 
-        uniFade=new CGL.Uniform(shader,'f',module.prefix+'_fade',0);
-        uniDoMorph=new CGL.Uniform(shader,'f',module.prefix+'_doMorph',1);
+        uniFade = new CGL.Uniform(shader, "f", module.prefix + "fade", 0);
+        uniDoMorph = new CGL.Uniform(shader, "f", module.prefix + "doMorph", 1);
     }
 
-    if(uniDoMorph)
+    if (uniDoMorph)
     {
         uniFade.setValue(anim.getValue(op.patch.freeTimer.get()));
 
         uniDoMorph.setValue(1.0);
-        if(mesh!==null) mesh.render(cgl.getShader());
+        if (mesh !== null) mesh.render(cgl.getShader());
         uniDoMorph.setValue(0);
         trigger.trigger();
     }
-
 }
-
