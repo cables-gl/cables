696155200dc44ff01762311753547b859f6ea513
Siamend Darwesh <simo@undev.de>
2020-11-30 17:40:20 +0100

    BiquadFilter_v2 clearer error messages

--- src/ops/base/Ops.WebAudio.BiquadFilter_v2/Ops.WebAudio.BiquadFilter_v2.js
+++ src/ops/base/Ops.WebAudio.BiquadFilter_v2/Ops.WebAudio.BiquadFilter_v2.js
@@ -20,17 +20,16 @@ const biquadFilter = audioContext.createBiquadFilter();
 const filterNode = audioContext.createBiquadFilter();
 
 const inAudio = op.inObject("Audio In");
-const inFilterType = op.inDropDown ("Type", ['peaking','lowpass','highpass','bandpass','lowshelf','highshelf','notch','allpass'],'peaking');
+const inFilterType = op.inDropDown("Type", ["peaking", "lowpass", "highpass", "bandpass", "lowshelf", "highshelf", "notch", "allpass"], "peaking");
 
 const inFrequency = op.inFloat("frequency", 2000);
 const inQ = op.inFloat("q", 0.0001);
 const inGain = op.inFloat("gain", 0);
 
-
-const inDetune = op.inInt("detune",0);
+const inDetune = op.inInt("detune", 0);
 
 op.setPortGroup("Filter Settings", [inFilterType, inFrequency, inQ, inGain]);
-op.setPortGroup("Detune (in cents)",[inDetune]);
+op.setPortGroup("Detune (in cents)", [inDetune]);
 const outAudio = op.outObject("Audio Out");
 
 let oldAudioIn = null;
@@ -67,59 +66,67 @@ inAudio.onChange = function ()
     outAudio.set(filterNode);
 };
 
-
-inFilterType.onChange = () => {
+inFilterType.onChange = () =>
+{
     const type = inFilterType.get();
     inGain.setUiAttribs({
-        greyout: ["lowpass", "highpass", "bandpass", "notch", "allpass"].includes(type)
-    })
+        "greyout": ["lowpass", "highpass", "bandpass", "notch", "allpass"].includes(type)
+    });
 
     inQ.setUiAttribs({
-        greyout: ["lowshelf", "highshelf"].includes(type)
-    })
+        "greyout": ["lowshelf", "highshelf"].includes(type)
+    });
 
     filterNode.type = type;
-}
+};
 
-inFrequency.onChange = () => {
+inFrequency.onChange = () =>
+{
     const freq = inFrequency.get();
-    if (freq) {
-        if (freq >= FREQUENCY_MIN && freq <= FREQUENCY_MAX) {
+    if (freq)
+    {
+        if (freq >= FREQUENCY_MIN && freq <= FREQUENCY_MAX)
+        {
             filterNode.frequency.setValueAtTime(clamp(freq, FREQUENCY_MIN, FREQUENCY_MAX), audioContext.currentTime);
             op.setUiError("freqRange", null);
         }
-        if (freq < FREQUENCY_MIN) {
-            op.setUiError("freqRange", "The frequency you selected is lower than the possible range.", 0);
-        } else if (freq > FREQUENCY_MAX) {
-
-            op.setUiError("freqRange", "The frequency you selected is higher than the possible range.", 0);
+        if (freq < FREQUENCY_MIN)
+        {
+            op.setUiError("freqRange", "The frequency you selected is lower than the possible frequency of " + FREQUENCY_MIN + " Hz.", 1);
+        }
+        else if (freq > FREQUENCY_MAX)
+        {
+            op.setUiError("freqRange", "The frequency you selected is higher than the possible frequency of " + FREQUENCY_MAX + " Hz.", 1);
         }
     }
-
-
 };
 
-inDetune.onChange = () => {
+inDetune.onChange = () =>
+{
     filterNode.detune.setValueAtTime(inDetune.get(), audioContext.currentTime);
 };
 
-inQ.onChange = () => {
+inQ.onChange = () =>
+{
     const q = inQ.get();
     filterNode.Q.setValueAtTime(clamp(q, Q_MIN, Q_MAX), audioContext.currentTime);
 
-    if (q < Q_MIN) op.setUiError("qRange", "Your Q value is below the minimum possible range of " + Q_MIN + ".", 1);
-    else if (q > Q_MAX) op.setUiError("qRange", "Your Q value is above the maximum possible range of " + Q_MAX + ".", 1);
-    else {
+    if (q < Q_MIN) op.setUiError("qRange", "Your Q value is below the minimum possible value of " + Q_MIN + ".", 1);
+    else if (q > Q_MAX) op.setUiError("qRange", "Your Q value is above the maximum possible value of " + Q_MAX + ".", 1);
+    else
+    {
         op.setUiError("qRange", null);
     }
 };
 
-inGain.onChange = () => {
+inGain.onChange = () =>
+{
     const gain = inGain.get();
     filterNode.gain.setValueAtTime(clamp(gain, GAIN_MIN, GAIN_MAX), audioContext.currentTime);
-    if (gain < GAIN_MIN) op.setUiError("gainRange", "Your gain value is below the minimum possible range of " + GAIN_MIN + ".", 1);
-    else if (gain > GAIN_MAX) op.setUiError("gainRange", "Your gain value is above the maximum possible range of " + GAIN_MAX + ".", 1);
-    else {
+    if (gain < GAIN_MIN) op.setUiError("gainRange", "Your gain value is below the minimum possible value of " + GAIN_MIN + " dB.", 1);
+    else if (gain > GAIN_MAX) op.setUiError("gainRange", "Your gain value is above the maximum possible value of " + GAIN_MAX + " dB.", 1);
+    else
+    {
         op.setUiError("gainRange", null);
     }
 };
