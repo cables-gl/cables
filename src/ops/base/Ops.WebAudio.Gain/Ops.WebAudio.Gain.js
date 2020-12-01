32b88e0d34d76c94fd3a564b5304e4ca3e5a9f61
Siamend Darwesh <simo@undev.de>
2020-12-01 15:34:26 +0100

    Add mute button to Gain op

--- src/ops/base/Ops.WebAudio.Gain/Ops.WebAudio.Gain.js
+++ src/ops/base/Ops.WebAudio.Gain/Ops.WebAudio.Gain.js
@@ -1,17 +1,29 @@
 const audioIn = op.inObject("audio in");
 const gain = op.inFloatSlider("gain", 1);
-gain.onChange = updateGain;
+const inMute = op.inBool("Mute", false);
 const audioOut = op.outObject("audio out");
 
 let audioContext = CABLES.WEBAUDIO.createAudioContext(op);
 const gainNode = audioContext.createGain();
 
-function updateGain()
+gain.onChange = () =>
 {
+    if (inMute.get()) return;
     // gainNode.gain.value = parseFloat( gain.get() )||0;
-    gainNode.gain.setValueAtTime(parseFloat(gain.get()) || 0, window.audioContext.currentTime);
-}
+    gainNode.gain.setValueAtTime(Number(gain.get()) || 0, window.audioContext.currentTime);
+};
 
+inMute.onChange = () =>
+{
+    if (inMute.get())
+    {
+        gainNode.gain.setValueAtTime(0, window.audioContext.currentTime);
+    }
+    else
+    {
+        gainNode.gain.setValueAtTime(Number(gain.get()) || 0, window.audioContext.currentTime);
+    }
+};
 let oldAudioIn = null;
 
 audioIn.onChange = function ()
@@ -26,10 +38,11 @@ audioIn.onChange = function ()
             }
             catch (e)
             {
-                console.log(e);
+                op.log(e);
             }
         }
         op.setUiError("audioCtx", null);
+        audioOut.set(null);
     }
     else
     {
@@ -41,7 +54,5 @@ audioIn.onChange = function ()
         else op.setUiError("audioCtx", "The passed input is not an audio context. Please make sure you connect an audio context to the input.", 2);
     }
     oldAudioIn = audioIn.get();
+    audioOut.set(gainNode);
 };
-
-gain.set(1.0);
-audioOut.set(gainNode);
