ea8ba685c4db8d5064b9e544cdabe288b56260fa
pandrr <tom@undev.de>
2021-01-19 13:52:31 +0100

    new localstorage ops

---
+++ src/ops/base/Ops.Website.LocalStorageString/Ops.Website.LocalStorageString.js
@@ -0,0 +1,26 @@
+const
+    inKey = op.inString("Key"),
+    inValue = op.inString("String", ""),
+    inStore = op.inTriggerButton("Store"),
+    outValue = op.outValue("Stored String");
+
+inKey.onChange = updateOutput;
+inStore.onTriggered = storeValue;
+updateOutput();
+
+function getKey()
+{
+    return (op.patch.namespace || "") + inKey.get();
+}
+
+function updateOutput()
+{
+    outValue.set(window.localStorage.getItem(getKey()));
+}
+
+function storeValue()
+{
+    const val = inValue.get();
+    window.localStorage.setItem(getKey(), val);
+    outValue.set(val);
+}
