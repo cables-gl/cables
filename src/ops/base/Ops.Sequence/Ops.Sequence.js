ae33089540ed24bb2a5721a3b437b174d96dab96
tom <tom@undev.de>
2021-01-25 21:53:58 +0100

    sequence clean up button

--- src/ops/base/Ops.Sequence/Ops.Sequence.js
+++ src/ops/base/Ops.Sequence/Ops.Sequence.js
@@ -1,22 +1,71 @@
-const exe=op.inTrigger("exe");
-const exes=[];
-const triggers=[];
-const num=16;
-exe.onTriggered=triggerAll;
+const
+    cleanup = op.inTriggerButton("Clean up connections"),
+    exe = op.inTrigger("exe");
+
+const
+    exes = [],
+    triggers = [],
+    num = 16;
+
+let updateTimeout = null;
+
+exe.onTriggered = triggerAll;
+cleanup.onTriggered = clean;
+cleanup.hidePort();
+
+for (let i = 0; i < num; i++)
+{
+    const p = op.outTrigger("trigger " + i);
+    triggers.push(p);
+    p.onLinkChanged = updateButton;
+
+    if (i < num - 1)
+    {
+        let newExe = op.inTrigger("exe " + i);
+        newExe.onTriggered = triggerAll;
+        exes.push(newExe);
+    }
+}
+
+function updateButton()
+{
+    clearTimeout(updateTimeout);
+    updateTimeout = setTimeout(() =>
+    {
+        let show = false;
+        for (let i = 0; i < triggers.length; i++)
+            if (triggers[i].links.length > 1) show = true;
+
+        cleanup.setUiAttribs({ "hideParam": !show });
+
+        if (op.isCurrentUiOp()) op.refreshParams();
+    }, 60);
+}
 
 function triggerAll()
 {
-    for(var i=0;i<triggers.length;i++) triggers[i].trigger();
+    for (let i = 0; i < triggers.length; i++) triggers[i].trigger();
 }
 
-for(var i=0;i<num;i++)
+function clean()
 {
-    triggers.push( op.outTrigger("trigger "+i));
-
-    if(i<num-1)
+    let count = 0;
+    for (let i = 0; i < triggers.length; i++)
     {
-        var newExe=op.inTrigger("exe "+i);
-        newExe.onTriggered=triggerAll;
-        exes.push( newExe );
+        let removeLinks = [];
+
+        if (triggers[i].links.length > 1)
+            for (let j = 1; j < triggers[i].links.length; j++)
+            {
+                while (triggers[count].links.length > 0) count++;
+
+                removeLinks.push(triggers[i].links[j]);
+                const otherPort = triggers[i].links[j].getOtherPort(triggers[i]);
+                op.patch.link(op, "trigger " + count, otherPort.parent, otherPort.name);
+                count++;
+            }
+
+        for (let j = 0; j < removeLinks.length; j++) removeLinks[j].remove();
     }
+    updateButton();
 }
