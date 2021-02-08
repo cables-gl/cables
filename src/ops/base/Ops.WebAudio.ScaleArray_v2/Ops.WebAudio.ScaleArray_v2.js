90acb6a6c64c1ba8783df55fe2b689730f8757a8
Siamend Darwesh <simo@undev.de>
2021-02-08 16:47:23 +0100

    ScaleArray_v2 finish

--- src/ops/base/Ops.WebAudio.ScaleArray_v2/Ops.WebAudio.ScaleArray_v2.js
+++ src/ops/base/Ops.WebAudio.ScaleArray_v2/Ops.WebAudio.ScaleArray_v2.js
@@ -1,117 +1,139 @@
+function clamp(val, min, max)
+{
+    return Math.min(Math.max(val, min), max);
+}
 
 // constants
+const NOTE_NUMBERS = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+
 const BASE_TONES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 const BASE_TONE_DEFAULT = "C";
 const SCALE_TYPE_DEFAULT = "Minor";
 const APPEND_OCTAVE_DEFAULT = true;
-const OCTAVE_DEFAULT = 3;
-const OCTAVE_MIN = 0;
-const OCTAVE_MAX = 7;
+const OCTAVE_DEFAULT = 4;
+const OCTAVE_MIN = -1;
+const OCTAVE_MAX = 9;
 const INCLUDE_HIGH_BASE_TONE_DEFAULT = false;
 
 // scale types taken from: https://github.com/stagas/scales/blob/master/index.js
 const SCALE_TYPES = {
-	'Major': [0, 2, 4, 5, 7, 9, 11],
-	'Minor': [0, 2, 3, 5, 7, 8, 10],
-  'Ionian': [0, 2, 4, 5, 7, 9, 11],
-	'Aeolian': [0, 2, 3, 5, 7, 8, 10],
-	'Dorian': [0, 2, 3, 5, 7, 9, 10],
-	'Mixolydian': [0, 2, 4, 5, 7, 9, 10],
-	'Lydian': [0, 2, 4, 6, 7, 9, 11],
-	'Phrygian': [0, 1, 3, 5, 7, 8, 10],
-	'Locrian': [0, 1, 3, 5, 6, 8, 10],
-	'Diminished': [0, 1, 3, 4, 6, 7, 9, 10],
-	'Whole Half': [0, 2, 3, 5, 6, 8, 9, 11],
-	'Whole Tone': [0, 2, 4, 6, 8, 10],
-	'Minor Blues': [0, 3, 5, 6, 7, 10],
-	'Minor Pentatonic': [0, 3, 5, 7, 10],
-	'Major Pentatonic': [0, 2, 4, 7, 9],
-	'Harmonic Minor': [0, 2, 3, 5, 7, 8, 11],
-	'Melodic Minor': [0, 2, 3, 5, 7, 9, 11],
-	'Super Locrian': [0, 1, 3, 4, 6, 8, 10],
-	'Bhairav': [0, 1, 4, 5, 7, 8, 11],
-	'Hungarian Minor': [0, 2, 3, 6, 7, 8, 11],
-	'Minor Gypsy': [0, 1, 4, 5, 7, 8, 10],
-	'Hirojoshi': [0, 2, 3, 7, 8],
-	'In Sen': [0, 1, 5, 7, 10],
-	'Iwato': [0, 1, 5, 6, 10],
-	'Kumoi': [0, 2, 3, 7, 9],
-	'Pelog': [0, 1, 3, 4, 7, 8],
-	'Spanish': [0, 1, 3, 4, 5, 6, 8, 10],
-	'Ion Aeol': [0, 2, 3, 4, 5, 7, 8, 9, 10, 11]
+    "Major": [0, 2, 4, 5, 7, 9, 11],
+    "Minor": [0, 2, 3, 5, 7, 8, 10],
+    "Ionian": [0, 2, 4, 5, 7, 9, 11],
+    "Aeolian": [0, 2, 3, 5, 7, 8, 10],
+    "Dorian": [0, 2, 3, 5, 7, 9, 10],
+    "Mixolydian": [0, 2, 4, 5, 7, 9, 10],
+    "Lydian": [0, 2, 4, 6, 7, 9, 11],
+    "Phrygian": [0, 1, 3, 5, 7, 8, 10],
+    "Locrian": [0, 1, 3, 5, 6, 8, 10],
+    "Diminished": [0, 1, 3, 4, 6, 7, 9, 10],
+    "Whole Half": [0, 2, 3, 5, 6, 8, 9, 11],
+    "Whole Tone": [0, 2, 4, 6, 8, 10],
+    "Minor Blues": [0, 3, 5, 6, 7, 10],
+    "Minor Pentatonic": [0, 3, 5, 7, 10],
+    "Major Pentatonic": [0, 2, 4, 7, 9],
+    "Harmonic Minor": [0, 2, 3, 5, 7, 8, 11],
+    "Melodic Minor": [0, 2, 3, 5, 7, 9, 11],
+    "Super Locrian": [0, 1, 3, 4, 6, 8, 10],
+    "Bhairav": [0, 1, 4, 5, 7, 8, 11],
+    "Hungarian Minor": [0, 2, 3, 6, 7, 8, 11],
+    "Minor Gypsy": [0, 1, 4, 5, 7, 8, 10],
+    "Hirojoshi": [0, 2, 3, 7, 8],
+    "In Sen": [0, 1, 5, 7, 10],
+    "Iwato": [0, 1, 5, 6, 10],
+    "Kumoi": [0, 2, 3, 7, 9],
+    "Pelog": [0, 1, 3, 4, 7, 8],
+    "Spanish": [0, 1, 3, 4, 5, 6, 8, 10],
+    "Ion Aeol": [0, 2, 3, 4, 5, 7, 8, 9, 10, 11]
 };
 
 // input
 const baseTonePort = op.inDropDown("Base Tone", BASE_TONES, BASE_TONE_DEFAULT);
 const scaleTypePort = op.inDropDown("Scale Type", Object.keys(SCALE_TYPES), SCALE_TYPE_DEFAULT);
-var octavePort = op.inInt("Octave", OCTAVE_DEFAULT);
-
+const includeHighBaseTonePort = op.inBool("Include Upper Root Note", INCLUDE_HIGH_BASE_TONE_DEFAULT);
+const octavePort = op.inInt("Octave", OCTAVE_DEFAULT);
 const appendOctavePort = op.inBool("Append Octave", APPEND_OCTAVE_DEFAULT);
 
-const includeHighBaseTonePort = op.inBool("Include High Base Tone", INCLUDE_HIGH_BASE_TONE_DEFAULT);
-
+op.setPortGroup("Scale Settings", [baseTonePort, scaleTypePort, includeHighBaseTonePort, octavePort]);
+op.setPortGroup("Scale Array Settings", [appendOctavePort]);
 // output
 const scaleArrayPort = op.outArray("Scale Array");
 const stepArrayPort = op.outArray("Note Step Number Array");
+const midiNoteArrayPort = op.outArray("Midi Note Array");
+
 // change listeners
 baseTonePort.onChange = scaleTypePort.onChange = octavePort.onChange
 = appendOctavePort.onChange = includeHighBaseTonePort.onChange = setOutput;
 
 // functions
-function getToneAt(index, offset) {
-    var i = index + offset;
-    if(i>=BASE_TONES.length) {
+function getToneAt(index, offset)
+{
+    let i = index + offset;
+    if (i >= BASE_TONES.length)
+    {
         i -= BASE_TONES.length;
     }
     return BASE_TONES[i];
 }
 
-function getToneIndex(tone) {
-    for(var i=0; i<BASE_TONES.length; i++) {
-        if(BASE_TONES[i] === tone) {
+function getToneIndex(tone)
+{
+    for (let i = 0; i < BASE_TONES.length; i++)
+    {
+        if (BASE_TONES[i] === tone)
+        {
             return i;
         }
     }
     return -1;
 }
 
-function setOutput() {
+function setOutput()
+{
     const baseTone = baseTonePort.get();
     const baseToneIndex = getToneIndex(baseTone);
     const scaleType = scaleTypePort.get();
 
-   if(SCALE_TYPES.hasOwnProperty(scaleType) && BASE_TONES.indexOf(baseTone) > -1) {
+    if (SCALE_TYPES.hasOwnProperty(scaleType) && BASE_TONES.indexOf(baseTone) > -1)
+    {
         const scale = SCALE_TYPES[scaleType];
         const scaleArray = [];
         const stepArray = [];
         const appendOctave = appendOctavePort.get();
         let octave = octavePort.get();
 
-        if(appendOctave && octave && octave >= OCTAVE_MIN && octave <= OCTAVE_MAX) { // "C4", "D4", ...
-            octave = Math.round(octave);
+        if (octave > OCTAVE_MAX) op.setUiError("octave", "Octave value higher than " + OCTAVE_MAX + ". Setting to " + OCTAVE_MAX + ". Highest midi note is 127.", 1);
+        else if (octave < OCTAVE_MIN) op.setUiError("octave", "Octave value lower than " + OCTAVE_MIN + ". Setting to " + OCTAVE_MIN + ". Lowest midi note is 0.", 1);
+        else op.setUiError("octave", null);
 
-            for(var i=0; i<scale.length; i++) {
-                scaleArray.push(getToneAt(scale[i], baseToneIndex) + octave);
-                stepArray.push(scale[i]);
+        octave = clamp(Math.round(octave), OCTAVE_MIN, OCTAVE_MAX);
 
-            }
-            // append the base tone in the next octave
-            if(includeHighBaseTonePort.get()) {
-                scaleArray.push(getToneAt(scale[0], baseToneIndex) + (octave+1));
-                stepArray.push(scale[0] + 12);
-            }
-        } else { // "C", "D", ...
-            for(var j=0; j<scale.length; j++) {
-                scaleArray.push(getToneAt(scale[j], baseToneIndex));
-                stepArray.push(scale[j]);
-            }
+        for (let i = 0; i < scale.length; i++)
+        {
+            scaleArray.push(getToneAt(scale[i], baseToneIndex) + (appendOctave ? octave : ""));
+            stepArray.push(scale[i]);
         }
+
+        // append the base tone in the next octave
+        if (includeHighBaseTonePort.get())
+        {
+            if (appendOctave) scaleArray.push(getToneAt(scale[0], baseToneIndex) + (octave + 1));
+            stepArray.push(scale[0] + 12);
+        }
+
+        const midiNotes = NOTE_NUMBERS
+            .filter((nr) => stepArray.includes(nr - 12)) // only get scale notes
+            .map((nr) => nr + BASE_TONES.indexOf(baseTone) + 12 * octave) // map to midi note number
+            .filter((midiNote) => midiNote <= 127); // only use values <= 127
+
         scaleArrayPort.set(null);
         scaleArrayPort.set(scaleArray);
 
         stepArrayPort.set(null);
         stepArrayPort.set(stepArray);
 
+        midiNoteArrayPort.set(null);
+        midiNoteArrayPort.set(midiNotes);
     }
 }
 
