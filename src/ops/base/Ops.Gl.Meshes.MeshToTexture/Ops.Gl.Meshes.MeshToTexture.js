3a7b4a43bcc8a8a02525f5cc0f304b6913372369
tom <tom@undev.de>
2021-02-28 07:46:07 +0100

    new op meshtotexture

---
+++ src/ops/base/Ops.Gl.Meshes.MeshToTexture/Ops.Gl.Meshes.MeshToTexture.js
@@ -0,0 +1,262 @@
+const
+    exec = op.inTrigger("Render"),
+    inGeom=op.inObject("Geometry",null,"geometry"),
+    inVPSize = op.inValueBool("Use Viewport Size", true),
+    inWidth = op.inValueInt("Tex Size", 512),
+
+    tfilter = op.inValueSelect("filter", ["nearest", "linear", "mipmap"]),
+    twrap = op.inValueSelect("wrap", ["clamp to edge", "repeat", "mirrored repeat"], "clamp to edge"),
+    inNumTex = op.inSwitch("Num Textures", ["1", "4"], "1"),
+    next = op.outTrigger("Next"),
+    outTex = op.outTexture("Texture"),
+    outTex2 = op.outTexture("Texture 2"),
+    outTex3 = op.outTexture("Texture 3"),
+    outTex4 = op.outTexture("Texture 4");
+
+op.setPortGroup("Texture settings", [tfilter, twrap]);
+
+let numTextures = 1;
+const cgl = op.patch.cgl;
+const prevViewPort = [0, 0, 0, 0];
+const effect = null;
+
+let lastShader = null;
+let shader = null;
+
+inWidth.onChange =
+    tfilter.onChange =
+    inNumTex.onChange =
+    twrap.onChange = initFbLater;
+
+inVPSize.onChange = updateUI;
+
+const showingError = false;
+
+let fb = null;
+const tex = null;
+let needInit = true;
+let mesh=null;
+
+tfilter.set("nearest");
+
+updateUI();
+
+const drawBuffArr = [];
+
+
+const vertModTitle = "vert_" + op.name;
+const mod = new CGL.ShaderModifier(cgl, op.name);
+mod.addModule({
+    "title": vertModTitle,
+    "name": "MODULE_VERTEX_POSITION",
+    srcHeadVert:"OUT vec3 MOD_pos;",
+    srcBodyVert:attachments.vertpos_vert
+});
+
+mod.addModule({
+    "title": op.name,
+    "name": "MODULE_COLOR",
+    "srcHeadFrag": "IN vec3 MOD_pos;",
+    "srcBodyFrag": "col.rgb=MOD_pos;col.a=1.0;"
+});
+
+mod.addUniformVert("f", "MOD_texSize", inWidth);
+
+
+
+inGeom.onChange=function()
+{
+    const g=inGeom.get();
+
+    if(g)
+    {
+        mesh = new CGL.Mesh(cgl, g,cgl.gl.POINTS);
+        mesh.addVertexNumbers = true;
+
+    }
+
+    else mesh=null;
+};
+
+function warning()
+{
+}
+
+function updateUI()
+{
+    // if (inVPSize.get() === true)
+    // {
+    //     inWidth.setUiAttribs({ "greyout": true });
+    //     inHeight.setUiAttribs({ "greyout": true });
+    // }
+    // else if (inVPSize.get() === false)
+    // {
+    //     inWidth.setUiAttribs({ "greyout": false });
+    //     inHeight.setUiAttribs({ "greyout": false });
+    // }
+    // inWidth.set(cgl.getViewPort()[2]);
+    // inHeight.set(cgl.getViewPort()[3]);
+}
+
+function initFbLater()
+{
+    needInit = true;
+    warning();
+}
+
+
+function resetShader()
+{
+    if (shader) shader.dispose();
+    lastShader = null;
+    shader = null;
+}
+
+function initFb()
+{
+    needInit = false;
+    if (fb)fb.delete();
+
+    const oldLen = drawBuffArr.length;
+    numTextures = parseInt(inNumTex.get());
+    drawBuffArr.length = 0;
+    for (let i = 0; i < numTextures; i++)drawBuffArr[i] = true;
+
+    if (oldLen != drawBuffArr.length)
+    {
+        resetShader();
+    }
+
+    fb = null;
+
+    let w = inWidth.get();
+    let h = inWidth.get();
+
+
+    let filter = CGL.Texture.FILTER_NEAREST;
+    if (tfilter.get() == "linear") filter = CGL.Texture.FILTER_LINEAR;
+    else if (tfilter.get() == "mipmap") filter = CGL.Texture.FILTER_MIPMAP;
+
+    let selectedWrap = CGL.Texture.WRAP_CLAMP_TO_EDGE;
+    if (twrap.get() == "repeat") selectedWrap = CGL.Texture.WRAP_REPEAT;
+    if (twrap.get() == "mirrored repeat") selectedWrap = CGL.Texture.WRAP_MIRRORED_REPEAT;
+
+    if (cgl.glVersion >= 2)
+    {
+        fb = new CGL.Framebuffer2(cgl, w, h,
+            {
+                "isFloatingPointTexture": true,
+                "multisampling": false,
+                "numRenderBuffers": numTextures,
+                "wrap": selectedWrap,
+                "filter": filter,
+                "depth": true,
+                "multisamplingSamples": 0,
+                "clear": true
+            });
+    }
+    else
+    {
+        fb = new CGL.Framebuffer(cgl, inWidth.get(), inWidth.get(),
+            {
+                "isFloatingPointTexture": true,
+                "filter": filter,
+                "wrap": selectedWrap
+            });
+    }
+}
+
+exec.onTriggered = function ()
+{
+    const vp = cgl.getViewPort();
+
+    if (!fb || needInit)initFb();
+    if (inVPSize.get() && fb && (vp[2] != fb.getTextureColor().width || vp[3] != fb.getTextureColor().height)) initFb();
+
+    // if (!inShader.get() || !inShader.get().setDrawBuffers) return;
+
+    // if (inShader.get() != lastShader)
+    // {
+    //     lastShader = inShader.get();
+    //     shader = inShader.get().copy();
+
+    //     shader.setDrawBuffers(drawBuffArr);
+    // }
+
+    // if (!shader)
+    // {
+    //     outTex.set(null);
+    //     return;
+    // }
+
+    prevViewPort[0] = vp[0];
+    prevViewPort[1] = vp[1];
+    prevViewPort[2] = vp[2];
+    prevViewPort[3] = vp[3];
+
+    fb.renderStart(cgl);
+
+    cgl.pushPMatrix();
+    mat4.identity(cgl.pMatrix);
+
+    cgl.pushViewMatrix();
+    mat4.identity(cgl.vMatrix);
+
+    cgl.pushModelMatrix();
+    mat4.identity(cgl.mMatrix);
+
+    // cgl.pushShader(inShader.get());
+    // if (shader.bindTextures) shader.bindTextures();
+    cgl.gl.viewport(0, 0, 512,512);
+
+    mat4.ortho(
+        cgl.pMatrix,
+        0,
+        512,
+        0,
+        512,
+        -1.001,
+        100
+    );
+
+    mod.bind();
+
+    // cgl.pushModelMatrix();
+    // mat4.identity(cgl.mMatrix);
+
+    // cgl.pushViewMatrix();
+    // mat4.identity(cgl.vMatrix);
+
+    // cgl.gl.viewport(0,512,0,221);
+
+
+
+    if(mesh)mesh.render(cgl.getShader());
+
+// cgl.popModelMatrix();
+// cgl.popViewMatrix();
+
+
+    cgl.popPMatrix();
+    cgl.popModelMatrix();
+    cgl.popViewMatrix();
+    fb.renderEnd(cgl);
+
+    if (numTextures >= 2)
+    {
+        outTex.set(fb.getTextureColorNum(0));
+        outTex2.set(fb.getTextureColorNum(1));
+        outTex3.set(fb.getTextureColorNum(2));
+        outTex4.set(fb.getTextureColorNum(3));
+    }
+    else outTex.set(fb.getTextureColor());
+
+    // cgl.popShader();
+
+    cgl.gl.viewport(prevViewPort[0], prevViewPort[1], prevViewPort[2], prevViewPort[3]);
+
+    next.trigger();
+
+        mod.unbind();
+
+};
