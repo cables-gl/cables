07abd08f3d7ac418c4d0998cae6ced4ed2c371ff
unknown <stephan@undev.de>
2021-03-05 13:59:57 +0100

    route number

--- src/ops/base/Ops.Value.RouteNumber/Ops.Value.RouteNumber.js
+++ src/ops/base/Ops.Value.RouteNumber/Ops.Value.RouteNumber.js
@@ -1,31 +1,37 @@
-// constants
-var NUM_PORTS = 10;
-var DEFAULT_VALUE_DEFAULT = 0;
+// constants and variables
+const NUM_PORTS = 10;
+const DEFAULT_VALUE_DEFAULT = 0;
+let lastIdx = null;
 
 // input
-var indexPort = op.inValue('Index');
-var valuePort = op.inValue('Value');
-var defaultValuePort = op.inValue('Default Value', DEFAULT_VALUE_DEFAULT);
+const indexPort = op.inValue("Index", 0);
+const valuePort = op.inValue("Value", 0);
+const defaultValuePort = op.inValue("Default Value", DEFAULT_VALUE_DEFAULT);
+const onlyOnePort = op.inBool("Only one port active", false);
 
 // output
-var valuePorts = createOutPorts(DEFAULT_VALUE_DEFAULT);
+const valuePorts = createOutPorts(DEFAULT_VALUE_DEFAULT);
 
 // change listeners
 indexPort.onChange = update;
 valuePort.onChange = update; // TODO: Maybe only one update needed!?
 defaultValuePort.onChange = setDefaultValues;
+onlyOnePort.onChange = onlyOnePortChange;
 
 setDefaultValues();
+
 
 // functions
 
 /**
  * creates the output-port array
  */
-function createOutPorts() {
-    var arr = [];
-    for(var i=0; i<NUM_PORTS; i++) {
-        var port = op.outValue('Index ' + i + ' Value');
+function createOutPorts()
+{
+    let arr = [];
+    for (let i = 0; i < NUM_PORTS; i++)
+    {
+        let port = op.outValue("Index " + i + " Value");
         arr.push(port);
     }
     return arr;
@@ -34,9 +40,11 @@ function createOutPorts() {
 /**
  * Sets all value ports to the default value
  */
-function setDefaultValues() {
-    var defaultValue = defaultValuePort.get();
-    valuePorts.forEach(function(valuePort) {
+function setDefaultValues()
+{
+    const defaultValue = defaultValuePort.get();
+    valuePorts.forEach((valuePort) =>
+    {
         valuePort.set(defaultValue);
     });
 }
@@ -44,17 +52,40 @@ function setDefaultValues() {
 /**
  * Update
  */
-function update() {
-    var index = indexPort.get();
-    var value = valuePort.get();
+function update()
+{
+    let index = indexPort.get();
     index = Math.round(index);
-    index = clamp(index, 0, NUM_PORTS-1);   
+    index = clamp(index, 0, NUM_PORTS - 1);
+
+    if (onlyOnePort.get() && lastIdx !== null && lastIdx != index)
+    {
+        valuePorts[lastIdx].set(defaultValuePort.get());
+    }
+
+    const value = valuePort.get();
     valuePorts[index].set(value);
+
+    lastIdx = index;
 }
 
 /**
  * Returns a number whose value is limited to the given range.
  */
-function clamp(value, min, max) {
-  return Math.min(Math.max(value, min), max);
+function clamp(value, min, max)
+{
+    return Math.min(Math.max(value, min), max);
+}
+
+/**
+ * Reset all ports to default value and set current index
+ * or let have several ports with old value
+ */
+function onlyOnePortChange()
+{
+    if (onlyOnePort.get())
+    {
+        setDefaultValues();
+        update();
+    }
 }
