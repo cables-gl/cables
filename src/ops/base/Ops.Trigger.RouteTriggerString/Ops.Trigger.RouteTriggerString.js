fc1cadf7c937c98c95478265109c5b54e6f297f6
pandrr <tom@undev.de>
2021-03-11 09:32:36 +0100

    new op: routetriggerstring

---
+++ src/ops/base/Ops.Trigger.RouteTriggerString/Ops.Trigger.RouteTriggerString.js
@@ -0,0 +1,55 @@
+const NUM_PORTS = 24;
+const exePort = op.inTriggerButton("Execute");
+const switchPort = op.inString("Switch Value");
+const nextTriggerPort = op.outTrigger("Next Trigger");
+const valueOutPort = op.outValue("Switched Index");
+
+let index=-1;
+
+const triggerPorts = [];
+const namePorts = [];
+
+for (let j = 0; j < NUM_PORTS; j++)
+{
+    triggerPorts[j] = op.outTrigger("Trigger " + j);
+    namePorts[j] = op.inString("String " + j);
+    namePorts[j].onChange=updateIndex;
+}
+const defaultTriggerPort = op.outTrigger("Default Trigger");
+
+op.onLoad=
+op.onInit=
+switchPort.onChange=updateIndex;
+
+function updateIndex()
+{
+    index=-1;
+    for(let i=0;i<namePorts.length;i++)
+    {
+        if(namePorts[i].get()==switchPort.get())
+        {
+            index=i;
+        }
+    }
+    // if (index >= 0 && index < NUM_PORTS)
+    // {
+    //     valueOutPort.set(index);
+    //     triggerPorts[index].trigger();
+    // }
+    // else
+    // {
+    //     valueOutPort.set(-1);
+    //     defaultTriggerPort.trigger();
+    // }
+    // nextTriggerPort.trigger();
+}
+
+exePort.onTriggered = ()=>
+{
+    if(index>=0) triggerPorts[index].trigger();
+
+    valueOutPort.set(index);
+
+};
+
+
