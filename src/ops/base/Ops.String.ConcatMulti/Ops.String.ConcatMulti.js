c5c1231c2e5707b7434d7bda246de59e37c4781d
tom <tom@undev.de>
2021-03-13 17:03:20 +0100

    hideport

--- src/ops/base/Ops.String.ConcatMulti/Ops.String.ConcatMulti.js
+++ src/ops/base/Ops.String.ConcatMulti/Ops.String.ConcatMulti.js
@@ -1,35 +1,35 @@
 
-const addSpacesCheckBox = op.inBool("add spaces",false),
-        newLinesCheckBox = op.inBool("new lines",false),
-        stringPorts = [],
-        result = op.outString("concat string");
+const addSpacesCheckBox = op.inBool("add spaces", false),
+    newLinesCheckBox = op.inBool("new lines", false),
+    stringPorts = [],
+    result = op.outString("concat string");
 
 
 stringPorts.onChange = addSpacesCheckBox.onChange =
 newLinesCheckBox.onChange = update;
 
-addSpacesCheckBox.hidePort(true);
-newLinesCheckBox.hidePort(true);
+addSpacesCheckBox.setUiAttribs({ "hidePort": true });
+newLinesCheckBox.setUiAttribs({ "hidePort": true });
 
-for(var i=0; i<8; i++)
+for (let i = 0; i < 8; i++)
 {
-    var p=op.inString("string " + i);
+    let p = op.inString("string " + i);
     stringPorts.push(p);
     p.onChange = update;
 }
 
 function update()
 {
-    var str = "";
-    var nl = "";
-    var space = addSpacesCheckBox.get();
+    let str = "";
+    let nl = "";
+    let space = addSpacesCheckBox.get();
 
-    for(var i=0; i<stringPorts.length; i++)
+    for (let i = 0; i < stringPorts.length; i++)
     {
-        const inString=stringPorts[i].get();
-        if(!inString)continue;
-        if(space) str += " ";
-        if(i>0 && newLinesCheckBox.get()) nl = '\n';
+        const inString = stringPorts[i].get();
+        if (!inString) continue;
+        if (space) str += " ";
+        if (i > 0 && newLinesCheckBox.get()) nl = "\n";
         str += nl;
         str += inString;
     }
