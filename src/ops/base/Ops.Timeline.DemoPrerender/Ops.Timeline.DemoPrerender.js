5292e5e8d2aca44b271b27aece433043e3063f05
tom <tom@undev.de>
2021-03-21 11:52:30 +0100

    demo prerenderer

--- src/ops/base/Ops.Timeline.DemoPrerender/Ops.Timeline.DemoPrerender.js
+++ src/ops/base/Ops.Timeline.DemoPrerender/Ops.Timeline.DemoPrerender.js
@@ -4,24 +4,16 @@ const
     inReset = op.inTriggerButton("Reset"),
     inClear = op.inTriggerButton("Clear"),
     next = op.outTrigger("Next"),
-<<<<<<< HEAD
-=======
     nextPrerendered = op.outTrigger("Prerendered Frame"),
->>>>>>> 90fd3a8d451f6e28b2817473d96c0d64a5f6c27e
     outProgress = op.outNumber("Progress"),
     numEvents = op.outNumber("Num Events");
 
 exec.onTriggered = render;
 inEvents.setUiAttribs({ "hidePort": true });
-<<<<<<< HEAD
-
-let firstTime = true;
-=======
 
 let isPrerendering = true;
 let prerenderCount = 0;
 
->>>>>>> 90fd3a8d451f6e28b2817473d96c0d64a5f6c27e
 let events = [];
 
 inEvents.onChange = () =>
@@ -42,11 +34,7 @@ let curTime = 0;
 
 inReset.onTriggered = () =>
 {
-<<<<<<< HEAD
-    firstTime = true;
-=======
     isPrerendering = true;
->>>>>>> 90fd3a8d451f6e28b2817473d96c0d64a5f6c27e
 };
 
 inClear.onTriggered = () =>
@@ -62,32 +50,19 @@ function fakeNow()
 
 function render()
 {
-<<<<<<< HEAD
-    if (firstTime)
-    {
-        for (let i = 0; i < events.length; i++)
-=======
     if (isPrerendering)
     {
         // for(let i=0;i<events.length;i++)
         // {
         if (prerenderCount < events.length)
->>>>>>> 90fd3a8d451f6e28b2817473d96c0d64a5f6c27e
         {
             const oldInternalNow = CABLES.internalNow;
             CABLES.internalNow = fakeNow;
 
-<<<<<<< HEAD
-            curTime = events[i];
-            op.patch._frameNum = i + 22;
-
-            console.log("prerendering at ", curTime);
-=======
             curTime = events[prerenderCount];
             op.patch._frameNum = prerenderCount + 22;
 
             console.log("isPrerendering at ", curTime);
->>>>>>> 90fd3a8d451f6e28b2817473d96c0d64a5f6c27e
 
             CABLES.overwriteTime = curTime;
             op.patch.timer.setTime(curTime);
@@ -95,25 +70,11 @@ function render()
 
             // console.log("the time",op.patch.timer.getTime());
 
-<<<<<<< HEAD
-            next.trigger();
-            next.trigger();
-            next.trigger();
-
-=======
->>>>>>> 90fd3a8d451f6e28b2817473d96c0d64a5f6c27e
             CABLES.overwriteTime = undefined;
             CABLES.internalNow = oldInternalNow;
 
             // op.patch.timer.setTime(0);
             // op.patch.freeTimer.setTime(0);
-<<<<<<< HEAD
-        }
-        firstTime = false;
-        op.patch.timer.setTime(0);
-        op.patch.freeTimer.setTime(0);
-        op.patch._frameNum = 0;
-=======
 
             // }
         }
@@ -133,7 +94,6 @@ function render()
         nextPrerendered.trigger();
 
         prerenderCount++;
->>>>>>> 90fd3a8d451f6e28b2817473d96c0d64a5f6c27e
     }
     else next.trigger();
 }
