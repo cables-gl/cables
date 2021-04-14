9a81a35135567419115a5d6cbd2725e464a82d61
pandrr <tom@undev.de>
2021-04-14 11:34:48 +0200

    new op /Ops.Array.ArrayMergeTrigger/

---
+++ src/ops/base/Ops.Array.ArrayMergeTrigger/Ops.Array.ArrayMergeTrigger.js
@@ -0,0 +1,45 @@
+const
+    exec=op.inTrigger("Merge"),
+    next=op.outTrigger("Next");
+
+const numArrays=12;
+const inArrs=[];
+let needsUpdate=true;
+
+for(let i =0;i<numArrays;i++)
+{
+    inArrs[i]=op.inArray("Array "+i);
+    inArrs[i].onChange=function()
+    {
+        // update();
+        needsUpdate=true;
+    }
+}
+
+const
+    outArr=op.outArray("Result"),
+    outArrayLength = op.outNumber("Array length");
+
+var arr=[];
+
+exec.onTriggered=()=>
+{
+
+    if(needsUpdate)
+    {
+        arr.length=0;
+
+        for(let i=0;i<numArrays;i++)
+        {
+            const ar=inArrs[i].get();
+            if(ar)arr=arr.concat(ar);
+        }
+
+        outArr.set(null);
+        outArr.set(arr);
+        outArrayLength.set(arr.length);
+    }
+
+    next.trigger();
+
+};
