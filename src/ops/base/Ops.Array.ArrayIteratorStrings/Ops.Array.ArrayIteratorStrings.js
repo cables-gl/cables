22a1bde635d3bca21e553d7bc64b041f69cb811c
pandrr <tom@undev.de>
2021-05-20 10:26:08 +0200

    new op arrayIteratorStrings

---
+++ src/ops/base/Ops.Array.ArrayIteratorStrings/Ops.Array.ArrayIteratorStrings.js
@@ -0,0 +1,24 @@
+const
+    exe = op.inTrigger("Exe"),
+    arr = op.inArray("Array"),
+    trigger = op.outTrigger("Trigger"),
+    idx = op.outNumber("Index"),
+    val = op.outString("Value");
+
+exe.onTriggered = function ()
+{
+    if (!arr.get()) return;
+
+    for (let i = 0; i < arr.get().length; i++)
+    {
+        idx.set(i);
+        let value = null;
+        if (arr.get()[i])
+        {
+            value = String(arr.get()[i]);
+        }
+        val.set(value);
+        trigger.trigger();
+        op.patch.instancing.increment();
+    }
+};
