e0a2b699bba72afd3f4061980f59bf2dcc1f1369
tom <tom@undev.de>
2021-05-27 09:35:27 +0200

    rgbmath greyout inactive colorchannels

--- src/ops/base/Ops.Gl.TextureEffects.RGBMath/Ops.Gl.TextureEffects.RGBMath.js
+++ src/ops/base/Ops.Gl.TextureEffects.RGBMath/Ops.Gl.TextureEffects.RGBMath.js
@@ -1,10 +1,10 @@
 const
     render = op.inTrigger("Render"),
-    inOp=op.inSwitch("Operation",['c-x','x-c','c+x','c*x','x/c','c/x','c%x'],'c*x'),
-    chanR=op.inBool("R Active",true),
-    chanG=op.inBool("G Active",true),
-    chanB=op.inBool("B Active",true),
-    chanA=op.inBool("A Active",false),
+    inOp = op.inSwitch("Operation", ["c-x", "x-c", "c+x", "c*x", "x/c", "c/x", "c%x"], "c*x"),
+    chanR = op.inBool("R Active", true),
+    chanG = op.inBool("G Active", true),
+    chanB = op.inBool("B Active", true),
+    chanA = op.inBool("A Active", false),
     r = op.inValue("r", 1),
     g = op.inValue("g", 1),
     b = op.inValue("b", 1),
@@ -21,30 +21,37 @@ const uniformG = new CGL.Uniform(shader, "f", "g", g);
 const uniformB = new CGL.Uniform(shader, "f", "b", b);
 const uniformA = new CGL.Uniform(shader, "f", "a", a);
 
-chanR.onChange=
-    chanG.onChange=
-    chanB.onChange=
-    chanA.onChange=
-    inOp.onChange=updateDefines;
+chanR.onChange =
+    chanG.onChange =
+    chanB.onChange =
+    chanA.onChange =
+    inOp.onChange = updateDefines;
 updateDefines();
 
 function updateDefines()
 {
-    shader.toggleDefine("MOD_OP_SUB_CX",inOp.get()==="c-x");
-    shader.toggleDefine("MOD_OP_SUB_XC",inOp.get()==="x-c");
+    shader.toggleDefine("MOD_OP_SUB_CX", inOp.get() === "c-x");
+    shader.toggleDefine("MOD_OP_SUB_XC", inOp.get() === "x-c");
 
-    shader.toggleDefine("MOD_OP_ADD",inOp.get()==="c+x");
-    shader.toggleDefine("MOD_OP_MUL",inOp.get()==="c*x");
+    shader.toggleDefine("MOD_OP_ADD", inOp.get() === "c+x");
+    shader.toggleDefine("MOD_OP_MUL", inOp.get() === "c*x");
 
-    shader.toggleDefine("MOD_OP_DIV_XC",inOp.get()==="x/c");
-    shader.toggleDefine("MOD_OP_DIV_CX",inOp.get()==="c/x");
+    shader.toggleDefine("MOD_OP_DIV_XC", inOp.get() === "x/c");
+    shader.toggleDefine("MOD_OP_DIV_CX", inOp.get() === "c/x");
 
-    shader.toggleDefine("MOD_OP_MODULO",inOp.get()==="c%x");
+    shader.toggleDefine("MOD_OP_MODULO", inOp.get() === "c%x");
 
-    shader.toggleDefine("MOD_CHAN_R",chanR.get());
-    shader.toggleDefine("MOD_CHAN_G",chanG.get());
-    shader.toggleDefine("MOD_CHAN_B",chanB.get());
-    shader.toggleDefine("MOD_CHAN_A",chanA.get());
+    shader.toggleDefine("MOD_CHAN_R", chanR.get());
+    r.setUiAttribs({ "greyout": !chanR.get() });
+
+    shader.toggleDefine("MOD_CHAN_G", chanG.get());
+    g.setUiAttribs({ "greyout": !chanG.get() });
+
+    shader.toggleDefine("MOD_CHAN_B", chanB.get());
+    b.setUiAttribs({ "greyout": !chanB.get() });
+
+    shader.toggleDefine("MOD_CHAN_A", chanA.get());
+    a.setUiAttribs({ "greyout": !chanA.get() });
 }
 
 render.onTriggered = function ()
