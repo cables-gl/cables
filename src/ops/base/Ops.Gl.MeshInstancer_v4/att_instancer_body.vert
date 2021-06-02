cc62d1c403a7d4d1374a35fbd3e1abaa9866a999
pandrr <tom@undev.de>
2021-06-02 08:49:23 +0200

    meshinstancer texcooords / sidebar stuff

--- src/ops/base/Ops.Gl.MeshInstancer_v4/att_instancer_body.vert
+++ src/ops/base/Ops.Gl.MeshInstancer_v4/att_instancer_body.vert
@@ -1,5 +1,16 @@
 
+
+#ifdef HAS_TEXCOORDS
+texCoord=(texCoord*instTexCoords.zw)+instTexCoords.xy;
+#endif
+
 mMatrix*=instMat;
 pos.xyz*=MOD_scale;
+
+#ifdef HAS_COLORS
 frag_instColor=instColor;
+#endif
+#ifndef HAS_COLORS
+frag_instColor=vec4(1.0);
+#endif
 
--- src/ops/base/Ops.Sidebar.SideBarSwitch/Ops.Sidebar.SideBarSwitch.js
+++ src/ops/base/Ops.Sidebar.SideBarSwitch/Ops.Sidebar.SideBarSwitch.js
@@ -1,41 +1,44 @@
 const parentPort = op.inObject("link"),
-    labelPort = op.inString("Text", "Value"),
-    inId = op.inValueString("Id", ""),
+    inArr = op.inArray("Names"),
     siblingsPort = op.outObject("childs"),
-    outIndex=op.outNumber("Index",-1);
+    outIndex = op.outNumber("Index", -1);
 
-let elTabActive=null;
+let elTabActive = null;
 const el = document.createElement("div");
 el.classList.add("sidebar__item");
 el.classList.add("sidebar_tabs");
 
 parentPort.onChange = onParentChanged;
-inId.onChange = onIdChanged;
 op.onDelete = onDelete;
 
 op.toWorkNeedsParent("Ops.Sidebar.Sidebar");
 
-const tabEles=[];
+const tabEles = [];
 
-addTab("BASIC");
-addTab("SPHERE");
-addTab("TEXT");
-addTab("EXPORT");
-
-function onIdChanged()
+inArr.onChange = () =>
 {
-    el.id = inId.get();
-}
+    el.innerHTML = "";
+    elTabActive = null;
+
+    const arr = inArr.get();
+    if (!arr) return;
+
+    for (let i = 0; i < arr.length; i++)
+    {
+        const el = addTab(String(arr[i]));
+        if (!elTabActive)setActiveTab(el);
+    }
+};
 
 function addTab(title)
 {
-    const tabEle=document.createElement("div");
+    const tabEle = document.createElement("div");
     tabEle.classList.add("sidebar_tab");
-    tabEle.id="tabEle"+tabEles.length;
-    tabEle.innerHTML=title;
-    tabEle.dataset.index=tabEles.length;
+    tabEle.id = "tabEle" + tabEles.length;
+    tabEle.innerHTML = title;
+    tabEle.dataset.index = tabEles.length;
 
-    tabEle.addEventListener("click",tabClicked);
+    tabEle.addEventListener("click", tabClicked);
 
     el.appendChild(tabEle);
 
@@ -44,13 +47,18 @@ function addTab(title)
     return tabEle;
 }
 
+function setActiveTab(el)
+{
+    elTabActive = el;
+    console.log(el.dataset.index);
+    outIndex.set(parseInt(el.dataset.index));
+    el.classList.add("sidebar_tab_active");
+}
+
 function tabClicked(e)
 {
-    if(elTabActive) elTabActive.classList.remove("sidebar_tab_active");
-    elTabActive=e.target;
-    console.log(e.target.dataset.index);
-    outIndex.set(parseInt(e.target.dataset.index));
-    e.target.classList.add("sidebar_tab_active");
+    if (elTabActive) elTabActive.classList.remove("sidebar_tab_active");
+    setActiveTab(e.target);
 }
 
 function onParentChanged()
@@ -71,13 +79,13 @@ function onParentChanged()
 
 function showElement(el)
 {
-    if (!el)return;
+    if (!el) return;
     el.style.display = "block";
 }
 
 function hideElement(el)
 {
-    if (!el)return;
+    if (!el) return;
     el.style.display = "none";
 }
 
