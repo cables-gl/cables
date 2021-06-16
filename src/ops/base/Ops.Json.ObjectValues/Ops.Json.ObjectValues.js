30a422b1bebdabc0d22974bb280c6e9350536c05
tom <tom@undev.de>
2021-06-16 09:06:50 +0200

    object values as array op from kirell

---
+++ src/ops/base/Ops.Json.ObjectValues/Ops.Json.ObjectValues.js
@@ -0,0 +1,17 @@
+const
+    inObj = op.inObject("Object"),
+    outNumValues = op.outValue("Num values"),
+    outValues = op.outArray("Values");
+
+inObj.onChange = () => {
+    const sourceObj = inObj.get();
+    if (!sourceObj) {
+        outNumValues.set(0);
+        outValues.set([]);
+        return;
+    }
+
+    const values = Object.values(sourceObj);
+    outNumValues.set(values.length);
+    outValues.set(values);
+};
