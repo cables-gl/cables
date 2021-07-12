ff55edc1e77e25e09b010a9266a1612751007808
pandrr <tom@undev.de>
2021-07-12 19:10:07 +0200

    dev changes / colormap etc

--- src/ops/base/Ops.Gl.TextureEffects.Pixelate/Ops.Gl.TextureEffects.Pixelate.js
+++ src/ops/base/Ops.Gl.TextureEffects.Pixelate/Ops.Gl.TextureEffects.Pixelate.js
@@ -1,10 +1,10 @@
-const render = op.inTrigger("render");
-const multiplierTex = op.inTexture("Multiplier");
-const blendMode = CGL.TextureEffect.AddBlendSelect(op, "Blend Mode", "normal");
-const amount = op.inValueSlider("Amount", 1);
-const amountX = op.inValue("width", 320.0);
-const amountY = op.inValue("height", 180.0);
-const trigger = op.outTrigger("trigger");
+const render = op.inTrigger("render"),
+    multiplierTex = op.inTexture("Multiplier"),
+    blendMode = CGL.TextureEffect.AddBlendSelect(op, "Blend Mode", "normal"),
+    amount = op.inValueSlider("Amount", 1),
+    amountX = op.inValue("width", 320.0),
+    amountY = op.inValue("height", 180.0),
+    trigger = op.outTrigger("trigger");
 
 const cgl = op.patch.cgl;
 const shader = new CGL.Shader(cgl, op.name);
@@ -14,18 +14,9 @@ shader.setSource(shader.getDefaultVertexShader(), attachments.pixelate_frag);
 const textureUniform = new CGL.Uniform(shader, "t", "tex", 0);
 const textureMultiplierUniform = new CGL.Uniform(shader, "t", "multiplierTex", 1);
 const amountUniform = new CGL.Uniform(shader, "f", "amount", amount);
-const amountXUniform = new CGL.Uniform(shader, "f", "amountX", 0.0);
-const amountYUniform = new CGL.Uniform(shader, "f", "amountY", 0.0);
+const amountXUniform = new CGL.Uniform(shader, "f", "amountX", amountX);
+const amountYUniform = new CGL.Uniform(shader, "f", "amountY", amountY);
 
-amountX.onChange = function ()
-{
-    amountXUniform.setValue(amountX.get());
-};
-
-amountY.onChange = function ()
-{
-    amountYUniform.setValue(amountY.get());
-};
 multiplierTex.onChange = function ()
 {
     shader.toggleDefine("PIXELATE_TEXTURE", multiplierTex.isLinked());
