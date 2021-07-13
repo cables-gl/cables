1fc21c24f8263091ebf8d5b61750771a1f22c8b6
pandrr <tom@undev.de>
2021-07-13 18:16:45 +0200

    rename randomnumbers v4

---
+++ src/ops/base/Ops.Math.RandomNumbers_v4/Ops.Math.RandomNumbers_v4.js
@@ -0,0 +1,23 @@
+const
+    inSeed = op.inValueFloat("Seed", 1),
+    min = op.inValueFloat("Min", 0),
+    max = op.inValueFloat("Max", 1),
+    outX = op.outNumber("X"),
+    outY = op.outNumber("Y"),
+    outZ = op.outNumber("Z"),
+    outW = op.outNumber("W");
+
+inSeed.onChange =
+    min.onChange =
+    max.onChange = update;
+
+function update()
+{
+    const inMin = min.get();
+    const inMax = max.get();
+    Math.randomSeed = Math.abs(inSeed.get() || 0) * 571.1 + 1.0;
+    outX.set(Math.seededRandom() * (inMax - inMin) + inMin);
+    outY.set(Math.seededRandom() * (inMax - inMin) + inMin);
+    outZ.set(Math.seededRandom() * (inMax - inMin) + inMin);
+    outW.set(Math.seededRandom() * (inMax - inMin) + inMin);
+}
