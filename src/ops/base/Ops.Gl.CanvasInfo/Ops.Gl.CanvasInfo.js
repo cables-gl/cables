2383ebc047a011222819420455658a0ffb7bb74d
pandrr <tom@undev.de>
2021-07-30 11:37:37 +0200

    dev changes

--- src/ops/base/Ops.Gl.CanvasInfo/Ops.Gl.CanvasInfo.js
+++ src/ops/base/Ops.Gl.CanvasInfo/Ops.Gl.CanvasInfo.js
@@ -1,14 +1,12 @@
-
-
 const
-    width=op.outValue("width"),
-    height=op.outValue("height"),
-    pixelRatio=op.outValue("Pixel Ratio"),
-    aspect=op.outValue("Aspect Ratio"),
-    landscape=op.outValueBool("Landscape");
+    width = op.outValue("width"),
+    height = op.outValue("height"),
+    pixelRatio = op.outValue("Pixel Ratio"),
+    aspect = op.outValue("Aspect Ratio"),
+    landscape = op.outBool("Landscape");
 
-var cgl=op.patch.cgl;
-cgl.addEventListener("resize",update);
+let cgl = op.patch.cgl;
+cgl.addEventListener("resize", update);
 update();
 
 function update()
@@ -16,6 +14,6 @@ function update()
     height.set(cgl.canvasHeight);
     width.set(cgl.canvasWidth);
     pixelRatio.set(window.devicePixelRatio);
-    aspect.set(cgl.canvasWidth/cgl.canvasHeight);
-    landscape.set(cgl.canvasWidth>cgl.canvasHeight);
+    aspect.set(cgl.canvasWidth / cgl.canvasHeight);
+    landscape.set(cgl.canvasWidth > cgl.canvasHeight ? 1 : 0);
 }
