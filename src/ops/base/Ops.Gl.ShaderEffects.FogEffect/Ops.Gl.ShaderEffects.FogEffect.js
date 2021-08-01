be3e5ab40f435d0814ba5b12010155ae93c70521
pandrr <tom@undev.de>
2021-08-01 11:37:55 +0200

    fogeffect op - alpha by distance mode

--- src/ops/base/Ops.Gl.ShaderEffects.FogEffect/Ops.Gl.ShaderEffects.FogEffect.js
+++ src/ops/base/Ops.Gl.ShaderEffects.FogEffect/Ops.Gl.ShaderEffects.FogEffect.js
@@ -2,6 +2,7 @@ const
     id = "mod" + Math.floor(Math.random() * 10000),
     render = op.inTrigger("render"),
     next = op.outTrigger("trigger"),
+    inMode = op.inSwitch("Mode", ["Color", "R-Opacity"], "Color"),
     inStart = op.inValue("Start", 2),
     inEnd = op.inValue("End", 12),
     inAmount = op.inValueSlider("Amount", 0.5),
@@ -9,10 +10,8 @@ const
     g = op.inValueSlider("g", Math.random()),
     b = op.inValueSlider("b", Math.random());
 
-
 const cgl = op.patch.cgl;
 r.setUiAttribs({ "colorPick": true });
-
 
 const srcHeadVert = ""
     .endl() + "OUT vec4 MOD_fogPos;"
@@ -31,14 +30,19 @@ const srcBodyFrag = ""
     .endl() + "   float mx=clamp(MOD_de*MOD_amount,0.0,1.0);"
 // .endl() + "       mx=1.0-mx;"
 
+    .endl() + "#ifdef MOD_FOGMODE_ALPHA"
+    .endl() + "       col.a*=mix(col.a,MOD_color.r,mx);"
+    .endl() + "#endif"
+
+    .endl() + "#ifndef MOD_FOGMODE_ALPHA"
     .endl() + "       col.rgb=mix(col.rgb,vec3(MOD_color.r,MOD_color.g,MOD_color.b), mx);"
+    .endl() + "#endif"
 
 // .endl() + "   #ifdef MOD_ALPHA"
 // .endl() + "       col.a=1.0-clamp(MOD_de*MOD_amount,0.0,1.0);"
 // .endl() + "   #endif"
 
     .endl();
-
 
 const moduleFrag = null;
 const moduleVert = null;
@@ -47,7 +51,8 @@ const mod = new CGL.ShaderModifier(cgl, op.name);
 
 mod.addModule(
     {
-        "title": op.objName,
+        "priority": 4,
+        "title": op.name,
         "name": "MODULE_VERTEX_POSITION",
         "srcHeadVert": srcHeadVert,
         "srcBodyVert": srcBodyVert
@@ -55,7 +60,7 @@ mod.addModule(
 
 mod.addModule(
     {
-        "title": op.objName,
+        "title": op.name,
         "name": "MODULE_COLOR",
         "srcHeadFrag": srcHeadFrag,
         "srcBodyFrag": srcBodyFrag
@@ -65,6 +70,13 @@ mod.addUniform("f", "MOD_amount", inAmount);
 mod.addUniform("f", "MOD_start", inStart);
 mod.addUniform("f", "MOD_end", inEnd);
 mod.addUniform("3f", "MOD_color", r, g, b);
+
+inMode.onChange = updateMode;
+
+function updateMode()
+{
+    mod.toggleDefine("MOD_FOGMODE_ALPHA", inMode.get() == "R-Opacity");
+}
 
 render.onTriggered = function ()
 {
