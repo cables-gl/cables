8647ba2725e306c6ab26b256cad9c6ba7c58d4c6
pandrr <tom+github@undev.de>
2021-08-23 11:57:02 +0200

    bool stuff again

--- src/core/core_port.js
+++ src/core/core_port.js
@@ -796,4 +796,21 @@ class ValueSelectPort extends SwitchPort
     }
 }
 
-export { Port, SwitchPort, ValueSelectPort };
+
+class BoolPort extends Port
+{
+    set(b)
+    {
+        super.set(b);// ? 1 : 0);
+        // console.log("bool set", b, this.get());
+    }
+
+    get()
+    {
+        // console.log("bool get", super.get());
+        return super.get() ? 1 : 0;
+    }
+}
+
+
+export { Port, SwitchPort, BoolPort, ValueSelectPort };
