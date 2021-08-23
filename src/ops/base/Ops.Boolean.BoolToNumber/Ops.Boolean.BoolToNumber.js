732d04da5f8ad6d5b2bd8491fbdef681e734519d
pandrr <tom@undev.de>
2021-08-23 10:46:42 +0200

    dev changes

--- src/ops/base/Ops.Boolean.BoolToNumber/Ops.Boolean.BoolToNumber.js
+++ src/ops/base/Ops.Boolean.BoolToNumber/Ops.Boolean.BoolToNumber.js
@@ -1,9 +1,9 @@
 const
-    bool=op.inValueBool("bool"),
-    number=op.outValue("number");
+    bool = op.inValueBool("bool"),
+    number = op.outValue("number");
 
-bool.onChange=function()
+bool.onChange = function ()
 {
-    if(bool.get()) number.set(1);
+    if (bool.get()) number.set(1);
     else number.set(0);
 };
