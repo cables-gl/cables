05291a2cf960f151e8eb4be5d875fbb7be6d6e3f
pandrr <tom+github@undev.de>
2021-11-19 08:39:23 +0100

    new op ui area

---
+++ src/ops/base/Ops.Ui.Area/Ops.Ui.Area.js
@@ -0,0 +1,24 @@
+// const
+    // exe=op.inTrigger("Trigger"),
+    // inNum=op.inInt("Num",5),
+    // trigger=op.outTrigger("Next"),
+    // idx=op.outNumber("Index");
+
+op.setUiAttrib({"hasArea":true});
+
+op.log("hund");
+
+// exe.onTriggered=function()
+// {
+//     op.patch.instancing.pushLoop(inNum.get());
+
+//     for(let i=0;i<inNum.get();i++)
+//     {
+//         idx.set(i);
+//         trigger.trigger();
+//         op.patch.instancing.increment();
+//     }
+
+//     op.patch.instancing.popLoop();
+// };
+
