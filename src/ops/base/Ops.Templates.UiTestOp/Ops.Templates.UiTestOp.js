7e6948ede9a8b46d9493ca6acdec2160509e6b2a
pandrr <tom+github@undev.de>
2021-11-20 09:42:53 +0100

    uitestop

--- src/ops/base/Ops.Templates.UiTestOp/Ops.Templates.UiTestOp.js
+++ src/ops/base/Ops.Templates.UiTestOp/Ops.Templates.UiTestOp.js
@@ -1,12 +1,20 @@
-const cgl = op.patch.cgl;
+const
+    inWarning1 = op.inBool("Warning", false),
+    inUiError = op.inBool("Error", false),
+    inUiHint = op.inBool("Hint", false),
+    innum = op.inFloatSlider("Slider", 0),
+    trig = op.inTrigger("trigger"),
+    inLog = op.inTriggerButton("op.log()"),
+    inLogWarn = op.inTriggerButton("op.logWarn()"),
+    inLogErr = op.inTriggerButton("op.logError()"),
+    inPrompt = op.inTriggerButton("Open Prompt"),
+    inModal = op.inTriggerButton("Open Modal"),
+    inTab = op.inTriggerButton("Open new Tab");
 
-const inWarning1 = op.inBool("Warning 1", false);
-const inWarning2 = op.inBool("Warning 2", false);
-const innum = op.inFloatSlider("Slider", 0);
-const trig = op.inTrigger("trigger");
-const log = op.inTriggerButton("op.log()");
+op.setPortGroup("Warnings", [inWarning1, inUiHint, inUiError]);
+op.setPortGroup("Logging", [inLog, inLogWarn, inLogErr]);
 
-trig.onTriggered = update;
+op.setPortGroup("Modal", [inPrompt, inModal]);
 
 inWarning1.onChange = () =>
 {
@@ -14,9 +22,15 @@ inWarning1.onChange = () =>
     else op.setUiError("Warn1", null);
 };
 
-inWarning2.onChange = () =>
+inUiHint.onChange = () =>
 {
-    if (inWarning2.get()) op.setUiError("Warn2", "Warning two", 2);
+    if (inUiHint.get()) op.setUiError("Hint1", "This is a hint!", 0);
+    else op.setUiError("Hint1", null);
+};
+
+inUiError.onChange = () =>
+{
+    if (inUiError.get()) op.setUiError("Warn2", "Warning two", 2);
     else op.setUiError("Warn2", null);
 };
 
@@ -34,12 +48,57 @@ innum.onChange = () =>
     op.log(q);
 };
 
-function update()
+inLog.onTriggered = () =>
 {
+    // if you dont see this open the logging filter
+    // by pressing cmd/ctrl+p "logging"
+    // and deactivate the filter for this op name.
+    op.log("this is verbose logging!");
+};
 
-}
-
-log.onTriggered = () =>
+inLogWarn.onTriggered = () =>
 {
-    op.log("op logging!!!!!");
+    op.logWarn("this is a warning!");
+};
+
+inLogErr.onTriggered = () =>
+{
+    op.logError("this is an error!");
+};
+
+inPrompt.onTriggered = () =>
+{
+    new CABLES.UI.ModalDialog({
+        "prompt": true,
+        "title": "How Much",
+        "text": "please enter something",
+        "promptValue": "default",
+        "promptOk": (v) =>
+        {
+            console.log("yes! prompt finished", v);
+        }
+    });
+};
+
+inModal.onTriggered = () =>
+{
+    new CABLES.UI.ModalDialog({
+        "title": "Title",
+        "text": "Dialog content <b>can be <i>html</i><b>"
+    });
+};
+
+inTab.onTriggered = () =>
+{
+    const tab = new CABLES.UI.Tab("Example",
+        {
+            "icon": "cube",
+            "padding": true,
+            "singleton": false
+        });
+
+    gui.mainTabs.addTab(tab, true);
+    tab.addEventListener("onClose", () => { console.log("tab was closed!"); });
+    tab.html("this is the tab content. ");
+    gui.maintabPanel.show(true);
 };
