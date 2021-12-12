b3fb1e312dec40bb0c168d19484720fe120009bb
pandrr <tom+github@undev.de>
2021-12-12 22:49:26 +0100

    noise offsetting

--- src/ops/base/Ops.Gl.TextureEffects.Noise.CellularNoise/Ops.Gl.TextureEffects.Noise.CellularNoise.js
+++ src/ops/base/Ops.Gl.TextureEffects.Noise.CellularNoise/Ops.Gl.TextureEffects.Noise.CellularNoise.js
@@ -6,16 +6,17 @@ const
     y = op.inValue("Y", 0),
     z = op.inValue("Z", 0),
     scale = op.inValue("Scale", 3),
-    trigger = op.outTrigger("trigger"),
-    tile = op.inValueBool("Tileable", false);
+    tile = op.inValueBool("Tileable", false),
+
+    trigger = op.outTrigger("trigger");
 
 const cgl = op.patch.cgl;
 const shader = new CGL.Shader(cgl, op.name);
 
-
 shader.setSource(shader.getDefaultVertexShader(), attachments.cellularnoise3d_frag);
 
 const textureUniform = new CGL.Uniform(shader, "t", "tex", 0),
+    uniOffset = new CGL.Uniform(shader, "t", "texOffsetZ", 1),
     amountUniform = new CGL.Uniform(shader, "f", "amount", amount),
     uniZ = new CGL.Uniform(shader, "f", "z", z),
     uniX = new CGL.Uniform(shader, "f", "x", x),
@@ -31,6 +32,46 @@ function updateTileable()
 
 CGL.TextureEffect.setupBlending(op, shader, blendMode, amount);
 
+// offsetMap
+
+const
+    inTexOffsetTex = op.inTexture("Offset"),
+    inOffsetMul = op.inFloat("Offset Multiply", 1),
+
+    offsetX = op.inSwitch("Offset X", ["None", "R", "G", "B"], "None"),
+    offsetY = op.inSwitch("Offset Y", ["None", "R", "G", "B"], "None"),
+    offsetZ = op.inSwitch("Offset Z", ["None", "R", "G", "B"], "R");
+
+const uniOffMul = new CGL.Uniform(shader, "f", "offMul", inOffsetMul);
+
+op.setPortGroup("Offset Map", [inTexOffsetTex, offsetZ, offsetY, offsetX, inOffsetMul]);
+
+offsetX.onChange =
+offsetY.onChange =
+offsetZ.onChange =
+inTexOffsetTex.onChange = () =>
+{
+    shader.toggleDefine("HAS_TEX_OFFSETMAP", inTexOffsetTex.get());
+
+    shader.toggleDefine("OFFSET_X_R", offsetX.get() == "R");
+    shader.toggleDefine("OFFSET_X_G", offsetX.get() == "G");
+    shader.toggleDefine("OFFSET_X_B", offsetX.get() == "B");
+
+    shader.toggleDefine("OFFSET_Y_R", offsetY.get() == "R");
+    shader.toggleDefine("OFFSET_Y_G", offsetY.get() == "G");
+    shader.toggleDefine("OFFSET_Y_B", offsetY.get() == "B");
+
+    shader.toggleDefine("OFFSET_Z_R", offsetZ.get() == "R");
+    shader.toggleDefine("OFFSET_Z_G", offsetZ.get() == "G");
+    shader.toggleDefine("OFFSET_Z_B", offsetZ.get() == "B");
+
+    offsetX.setUiAttribs({ "greyout": !inTexOffsetTex.isLinked() });
+    offsetY.setUiAttribs({ "greyout": !inTexOffsetTex.isLinked() });
+    offsetZ.setUiAttribs({ "greyout": !inTexOffsetTex.isLinked() });
+};
+
+// end offsetmap
+
 render.onTriggered = function ()
 {
     if (!CGL.TextureEffect.checkOpInEffect(op)) return;
@@ -39,6 +80,7 @@ render.onTriggered = function ()
     cgl.currentTextureEffect.bind();
 
     cgl.setTexture(0, cgl.currentTextureEffect.getCurrentSourceTexture().tex);
+    if (inTexOffsetTex.get()) cgl.setTexture(1, inTexOffsetTex.get().tex);
 
     cgl.currentTextureEffect.finish();
     cgl.popShader();
--- src/ops/base/Ops.Gl.TextureEffects.Noise.PerlinNoise/Ops.Gl.TextureEffects.Noise.PerlinNoise.js
+++ src/ops/base/Ops.Gl.TextureEffects.Noise.PerlinNoise/Ops.Gl.TextureEffects.Noise.PerlinNoise.js
@@ -4,6 +4,7 @@ const
     amount = op.inValueSlider("Amount", 1),
     inMode = op.inSwitch("Color", ["Mono", "RGB"], "Mono"),
     scale = op.inValue("Scale", 22),
+    rangeMul = op.inValue("Multiply", 1),
     x = op.inValue("X", 0),
     y = op.inValue("Y", 0),
     z = op.inValue("Z", 0),
@@ -18,13 +19,56 @@ shader.setSource(shader.getDefaultVertexShader(), attachments.perlinnoise3d_frag
 
 const
     textureUniform = new CGL.Uniform(shader, "t", "tex", 0),
+    textureUniformOffZ = new CGL.Uniform(shader, "t", "texOffsetZ", 1),
+
     uniZ = new CGL.Uniform(shader, "f", "z", z),
     uniX = new CGL.Uniform(shader, "f", "x", x),
     uniY = new CGL.Uniform(shader, "f", "y", y),
     uniScale = new CGL.Uniform(shader, "f", "scale", scale),
-    amountUniform = new CGL.Uniform(shader, "f", "amount", amount);
+    amountUniform = new CGL.Uniform(shader, "f", "amount", amount),
+    rangeMulUniform = new CGL.Uniform(shader, "f", "rangeMul", rangeMul);
 
 CGL.TextureEffect.setupBlending(op, shader, blendMode, amount);
+
+// offsetMap
+
+const
+    inTexOffsetZ = op.inTexture("Offset"),
+    inOffsetMul = op.inFloat("Offset Multiply", 1),
+    offsetX = op.inSwitch("Offset X", ["None", "R", "G", "B"], "None"),
+    offsetY = op.inSwitch("Offset Y", ["None", "R", "G", "B"], "None"),
+    offsetZ = op.inSwitch("Offset Z", ["None", "R", "G", "B"], "R");
+
+op.setPortGroup("Offset Map", [inTexOffsetZ, offsetZ, offsetY, offsetX, inOffsetMul]);
+
+const uniOffMul = new CGL.Uniform(shader, "f", "offMul", inOffsetMul);
+
+offsetX.onChange =
+offsetY.onChange =
+offsetZ.onChange =
+inTexOffsetZ.onChange = () =>
+{
+    shader.toggleDefine("HAS_TEX_OFFSETMAP", inTexOffsetZ.get());
+
+    shader.toggleDefine("OFFSET_X_R", offsetX.get() == "R");
+    shader.toggleDefine("OFFSET_X_G", offsetX.get() == "G");
+    shader.toggleDefine("OFFSET_X_B", offsetX.get() == "B");
+
+    shader.toggleDefine("OFFSET_Y_R", offsetY.get() == "R");
+    shader.toggleDefine("OFFSET_Y_G", offsetY.get() == "G");
+    shader.toggleDefine("OFFSET_Y_B", offsetY.get() == "B");
+
+    shader.toggleDefine("OFFSET_Z_R", offsetZ.get() == "R");
+    shader.toggleDefine("OFFSET_Z_G", offsetZ.get() == "G");
+    shader.toggleDefine("OFFSET_Z_B", offsetZ.get() == "B");
+
+    offsetX.setUiAttribs({ "greyout": !inTexOffsetZ.isLinked() });
+    offsetY.setUiAttribs({ "greyout": !inTexOffsetZ.isLinked() });
+    offsetZ.setUiAttribs({ "greyout": !inTexOffsetZ.isLinked() });
+    inOffsetMul.setUiAttribs({ "greyout": !inTexOffsetZ.isLinked() });
+};
+
+// end offsetmap
 
 inMode.onChange = () =>
 {
@@ -39,6 +83,7 @@ render.onTriggered = function ()
     cgl.currentTextureEffect.bind();
 
     cgl.setTexture(0, cgl.currentTextureEffect.getCurrentSourceTexture().tex);
+    if (inTexOffsetZ.get()) cgl.setTexture(1, inTexOffsetZ.get().tex);
 
     cgl.currentTextureEffect.finish();
     cgl.popShader();
