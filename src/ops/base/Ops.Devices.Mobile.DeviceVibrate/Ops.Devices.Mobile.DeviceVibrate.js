c50805d456a9ff905822bcaf48b01a388128bd3d
pandrr <tom+github@undev.de>
2022-01-05 11:15:12 +0100

    test if iframe warning

--- src/ops/base/Ops.Devices.Mobile.DeviceVibrate/Ops.Devices.Mobile.DeviceVibrate.js
+++ src/ops/base/Ops.Devices.Mobile.DeviceVibrate/Ops.Devices.Mobile.DeviceVibrate.js
@@ -1,16 +1,22 @@
-var inVibrate=op.inTriggerButton("Vibrate");
-var outSupported=op.outValue("Supported");
+let inVibrate = op.inTriggerButton("Vibrate");
+let outSupported = op.outValue("Supported");
 
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate ||
 navigator.mozVibrate || navigator.msVibrate;
 
 if (navigator.vibrate) outSupported.set(true);
-    else outSupported.set(false);
+else outSupported.set(false);
 
-inVibrate.onTriggered=function()
+inVibrate.onTriggered = function ()
 {
-    if(navigator.vibrate)
+    if (navigator.vibrate)
     {
         navigator.vibrate(1500);
     }
 };
+
+if (window.self !== window.top)
+{
+    op.setUiError("iframe", "Device Vibratge does not work in an iframe, open the patch without an iframe to get it to work", 1);
+    op.warn("Device Vibratge does not work in an iframe, open the patch without an iframe to get it to work");
+}
