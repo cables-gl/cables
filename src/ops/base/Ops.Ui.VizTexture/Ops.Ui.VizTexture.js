70c99e88ee3219e9693f2fdf2e31107ecf274c91
pandrr <tom+github@undev.de>
2022-01-12 16:13:53 +0100

    fullscreenrect fit

--- src/ops/base/Ops.Ui.VizTexture/Ops.Ui.VizTexture.js
+++ src/ops/base/Ops.Ui.VizTexture/Ops.Ui.VizTexture.js
@@ -2,6 +2,6 @@ const
     inTex = op.inTexture("Texture In"),
     outTex = op.outTexture("Texture Out");
 
-op.setUiAttrib({ "height": 150 });
+op.setUiAttrib({ "height": 150, "resizable": true });
 
 inTex.onChange = () => { outTex.set(inTex.get()); };
