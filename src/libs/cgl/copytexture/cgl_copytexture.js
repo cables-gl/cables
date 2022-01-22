53bf8bd40f92a4890a64b6c683b22aa882be405f
pandrr <tom+github@undev.de>
2022-01-22 18:36:54 +0100

    new op Ops.Dev.Rgbe2HdrTexture

--- src/libs/cgl/copytexture/cgl_copytexture.js
+++ src/libs/cgl/copytexture/cgl_copytexture.js
@@ -1,84 +1,96 @@
+import { MESHES } from "../../../core/cgl/cgl_simplerect";
+
+
 class CopyTexture
 {
     constructor(cgl, name, options)
     {
         this.cgl = cgl;
-        this.texCopy = new CGL.Texture(cgl);
 
         this.effect = null;
+        this._options = options;
+        this._fb = null;
+        this._prevViewPort = [];
 
+        const shader = options.shader || ""
+            .endl() + "UNI sampler2D tex;"
+            .endl() + "IN vec2 texCoord;"
+            .endl() + "void main()"
+            .endl() + "{"
+            .endl() + "    vec4 col=texture(tex,texCoord);"
+            .endl() + "    outColor= col;"
+            .endl() + "}";
 
-        const shader = options.shader ||
-             "UNI sampler2D tex;"
-                 .endl() + "IN vec2 texCoord;"
-                 .endl() + "void main()"
-                 .endl() + "{"
-                 .endl() + "    vec4 col=texture(tex,texCoord);"
-                 .endl() + "    outColor= col;"
-                 .endl() + "}";
+        const verts = ""
+            .endl() + "IN vec3 vPosition;"
+            .endl() + "IN vec2 attrTexCoord;"
+
+            .endl() + "OUT vec2 texCoord;"
+
+            .endl() + "void main()"
+            .endl() + "{"
+            .endl() + "   texCoord=attrTexCoord;"
+            .endl() + "   gl_Position = vec4(vPosition,  1.0);"
+            .endl() + "}";
+
 
         this.bgShader = new CGL.Shader(cgl, "copytexture");
-        this.bgShader.setSource(this.bgShader.getDefaultVertexShader(), shader);
+        this.bgShader.setSource(verts, shader);
+
         const textureUniform = new CGL.Uniform(this.bgShader, "t", "tex", 0);
+
+        // this.effect = new CGL.TextureEffect(this.cgl, { "isFloatingPointTexture": this._options.isFloatingPointTexture, "clear": false });
+
+        this.mesh = MESHES.getSimpleRect(this.cgl, "texEffectRect");
+
+        this._uniTexInput =
+
+        console.log(this.effect);
     }
 
     copy(tex)
     {
-        if (!tex.compareSettings(this.texCopy))
-        {
-            this.texCopy = tex.clone();
+        const
+            w = this._options.width || tex.width,
+            h = this._options.height || tex.height,
+            cgl = this.cgl;
 
-            if (this.effect) this.effect.delete();
-            this.effect = new CGL.TextureEffect(this.cgl, { "isFloatingPointTexture": tex.isFloatingPointTexture, "clear": false });
+        if (this._fb)
+        {
+            this._fb.setSize(w, h);
+        }
+        else
+        {
+            // if (IS_WEBGL_1)
+            // {
+            //     this._fb = new CGL.Framebuffer(cgl, size, size, {
+            //         "isFloatingPointTexture": true,
+            //         "filter": CGL.Texture.FILTER_LINEAR,
+            //         "wrap": CGL.Texture.WRAP_CLAMP_TO_EDGE
+            //     });
+            // }
+            // else
+            // {
+            this._fb = new CGL.Framebuffer2(cgl, w, h, {
+                "isFloatingPointTexture": this._options.isFloatingPointTexture,
+                "filter": CGL.Texture.FILTER_LINEAR,
+                "wrap": CGL.Texture.WRAP_CLAMP_TO_EDGE,
+            });
+            // }
         }
 
-        this.effect.setSourceTexture(tex);
+        cgl.frameStore.renderOffscreen = true;
+        this._fb.renderStart(cgl);
 
-        const cgl = this.cgl;
-        const effect = this.effect;
-
-        if (!cgl) return;
-        if (!effect) return;
-
-        const prevViewPort = [0, 0, 0, 0];
-        const vp = cgl.getViewPort();
-        prevViewPort[0] = vp[0];
-        prevViewPort[1] = vp[1];
-        prevViewPort[2] = vp[2];
-        prevViewPort[3] = vp[3];
-
-
-        const oldEffect = cgl.currentTextureEffect;
-        cgl.currentTextureEffect = effect;
-        effect.setSourceTexture(tex);
-
-        effect.startEffect();
-
-        // render background color...
-        cgl.pushShader(this.bgShader);
-        cgl.currentTextureEffect.bind();
         cgl.setTexture(0, tex.tex);
-        // if (inTextureMask.get())cgl.setTexture(1, inTextureMask.get().tex);
 
-        cgl.pushBlend(false);
+        this.mesh.render(this.bgShader);
 
-        cgl.currentTextureEffect.finish();
-        cgl.popShader();
-
-        cgl.popBlend();
-
-        this.texCopy = effect.getCurrentSourceTexture();
-
-        effect.endEffect();
-
-        cgl.setViewPort(prevViewPort[0], prevViewPort[1], prevViewPort[2], prevViewPort[3]);
-
-        cgl.currentTextureEffect = oldEffect;
-
-        cgl.setTexture(0, CGL.Texture.getEmptyTexture(cgl).tex);
+        this._fb.renderEnd();
+        cgl.frameStore.renderOffscreen = false;
 
 
-        return this.texCopy;
+        return this._fb.getTextureColor();
     }
 }
 
