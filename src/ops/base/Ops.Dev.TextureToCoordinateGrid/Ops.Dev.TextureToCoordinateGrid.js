9161a004dd1e536cd2e9840e6a4250f7526d5ddf
pandrr <tom@undev.de>
2022-02-02 12:27:16 +0100

    new op Ops.Dev.TextureToCoordinateGrid

---
+++ src/ops/base/Ops.Dev.TextureToCoordinateGrid/Ops.Dev.TextureToCoordinateGrid.js
@@ -0,0 +1,24 @@
+const
+    exec = op.inTrigger("Execute"),
+    inTex = op.inTexture("RGBE Texture"),
+    inAspect = op.inFloat("Aspect", 1),
+    inThreshold = op.inFloatSlider("Threshold", 0.2),
+    next = op.outTrigger("Next"),
+    outFpTex = op.outTexture("HDR Texture");
+
+const tc = new CGL.CopyTexture(op.patch.cgl, "rgbe2hdr",
+    {
+        "shader": attachments.rgbe2fp_frag,
+        "isFloatingPointTexture": true
+    });
+
+const uni1 = new CGL.Uniform(tc.bgShader, "f", "aspect", inAspect);
+const uni2 = new CGL.Uniform(tc.bgShader, "f", "threshold", inThreshold);
+
+exec.onTriggered = () =>
+{
+    if (!inTex.get()) return;
+
+    outFpTex.set(null);
+    outFpTex.set(tc.copy(inTex.get()));
+};
