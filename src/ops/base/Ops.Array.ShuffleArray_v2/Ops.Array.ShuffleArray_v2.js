2d2c166b6e4dac84ce35266bd1203e85dac7962e
pandrr <tom+github@undev.de>
2022-03-02 14:29:32 +0100

    fix shufflearray

--- src/ops/base/Ops.Array.ShuffleArray_v2/Ops.Array.ShuffleArray_v2.js
+++ src/ops/base/Ops.Array.ShuffleArray_v2/Ops.Array.ShuffleArray_v2.js
@@ -1,18 +1,18 @@
 const
-    inArr=op.inArray("Array3"),
-    inSeed=op.inFloat("Seed",5711),
-    outArr=op.outArray("Result");
+    inArr = op.inArray("Array3"),
+    inSeed = op.inFloat("Seed", 5711),
+    outArr = op.outArray("Result");
 
-var newArr=[];
-var rndArr=[];
-inArr.onChange=update;
-inSeed.onChange=update;
+let newArr = [];
+let rndArr = [];
+inArr.onChange = update;
+inSeed.onChange = update;
 
 function fisherYatesShuffle(array)
 {
-    var i = 0;
-    var j = 0;
-    var temp = null;
+    let i = 0;
+    let j = 0;
+    let temp = null;
 
     for (i = array.length - 1; i > 0; i -= 1)
     {
@@ -23,37 +23,38 @@ function fisherYatesShuffle(array)
     }
 }
 
-
 function update()
 {
     const arr = inArr.get();
 
-    if(!arr)
+    if (!arr)
     {
         outArr.set(null);
         return;
     }
-    if(arr.length!=newArr.length) newArr.length = arr.length;
-
-    var i = 0;
-    var j = 0;
-    var temp = null;
-    Math.randomSeed=inSeed.get();
-
-    for (i = 0; i < arr.length; i++)
+    if (arr.length != newArr.length)
     {
-        rndArr[i]=i;
+        rndArr.length =
+        newArr.length = arr.length;
+    }
+
+    let j = 0;
+    let temp = null;
+    Math.randomSeed = inSeed.get();
+
+    for (let i = 0; i < arr.length; i++)
+    {
+        rndArr[i] = i;
     }
 
     fisherYatesShuffle(rndArr);
 
-    for (i = 0; i < arr.length; i ++)
+    for (let i = 0; i < arr.length; i++)
     {
-        j=rndArr[i];
+        j = rndArr[i];
         newArr[i] = arr[j];
     }
 
     outArr.set(null);
     outArr.set(newArr);
 }
-
