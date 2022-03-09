0bfa6d4983bca9a9f62c9a616a35c96a63c82c25
Stephan Maihöfer <stephan@undev.de>
2022-03-09 16:33:54 +0100

    add logging events

--- src/core/core_logger.js
+++ src/core/core_logger.js
@@ -32,28 +32,33 @@ export default class Logger
     error(args)
     {
         console.error("[" + this.initiator + "]", ...arguments);
+        if (window.gui) window.gui.emitEvent("coreLogEvent", this.initiator, "error", arguments);
     }
 
     info(args)
     {
         console.error("[" + this.initiator + "]", ...arguments);
+        if (window.gui) window.gui.emitEvent("coreLogEvent", this.initiator, "info", arguments);
     }
 
     warn(args)
     {
         console.warn("[" + this.initiator + "]", ...arguments);
+        if (window.gui) window.gui.emitEvent("coreLogEvent", this.initiator, "warn", arguments);
     }
 
     verbose()
     {
         if ((CABLES.UI && CABLES.UI.logFilter.shouldPrint(this.initiator, ...arguments)) || !CABLES.logSilent)
             console.log("[" + this.initiator + "]", ...arguments);
+        if (window.gui) window.gui.emitEvent("coreLogEvent", this.initiator, "verbose", arguments);
     }
 
     log(args)
     {
         if ((CABLES.UI && CABLES.UI.logFilter.shouldPrint(this.initiator, ...arguments)) || !CABLES.logSilent)
             console.log("[" + this.initiator + "]", ...arguments);
+        if (window.gui) window.gui.emitEvent("coreLogEvent", this.initiator, "log", arguments);
     }
 
     userInteraction(text)
