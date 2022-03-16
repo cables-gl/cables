c90128032e9caab5723c3f41dba96bc683938fce
pandrr <tom@undev.de>
2022-03-16 17:40:36 +0100

    stuff

--- src/ops/base/Ops.Gl.Geometry.FlipNormals/Ops.Gl.Geometry.FlipNormals.js
+++ src/ops/base/Ops.Gl.Geometry.FlipNormals/Ops.Gl.Geometry.FlipNormals.js
@@ -1,55 +1,48 @@
 const
-    geometry=op.inObject("Geometry"),
-    outGeom=op.outObject("Result"),
-    doFlip=op.inValueBool("Flip",true),
-    doNormalize=op.inValueBool("Normalize",true);
+    geometry = op.inObject("Geometry"),
+    outGeom = op.outObject("Result"),
+    doFlip = op.inValueBool("Flip", true),
+    doNormalize = op.inValueBool("Normalize", true);
 
-doFlip.onChange=
-    doNormalize.onChange=
-    geometry.onChange=flip;
+doFlip.onChange =
+    doNormalize.onChange =
+    geometry.onChange = flip;
 
 function flip()
 {
-    var oldGeom=geometry.get();
+    let oldGeom = geometry.get();
 
-    if(!oldGeom)
+    if (!oldGeom)
     {
         outGeom.set(null);
         return;
     }
 
+    let geom = oldGeom.copy();
 
-    var geom=oldGeom.copy();
-
-
-    if(doFlip.get())
+    if (doFlip.get())
     {
-        var i=0;
+        for (let i = 0; i < geom.vertexNormals.length; i++)
+            geom.vertexNormals[i] *= -1;
 
-        for(i=0;i<geom.vertexNormals.length;i++)
+        if (doNormalize.get())
         {
-            geom.vertexNormals[i]*=-1;
-        }
+            let vec = vec3.create();
 
-        if(doNormalize.get())
-        {
-            var vec=vec3.create();
-
-            for(i=0;i<geom.vertexNormals.length;i+=3)
+            for (let i = 0; i < geom.vertexNormals.length; i += 3)
             {
                 vec3.set(vec,
-                    geom.vertexNormals[i+0],
-                    geom.vertexNormals[i+1],
-                    geom.vertexNormals[i+2]);
-                vec3.normalize(vec,vec);
+                    geom.vertexNormals[i + 0],
+                    geom.vertexNormals[i + 1],
+                    geom.vertexNormals[i + 2]);
+                vec3.normalize(vec, vec);
 
-                geom.vertexNormals[i+0]=vec[0];
-                geom.vertexNormals[i+1]=vec[1];
-                geom.vertexNormals[i+2]=vec[2];
-             }
-         }
+                geom.vertexNormals[i + 0] = vec[0];
+                geom.vertexNormals[i + 1] = vec[1];
+                geom.vertexNormals[i + 2] = vec[2];
+            }
+        }
     }
 
     outGeom.set(geom);
-
 }
