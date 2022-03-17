7c677ada083e93791a2a676ef84cedf70d29d28c
pandrr <tom+github@undev.de>
2022-03-17 19:26:33 +0100

    ammo character / opentype op

--- src/ops/base/Ops.Dev.Opentype/Ops.Dev.Opentype.js
+++ src/ops/base/Ops.Dev.Opentype/Ops.Dev.Opentype.js
@@ -1,16 +1,18 @@
 const
     filename = op.inUrl("Font File", [".otf", ".ttf", ".woff", ".woff2"]),
-    inStr = op.inString("Text", "cables"),
-    outPathStr = op.outString("Path String");
+    // inStr = op.inString("Text", "cables"),
+    outFont = op.outObject("Opentype Font", null, "opentype");
+    // outPathStr = op.outString("Path String");
 
-inStr.onChange =
+// inStr.onChange =
 filename.onChange = async function ()
 {
     const font = await opentype.load(filename.get());
 
-    const path = font.getPath(inStr.get(), 0, 0, 72);
+    outFont.set(font);
+    // const path = font.getPath(inStr.get(), 0, 0, 72);
 
-    // console.log(font)
-    const pathStr = path.toPathData(8);
-    outPathStr.set(pathStr);
+    // // console.log(font)
+    // const pathStr = path.toPathData(8);
+    // outPathStr.set(pathStr);
 };
