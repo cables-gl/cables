c0206b72684f2cd01a15c5930dfd505c03901d35
pandrr <tom+github@undev.de>
2022-03-23 08:35:27 +0100

    debug m4

--- src/core/cgl/cgl_shader_uniform.js
+++ src/core/cgl/cgl_shader_uniform.js
@@ -653,7 +653,7 @@ Uniform.prototype.updateValueM4 = function ()
         this._cgl.profileData.profileShaderGetUniform++;
         this._cgl.profileData.profileShaderGetUniformName = this._name;
     }
-    if (!this._value || this.value.length != 16)console.log("this.name", this.name, this._value);
+    if (!this._value || this._value.length != 16)console.log("this.name", this.name, this._value);
     console.log("typem4 ", this._value.constructor.name);
     this._shader.getCgl().gl.uniformMatrix4fv(this._loc, false, this._value);
     this._cgl.profileData.profileUniformCount++;
