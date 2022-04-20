ba97c330337f30d85cd783ae2b7cba91eacf5a57
pandrr <tom+github@undev.de>
2022-04-20 09:28:01 +0200

    new imgcomp update ops

---
+++ src/ops/base/Ops.Dev.PerlinNoiseV2/Ops.Dev.PerlinNoiseV2.js
@@ -0,0 +1,112 @@
+const
+    render = op.inTrigger("render"),
+    inTexMask = op.inTexture("Mask"),
+    blendMode = CGL.TextureEffect.AddBlendSelect(op),
+    maskAlpha = CGL.TextureEffect.AddBlendAlphaMask(op),
+
+    amount = op.inValueSlider("Amount", 1),
+    inMode = op.inSwitch("Color", ["Mono", "RGB", "R", "G", "B"], "Mono"),
+    scale = op.inValue("Scale", 22),
+    rangeMul = op.inValue("Multiply", 1),
+    inHarmonics = op.inSwitch("Harmonics", ["1", "2", "3", "4", "5"], "1"),
+    x = op.inValue("X", 0),
+    y = op.inValue("Y", 0),
+    z = op.inValue("Z", 0),
+    trigger = op.outTrigger("trigger");
+
+const cgl = op.patch.cgl;
+const shader = new CGL.Shader(cgl, "perlinnoise");
+
+op.setPortGroup("Position", [x, y, z]);
+
+shader.setSource(shader.getDefaultVertexShader(), attachments.perlinnoise3d_frag);
+
+const
+    textureUniform = new CGL.Uniform(shader, "t", "tex", 0),
+    textureUniformOffZ = new CGL.Uniform(shader, "t", "texOffsetZ", 1),
+    textureUniformMask = new CGL.Uniform(shader, "t", "texMask", 2),
+
+    uniZ = new CGL.Uniform(shader, "f", "z", z),
+    uniX = new CGL.Uniform(shader, "f", "x", x),
+    uniY = new CGL.Uniform(shader, "f", "y", y),
+    uniScale = new CGL.Uniform(shader, "f", "scale", scale),
+    amountUniform = new CGL.Uniform(shader, "f", "amount", amount),
+    rangeMulUniform = new CGL.Uniform(shader, "f", "rangeMul", rangeMul);
+
+CGL.TextureEffect.setupBlending(op, shader, blendMode, amount, maskAlpha);
+
+// offsetMap
+
+const
+    inTexOffsetZ = op.inTexture("Offset"),
+    inOffsetMul = op.inFloat("Offset Multiply", 1),
+    offsetX = op.inSwitch("Offset X", ["None", "R", "G", "B"], "None"),
+    offsetY = op.inSwitch("Offset Y", ["None", "R", "G", "B"], "None"),
+    offsetZ = op.inSwitch("Offset Z", ["None", "R", "G", "B"], "R");
+
+op.setPortGroup("Offset Map", [inTexOffsetZ, offsetZ, offsetY, offsetX, inOffsetMul]);
+
+const uniOffMul = new CGL.Uniform(shader, "f", "offMul", inOffsetMul);
+
+const uniAspect = new CGL.Uniform(shader, "f", "aspect", 1);
+const uniHarmonics = new CGL.Uniform(shader, "f", "harmonics", 0);
+inHarmonics.onChange = () =>
+{
+    uniHarmonics.setValue(parseFloat(inHarmonics.get()));
+};
+
+offsetX.onChange =
+offsetY.onChange =
+offsetZ.onChange =
+inTexMask.onChange =
+inMode.onChange =
+inTexOffsetZ.onChange = updateDefines;
+updateDefines();
+
+function updateDefines()
+{
+    shader.toggleDefine("NO_CHANNEL_R", inMode.get() == "G" || inMode.get() == "B");
+    shader.toggleDefine("NO_CHANNEL_G", inMode.get() == "R" || inMode.get() == "B");
+    shader.toggleDefine("NO_CHANNEL_B", inMode.get() == "R" || inMode.get() == "G");
+
+    shader.toggleDefine("HAS_TEX_OFFSETMAP", inTexOffsetZ.get());
+    shader.toggleDefine("HAS_TEX_MASK", inTexMask.get());
+
+    shader.toggleDefine("OFFSET_X_R", offsetX.get() == "R");
+    shader.toggleDefine("OFFSET_X_G", offsetX.get() == "G");
+    shader.toggleDefine("OFFSET_X_B", offsetX.get() == "B");
+
+    shader.toggleDefine("OFFSET_Y_R", offsetY.get() == "R");
+    shader.toggleDefine("OFFSET_Y_G", offsetY.get() == "G");
+    shader.toggleDefine("OFFSET_Y_B", offsetY.get() == "B");
+
+    shader.toggleDefine("OFFSET_Z_R", offsetZ.get() == "R");
+    shader.toggleDefine("OFFSET_Z_G", offsetZ.get() == "G");
+    shader.toggleDefine("OFFSET_Z_B", offsetZ.get() == "B");
+
+    offsetX.setUiAttribs({ "greyout": !inTexOffsetZ.isLinked() });
+    offsetY.setUiAttribs({ "greyout": !inTexOffsetZ.isLinked() });
+    offsetZ.setUiAttribs({ "greyout": !inTexOffsetZ.isLinked() });
+    inOffsetMul.setUiAttribs({ "greyout": !inTexOffsetZ.isLinked() });
+
+    shader.toggleDefine("RGB", inMode.get() == "RGB");
+}
+
+render.onTriggered = function ()
+{
+    if (!CGL.TextureEffect.checkOpInEffect(op,3)) return;
+
+    cgl.pushShader(shader);
+    cgl.currentTextureEffect.bind();
+
+    uniAspect.setValue(cgl.currentTextureEffect.aspectRatio);
+
+    cgl.setTexture(0, cgl.currentTextureEffect.getCurrentSourceTexture().tex);
+    if (inTexOffsetZ.get()) cgl.setTexture(1, inTexOffsetZ.get().tex);
+    if (inTexMask.get()) cgl.setTexture(2, inTexMask.get().tex);
+
+    cgl.currentTextureEffect.finish();
+    cgl.popShader();
+
+    trigger.trigger();
+};
