2d901e4591f009ae278b766523e8f615aff5bcdd
pandrr <tom+github@undev.de>
2022-04-21 14:12:47 +0200

    noise v2

---
+++ src/ops/base/Ops.Dev.NoiseV2/Ops.Dev.NoiseV2.js
@@ -0,0 +1,53 @@
+const
+    render = op.inTrigger("Render"),
+    blendMode = CGL.TextureEffect.AddBlendSelect(op, "Blend Mode", "normal"),
+    maskAlpha = CGL.TextureEffect.AddBlendAlphaMask(op),
+    amount = op.inValueSlider("Amount", 1),
+    animated = op.inValueBool("Animated", true),
+    inRGB = op.inValueBool("RGB", false),
+    normalize = op.inValueBool("Normalize", false),
+    inTexMul = op.inTexture("Multiply"),
+    trigger = op.outTrigger("Next");
+
+const
+    cgl = op.patch.cgl,
+    shader = new CGL.Shader(cgl, op.name),
+    amountUniform = new CGL.Uniform(shader, "f", "amount", amount),
+    timeUniform = new CGL.Uniform(shader, "f", "time", 1.0),
+    textureUniform = new CGL.Uniform(shader, "t", "tex", 0),
+    mulUniform = new CGL.Uniform(shader, "t", "texMul", 1);
+
+shader.setSource(shader.getDefaultVertexShader(), attachments.noise_frag);
+
+CGL.TextureEffect.setupBlending(op, shader, blendMode, amount,maskAlpha);
+
+op.toWorkPortsNeedToBeLinked(render);
+
+inTexMul.onChange =
+normalize.onChange =
+inRGB.onChange = function ()
+{
+    shader.toggleDefine("HAS_MULMASK", inTexMul.get());
+    shader.toggleDefine("RGB", inRGB.get());
+    shader.toggleDefine("NORMALIZE", normalize.get());
+};
+
+render.onTriggered = function ()
+{
+    if (!CGL.TextureEffect.checkOpInEffect(op,3)) return;
+
+    if (animated.get()) timeUniform.setValue(op.patch.freeTimer.get() / 1000 % 100);
+    else timeUniform.setValue(0);
+
+    cgl.pushShader(shader);
+
+    cgl.setTexture(0, cgl.currentTextureEffect.getCurrentSourceTexture().tex);
+    if (inTexMul.get())cgl.setTexture(1, inTexMul.get().tex);
+
+    cgl.currentTextureEffect.bind();
+
+    cgl.currentTextureEffect.finish();
+    cgl.popShader();
+
+    trigger.trigger();
+};
