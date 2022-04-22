72af37e1f0fdb73e33a1ebda8ad41afe64b5f5d1
undev-studio <services@undev.de>
2022-04-22 11:49:38 +0200

    new op scalebynormal2

---
+++ src/ops/base/Ops.Gl.ShaderEffects.ScaleByNormal_v2/Ops.Gl.ShaderEffects.ScaleByNormal_v2.js
@@ -0,0 +1,25 @@
+const
+    render = op.inTrigger("render"),
+    inStrength = op.inValue("Strength", 1),
+    next = op.outTrigger("trigger"),
+    cgl = op.patch.cgl;
+
+const srcBodyVert = ""
+    .endl() + "pos=MOD_scaler(pos,mat3(modelMatrix)*attrVertNormal);"
+    .endl();
+
+const mod = new CGL.ShaderModifier(cgl, op.name);
+mod.addUniform("f", "MOD_strength", inStrength);
+mod.addModule({
+    "title": op.objName,
+    "name": "MODULE_VERTEX_POSITION",
+    "srcHeadVert": attachments.scalebynormal_vert,
+    "srcBodyVert": srcBodyVert
+});
+
+render.onTriggered = function ()
+{
+    mod.bind();
+    next.trigger();
+    mod.unbind();
+};
