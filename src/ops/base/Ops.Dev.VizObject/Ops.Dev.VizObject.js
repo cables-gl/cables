f30356fadcc77ba9c5c73e0c04f699f2450d3153
pandrr <tom@undev.de>
2022-04-28 09:39:18 +0200

    new op vizobject

---
+++ src/ops/base/Ops.Dev.VizObject/Ops.Dev.VizObject.js
@@ -0,0 +1,55 @@
+const inArr = op.inObject("Object");
+
+op.setUiAttrib({ "height": 200, "width": 400, "resizable": true });
+
+op.renderVizLayer = (ctx, layer) =>
+{
+    ctx.fillStyle = "#222";
+    ctx.fillRect(layer.x, layer.y, layer.width, layer.height);
+
+    ctx.save();
+    ctx.scale(layer.scale, layer.scale);
+
+    ctx.font = "normal 10px sourceCodePro";
+    ctx.fillStyle = "#ccc";
+
+    let obj = inArr.get();
+    const padding = 10;
+
+    let str = "???";
+
+    if (obj && obj.getInfo)
+    {
+        obj=obj.getInfo();
+    }
+
+    if(obj instanceof Element)
+    {
+        const o={};
+
+        o.id=obj.getAttribute("id");
+        o.classes=obj.classList.value;
+        o.innerText=obj.innerText;
+
+        obj=o;
+    }
+
+    try
+    {
+        str = JSON.stringify(obj, false, 4);
+    }
+    catch (e)
+    {
+        str = "object can not be displayed as string";
+    }
+
+    if(str===undefined)str="undefined";
+    if(str===null)str="null";
+    str=String(str);
+    let lines = str.split("\n");
+
+    for (let j = 0; j < lines.length; j++)
+        ctx.fillText(lines[j], layer.x / layer.scale + padding, layer.y / layer.scale + ((j + 1) * 12));
+
+    ctx.restore();
+};
