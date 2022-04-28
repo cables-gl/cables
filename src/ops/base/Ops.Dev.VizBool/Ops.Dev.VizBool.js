e6c6f6ebdceb83ed19e67a819133bd54803b0427
pandrr <tom@undev.de>
2022-04-28 08:28:01 +0200

    vizbool tweaks

--- src/ops/base/Ops.Dev.VizBool/Ops.Dev.VizBool.js
+++ src/ops/base/Ops.Dev.VizBool/Ops.Dev.VizBool.js
@@ -11,22 +11,22 @@ op.renderPreviewLayer = (ctx, pos, size) =>
 
     const sc = 1000 / gui.patchView._patchRenderer.viewBox.zoom * 1.5;
 
-    let v = !!inNum.get();
+    let isTrue = !!inNum.get();
 
-    let circle = new Path2D(); // <<< Declaration
-    let radius = size[1] / 2.4;
+    let circle = new Path2D();
+    let radius = Math.min(size[1], size[0]) / 2.4 * 0.8;
     if (radius < 0)radius = 0;
-    circle.arc(pos[0] + size[0] / 2, pos[1] + size[1] / 2, radius * 0.8, 0, 2 * Math.PI, false);
+    circle.arc(pos[0] + size[0] / 2, pos[1] + size[1] / 2, radius, 0, 2 * Math.PI, false);
 
     ctx.strokeStyle = "#555";
     ctx.lineWidth = 7 * sc;
     ctx.stroke(circle);
 
-    if (v)
+    if (isTrue)
     {
         ctx.fillStyle = "#ccc";
-        let circle = new Path2D(); // <<< Declaration
-        circle.arc(pos[0] + size[0] / 2, pos[1] + size[1] / 2, radius / 1.5, 0, 2 * Math.PI, false);
+        let circle = new Path2D();
+        circle.arc(pos[0] + size[0] / 2, pos[1] + size[1] / 2, radius - (ctx.lineWidth / 2), 0, 2 * Math.PI, false);
         ctx.fill(circle);
     }
 };
