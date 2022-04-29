04d64f7d81b290fc327dd3158e87ceba71dd2003
pandrr <tom@tomkom.net>
2022-04-29 17:30:55 +0200

    wrap and filter for hdr/rgbe converters

--- src/ops/base/Ops.Gl.Textures.HdrToRgbeTexture/Ops.Gl.Textures.HdrToRgbeTexture.js
+++ src/ops/base/Ops.Gl.Textures.HdrToRgbeTexture/Ops.Gl.Textures.HdrToRgbeTexture.js
@@ -2,14 +2,35 @@ const
     exec = op.inTrigger("Execute"),
     inTex = op.inTexture("RGBE Texture"),
     next = op.outTrigger("Next"),
-    outFpTex = op.outTexture("HDR Textiure");
+    outFpTex = op.outTexture("HDR Texture"),
+    tfilter = op.inSwitch("Filter", ["nearest", "linear", "mipmap"], "linear"),
+    twrap = op.inValueSelect("Wrap", ["clamp to edge", "repeat", "mirrored repeat"], "repeat");
 
-const tc = new CGL.CopyTexture(op.patch.cgl, "rgbe2hdr",
-    {
-        "shader": attachments.rgbe2fp_frag,
-        "isFloatingPointTexture": true,
-        "filter": CGL.Texture.FILTER_NEAREST
-    });
+let tc = null;
+
+twrap.onChange =
+    tfilter.onChange = init;
+init();
+
+function init()
+{
+    let wrap = CGL.Texture.WRAP_REPEAT;
+    if (twrap.get() == "mirrored repeat") wrap = CGL.Texture.WRAP_MIRRORED_REPEAT;
+    if (twrap.get() == "clamp to edge") wrap = CGL.Texture.WRAP_CLAMP_TO_EDGE;
+
+    let filter = CGL.Texture.FILTER_NEAREST;
+    if (tfilter.get() == "linear") filter = CGL.Texture.FILTER_LINEAR;
+    if (tfilter.get() == "mipmap") filter = CGL.Texture.FILTER_MIPMAP;
+
+    if (tc)tc.dispose();
+    tc = new CGL.CopyTexture(op.patch.cgl, "rgbe2hdr",
+        {
+            "shader": attachments.rgbe2fp_frag,
+            "isFloatingPointTexture": false,
+            "filter": filter,
+            "wrap": wrap
+        });
+}
 
 outFpTex.set(CGL.Texture.getEmptyTexture(op.patch.cgl));
 
