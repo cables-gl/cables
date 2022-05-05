217dcb6293db76159646aad3d0bfaac0a74e4e2d
pandrr <tom@undev.de>
2022-05-05 10:59:07 +0200

    VizNumberBar

---
+++ src/ops/base/Ops.Ui.VizNumberBar/Ops.Ui.VizNumberBar.js
@@ -0,0 +1,55 @@
+const
+    inNum = op.inFloat("Number", 0);
+    // inDrawBar=op.inBool("Draw Bar",true),
+    // inDrawNUm=op.inBool("Draw Number",true);
+
+op.setUiAttrib({ "height": 100, "width": 100, "resizable": true });
+
+let max = -Number.MAX_VALUE;
+let min = Number.MAX_VALUE;
+
+inNum.onLinkChanged = () =>
+{
+    max = -Number.MAX_VALUE;
+    min = Number.MAX_VALUE;
+};
+
+op.renderVizLayer = (ctx, layer) =>
+{
+    ctx.fillStyle = "#222";
+    ctx.fillRect(
+        layer.x, layer.y,
+        layer.width, layer.height);
+
+    // if(inDrawBar.get())
+    {
+        max = Math.max(max, inNum.get());
+        min = Math.min(min, inNum.get());
+
+
+        if(op.uiAttribs.color)ctx.fillStyle = op.uiAttribs.color;
+        else ctx.fillStyle = "#555";
+
+        let a=CABLES.map(0,min,max,0,layer.width);
+        let b=CABLES.map(inNum.get(),min,max,0,layer.width);
+
+        let xMin=Math.min(a,b);
+        let xMax=Math.max(a,b);
+
+        ctx.fillRect(
+            xMin+layer.x, layer.y,
+            xMax-xMin, layer.height);
+    }
+
+    // if(inDrawNUm.get())
+    {
+        const padding = 10;
+        if(op.uiAttribs.color)ctx.fillStyle = "#fff";
+        else ctx.fillStyle = "#ccc";
+
+        const fontSize = layer.height * 0.7;
+        ctx.font = "normal " + fontSize + "px sourceCodePro";
+        ctx.fillText(Math.round(inNum.get() * 10000) / 10000, layer.x + padding, layer.y + fontSize);
+    }
+
+};
