60bc0b8cfe47bcda390294211904872d9b8c10a2
pandrr <tom@tomkom.net>
2022-05-19 08:28:09 +0200

    new version concatmulti

---
+++ src/ops/base/Ops.String.ConcatMulti_v2/Ops.String.ConcatMulti_v2.js
@@ -0,0 +1,35 @@
+const addSpacesCheckBox = op.inBool("add spaces", false),
+    newLinesCheckBox = op.inBool("new lines", false),
+    stringPorts = [],
+    result = op.outString("concat string");
+
+stringPorts.onChange = addSpacesCheckBox.onChange =
+newLinesCheckBox.onChange = update;
+
+addSpacesCheckBox.setUiAttribs({ "hidePort": true });
+newLinesCheckBox.setUiAttribs({ "hidePort": true });
+
+for (let i = 0; i < 8; i++)
+{
+    let p = op.inString("string " + i);
+    stringPorts.push(p);
+    p.onChange = update;
+}
+
+function update()
+{
+    let str = "";
+    let nl = "";
+    let space = addSpacesCheckBox.get();
+
+    for (let i = 0; i < stringPorts.length; i++)
+    {
+        const inString = stringPorts[i].get();
+        if (!inString) continue;
+        if (i > 0 && space) str += " ";
+        if (i > 0 && newLinesCheckBox.get()) nl = "\n";
+        str += nl;
+        str += inString;
+    }
+    result.set(str);
+}
