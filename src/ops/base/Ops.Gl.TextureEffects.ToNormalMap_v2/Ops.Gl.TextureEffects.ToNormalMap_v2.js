6c21758b16ae76868251d265da40ecb3f9cfb01a
pandrr <tom@tomkom.net>
2022-05-28 12:19:52 +0200

    tonormalmap pandrr/cables/issues/3610

---
+++ src/ops/base/Ops.Gl.TextureEffects.ToNormalMap_v2/Ops.Gl.TextureEffects.ToNormalMap_v2.js
@@ -0,0 +1,37 @@
+const
+    render = op.inTrigger("render"),
+    trigger = op.outTrigger("trigger"),
+    strength = op.inValue("Strength", 4),
+    sizeMul = op.inValue("Step Multiplier", 1);
+
+const
+    cgl = op.patch.cgl,
+    shader = new CGL.Shader(cgl, op.name);
+
+// from: https://forum.openframeworks.cc/t/compute-normal-map-from-image/1400/11
+shader.setSource(shader.getDefaultVertexShader(), attachments.tonormal_frag);
+
+const
+    textureUniform = new CGL.Uniform(shader, "t", "tex", 0),
+    uniStrength = new CGL.Uniform(shader, "f", "strength", strength),
+    unisizeMul = new CGL.Uniform(shader, "f", "sizeMul", sizeMul),
+    uniSize = new CGL.Uniform(shader, "2f", "size", 0,0);
+
+render.onTriggered = function ()
+{
+    if (!CGL.TextureEffect.checkOpInEffect(op)) return;
+
+    cgl.pushShader(shader);
+    cgl.currentTextureEffect.bind();
+
+    const effectTex=cgl.currentTextureEffect.getCurrentSourceTexture();
+
+    cgl.setTexture(0, effectTex.tex);
+
+    uniSize.setValue([1/effectTex.width,1/effectTex.height]);
+
+    cgl.currentTextureEffect.finish();
+    cgl.popShader();
+
+    trigger.trigger();
+};
