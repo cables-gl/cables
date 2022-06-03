768d4569df5d799913167650d61dbbc768bbe5dc
pandrr <tom@tomkom.net>
2022-06-03 12:55:00 +0200

    docs/noisetexture seed

--- src/ops/base/Ops.Physics.Ammo.AmmoCharacter/Ops.Physics.Ammo.AmmoCharacter.js
+++ src/ops/base/Ops.Physics.Ammo.AmmoCharacter/Ops.Physics.Ammo.AmmoCharacter.js
@@ -129,6 +129,9 @@ function renderTransformed()
         let p = tmpTrans.getOrigin();
         let q = tmpTrans.getRotation();
 
+        if (inStyle.get() == "3rd Person")
+            q.setValue(tmpQuat[0], tmpQuat[1], tmpQuat[2], tmpQuat[3]);
+
         cgl.pushModelMatrix();
 
         mat4.identity(cgl.mMatrix);
@@ -183,21 +186,24 @@ function update()
 
     if (inStyle.get() == "3rd Person")
     {
-        if (inMoveXM.get()) vx = -speed;
-        if (inMoveXP.get()) vx = speed;
+    //     if (inMoveXM.get()) vx = -speed;
+    //     if (inMoveXP.get()) vx = speed;
 
-        if (inMoveZP.get()) vz = -speed;
-        if (inMoveZM.get()) vz = speed;
+        //     if (inMoveZP.get()) vz = -speed;
+        //     if (inMoveZM.get()) vz = speed;
 
-        if (inMoveYP.get()) vy = speed;
+        //     if (inMoveYP.get()) vy = speed;
 
-        if (vx != 0 || vy != 0 || vz != 0)
-        {
-            btVelocity.setValue(vx, vy, vz);
-            body.setLinearVelocity(btVelocity);
-        }
+    //     if (vx != 0 || vy != 0 || vz != 0)
+    //     {
+    //         btVelocity.setValue(vx, vy, vz);
+    //         body.setLinearVelocity(btVelocity);
+    //     }
+        // inDirX.set(1);
+        // inDirY.set(0);
+        // inDirZ.set(1);
     }
-    else
+    // else
     {
         let doMove = false;
         if (inMoveZP.get())
