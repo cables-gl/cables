38e698d4efcac26803210d2761db30b946ad6d06
Alexander Scholten <alexander.scholten@lightshape.net>
2022-08-08 10:04:22 +0200

    added option to use sharpening as modifier

--- src/ops/base/Ops.Dev.AlphaToCoverage/Ops.Dev.AlphaToCoverage.js
+++ src/ops/base/Ops.Dev.AlphaToCoverage/Ops.Dev.AlphaToCoverage.js
@@ -2,13 +2,27 @@ const
     render=op.inTrigger("render"),
     trigger=op.outTrigger("trigger"),
     enable=op.inValueBool("enable",true),
+    sharpen=op.inValueBool("sharpen",false),
+    cutoff=op.inFloat("cutoff", 0.5),
     cgl=op.patch.cgl;
+
+const srcHeadFrag = "";
+const mod = new CGL.ShaderModifier(cgl, op.name);
+mod.addModule({
+    "title": op.name,
+    "name": "MODULE_COLOR",
+    "srcHeadFrag": srcHeadFrag,
+    "srcBodyFrag": attachments.ATCSharpen_frag
+});
+mod.addUniform("f", "MOD_cutoff", cutoff);
 
 render.onTriggered=function()
 {
     if(enable.get()) cgl.gl.enable(cgl.gl.SAMPLE_ALPHA_TO_COVERAGE);
         else cgl.gl.disable(cgl.gl.SAMPLE_ALPHA_TO_COVERAGE);
 
+    if(sharpen.get()) mod.bind();
     trigger.trigger();
+    if(sharpen.get()) mod.unbind();
     cgl.gl.disable(cgl.gl.SAMPLE_ALPHA_TO_COVERAGE);
 };
---
+++ src/ops/base/Ops.Dev.AlphaToCoverage/att_ATCSharpen.frag
@@ -0,0 +1,3 @@
+    
+    col.a = (col.a - MOD_cutoff) / max(fwidth(col.a), 0.0001) + 0.5;
+    
