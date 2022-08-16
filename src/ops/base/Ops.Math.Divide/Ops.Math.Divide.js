45c62f148842d004fce0305f19a5a000a782d296
pandrr <tom@undev.de>
2022-08-16 19:07:47 +0200

    math op titles, lets see if we hate this...

--- src/ops/base/Ops.Math.Divide/Ops.Math.Divide.js
+++ src/ops/base/Ops.Math.Divide/Ops.Math.Divide.js
@@ -1,13 +1,14 @@
 const
-    number1 = op.inValueFloat("number1",1),
-    number2 = op.inValueFloat("number2",2),
+    number1 = op.inValueFloat("number1", 1),
+    number2 = op.inValueFloat("number2", 2),
     result = op.outValue("result");
 
-number1.onChange=number2.onChange=exec;
+op.setTitle("/");
+
+number1.onChange = number2.onChange = exec;
 exec();
 
 function exec()
 {
-    result.set( number1.get() / number2.get() );
+    result.set(number1.get() / number2.get());
 }
-
--- src/ops/base/Ops.Math.Subtract/Ops.Math.Subtract.js
+++ src/ops/base/Ops.Math.Subtract/Ops.Math.Subtract.js
@@ -1,15 +1,16 @@
 const
-    number1=op.inValue("number1",1),
-    number2=op.inValue("number2",1),
-    result=op.outValue("result");
+    number1 = op.inValue("number1", 1),
+    number2 = op.inValue("number2", 1),
+    result = op.outValue("result");
 
-number1.onChange=exec;
-number2.onChange=exec;
+op.setTitle("-");
+
+number1.onChange =
+    number2.onChange = exec;
 exec();
 
 function exec()
 {
-    var v=number1.get()-number2.get();
-    if(!isNaN(v)) result.set( v );
+    let v = number1.get() - number2.get();
+    if (!isNaN(v)) result.set(v);
 }
-
