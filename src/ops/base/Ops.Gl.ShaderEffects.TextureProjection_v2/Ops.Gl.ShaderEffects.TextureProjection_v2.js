6d7c047d004384a0ac045229907e4422a5aae237
pandrr <tom@undev.de>
2022-08-31 13:40:58 +0200

    textureprojection v2 add blendmodes pandrr/cables/issues/3860

---
+++ src/ops/base/Ops.Gl.ShaderEffects.TextureProjection_v2/Ops.Gl.ShaderEffects.TextureProjection_v2.js
@@ -0,0 +1,105 @@
+const
+    render = op.inTrigger("render"),
+    next = op.outTrigger("trigger"),
+    inScale = op.inValue("Scale", 10),
+
+
+    inTarget = op.inSwitch("Target", ["Color", "Pointsize"], "Color"),
+
+    inBlend = CGL.TextureEffect.AddBlendSelect(op, "blendMode"),
+    inAmount = op.inValueSlider("Amount", 0.3),
+
+    inUseTexAlpha = op.inBool("Use Texture Alpha", false),
+
+    inPosX = op.inFloat("Pos X", 0),
+    inPosY = op.inFloat("Pos Y", 0),
+
+    inRotX = op.inFloat("Rot X", 0),
+    inRotY = op.inFloat("Rot Y", 0),
+    inRotZ = op.inFloat("Rot Z", 0),
+
+    inTex = op.inTexture("Texture"),
+    inMethod = op.inValueSelect("Mapping", ["Triplanar", "XY", "XZ", "YZ", "Screen"], "XY"),
+    inDiscard = op.inValueBool("Discard"),
+    inWorldSpace = op.inValueBool("WorldSpace");
+
+const cgl = op.patch.cgl;
+
+inUseTexAlpha.onChange =
+inTarget.onChange =
+inBlend.onChange = inDiscard.onChange = inWorldSpace.onChange = inMethod.onChange = updateDefines;
+
+op.setPortGroup("Rotation", [inRotX, inRotY, inRotZ]);
+op.setPortGroup("Position", [inPosX, inPosY]);
+
+const mod = new CGL.ShaderModifier(cgl, op.name);
+mod.addModule({
+    "title": op.name,
+    "name": "MODULE_VERTEX_POSITION",
+    "srcHeadVert": attachments.maptexture_body_vert,
+    "srcBodyVert": attachments.maptexture_vert
+});
+
+let head_frag=attachments.maptexture_frag;
+head_frag=head_frag.replace("{{BLENDCODE}}",CGL.TextureEffect.getBlendCode(3));
+
+mod.addModule({
+    "title": op.name,
+    "name": "MODULE_COLOR",
+    "srcHeadFrag": head_frag,
+    "srcBodyFrag": attachments.maptexture_body_frag
+});
+
+mod.addUniformBoth("f", "MOD_rotX", inRotX);
+mod.addUniformBoth("f", "MOD_rotY", inRotY);
+mod.addUniformBoth("f", "MOD_rotZ", inRotZ);
+
+mod.addUniformBoth("t", "MOD_tex");
+mod.addUniformBoth("f", "MOD_scale", inScale);
+mod.addUniformBoth("f", "MOD_amount", inAmount);
+mod.addUniformBoth("2f", "MOD_offset", inPosX, inPosY);
+
+const uniWidth = mod.addUniformFrag("f", "MOD_viewPortW");
+const uniHeight = mod.addUniformFrag("f", "MOD_viewPortH");
+
+CGL.TextureEffect.setupBlending(op, mod, inBlend, inAmount);
+
+updateDefines();
+
+function updateDefines()
+{
+    mod.toggleDefine("MOD_USE_IMGALPHA", inUseTexAlpha.get());
+    mod.toggleDefine("MOD_WORLDSPACE", inWorldSpace.get());
+    mod.toggleDefine("MOD_MAP_XY", inMethod.get() == "XY");
+    mod.toggleDefine("MOD_MAP_XZ", inMethod.get() == "XZ");
+    mod.toggleDefine("MOD_MAP_YZ", inMethod.get() == "YZ");
+    mod.toggleDefine("MOD_MAP_SCREEN", inMethod.get() == "Screen");
+    mod.toggleDefine("MOD_MAP_TRIPLANAR", inMethod.get() == "Triplanar");
+    mod.toggleDefine("MOD_DISCARD", inDiscard.get());
+
+    mod.toggleDefine("MOD_BLEND_NORMAL", inBlend.get() == "Normal");
+    mod.toggleDefine("MOD_BLEND_ADD", inBlend.get() == "Add");
+    mod.toggleDefine("MOD_BLEND_MUL", inBlend.get() == "Mul");
+    mod.toggleDefine("MOD_BLEND_MUL", inBlend.get() == "Mul");
+
+    mod.toggleDefine("MOD_TARGET_COLOR", inTarget.get() == "Color");
+    mod.toggleDefine("MOD_TARGET_POINTSIZE", inTarget.get() == "Pointsize");
+}
+
+render.onTriggered = function ()
+{
+    const vp = cgl.getViewPort();
+
+    mod.setUniformValue("MOD_viewPortW", vp[2]);
+    mod.setUniformValue("MOD_viewPortH", vp[3]);
+
+    mod.bind();
+    let tex = inTex.get();
+    if (!tex) tex = CGL.Texture.getEmptyTexture(cgl).tex;
+    else tex = tex.tex;
+
+    mod.pushTexture("MOD_tex", tex);
+
+    next.trigger();
+    mod.unbind();
+};
---
+++ src/ops/base/Ops.Gl.ShaderEffects.TextureProjection_v2/att_maptexture.vert
@@ -0,0 +1,38 @@
+vec3 MOD_pos;
+
+#ifndef MOD_WORLDSPACE
+   MOD_pos=(vec4(vPosition,1.0)*1.0/MOD_scale).xyz;
+#endif
+#ifdef MOD_WORLDSPACE
+   MOD_pos=(mMatrix*pos).xyz*1.0/MOD_scale;
+#endif
+
+MOD_pos=(vec4(MOD_pos,1.0)*MOD_rotationX(MOD_rotX*MOD_DEG2RAD)).xyz;
+MOD_pos=(vec4(MOD_pos,1.0)*MOD_rotationY(MOD_rotY*MOD_DEG2RAD)).xyz;
+MOD_pos=(vec4(MOD_pos,1.0)*MOD_rotationZ(MOD_rotZ*MOD_DEG2RAD)).xyz;
+
+#ifdef MOD_MAP_XY
+    MOD_tc=MOD_pos.xy;
+#endif
+#ifdef MOD_MAP_XZ
+    MOD_tc=MOD_pos.xz;
+#endif
+#ifdef MOD_MAP_YZ
+    MOD_tc=MOD_pos.yz;
+#endif
+
+MOD_tc.xy+=vec2(0.5,0.5);
+MOD_tc.xy+=MOD_offset;
+
+
+#ifdef MOD_TARGET_POINTSIZE
+
+    gl_PointSize+=texture(MOD_tex,MOD_tc).x*MOD_amount;
+
+#endif
+
+
+#ifdef MOD_MAP_TRIPLANAR
+    mapTriplanar((mMatrix*vec4(attrVertNormal,1.0)).xyz,MOD_pos);
+#endif
+
