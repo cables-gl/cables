05c91c249a8e249b419af0bee483add738db29cc
pandrr <tom@undev.de>
2022-09-06 09:39:29 +0200

    dont render helpers if current framebuffer ratio is different than canvas pandrr/cables/issues/2668  cables-gl/cables_docs/discussions/327

--- src/core/cgl/cgl_state.js
+++ src/core/cgl/cgl_state.js
@@ -1247,6 +1247,13 @@ Context.prototype.shouldDrawHelpers = function (op)
     if (this.frameStore.shadowPass) return false;
     if (!op.patch.isEditorMode()) return false;
 
+    const fb = this.getCurrentFrameBuffer();
+    if (fb && fb.getWidth)
+    {
+        const fbshould = this.canvasWidth / this.canvasHeight == fb.getWidth() / fb.getHeight();
+        if (!fbshould) return false;
+    }
+
     return CABLES.UI.renderHelper || (CABLES.UI.renderHelperCurrent && op.isCurrentUiOp());
 };
 
