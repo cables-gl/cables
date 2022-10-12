2d4da790b8cf9e5990c911ff550275f7f5026350
pandrr <tom@tomkom.net>
2022-10-12 10:59:49 +0200

    outvalue

--- src/ops/base/Ops.Anim.Bang/Ops.Anim.Bang.js
+++ src/ops/base/Ops.Anim.Bang/Ops.Anim.Bang.js
@@ -1,29 +1,29 @@
 const
-    inUpdate=op.inTrigger("update"),
-    inBang=op.inTriggerButton("Bang"),
-    inDuration=op.inValue("Duration",0.1),
-    invert=op.inBool("Invert",false),
+    inUpdate = op.inTrigger("update"),
+    inBang = op.inTriggerButton("Bang"),
+    inDuration = op.inValue("Duration", 0.1),
+    invert = op.inBool("Invert", false),
     outTrigger = op.outTrigger("Trigger Out"),
-    outValue=op.outValue("Value");
+    outValue = op.outNumber("Value");
 
-const anim=new CABLES.Anim();
-var startTime=CABLES.now();
+const anim = new CABLES.Anim();
+let startTime = CABLES.now();
 op.toWorkPortsNeedToBeLinked(inUpdate);
 
-inBang.onTriggered=function()
+inBang.onTriggered = function ()
 {
-    startTime=CABLES.now();
+    startTime = CABLES.now();
 
     anim.clear();
-    anim.setValue(0,1);
-    anim.setValue(inDuration.get(),0);
+    anim.setValue(0, 1);
+    anim.setValue(inDuration.get(), 0);
 };
 
-inUpdate.onTriggered=function()
+inUpdate.onTriggered = function ()
 {
-    var v=anim.getValue((CABLES.now()-startTime)/1000);
-    if(invert.get()) outValue.set(1.0-v);
-        else outValue.set(v);
+    let v = anim.getValue((CABLES.now() - startTime) / 1000);
+    if (invert.get()) outValue.set(1.0 - v);
+    else outValue.set(v);
 
     outTrigger.trigger();
 };
