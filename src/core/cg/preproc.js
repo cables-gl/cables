32e697b9797c4e3959904a46ae0f764c91a5cd34
pandrr <tom@undev.de>
2022-10-25 13:59:54 +0200

    subpatch patchoutput copypaste pandrr/cables/issues/3867

--- src/core/cg/preproc.js
+++ src/core/cg/preproc.js
@@ -534,3 +534,9 @@ Preprocessor.prototype.process = function (defines, verbose)
 };
 
 export default Preprocessor;
+
+
+// const pp = new Preprocessor("// #define AAAA\nhallo AAAA!\n// #ifdef AAAA\njaja is defined!\n// #else\n hurziburzi\n// #endif\nhuhu", ".", true);
+// // debugger;
+// const r = pp.process({ "a": 1 });
+// console.log(r);
