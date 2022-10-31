759678a96f32093186adb72166ee1b04ee95c219
pandrr <tom@tomkom.net>
2022-10-31 14:36:41 +0100

    reducearray

--- src/ops/base/Ops.Array.ReduceArray3_v3/Ops.Array.ReduceArray3_v3.js
+++ src/ops/base/Ops.Array.ReduceArray3_v3/Ops.Array.ReduceArray3_v3.js
@@ -1,32 +1,99 @@
 const
-    arr=op.inArray("Array"),
-    num=op.inValueInt("Every xth Item",2),
-    outArr=op.outArray("Result Array");
+    arr = op.inArray("Array"),
+    inMeth = op.inSwitch("Remove", ["Xth Item", "Random", "Duplicates"], "Xth Item"),
+    num = op.inValueInt("Every xth Item", 2),
+    inThresh = op.inFloatSlider("Threshold", 0.5),
+    inSeed = op.inFloat("Seed", 1),
+    outArr = op.outArray("Result Array");
 
-num.onChange=
-    arr.onChange=update;
+num.onChange =
+    inThresh.onChange =
+    inSeed.onChange =
+    arr.onChange = update;
 
-const newArray=[];
+const newArray = [];
+
+let updateMethod = updateXth;
+
+inMeth.onChange = () =>
+{
+    if (inMeth.get() == "Xth Item")updateMethod = updateXth;
+    if (inMeth.get() == "Random")updateMethod = updateRandom;
+    if (inMeth.get() == "Duplicates")updateMethod = updateDupes;
+
+    update();
+};
 
 function update()
 {
-    var theArray=arr.get();
-    if(!theArray)return;
+    const theArray = arr.get();
+    if (!theArray) return;
 
-    const step=Math.max(0,Math.floor(num.get()));
-    const newArray=[];
+    let newArray = updateMethod(theArray);
 
-    if(step===0)
+    outArr.set(null);
+    outArr.set(newArray);
+}
+
+function updateDupes(theArray)
+{
+    const noDupesArr = [];
+
+    for (let i = 0; i < theArray.length; i += 3)
+    {
+        let found = false;
+        for (let j = 0; j < noDupesArr.length; j += 3)
+        {
+            if (
+                theArray[i] == noDupesArr[j] &&
+                theArray[i + 1] == noDupesArr[j + 1] &&
+                theArray[i + 2] == noDupesArr[j + 2]
+            )
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            noDupesArr.push(theArray[i], theArray[i + 1], theArray[i + 2]);
+        }
+    }
+    return noDupesArr;
+}
+
+function updateRandom(theArray)
+{
+    Math.randomSeed = inSeed.get();
+
+    const newArray = [];
+
+    const thresh = inThresh.get();
+
+    for (let i = 0; i < theArray.length; i += 3)
+    {
+        if (Math.seededRandom() > thresh)
+            newArray.push(theArray[i + 0], theArray[i + 1], theArray[i + 2]);
+    }
+
+    return newArray;
+}
+
+function updateXth(theArray)
+{
+    const step = Math.max(0, Math.floor(num.get()));
+    const newArray = [];
+
+    if (step === 0)
     {
         outArr.set(null);
         outArr.set([]);
         return;
     }
 
-    for(var i=0;i<theArray.length;i+=step*3)
-        newArray.push(theArray[i+0],theArray[i+1],theArray[i+2]);
+    for (let i = 0; i < theArray.length; i += step * 3)
+        newArray.push(theArray[i + 0], theArray[i + 1], theArray[i + 2]);
 
-    outArr.set(null);
-    outArr.set(newArray);
-
+    return newArray;
 }
