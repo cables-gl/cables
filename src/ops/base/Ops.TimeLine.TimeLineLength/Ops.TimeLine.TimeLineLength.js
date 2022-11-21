7b49fc1739d288e882b0aa894f6e7fae20f08391
pandrr <tom@tomkom.net>
2022-11-21 12:16:21 +0100

    new op timelinelength

---
+++ src/ops/base/Ops.TimeLine.TimeLineLength/Ops.TimeLine.TimeLineLength.js
@@ -0,0 +1,27 @@
+// uses its input port for caching the length of the timeline...
+
+const
+    inLength = op.inFloat("len", 0),
+    outLength = op.outNumber("Length");
+
+inLength.setUiAttribs({ "hidePort": true, "hideParam": true });
+
+if (CABLES.UI)
+{
+    inLength.set(gui.timeLine().getTimeLineLength());
+
+    gui.on("timelineControl", (cmd, l) =>
+    {
+        if (cmd === "setLength")
+        {
+            inLength.set(l);
+            outLength.set(inLength.get());
+        }
+    });
+    outLength.set(inLength.get());
+}
+
+op.on("loadedValueSet", () =>
+{
+    outLength.set(inLength.get());
+});
