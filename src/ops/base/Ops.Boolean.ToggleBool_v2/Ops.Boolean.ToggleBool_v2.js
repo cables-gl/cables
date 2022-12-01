6e5bd1798b6a9487b4d666c8757ea3dc66f6e67f
pandrr <tom@undev.de>
2022-12-01 09:43:36 +0100

    pandrr/cables/issues/4117 togglebool default

---
+++ src/ops/base/Ops.Boolean.ToggleBool_v2/Ops.Boolean.ToggleBool_v2.js
@@ -0,0 +1,24 @@
+const
+    trigger = op.inTriggerButton("trigger"),
+    reset = op.inTriggerButton("reset"),
+    outBool = op.outBool("result"),
+    inDefault = op.inBool("Default", false);
+
+let theBool = false;
+
+op.onLoadedValueSet = () =>
+{
+    outBool.set(inDefault.get());
+};
+
+trigger.onTriggered = function ()
+{
+    theBool = !theBool;
+    outBool.set(theBool);
+};
+
+reset.onTriggered = function ()
+{
+    theBool = inDefault.get();
+    outBool.set(theBool);
+};
---
+++ src/ops/base/Ops.Boolean.ToggleBool_v2/Ops.Boolean.ToggleBool_v2.md
@@ -0,0 +1,1 @@
+Outputs a boolean value which changes state every time `Trigger` is executed, this will result in an output like `true` -> `false` -> `true` -> `false` …  
