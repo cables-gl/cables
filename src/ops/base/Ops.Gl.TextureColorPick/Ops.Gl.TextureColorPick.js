917aeea16a835448868a57cf160f438c84a25bb4
pandrr <tom@undev.de>
2023-02-02 18:27:18 +0100

    fence sync and pixel pack buffers

--- src/ops/base/Ops.Gl.TextureColorPick/Ops.Gl.TextureColorPick.js
+++ src/ops/base/Ops.Gl.TextureColorPick/Ops.Gl.TextureColorPick.js
@@ -18,6 +18,7 @@ let
     wasTriggered = true,
     texChanged = false;
 
+let finishedFence = true;
 tex.onChange = function () { texChanged = true; };
 
 let isFloatingPoint = false;
@@ -25,105 +26,50 @@ let channelType = op.patch.cgl.gl.UNSIGNED_BYTE;
 
 function fence()
 {
-    return new Promise(function (resolve)
+    finishedFence = false;
+    return new Promise(function (resolve, reject)
     {
         const gl = op.patch.cgl.gl;
         let sync = gl.fenceSync(gl.SYNC_GPU_COMMANDS_COMPLETE, 0);
+        if (!sync) return;
         gl.flush(); // Ensure the fence is submitted.
         function check()
         {
-            let status = gl.getSyncParameter(sync, gl.SYNC_STATUS);
-            if (status == gl.SIGNALED)
+            // let status = gl.getSyncParameter(sync, gl.SYNC_STATUS);
+            const status = gl.clientWaitSync(sync, 0, 0);
+
+            if (status == gl.WAIT_FAILED)
             {
-                gl.deleteSync(sync);
+                // gl.deleteSync(sync);
+                reject();
+                console.log("wait failed");
+            }
+            else
+            if (status == gl.TIMEOUT_EXPIRED)
+            {
+                // gl.deleteSync(sync);
+                // console.log("timeout expired");
+                setTimeout(check, 0);
+            }
+            else
+            if (status == gl.CONDITION_SATISFIED)
+            {
+                // console.log("RESOLVE", status);
                 resolve();
+                gl.deleteSync(sync);
             }
             else
             {
-                setTimeout(check, 0);
+                console.log("WRTF", status);
             }
         }
 
-        setTimeout(check, 0);
+        check();
     });
 }
 
-op.patch.cgl.on("endframe", () =>
-{
-    if (!wasTriggered) return;
-
-    const gl = cgl.gl;
-
-    fence().then(function ()
-    {
-        if (!pbo)
-        {
-            pbo = gl.createBuffer();
-            gl.bindBuffer(gl.PIXEL_PACK_BUFFER, pbo);
-            gl.bufferData(gl.PIXEL_PACK_BUFFER, 4 * 4, gl.DYNAMIC_READ);
-            gl.bindBuffer(gl.PIXEL_PACK_BUFFER, null);
-        }
-
-        let starttime = performance.now();
-        wasTriggered = false;
-
-        if (isFloatingPoint) channelType = gl.FLOAT;
-        else channelType = gl.UNSIGNED_BYTE;
-
-        const size = 4 * 4;
-        if (!pixelData)
-            if (isFloatingPoint) pixelData = new Float32Array(size);
-            else pixelData = new Uint8Array(size);
-
-        texChanged = false;
-        // gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-        // }
-
-        // gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-
-        // gl.bindFramebuffer(gl.GL_PIXEL_PACK_BUFFER, fb);
-
-        gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-        gl.bindBuffer(gl.PIXEL_PACK_BUFFER, pbo);
-        //   gl.readPixels(mouse.x, pickingTexture.height - mouse.y, 1, 1, gl.RGBA, gl.UNSIGNED_BYTE, 0);
-
-        gl.readPixels(
-            inX.get(), inY.get(),
-            1, 1,
-            gl.RGBA,
-            channelType,
-            0
-        );
-
-        gl.getBufferSubData(gl.PIXEL_PACK_BUFFER, 0, pixelData);
-        gl.bindBuffer(gl.PIXEL_PACK_BUFFER, null);
-
-        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-        // gl.bindFramebuffer(gl.GL_PIXEL_PACK_BUFFER, null);
-
-        if (isFloatingPoint)
-        {
-            outR.set(pixelData[0]);
-            outG.set(pixelData[1]);
-            outB.set(pixelData[2]);
-            outA.set(pixelData[3]);
-        }
-        else
-        {
-            outR.set(pixelData[0] / 255);
-            outG.set(pixelData[1] / 255);
-            outB.set(pixelData[2] / 255);
-            outA.set(pixelData[3] / 255);
-        }
-
-        // console.log(Math.round(performance.now()-starttime ));
-    });
-});
-
 pUpdate.onTriggered = function ()
 {
-    wasTriggered = true;
-
     // gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
     const realTexture = tex.get();
     const gl = cgl.gl;
@@ -132,8 +78,14 @@ pUpdate.onTriggered = function ()
     if (!fb) fb = gl.createFramebuffer();
     isFloatingPoint = realTexture.textureType == CGL.Texture.TYPE_FLOAT;
 
-    // if (texChanged)
-    // {
+    if (isFloatingPoint) channelType = gl.FLOAT;
+    else channelType = gl.UNSIGNED_BYTE;
+
+    const size = 4 * 4;
+    if (!pixelData)
+        if (isFloatingPoint) pixelData = new Float32Array(size);
+        else pixelData = new Uint8Array(size);
+
     gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
     gl.framebufferTexture2D(
         gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0,
@@ -149,6 +101,77 @@ pUpdate.onTriggered = function ()
     // );
 
     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    if (finishedFence)
+    {
+    // if (!pbo)
+        // {
+        pbo = gl.createBuffer();
+        gl.bindBuffer(gl.PIXEL_PACK_BUFFER, pbo);
+
+        // gl.bufferData(gl.PIXEL_PACK_BUFFER, buffer.byteLength, gl.STREAM_READ);
+
+        gl.bufferData(gl.PIXEL_PACK_BUFFER, 4 * 4, gl.DYNAMIC_READ);
+        // gl.bindBuffer(gl.PIXEL_PACK_BUFFER, null);
+        // }
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+        gl.bindBuffer(gl.PIXEL_PACK_BUFFER, pbo);
+
+        gl.readPixels(
+            inX.get(), inY.get(),
+            1, 1,
+            gl.RGBA,
+            channelType,
+            0
+        );
+
+        gl.bindBuffer(gl.PIXEL_PACK_BUFFER, null);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    }
+
+    if (finishedFence)
+        fence().then(function ()
+        {
+        // console.log("yeap...");
+            let starttime = performance.now();
+            wasTriggered = false;
+
+            texChanged = false;
+            // gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+            // }
+
+            // gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+
+            // gl.bindFramebuffer(gl.GL_PIXEL_PACK_BUFFER, fb);
+
+            gl.bindBuffer(gl.PIXEL_PACK_BUFFER, pbo);
+            gl.getBufferSubData(gl.PIXEL_PACK_BUFFER, 0, pixelData);
+            gl.bindBuffer(gl.PIXEL_PACK_BUFFER, null);
+            finishedFence = true;
+            gl.deleteBuffer(pbo);
+            // pbo = null;
+
+            // gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+            // gl.bindFramebuffer(gl.GL_PIXEL_PACK_BUFFER, null);
+
+            if (isFloatingPoint)
+            {
+                outR.set(pixelData[0]);
+                outG.set(pixelData[1]);
+                outB.set(pixelData[2]);
+                outA.set(pixelData[3]);
+            }
+            else
+            {
+                outR.set(pixelData[0] / 255);
+                outG.set(pixelData[1] / 255);
+                outB.set(pixelData[2] / 255);
+                outA.set(pixelData[3] / 255);
+            }
+
+            // console.log(Math.round(performance.now() - starttime));
+        });
 
     outTrigger.trigger();
 };
