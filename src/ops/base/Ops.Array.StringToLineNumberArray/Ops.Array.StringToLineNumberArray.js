0d486e7fdd90fea04d93de6e2802accbbc831259
pandrr <tom@undev.de>
2023-02-09 15:40:40 +0100

    new op stringtolinenumberarray

---
+++ src/ops/base/Ops.Array.StringToLineNumberArray/Ops.Array.StringToLineNumberArray.js
@@ -0,0 +1,32 @@
+const
+    inStr = op.inStringEditor("String"),
+    outArr = op.outArray("Result");
+
+// outIdx=op.outNumber("Line"),
+// outFound=op.outBoolNum("Found");
+
+inStr.onChange = () =>
+{
+    let str = inStr.get() || "";
+    // let idx=inIdx.get()||0;
+    let line = 0;
+
+    // if(idx<0)idx=0;
+
+    // let off=0;
+    // let found=false;
+
+    let arr = [];
+
+    for (let i = 0; i < str.length; i++)
+    {
+        if (str.charAt(i) == "\n")
+        {
+            line++;
+            continue;
+        }
+        arr.push(line);
+    }
+
+    outArr.set(arr);
+};
