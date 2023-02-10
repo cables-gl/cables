04eb98200e661985a96ffdba3bf2296680050937
pandrr <tom@tomkom.net>
2023-02-10 17:57:49 +0100

    webaudiocontext

--- src/ops/base/Ops.Dev.WebAudioContextStart/Ops.Dev.WebAudioContextStart.js
+++ src/ops/base/Ops.Dev.WebAudioContextStart/Ops.Dev.WebAudioContextStart.js
@@ -1,8 +1,18 @@
-const exec = op.inTrigger("Start");
+const
+    exec = op.inTriggerButton("Start"),
+    outState = op.outString("Audiocontext State");
 
 let audioCtx = CABLES.WEBAUDIO.createAudioContext(op);
+updateState();
 
 exec.onTriggered = () =>
 {
     if (audioCtx && audioCtx.state == "suspended")audioCtx.resume();
+    updateState();
+    setTimeout(updateState, 100);
 };
+
+function updateState()
+{
+    outState.set(audioCtx.state);
+}
