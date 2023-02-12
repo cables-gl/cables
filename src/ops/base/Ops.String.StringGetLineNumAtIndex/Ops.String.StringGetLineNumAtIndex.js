b8750dc0df03226eae61a204475762251e593904
pandrr <tom@undev.de>
2023-02-12 15:01:54 +0100

    new op Ops.String.StringGetLineNumAtIndex

---
+++ src/ops/base/Ops.String.StringGetLineNumAtIndex/Ops.String.StringGetLineNumAtIndex.js
@@ -0,0 +1,35 @@
+const
+    inStr = op.inStringEditor("String"),
+    inIdx = op.inInt("Index", 0),
+
+    outIdx = op.outNumber("Line"),
+    outFound = op.outBoolNum("Found");
+
+inStr.onChange = inIdx.onChange = () =>
+{
+    let str = inStr.get() || "";
+    let idx = inIdx.get() || 0;
+    let line = 0;
+
+    if (idx < 0)idx = 0;
+
+    let off = 0;
+    let found = false;
+
+    for (let i = 0; i < str.length; i++)
+    {
+        if (i - off == idx)
+        {
+            found = true;
+            break;
+        }
+        if (str.charAt(i) == "\n")
+        {
+            line++;
+            off++;
+        }
+    }
+
+    outFound.set(found);
+    outIdx.set(line);
+};
