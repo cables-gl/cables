4588f6bdd119c23242fb58da6c93473cfbd63bb9
pandrr <tom@undev.de>
2023-03-07 17:56:08 +0100

    dropdown/switch values strings

--- src/libs/cables/subpatchop.js
+++ src/libs/cables/subpatchop.js
@@ -1,4 +1,7 @@
 
+const subpatchInputOpName = "Ops.Dev.Ui.PatchInput";
+const subpatchOutputOpName = "Ops.Dev.Ui.PatchOutput";
+
 const SubPatchOp = class
 {
     constructor(op)
@@ -6,8 +9,6 @@ const SubPatchOp = class
         this._op = op;
 
         op.patchId = op.addInPort(new CABLES.Port(op, "patchId", CABLES.OP_PORT_TYPE_STRING, { "display": "readonly", "isSubPatchOpExposable": true }));
-
-        // op.patchId.setUiAttribs({ "hideParam": true });
 
         if (op.uiAttribs.parentOfSubpatch)
         {
@@ -18,6 +19,20 @@ const SubPatchOp = class
             let newPatchId = CABLES.generateUUID();
             op.patchId.set(newPatchId);
         }
+
+        op.patch.on("subpatchCreated", () => { this.createInOutOps(); });
+        op.on("loadedValueSet", () => { this.createInOutOps(); });
+    }
+
+    createInOutOps()
+    {
+        let patchInputOP = this._op.patch.getSubPatchOp(this._op.patchId.get(), subpatchInputOpName);
+        let patchOutputOP = this._op.patch.getSubPatchOp(this._op.patchId.get(), subpatchOutputOpName);
+
+        if (!patchInputOP) this._op.patch.addOp(subpatchInputOpName, { "subPatch": this._op.patchId.get(), "translate": { "x": 0, "y": 0 } });
+        if (!patchOutputOP) this._op.patch.addOp(subpatchOutputOpName, { "subPatch": this._op.patchId.get(), "translate": { "x": 0, "y": 0 } });
+
+        // todo: move to correct positions...
     }
 };
 
