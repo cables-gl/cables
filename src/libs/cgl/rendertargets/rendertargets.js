465144d2c455e2c50d65c40ec9a25f52316f0f48
pandrr <tom@tomkom.net>
2023-03-29 19:21:16 +0200

    render2slots

--- src/libs/cgl/rendertargets/rendertargets.js
+++ src/libs/cgl/rendertargets/rendertargets.js
@@ -29,6 +29,12 @@ class RenderTargets
                 "srcHeadVert": slots_head_vert,
                 "srcBodyVert": slots_vert
             });
+
+        this.mod.onBind = (currentShader) =>
+        {
+            // console.log(currentShader);
+            // currentShader.setDrawBuffers([true, true, true, true]);
+        };
     }
 
     getTypes()
@@ -59,38 +65,39 @@ class RenderTargets
         let hasPosLocal = false;
         let hasPosObject = false;
         let hasNormalModelView = false;
+        this._numBuffers = slots.length;
 
         for (let i = 0; i < this._numBuffers; i++)
         {
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_NORMAL", slots[i] == "Normal");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_COLOR", slots[i] == "Color" || slots[i] == "Default");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_BLACK", slots[i] == "Black");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_1", slots[i] == "1");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_0", slots[i] == "0");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_ALPHA0", slots[i] == "Alpha 0");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_NORMAL", slots[i] == "Normal");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_COLOR", slots[i] == "Color" || slots[i] == "Default");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_BLACK", slots[i] == "Black");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_1", slots[i] == "1");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_0", slots[i] == "0");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_ALPHA0", slots[i] == "Alpha 0");
 
             hasPosWorld = (slots[i] == "Position World") || hasPosWorld;
             hasNormalModelView = (slots[i] == "Normal * ModelView") || hasNormalModelView;
             hasPosLocal = (slots[i] == "Position Local") || hasPosLocal;
             hasPosObject = (slots[i] == "Position Object") || hasPosObject;
 
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_POS_WORLD", slots[i] == "Position World");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_POS_LOCAL", slots[i] == "Position Local");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_POS_OBJECT", slots[i] == "Position Object");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_TEXCOORD", slots[i] == "TexCoord");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_MATERIALID", slots[i] == "Material Id");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_POS_WORLD", slots[i] == "Position World");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_POS_LOCAL", slots[i] == "Position Local");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_POS_OBJECT", slots[i] == "Position Object");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_TEXCOORD", slots[i] == "TexCoord");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_MATERIALID", slots[i] == "Material Id");
 
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_NORMAL_MV", slots[i] == "Normal * ModelView");
-            this.mod.toggleDefine("SLOT_TEX_" + i + "_FRAGZ", slots[i] == "FragCoord.z");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_NORMAL_MV", slots[i] == "Normal * ModelView");
+            this.mod.toggleDefine("MOD_SLOT_TEX_" + i + "_FRAGZ", slots[i] == "FragCoord.z");
 
             this.asString += slots[i];
             if (i != this._numBuffers - 1) this.asString += " | ";
         }
 
-        this.mod.toggleDefine("SLOT_POS_WORLD", hasPosWorld);
-        this.mod.toggleDefine("SLOT_POS_LOCAL", hasPosLocal);
-        this.mod.toggleDefine("SLOT_POS_OBJECT", hasPosObject);
-        this.mod.toggleDefine("SLOT_POS_NORMAL_MV", hasNormalModelView);
+        this.mod.toggleDefine("MOD_SLOT_POS_WORLD", hasPosWorld);
+        this.mod.toggleDefine("MOD_SLOT_POS_LOCAL", hasPosLocal);
+        this.mod.toggleDefine("MOD_SLOT_POS_OBJECT", hasPosObject);
+        this.mod.toggleDefine("MOD_SLOT_POS_NORMAL_MV", hasNormalModelView);
     }
 }
 
--- src/libs/cgl/rendertargets/slots_head.frag
+++ src/libs/cgl/rendertargets/slots_head.frag
@@ -1,16 +1,16 @@
-#ifdef SLOT_POS_WORLD
+#ifdef MOD_SLOT_POS_WORLD
     in vec3 MOD_pos_world;
 #endif
 
-#ifdef SLOT_POS_LOCAL
+#ifdef MOD_SLOT_POS_LOCAL
     in vec3 MOD_pos_local;
 #endif
 
-#ifdef SLOT_POS_OBJECT
+#ifdef MOD_SLOT_POS_OBJECT
     in vec3 MOD_pos_object;
 #endif
 
-#ifdef SLOT_POS_NORMAL_MV
+#ifdef MOD_SLOT_POS_NORMAL_MV
     in vec3 MOD_normal_mv;
 #endif
 
