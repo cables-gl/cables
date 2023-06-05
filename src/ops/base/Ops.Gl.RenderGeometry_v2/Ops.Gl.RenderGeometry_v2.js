0231f88274e56814a542f044ec5b3376fc2d3c32
pandrr <tom@tomkom.net>
2023-06-05 15:54:54 +0200

    render geom vertexnumbers

--- src/ops/base/Ops.Gl.RenderGeometry_v2/Ops.Gl.RenderGeometry_v2.js
+++ src/ops/base/Ops.Gl.RenderGeometry_v2/Ops.Gl.RenderGeometry_v2.js
@@ -1,24 +1,32 @@
 const
     render = op.inTrigger("render"),
     geometry = op.inObject("Geometry", null, "geometry"),
+
+    inVertNums = op.inBool("Add Vertex Numbers", true),
     inActive = op.inBool("Render", true),
     trigger = op.outTrigger("trigger");
 
 op.toWorkPortsNeedToBeLinked(geometry, render);
 
 geometry.ignoreValueSerialize = true;
-geometry.onChange = update;
 
 let mesh = null;
+let needsUpdate = true;
+
+geometry.onLinkChanged =
+inVertNums.onChange =
+    geometry.onChange = () => { needsUpdate = true; };
 
 render.onTriggered = function ()
 {
+    if (needsUpdate) update();
     if (mesh && inActive.get()) mesh.render(op.patch.cgl.getShader());
     trigger.trigger();
 };
 
 function update()
 {
+    needsUpdate = false;
     const geom = geometry.get();
     if (geom && geom.isGeometry)
     {
@@ -30,6 +38,7 @@ function update()
         if (!mesh)
         {
             mesh = new CGL.Mesh(op.patch.cgl, geom);
+            mesh.addVertexNumbers = inVertNums.get();
             mesh.setGeom(geom);
         }
     }
