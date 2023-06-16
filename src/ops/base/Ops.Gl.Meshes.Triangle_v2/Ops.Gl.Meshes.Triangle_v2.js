ee717616c30bb9d46ec4d33b5e8cce4757880721
pandrr <tom@undev.de>
2023-06-16 15:07:45 +0200

    triangle v2

---
+++ src/ops/base/Ops.Gl.Meshes.Triangle_v2/Ops.Gl.Meshes.Triangle_v2.js
@@ -0,0 +1,67 @@
+const
+    render = op.inTrigger("render"),
+    trigger = op.outTrigger("trigger"),
+    sizeW = op.inValueFloat("width", 1),
+    sizeH = op.inValueFloat("height", 1),
+    draw = op.inValueBool("Draw", true),
+    geom = new CGL.Geometry("triangle"),
+    geomOut = op.outObject("geometry");
+
+geomOut.ignoreValueSerialize = true;
+
+op.toWorkPortsNeedToBeLinked(render);
+op.setPortGroup("Size", [sizeW, sizeH]);
+
+const cgl = op.patch.cgl;
+let mesh = null;
+sizeW.onChange = sizeH.onChange = () => { mesh = null; };
+
+render.onLinkChanged = () =>
+{
+    if (!render.isLinked()) geomOut.set(null);
+};
+
+render.onTriggered = function ()
+{
+    if (!mesh)create();
+    if (draw.get() && mesh)mesh.render(cgl.getShader());
+    trigger.trigger();
+};
+
+function create()
+{
+    geom.vertices = [
+        0.0, sizeH.get(), 0.0,
+        -sizeW.get(), -sizeH.get(), 0.0,
+        sizeW.get(), -sizeH.get(), 0.0
+    ];
+
+    geom.vertexNormals = [
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 1.0
+    ];
+    geom.tangents = [
+        1, 0, 0,
+        1, 0, 0,
+        1, 0, 0
+    ];
+    geom.biTangents = [
+        0, 1, 0,
+        0, 1, 0,
+        0, 1, 0
+    ];
+
+    geom.texCoords = [
+        0.5, 0.0,
+        1.0, 1.0,
+        0.0, 1.0,
+    ];
+
+    geom.verticesIndices = [
+        0, 1, 2
+    ];
+
+    mesh = new CGL.Mesh(cgl, geom);
+    geomOut.setRef(geom);
+}
