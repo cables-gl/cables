a385f6828990c8b4286f18d3d96eed25a8013a75
undev-studio <services@undev.de>
2023-06-16 13:57:53 +0200

    move to extensions

--- src/ops/base/Ops.Libs.OpenType.OpentypeFont/Ops.Libs.OpenType.OpentypeFont.js
+++
@@ -1,9 +0,0 @@
-const
-    filename = op.inUrl("Font File", [".otf", ".ttf", ".woff", ".woff2"]),
-    outFont = op.outObject("Opentype Font", null, "opentype");
-
-filename.onChange = async function ()
-{
-    const font = await opentype.load(filename.get());
-    outFont.set(font);
-};
