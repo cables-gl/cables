d18a8fa223cb8854b331c5ca04db651e5589832f
pandrr <tom@tomkom.net>
2023-06-22 17:28:26 +0200

    new render2texture version

---
+++ src/ops/base/Ops.Gl.Render2Texture_v2/Ops.Gl.Render2Texture_v2.js
@@ -0,0 +1,153 @@
+const cgl = op.patch.cgl;
+
+const
+    render = op.inTrigger("render"),
+    useVPSize = op.inValueBool("use viewport size", true),
+    width = op.inValueInt("texture width", 512),
+    height = op.inValueInt("texture height", 512),
+    aspect = op.inBool("Auto Aspect", false),
+    tfilter = op.inSwitch("filter", ["nearest", "linear", "mipmap"], "linear"),
+    twrap = op.inSwitch("Wrap", ["Clamp", "Repeat", "Mirror"], "Repeat"),
+    msaa = op.inSwitch("MSAA", ["none", "2x", "4x", "8x"], "none"),
+    trigger = op.outTrigger("trigger"),
+    tex = op.outTexture("texture"),
+    texDepth = op.outTexture("textureDepth"),
+    // fpTexture = op.inValueBool("HDR"),
+    inPixelFormat = op.inDropDown("Pixel Format", CGL.Texture.PIXELFORMATS, CGL.Texture.PFORMATSTR_RGBA8UB),
+
+    depth = op.inValueBool("Depth", true),
+    clear = op.inValueBool("Clear", true);
+
+let fb = null;
+let reInitFb = true;
+tex.set(CGL.Texture.getEmptyTexture(cgl));
+
+op.setPortGroup("Size", [useVPSize, width, height, aspect]);
+
+const prevViewPort = [0, 0, 0, 0];
+
+// fpTexture.setUiAttribs({ "title": "Pixelformat Float 32bit" });
+
+inPixelFormat.onChange =
+    depth.onChange =
+    clear.onChange =
+    tfilter.onChange =
+    twrap.onChange =
+    msaa.onChange = initFbLater;
+
+useVPSize.onChange = updateVpSize;
+
+render.onTriggered =
+    op.preRender = doRender;
+
+updateVpSize();
+
+function updateVpSize()
+{
+    width.setUiAttribs({ "greyout": useVPSize.get() });
+    height.setUiAttribs({ "greyout": useVPSize.get() });
+    aspect.setUiAttribs({ "greyout": useVPSize.get() });
+}
+
+function initFbLater()
+{
+    reInitFb = true;
+}
+
+function doRender()
+{
+    const vp = cgl.getViewPort();
+    prevViewPort[0] = vp[0];
+    prevViewPort[1] = vp[1];
+    prevViewPort[2] = vp[2];
+    prevViewPort[3] = vp[3];
+
+    if (!fb || reInitFb)
+    {
+        if (fb) fb.delete();
+
+        let selectedWrap = CGL.Texture.WRAP_REPEAT;
+        if (twrap.get() == "Clamp") selectedWrap = CGL.Texture.WRAP_CLAMP_TO_EDGE;
+        else if (twrap.get() == "Mirror") selectedWrap = CGL.Texture.WRAP_MIRRORED_REPEAT;
+
+        let selectFilter = CGL.Texture.FILTER_NEAREST;
+        if (tfilter.get() == "nearest") selectFilter = CGL.Texture.FILTER_NEAREST;
+        else if (tfilter.get() == "linear") selectFilter = CGL.Texture.FILTER_LINEAR;
+        else if (tfilter.get() == "mipmap") selectFilter = CGL.Texture.FILTER_MIPMAP;
+
+        if (inPixelFormat.get().indexOf("loat") && tfilter.get() == "mipmap") op.setUiError("fpmipmap", "Can't use mipmap and float texture at the same time");
+        else op.setUiError("fpmipmap", null);
+
+        if (cgl.glVersion >= 2)
+        {
+            let ms = true;
+            let msSamples = 4;
+
+            if (msaa.get() == "none")
+            {
+                msSamples = 0;
+                ms = false;
+            }
+            if (msaa.get() == "2x") msSamples = 2;
+            if (msaa.get() == "4x") msSamples = 4;
+            if (msaa.get() == "8x") msSamples = 8;
+
+            fb = new CGL.Framebuffer2(cgl, 8, 8,
+                {
+                    "name": "render2texture " + op.id,
+                    "isFloatingPointTexture": CGL.Texture.isPixelFormatFloat(inPixelFormat.get()),
+                    "pixelFormat": inPixelFormat.get(),
+                    "multisampling": ms,
+                    "wrap": selectedWrap,
+                    "filter": selectFilter,
+                    "depth": depth.get(),
+                    "multisamplingSamples": msSamples,
+                    "clear": clear.get()
+                });
+        }
+        else
+        {
+            fb = new CGL.Framebuffer(cgl, 8, 8, { "isFloatingPointTexture": fpTexture.get(), "clear": clear.get() });
+        }
+
+        if (fb && fb.valid)
+        {
+            texDepth.set(fb.getTextureDepth());
+            reInitFb = false;
+        }
+        else
+        {
+            fb = null;
+            reInitFb = true;
+        }
+    }
+
+    if (useVPSize.get())
+    {
+        width.set(cgl.getViewPort()[2]);
+        height.set(cgl.getViewPort()[3]);
+    }
+
+    if (fb.getWidth() != Math.ceil(width.get()) || fb.getHeight() != Math.ceil(height.get()))
+    {
+        fb.setSize(
+            Math.max(1, Math.ceil(width.get())),
+            Math.max(1, Math.ceil(height.get())));
+    }
+
+    fb.renderStart(cgl);
+
+    if (aspect.get()) mat4.perspective(cgl.pMatrix, 45, width.get() / height.get(), 0.1, 1000.0);
+
+    trigger.trigger();
+    fb.renderEnd(cgl);
+
+    // cgl.resetViewPort();
+    cgl.setViewPort(prevViewPort[0], prevViewPort[1], prevViewPort[2], prevViewPort[3]);
+
+    // texDepth.set(CGL.Texture.getEmptyTexture(op.patch.cgl));
+    texDepth.setRef(fb.getTextureDepth());
+
+    // tex.set(CGL.Texture.getEmptyTexture(op.patch.cgl));
+    tex.setRef(fb.getTextureColor());
+}
