1e9760d36f1f077dff21464efa5384d6b9314390
pandrr <tom@tomkom.net>
2023-07-23 08:06:50 +0200

    divide coreport

---
+++ src/core/core_port_select.js
@@ -0,0 +1,23 @@
+
+import { SwitchPort } from "./core_port_switch";
+
+
+class ValueSelectPort extends SwitchPort
+{
+    setUiAttribs(newAttribs)
+    {
+        // never unhide valuePort when indexPort is linked
+        if (this.indexPort.isLinked())
+        {
+            for (const p in newAttribs)
+            {
+                if (p == "greyout" && !newAttribs[p]) newAttribs[p] = "true";
+            }
+        }
+        super.setUiAttribs(newAttribs);
+    }
+}
+
+
+
+export { ValueSelectPort };
