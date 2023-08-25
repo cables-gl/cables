100808c1e0e0ab4e1515f084dddcf39dc4140e3b
pandrr <tom@tomkom.net>
2023-08-25 14:52:59 +0200

    bp templ

--- src/ops/base/Ops.Templates.Blueprint2/Ops.Templates.Blueprint2.js
+++ src/ops/base/Ops.Templates.Blueprint2/Ops.Templates.Blueprint2.js
@@ -7,46 +7,13 @@ function initializeSubpatch()
 {
     const p = JSON.parse(attachments.subpatch_json);
 
-    CABLES.Patch.replaceOpIds(p, { "parentSubPatchId": patchId, "prefixHash": patchId, "oldIdAsRef": true });
+    CABLES.Patch.replaceOpIds(p, { "parentSubPatchId": patchId, "prefixHash": patchId, "oldIdAsRef": true, "doNotUnlinkLostLinks": true });
 
     for (let i = 0; i < p.ops.length; i++)
     {
         p.ops[i].uiAttribs.blueprintSubpatch2 = true;
     }
 
-    if (CABLES.UI)
-    {
-        let allLoaded = true;
-        for (let i = 0; i < p.ops.length; i++)
-        {
-            const op = p.ops[i];
-            if (!gui.serverOps.isLoaded(op))
-            {
-                allLoaded = false;
-                break;
-            }
-        }
-
-        if (allLoaded)
-        {
-            finish(p);
-        }
-        else
-        {
-            gui.serverOps.loadProjectDependencies(p, () =>
-            {
-                finish(p);
-            });
-        }
-    }
-    else
-    {
-        finish(p);
-    }
-}
-
-function finish(p)
-{
     op.patch.deSerialize(p);
     op.patch.emitEvent("subpatchExpose", patchId);
     op.setStorage({ "blueprintVer": 2 });
