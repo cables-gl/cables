0e2418b7a92f9048fc201642e87642bd5b0879d3
pandrr <tom@tomkom.net>
2023-08-25 10:35:31 +0200

    sp

--- src/core/core_patch.js
+++ src/core/core_patch.js
@@ -1367,26 +1367,26 @@ Patch.replaceOpIds = function (json, options)
         opids[json.ops[i].id] = json.ops[i];
     }
 
-
-    for (const j in json.ops)
-    {
-        for (const k in json.ops[j].portsOut)
+    if (!options.doNotUnlinkLostLinks)
+        for (const j in json.ops)
         {
-            const links = json.ops[j].portsOut[k].links;
-            if (links)
+            for (const k in json.ops[j].portsOut)
             {
-                let l = links.length;
-
-                while (l--)
+                const links = json.ops[j].portsOut[k].links;
+                if (links)
                 {
-                    if (links[l] && (!opids[links[l].objIn] || !opids[links[l].objOut]))
+                    let l = links.length;
+
+                    while (l--)
                     {
-                        links.splice(l, 1);
+                        if (links[l] && (!opids[links[l].objIn] || !opids[links[l].objOut]))
+                        {
+                            links.splice(l, 1);
+                        }
                     }
                 }
             }
         }
-    }
 
     for (const i in json.ops)
     {
