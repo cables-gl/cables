f6f13533ef8d382717355b76349fba8334b64fa6
pandrr <tom@tomkom.net>
2023-09-14 09:44:17 +0200

    renaming ops

--- src/ops/base/Ops.Physics.Ammo.AmmoDebugRenderer/Ops.Physics.Ammo.AmmoDebugRenderer.md
+++
@@ -1,6 +0,0 @@
-This handy operator will let you debug your physics simulation and hitboxes/meshes.  One great use case is to check that your GLTF scene hitboxes are in the correct locations or to find the best setting for a convex hull triangle simplification parameter.
-
-It is handy to use this operator in a setup where you can toggle the debug view or hide it since the wireframe rendering can become quite CPU intensive. You may also simply uncheck the Wireframe rendering view and use the AABB mode instead.
-
-tip:
-when using different camera settings, field of view, view scaling - make sure to place the debug renderer after the camera - so your graphics and physics debug rendering match correctly.
--- src/ops/base/Ops.Trigger.TriggerNumber/Ops.Trigger.TriggerNumber.js
+++ src/ops/base/Ops.Trigger.TriggerNumber/Ops.Trigger.TriggerNumber.js
@@ -1,22 +1,13 @@
 const
-    exe0 = op.inTriggerButton("0"),
-    exe1 = op.inTriggerButton("1"),
-    exe2 = op.inTriggerButton("2"),
-    exe3 = op.inTriggerButton("3"),
-    exe4 = op.inTriggerButton("4"),
-    exe5 = op.inTriggerButton("5"),
-    exe6 = op.inTriggerButton("6"),
-    exe7 = op.inTriggerButton("7"),
-    number = op.outNumber("number");
+    setValuePort = op.inTriggerButton("Set"),
+    valuePort = op.inValueFloat("Number"),
+    outNext = op.outTrigger("Next"),
+    outValuePort = op.outNumber("Out Value");
 
-number.changeAlways = true;
-const outTrig = op.outTrigger("Triggered");
+outValuePort.changeAlways = true;
 
-exe0.onTriggered = function () { number.set(0); outTrig.trigger(); };
-exe1.onTriggered = function () { number.set(1); outTrig.trigger(); };
-exe2.onTriggered = function () { number.set(2); outTrig.trigger(); };
-exe3.onTriggered = function () { number.set(3); outTrig.trigger(); };
-exe4.onTriggered = function () { number.set(4); outTrig.trigger(); };
-exe5.onTriggered = function () { number.set(5); outTrig.trigger(); };
-exe6.onTriggered = function () { number.set(6); outTrig.trigger(); };
-exe7.onTriggered = function () { number.set(7); outTrig.trigger(); };
+setValuePort.onTriggered = function ()
+{
+    outValuePort.set(valuePort.get());
+    outNext.trigger();
+};
