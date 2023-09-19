3aeed71d0710e505926019fd35cb9c7b81770225
pandrr <tom@tomkom.net>
2023-09-19 10:41:16 +0200

    new op rectangle9slice

---
+++ src/ops/base/Ops.Gl.Meshes.Rectangle9Slice/Ops.Gl.Meshes.Rectangle9Slice.js
@@ -0,0 +1,141 @@
+const
+    render = op.inTrigger("Render"),
+    width = op.inValueFloat("Width", 1),
+    height = op.inValueFloat("Height", 1),
+
+    inUVScalar = op.inValueFloat("Border Width", 0.2),
+
+    inScaleX = op.inValueFloat("Scale X", 1),
+    inScaleY = op.inValueFloat("Scale Y", 1),
+
+    dodraw = op.inValueBool("Draw", true),
+    pivotX = op.inValueSelect("pivot x", ["center", "left", "right"]),
+    pivotY = op.inValueSelect("pivot y", ["center", "top", "bottom"]),
+
+    trigger = op.outTrigger("trigger"),
+    geomOut = op.outObject("Geometry");
+
+op.setPortGroup("Size", [width, height]);
+op.setPortGroup("Align", [pivotX, pivotY]);
+
+let cgl = op.patch.cgl;
+let mesh = null;
+let geom = new CGL.Geometry(op.name);
+geom.tangents = [];
+geom.biTangents = [];
+
+pivotX.set("center");
+pivotY.set("center");
+
+geomOut.ignoreValueSerialize = true;
+
+width.onChange =
+    pivotX.onChange =
+    pivotY.onChange =
+    height.onChange =
+    inScaleY.onChange =
+    inScaleX.onChange =
+    inUVScalar.onChange =
+        () => { mesh = null; };
+
+render.onTriggered = function ()
+{
+    if (!mesh)create();
+    if (dodraw.get()) mesh.render(cgl.getShader());
+    trigger.trigger();
+};
+
+function create()
+{
+    let w = width.get();
+    let h = height.get();
+
+    const minW = inUVScalar.get() * 2 * inScaleX.get();
+    w = Math.max(minW, w);
+
+    const minH = inUVScalar.get() * 2 * inScaleY.get();
+    h = Math.max(minH, h);
+
+    let x = -w / 2;
+    let y = -h / 2;
+
+    let pivot = pivotX.get();
+    if (pivot == "right") x = -w;
+    else if (pivot == "left") x = 0;
+
+    pivot = pivotY.get();
+    if (pivot == "top") y = -w;
+    else if (pivot == "bottom") y = 0;
+
+    let uvLeft = inUVScalar.get();
+    let uvRight = inUVScalar.get();
+    let uvTop = inUVScalar.get();
+    let uvBottom = inUVScalar.get();
+    let uvToWorldScaleX = inScaleX.get();
+    let uvToWorldScaleY = inScaleY.get();
+
+    let l = uvLeft * uvToWorldScaleX;
+    let r = w - uvRight * uvToWorldScaleX;
+    let t = h - uvTop * uvToWorldScaleY;
+    let b = uvBottom * uvToWorldScaleY;
+
+    geom = new CGL.Geometry(op.name);
+
+    geom.vertices = [
+        x + 0, y + 0, 0,
+        x + 0, y + b, 0,
+        x + 0, y + t, 0,
+        x + 0, y + h, 0,
+        x + l, y + h, 0,
+        x + r, y + h, 0,
+        x + w, y + h, 0,
+        x + w, y + t, 0,
+        x + w, y + b, 0,
+        x + w, y + 0, 0,
+        x + r, y + 0, 0,
+        x + l, y + 0, 0,
+        x + l, y + b, 0,
+        x + l, y + t, 0,
+        x + r, y + t, 0,
+        x + r, y + b, 0];
+
+    geom.verticesIndices = [
+        0, 1, 12, 0, 12, 11,
+        1, 2, 13, 1, 13, 12,
+        2, 3, 4, 2, 4, 13,
+        13, 4, 5, 13, 5, 14,
+        14, 5, 6, 14, 6, 7,
+        15, 14, 7, 15, 7, 8,
+        10, 15, 8, 10, 8, 9,
+        11, 12, 15, 11, 15, 10,
+        12, 13, 14, 12, 14, 15];
+
+    l = uvLeft;
+    r = 1 - uvRight;
+    t = 1 - uvTop;
+    const uvh = 1;
+    const uvw = 1;
+
+    geom.texCoords = [
+        0, 1 - 0,
+        0, 1 - b,
+        0, 1 - t,
+        0, 1 - uvh,
+        l, 1 - uvh,
+        r, 1 - uvh,
+        uvw, 1 - uvh,
+        uvw, 1 - t,
+        uvw, 1 - b,
+        uvw, 1 - 0,
+        r, 1 - 0,
+        l, 1 - 0,
+        l, 1 - b,
+        l, 1 - t,
+        r, 1 - t,
+        r, 1 - b];
+
+    if (!mesh) mesh = new CGL.Mesh(cgl, geom);
+    else mesh.setGeom(geom);
+
+    geomOut.setRef(geom);
+}
