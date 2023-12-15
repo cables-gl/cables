c355e8e1bdc625b3c16e0111edf1f92b4583c614
pandrr <tom@tomkom.net>
2023-12-15 13:27:12 +0100

    fix circle vertex order

---
+++ src/ops/base/Ops.Gl.Meshes.Circle_v3/Ops.Gl.Meshes.Circle_v3.js
@@ -0,0 +1,286 @@
+const
+    render = op.inTrigger("render"),
+    radius = op.inValue("radius", 0.5),
+    innerRadius = op.inValueSlider("innerRadius", 0),
+    segments = op.inValueInt("segments", 40),
+    percent = op.inValueSlider("percent", 1),
+    steps = op.inValue("steps", 0),
+    invertSteps = op.inValueBool("invertSteps", false),
+    mapping = op.inSwitch("mapping", ["flat", "round"]),
+    drawSpline = op.inValueBool("Spline", false),
+    inDraw = op.inValueBool("Draw", true),
+    trigger = op.outTrigger("trigger"),
+    geomOut = op.outObject("geometry", null, "geometry");
+
+op.setPortGroup("Size", [radius, innerRadius]);
+op.setPortGroup("Display", [percent, steps, invertSteps]);
+op.toWorkShouldNotBeChild("Ops.Gl.TextureEffects.ImageCompose", CABLES.OP_PORT_TYPE_FUNCTION);
+
+inDraw.setUiAttribs({ "title": "Render mesh" });
+
+mapping.set("flat");
+
+mapping.onChange =
+    segments.onChange =
+    radius.onChange =
+    innerRadius.onChange =
+    percent.onChange =
+    steps.onChange =
+    invertSteps.onChange =
+    drawSpline.onChange = calcLater;
+
+geomOut.ignoreValueSerialize = true;
+const cgl = op.patch.cgl;
+
+let geom = new CGL.Geometry("circle");
+let mesh = null;
+const lastSegs = -1;
+
+let oldPrim = 0;
+let shader = null;
+let needsCalc = true;
+
+render.onTriggered = renderMesh;
+
+op.preRender = () =>
+{
+    renderMesh();
+};
+
+render.onLinkChanged = function ()
+{
+    if (!render.isLinked()) geomOut.set(null);
+    else geomOut.setRef(geom);
+};
+
+function renderMesh()
+{
+    if (needsCalc)calc();
+
+    if (!CGL.TextureEffect.checkOpNotInTextureEffect(op)) return;
+
+    shader = op.patch.cg.getShader();
+    if (!shader) return;
+    oldPrim = shader.glPrimitive;
+
+    if (drawSpline.get()) shader.glPrimitive = cgl.gl.LINE_STRIP;
+
+    if (inDraw.get() && mesh)mesh.render(shader);
+    trigger.trigger();
+
+    shader.glPrimitive = oldPrim;
+}
+
+function calc()
+{
+    const segs = Math.max(3, Math.floor(segments.get()));
+
+    geom.clear();
+
+    const faces = [];
+    const texCoords = [];
+    const vertexNormals = [];
+    const tangents = [];
+    const biTangents = [];
+
+    let i = 0, degInRad = 0;
+    let oldPosX = 0, oldPosY = 0;
+    let oldPosXTexCoord = 0, oldPosYTexCoord = 0;
+
+    let oldPosXIn = 0, oldPosYIn = 0;
+    let oldPosXTexCoordIn = 0, oldPosYTexCoordIn = 0;
+
+    let posxTexCoordIn = 0, posyTexCoordIn = 0;
+    let posxTexCoord = 0, posyTexCoord = 0;
+    let posx = 0, posy = 0;
+
+    const perc = Math.max(0.0, percent.get());
+    const verts = [];
+
+    if (drawSpline.get())
+    {
+        let lastX = 0;
+        let lastY = 0;
+        const tc = [];
+        for (i = 0; i <= segs * perc; i++)
+        {
+            degInRad = (360 / segs) * i * CGL.DEG2RAD;
+            posx = Math.cos(degInRad) * radius.get();
+            posy = Math.sin(degInRad) * radius.get();
+
+            posyTexCoord = 0.5;
+
+            if (i > 0)
+            {
+                verts.push(lastX);
+                verts.push(lastY);
+                verts.push(0);
+                posxTexCoord = 1.0 - (i - 1) / segs;
+
+                tc.push(posxTexCoord, posyTexCoord);
+            }
+            verts.push(posx);
+            verts.push(posy);
+            verts.push(0);
+
+            lastX = posx;
+            lastY = posy;
+        }
+        geom.setPointVertices(verts);
+    }
+    else
+    if (innerRadius.get() <= 0)
+    {
+        for (i = 0; i <= segs * perc; i++)
+        {
+            degInRad = (360 / segs) * i * CGL.DEG2RAD;
+            posx = Math.cos(degInRad) * radius.get();
+            posy = Math.sin(degInRad) * radius.get();
+
+            if (mapping.get() == "flat")
+            {
+                posxTexCoord = (Math.cos(degInRad) + 1.0) / 2;
+                posyTexCoord = 1.0 - (Math.sin(degInRad) + 1.0) / 2;
+                posxTexCoordIn = 0.5;
+                posyTexCoordIn = 0.5;
+            }
+            else if (mapping.get() == "round")
+            {
+                posxTexCoord = 1.0 - i / segs;
+                posyTexCoord = 0;
+                posxTexCoordIn = posxTexCoord;
+                posyTexCoordIn = 1;
+            }
+
+            faces.push(
+                [0, 0, 0],
+                [oldPosX, oldPosY, 0],
+                [posx, posy, 0]
+            );
+
+            texCoords.push(
+                posxTexCoordIn, posyTexCoordIn,
+                oldPosXTexCoord, oldPosYTexCoord,
+                posxTexCoord, posyTexCoord
+            );
+            vertexNormals.push(0, 0, 1, 0, 0, 1, 0, 0, 1);
+            tangents.push(1, 0, 0, 1, 0, 0, 1, 0, 0);
+            biTangents.push(0, -1, 0, 0, -1, 0, 0, -1, 0);
+
+            oldPosXTexCoord = posxTexCoord;
+            oldPosYTexCoord = posyTexCoord;
+
+            oldPosX = posx;
+            oldPosY = posy;
+        }
+
+        geom = CGL.Geometry.buildFromFaces(faces, "circle");
+        geom.vertexNormals = vertexNormals;
+        geom.tangents = tangents;
+        geom.biTangents = biTangents;
+        geom.texCoords = texCoords;
+    }
+    else
+    {
+        let count = 0;
+        const numSteps = segs * perc;
+        const pos = 0;
+
+        for (i = 0; i <= numSteps; i++)
+        {
+            count++;
+
+            degInRad = (360 / segs) * i * CGL.DEG2RAD;
+            posx = Math.cos(degInRad) * radius.get();
+            posy = Math.sin(degInRad) * radius.get();
+
+            const posxIn = Math.cos(degInRad) * innerRadius.get() * radius.get();
+            const posyIn = Math.sin(degInRad) * innerRadius.get() * radius.get();
+
+            if (mapping.get() == "round")
+            {
+                posxTexCoord = 1.0 - i / segs;
+                posyTexCoord = 0;
+                posxTexCoordIn = posxTexCoord;
+                posyTexCoordIn = 1;
+            }
+
+            if (steps.get() === 0.0 ||
+                (count % parseInt(steps.get(), 10) === 0 && !invertSteps.get()) ||
+                (count % parseInt(steps.get(), 10) !== 0 && invertSteps.get()))
+            {
+                faces.push(
+                    [posxIn, posyIn, 0],
+                    [oldPosX, oldPosY, 0],
+                    [posx, posy, 0]
+                );
+
+                faces.push(
+                    [oldPosXIn, oldPosYIn, 0],
+                    [oldPosX, oldPosY, 0],
+                    [posxIn, posyIn, 0]
+                );
+
+                texCoords.push(
+                    posxTexCoord, 0,
+                    oldPosXTexCoord, 0,
+                    posxTexCoordIn, 1,
+
+                    posxTexCoord, 1,
+                    oldPosXTexCoord, 0,
+                    oldPosXTexCoordIn, 1);
+
+                vertexNormals.push(
+                    0, 0, 1, 0, 0, 1, 0, 0, 1,
+                    0, 0, 1, 0, 0, 1, 0, 0, 1
+                );
+                tangents.push(
+                    1, 0, 0, 1, 0, 0, 1, 0, 0,
+                    1, 0, 0, 1, 0, 0, 1, 0, 0
+                );
+                biTangents.push(
+                    0, 0, -1, 0, 0, -1, 0, 0, -1,
+                    0, 0, -1, 0, 0, -1, 0, 0, -1
+                );
+            }
+
+            oldPosXTexCoordIn = posxTexCoordIn;
+            oldPosYTexCoordIn = posyTexCoordIn;
+
+            oldPosXTexCoord = posxTexCoord;
+            oldPosYTexCoord = posyTexCoord;
+
+            oldPosX = posx;
+            oldPosY = posy;
+
+            oldPosXIn = posxIn;
+            oldPosYIn = posyIn;
+        }
+
+        geom = CGL.Geometry.buildFromFaces(faces, "circle");
+        geom.vertexNormals = vertexNormals;
+        geom.tangents = tangents;
+        geom.biTangents = biTangents;
+
+        if (mapping.get() == "flat") geom.mapTexCoords2d();
+        else geom.texCoords = texCoords;
+    }
+
+    geomOut.setRef(geom);
+
+    if (geom.vertices.length == 0) return;
+    if (mesh) mesh.dispose();
+    mesh = null;
+    mesh = op.patch.cg.createMesh(geom);
+    needsCalc = false;
+}
+
+function calcLater()
+{
+    needsCalc = true;
+}
+
+op.onDelete = function ()
+{
+    if (mesh)mesh.dispose();
+};
