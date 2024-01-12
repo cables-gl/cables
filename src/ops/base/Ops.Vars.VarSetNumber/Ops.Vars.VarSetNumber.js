2ae1368d181e136b9e7d62d2e73aef6fbd74dce9
pandrr <tom@tomkom.net>
2024-01-12 16:05:34 +0100

    perf

--- src/ops/base/Ops.Vars.VarSetNumber/Ops.Vars.VarSetNumber.js
+++ src/ops/base/Ops.Vars.VarSetNumber/Ops.Vars.VarSetNumber.js
@@ -1,42 +1,42 @@
-op.varName=op.inDropDown("Variable",[],"",true);
-var val=op.inValueFloat("Value",0);
+op.varName = op.inDropDown("Variable", [], "", true);
+let val = op.inValueFloat("Value", 0);
 
-op.varName.onChange=updateName;
-val.onChange=update;
-val.changeAlways=true;
+op.varName.onChange = updateName;
+val.onChange = update;
+val.changeAlways = true;
 
-op.patch.addEventListener("variablesChanged",updateVarNamesDropdown);
+op.patch.addEventListener("variablesChanged", updateVarNamesDropdown);
 
 updateVarNamesDropdown();
 
 function updateVarNamesDropdown()
 {
-    if(CABLES.UI)
+    if (CABLES.UI)
     {
-        var varnames=[];
-        var vars=op.patch.getVars();
-        varnames.push('+ create new one');
-        for(var i in vars) varnames.push(i);
-        op.varName.uiAttribs.values=varnames;
+        let varnames = [];
+        let vars = op.patch.getVars();
+        varnames.push("+ create new one");
+        for (let i in vars) varnames.push(i);
+        op.varName.uiAttribs.values = varnames;
     }
 }
 
 function updateName()
 {
-    if(CABLES.UI)
+    if (CABLES.UI)
     {
-        if(op.varName.get()=='+ create new one')
+        if (op.varName.get() == "+ create new one")
         {
             CABLES.CMD.PATCH.createVariable(op);
             return;
         }
 
-        op.setTitle('set #' + op.varName.get());
+        op.setTitle("set #" + op.varName.get());
     }
     update();
 }
 
 function update()
 {
-    op.patch.setVarValue(op.varName.get(),val.get());
+    op.patch.setVarValue(op.varName.get(), val.get());
 }
