bff31e504c0e1d8e77dbe63ea9d5e98f7b2580f7
pandrr <tom@tomkom.net>
2024-02-01 08:23:14 +0100

    dev changes

--- src/ops/base/Ops.Array.CropArray/Ops.Array.CropArray.js
+++ src/ops/base/Ops.Array.CropArray/Ops.Array.CropArray.js
@@ -1,7 +1,7 @@
 const
     srcArrayPort = op.inArray("Source Array"),
     inStartIndex = op.inInt("Start Index"),
-    newLengthPort = op.inInt("New Length"),
+    newLengthPort = op.inInt("New Length", 20),
     croppedArrayOutPort = op.outArray("Cropped Array"),
     outArrayLength = op.outNumber("Array length");
 
@@ -11,22 +11,21 @@ function setOutPort()
 {
     const srcArray = srcArrayPort.get();
 
-    if(!srcArray)
+    if (!srcArray)
     {
         croppedArrayOutPort.set(null);
         outArrayLength.set(0);
         return;
     }
-    var newLength = parseInt(newLengthPort.get());
-    const start=Math.floor(Math.abs(inStartIndex.get()));
+    let newLength = parseInt(newLengthPort.get());
+    const start = Math.floor(Math.abs(inStartIndex.get()));
 
-    if(start+newLength >= srcArray.length) newLength = srcArray.length-start;
-    if(start+newLength <= srcArray.length)
+    if (start + newLength >= srcArray.length) newLength = srcArray.length - start;
+    if (start + newLength <= srcArray.length)
     {
-        var croppedArr = srcArray.slice(start, start+newLength);
+        let croppedArr = srcArray.slice(start, start + newLength);
         croppedArrayOutPort.set(null);
         croppedArrayOutPort.set(croppedArr);
         outArrayLength.set(croppedArr.length);
     }
-
 }
