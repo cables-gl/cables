3529641a74a8d14c1f8c5c9772010f8d9c6f89c7
pandrr <tom@tomkom.net>
2024-02-26 12:24:39 +0100

    new ops canvas2d drawimage/blendmode/transform

---
+++ src/ops/base/Ops.Graphics.Canvas2d.Canvas2dBlendmode/Ops.Graphics.Canvas2d.Canvas2dBlendmode.js
@@ -0,0 +1,19 @@
+const
+    inExec = op.inTrigger("Render"),
+    inMode = op.inDropDown("Blendmode", ["source-over", "source-in", "source-out", "source-atop", "destination-over", "destination-in", "destination-out", "destination-atop", "lighter", "copy", "xor", "multiply", "screen", "overlay", "darken", "lighten", "color-dodge", "color-burn", "hard-light", "soft-light", "difference", "exclusion", "hue", "saturation", "color", "luminosity"], "source-over"),
+    outNext = op.outTrigger("Next");
+
+inExec.onTriggered = () =>
+{
+    if (op.patch.frameStore.canvasCompose)
+    {
+        const ctx = op.patch.frameStore.canvasCompose.ctx;
+
+        ctx.save();
+
+        ctx.globalCompositeOperation = inMode.get();
+        outNext.trigger();
+
+        ctx.restore();
+    }
+};
---
+++ src/ops/base/Ops.Graphics.Canvas2d.Canvas2dDrawimage/Ops.Graphics.Canvas2d.Canvas2dDrawimage.js
@@ -0,0 +1,42 @@
+const
+    inExec = op.inTrigger("Render"),
+    inCanv = op.inObject("Canvas", null, "element"),
+    inCenter = op.inBool("Center", false),
+    inSmooth = op.inBool("Smooth", true),
+    outNext = op.outTrigger("Next");
+
+inExec.onTriggered = () =>
+{
+    if (op.patch.frameStore.canvasCompose)
+    {
+        const ctx = op.patch.frameStore.canvasCompose.ctx;
+
+        if (inCanv.isLinked())
+            try
+            {
+                let x = 0;
+                let y = 0;
+
+                const drawable = inCanv.get();
+
+                if (drawable)
+                {
+                    if (inCenter.get())
+                    {
+                        x = -drawable.width / 2;
+                        y = -drawable.height / 2;
+                    }
+
+                    ctx.imageSmoothingEnabled = !!inSmooth.get();
+                    ctx.imageSmoothingQuality = "low";
+
+                    ctx.drawImage(drawable, x, y);
+
+                    ctx.imageSmoothingEnabled = false;
+                }
+            }
+            catch (e) { console.log(e); }
+
+        outNext.trigger();
+    }
+};
