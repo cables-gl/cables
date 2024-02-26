1d0f53383262bc07683b2ecde6d9a7e19beb639d
pandrr <tom@tomkom.net>
2024-02-26 16:10:12 +0100

    canvas'

--- src/ops/base/Ops.Graphics.Canvas2d.Canvas2dRect/Ops.Graphics.Canvas2d.Canvas2dRect.js
+++ src/ops/base/Ops.Graphics.Canvas2d.Canvas2dRect/Ops.Graphics.Canvas2d.Canvas2dRect.js
@@ -1,10 +1,9 @@
 const
     inExec = op.inTrigger("Exec"),
-    inX = op.inFloat("X", 0),
-    inY = op.inFloat("Y", 0),
     inWidth = op.inFloat("Width", 100),
     inHeight = op.inFloat("Height", 100),
     inRound = op.inFloat("Roundness", 0),
+    inCenter = op.inBool("Center", false),
     inMeth = op.inSwitch("Method", ["Fill", "Stroke", "Clip"], "Fill"),
     next = op.outTrigger("Next");
 
@@ -14,12 +13,20 @@ inExec.onTriggered = () =>
     {
         const ctx = op.patch.frameStore.canvasCompose.ctx;
 
-        if (inRound.get() <= 0 && inMeth.get() == "Fill") ctx.fillRect(inX.get(), inY.get(), inWidth.get(), inHeight.get());
-        else if (inRound.get() <= 0 && inMeth.get() == "Stroke") ctx.strokeRect(inX.get(), inY.get(), inWidth.get(), inHeight.get());
+        let x = 0;
+        let y = 0;
+        if (inCenter.get())
+        {
+            x = -inWidth.get() / 2;
+            y = -inHeight.get() / 2;
+        }
+
+        if (inRound.get() <= 0 && inMeth.get() == "Fill") ctx.fillRect(x, y, inWidth.get(), inHeight.get());
+        else if (inRound.get() <= 0 && inMeth.get() == "Stroke") ctx.strokeRect(x, y, inWidth.get(), inHeight.get());
         else
         {
             ctx.beginPath();
-            ctx.roundRect(inX.get(), inY.get(), inWidth.get(), inHeight.get(), inRound.get());
+            ctx.roundRect(x, y, inWidth.get(), inHeight.get(), inRound.get());
             ctx.closePath();
 
             if (inMeth.get() == "Fill") ctx.fill();
