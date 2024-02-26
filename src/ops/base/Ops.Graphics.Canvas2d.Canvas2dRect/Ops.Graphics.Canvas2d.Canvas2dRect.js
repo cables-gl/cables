3fac02373c7b8d5b63443181b2883eda28572931
pandrr <tom@tomkom.net>
2024-02-26 13:36:38 +0100

    shapes clipping

--- src/ops/base/Ops.Graphics.Canvas2d.Canvas2dRect/Ops.Graphics.Canvas2d.Canvas2dRect.js
+++ src/ops/base/Ops.Graphics.Canvas2d.Canvas2dRect/Ops.Graphics.Canvas2d.Canvas2dRect.js
@@ -5,7 +5,7 @@ const
     inWidth = op.inFloat("Width", 100),
     inHeight = op.inFloat("Height", 100),
     inRound = op.inFloat("Roundness", 0),
-    inStroke = op.inSwitch("Fill", ["Stroke", "Fill"], "Fill"),
+    inMeth = op.inSwitch("Method", ["Fill", "Stroke", "Clip"], "Fill"),
     next = op.outTrigger("Next");
 
 inExec.onTriggered = () =>
@@ -14,17 +14,17 @@ inExec.onTriggered = () =>
     {
         const ctx = op.patch.frameStore.canvasCompose.ctx;
 
-        if (inRound.get() <= 0)
-            if (inStroke.get() == "Fill") ctx.fillRect(inX.get(), inY.get(), inWidth.get(), inHeight.get());
-            else ctx.strokeRect(inX.get(), inY.get(), inWidth.get(), inHeight.get());
+        if (inRound.get() <= 0 && inMeth.get() == "Fill") ctx.fillRect(inX.get(), inY.get(), inWidth.get(), inHeight.get());
+        else if (inRound.get() <= 0 && inMeth.get() == "Stroke") ctx.strokeRect(inX.get(), inY.get(), inWidth.get(), inHeight.get());
         else
         {
             ctx.beginPath();
             ctx.roundRect(inX.get(), inY.get(), inWidth.get(), inHeight.get(), inRound.get());
             ctx.closePath();
 
-            if (inStroke.get() == "Fill") ctx.fill();
-            else ctx.stroke();
+            if (inMeth.get() == "Fill") ctx.fill();
+            else if (inMeth.get() == "Stroke") ctx.stroke();
+            else if (inMeth.get() == "Clip") ctx.clip();
         }
 
         next.trigger();
