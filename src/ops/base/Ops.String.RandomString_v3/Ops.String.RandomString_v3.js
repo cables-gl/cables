dd53e2264365a01e1e711d04e6c7db801369ec57
pandrr <tom@tomkom.net>
2024-03-01 09:52:40 +0100

    new op randomstring v4

---
+++ src/ops/base/Ops.String.RandomString_v3/Ops.String.RandomString_v3.js
@@ -0,0 +1,26 @@
+const
+    chars = op.inString("chars", "cables"),
+    len = op.inInt("Length", 10),
+    inSeed = op.inFloat("Seed", 0),
+    result = op.outString("Result");
+
+len.onChange =
+    chars.onChange =
+    inSeed.onChange = gen;
+gen();
+
+function gen()
+{
+    Math.randomSeed = Math.abs(inSeed.get());
+
+    const charArray = Array.from(chars.get());
+    let str = "";
+    if (charArray.length > 0)
+    {
+        let numChars = charArray.length - 1;
+        for (let i = 0; i < Math.abs(len.get()); i++)
+            str += charArray[Math.round(Math.seededRandom() * numChars)];
+    }
+
+    result.set(str);
+}
