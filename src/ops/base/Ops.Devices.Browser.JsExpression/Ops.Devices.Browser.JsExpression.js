805b1f00532d6502c5ed8ecadada4293790171a0
pandrr <tom@tomkom.net>
2024-03-01 16:20:25 +0100

    new op Ops.Devices.Browser.JsExpression

---
+++ src/ops/base/Ops.Devices.Browser.JsExpression/Ops.Devices.Browser.JsExpression.js
@@ -0,0 +1,49 @@
+const
+    inJs = op.inString("JS Expression", "document.body.clientWidth"),
+    outStr = op.outString("Result String"),
+    outNum = op.outNumber("Result Number"),
+    outArr = op.outArray("Result Array"),
+    outObj = op.outObject("Result Object"),
+    outErr = op.outBoolNum("Error");
+
+inJs.onChange = update;
+update();
+
+function update()
+{
+    outErr.set(false);
+    try
+    {
+        const a = eval("\"use strict\";" + inJs.get());
+
+        if (CABLES.UTILS.isNumeric(a))
+        {
+            outNum.set(a);
+            outStr.set(a + "");
+        }
+        else if (typeof a === "string")
+        {
+            outNum.set(0);
+            outStr.set(a);
+        }
+        else if (Array.isArray(a))
+        {
+            outNum.set(0);
+            outStr.set("");
+            outObj.set(null);
+            outArr.set(a);
+        }
+        else if (typeof a === "object")
+        {
+            outArr.set(null);
+            outNum.set(0);
+            outStr.set("");
+            outObj.set(a);
+        }
+    }
+    catch (e)
+    {
+        outErr.set(true);
+        console.log("error", e);
+    }
+}
