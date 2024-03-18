db417cbbdbfa3751909619542144de1229caeb01
pandrr <tom@tomkom.net>
2024-03-18 13:28:12 +0100

    new op compareimages

---
+++ src/ops/base/Ops.Html.CompareImages/Ops.Html.CompareImages.js
@@ -0,0 +1,39 @@
+const
+    inCanv1 = op.inUrl("Image 1"),
+    inCanv2 = op.inUrl("Image 2"),
+
+    outImageUrl = op.outString("Difference Image"),
+    outMatchPercentage = op.outNumber("Mismatch Percentage"),
+    outSameDimensions = op.outNumber("Same Dimensions"),
+    outFinished = op.outTrigger("Finished");
+
+const img1 = new Image();
+const img2 = new Image();
+
+img2.onload =
+img1.onload = compare;
+
+inCanv1.onChange = () =>
+{
+    img1.src = inCanv1.get();
+};
+
+inCanv2.onChange = () =>
+{
+    img2.src = inCanv2.get();
+};
+
+function compare()
+{
+    resemble(img1.src)
+        .compareTo(img2.src)
+        .onComplete(
+            (data) =>
+            {
+                console.log(data);
+                outImageUrl.set(data.getImageDataUrl());
+                outSameDimensions.set(data.isSameDimensions);
+                outMatchPercentage.set(data.rawMisMatchPercentage);
+                outFinished.trigger();
+            });
+}
