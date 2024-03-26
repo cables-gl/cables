ea76a8c79cc7b96eb4090be79f8ea5f5a1096de0
pandrr <tom@tomkom.net>
2024-03-26 18:29:41 +0100

    cursor

--- src/ops/base/Ops.Html.Cursor_v2/Ops.Html.Cursor_v2.js
+++ src/ops/base/Ops.Html.Cursor_v2/Ops.Html.Cursor_v2.js
@@ -1,8 +1,7 @@
 const
     exec = op.inTriggerButton("Update"),
-
     cursorPort = op.inDropDown("CSS Cursors", ["auto", "crosshair", "pointer", "hand", "move", "n-resize", "ne-resize", "e-resize", "se-resize", "s-resize", "sw-resize", "w-resize", "nw-resize", "ew-resize", "text", "wait", "help", "none"], "pointer"),
-
+    parentEle = op.inBool("Set Parent Element", true),
     next = op.outTrigger("Next");
 
 const cursorStr = "";
@@ -15,6 +14,8 @@ next.onLinkChanged = () =>
 
 exec.onTriggered = () =>
 {
-    op.patch.cgl.setCursor(cursorPort.get());
+    let arg2 = null;
+    if (parentEle.get())arg2 = cursorPort.get();
+    op.patch.cgl.setCursor(cursorPort.get(), arg2);
     next.trigger();
 };
