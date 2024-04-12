335a0ee36cb3b70434a2e56cbc186e59cbd36f0c
pandrr <tom@tomkom.net>
2024-04-12 15:23:54 +0200

    gateObject / gateArray

--- src/ops/base/Ops.Json.GateObject/Ops.Json.GateObject.js
+++ src/ops/base/Ops.Json.GateObject/Ops.Json.GateObject.js
@@ -1,10 +1,13 @@
 const
     valueInPort = op.inObject("Object In"),
-    passThroughPort = op.inValueBool("Pass Through", false),
+    passThroughPort = op.inValueBool("Pass Through", true),
+    inIfNull = op.inSwitch("When False", ["keep last object", "null"], "keep last object"),
     onlyValid = op.inValueBool("Only Valid Objects", false),
+
     valueOutPort = op.outObject("Object Out");
 
 valueInPort.onChange =
+    inIfNull.onChange =
     passThroughPort.onChange = update;
 valueInPort.changeAlways = true;
 
@@ -12,5 +15,8 @@ function update()
 {
     if (!valueInPort.get() && onlyValid.get()) return;
     if (passThroughPort.get()) valueOutPort.setRef(valueInPort.get());
-    // else valueOutPort.set(null);
+    else
+    {
+        if (inIfNull.get() == "null") valueOutPort.setRef(null);
+    }
 }
