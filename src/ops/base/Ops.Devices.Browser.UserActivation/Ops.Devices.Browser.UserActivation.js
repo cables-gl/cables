23078ea79a237f4a70ac70d37c0226d7af7f17bb
pandrr <tom@tomkom.net>
2024-05-08 17:16:26 +0200

    userinteraction api op

---
+++ src/ops/base/Ops.Devices.Browser.UserActivation/Ops.Devices.Browser.UserActivation.js
@@ -0,0 +1,28 @@
+// welcome to your new op!
+// have a look at the documentation:
+// https://cables.gl/docs/5_writing_ops/dev_ops/dev_ops
+
+const
+    exec = op.inTrigger("Update"),
+    next = op.outTrigger("Next"),
+    resultActive = op.outBoolNum("user is or was active"),
+    resultHasBeenActive = op.outBoolNum("user has been active"),
+    resultIsActive = op.outBoolNum("user is active"),
+    outSupport = op.outBoolNum("Supported");
+
+op.toWorkPortsNeedToBeLinked(exec);
+
+exec.onTriggered = () =>
+{
+    if (!navigator.userActivation)
+    {
+        outSupport.set(false);
+    }
+    else
+    {
+        outSupport.set(true);
+        resultActive.set(navigator.userActivation.isActive || navigator.userActivation.hasBeenActive);
+        resultIsActive.set(navigator.userActivation.hasBeenActive);
+        resultIsActive.set(navigator.userActivation.isActive);
+    }
+};
