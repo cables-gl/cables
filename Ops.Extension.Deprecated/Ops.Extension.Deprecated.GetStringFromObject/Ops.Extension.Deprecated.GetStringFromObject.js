d987652ed5de99731e6017abf4fd3147f1a024b2
undev-studio <services@undev.de>
2024-07-15 14:00:09 +0200

    auto commit

---
+++ Ops.Extension.Deprecated/Ops.Extension.Deprecated.GetStringFromObject/Ops.Extension.Deprecated.GetStringFromObject.js
@@ -0,0 +1,25 @@
+const
+    data = op.inObject("data"),
+    key = op.inString("key"),
+    result = op.outString("result");
+
+result.ignoreValueSerialize = true;
+data.ignoreValueSerialize = true;
+key.setUiAttribs({ "stringTrim": true });
+
+data.onChange = exec;
+
+key.onChange = function ()
+{
+    op.setUiAttrib({ "extendTitle": key.get() });
+    exec();
+};
+
+function exec()
+{
+    const dat = data.get();
+    const theKey = key.get();
+
+    if (dat && dat.hasOwnProperty(theKey)) result.set(dat[theKey]);
+    else result.set(null);
+}
