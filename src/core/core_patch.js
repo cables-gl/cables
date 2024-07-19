a5a4916eda5d7c30a7c499a6b36d8422157438d8
pandrr <tom@tomkom.net>
2024-07-19 14:19:42 +0200

    logging

--- src/core/core_patch.js
+++ src/core/core_patch.js
@@ -488,7 +488,7 @@ Patch.prototype.addOp = function (opIdentifier, uiAttribs, id, fromDeserialize, 
 
         if (this._opIdCache[op.id])
         {
-            console.log("opid with id " + op.id + " already exists in patch!");
+            this._log.warn("opid with id " + op.id + " already exists in patch!");
             this.deleteOp(op.id); // strange with subpatch ops: why is this needed, somehow ops get added twice ???.....
             // return;
         }
@@ -579,7 +579,7 @@ Patch.prototype.deleteOp = function (opid, tryRelink, reloadingOp)
                 if (this.onDelete)
                 {
                     // todo: remove
-                    console.log("deprecated this.onDelete", this.onDelete);
+                    this._log.warn("deprecated this.onDelete", this.onDelete);
                     this.onDelete(opToDelete);
                 }
 
@@ -603,7 +603,7 @@ Patch.prototype.deleteOp = function (opid, tryRelink, reloadingOp)
         }
     }
 
-    if (!found) console.log("core patch deleteop: not found...", opid);
+    if (!found) this._log.warn("core patch deleteop: not found...", opid);
 };
 
 Patch.prototype.getFrameNum = function ()
@@ -717,36 +717,17 @@ Patch.prototype.exec = function (timestamp)
  */
 Patch.prototype.link = function (op1, port1Name, op2, port2Name, lowerCase, fromDeserialize)
 {
-    if (!op1)
-    {
-        console.warn("link: op1 is null ");
-        return;
-    }
-    if (!op2)
-    {
-        console.warn("link: op2 is null");
-        return;
-    }
+    if (!op1) return op1._log.warn("link: op1 is null ");
+    if (!op2) return op1._log.warn("link: op2 is null");
 
     const port1 = op1.getPort(port1Name, lowerCase);
     const port2 = op2.getPort(port2Name, lowerCase);
 
-    if (!port1)
-    {
-        console.warn("port1 not found! " + port1Name + "(" + op1.objName + ")");
-        return;
-    }
+    if (!port1) return op1._log.warn("port1 not found! " + port1Name + "(" + op1.objName + ")");
 
-    if (!port2)
-    {
-        console.warn("port2 not found! " + port2Name + " of " + op2.name + "(" + op2.objName + ")", op2);
-        return;
-    }
+    if (!port2) return op1._log.warn("port2 not found! " + port2Name + " of " + op2.name + "(" + op2.objName + ")", op2);
 
-    if (!port1.shouldLink(port1, port2) || !port2.shouldLink(port1, port2))
-    {
-        return false;
-    }
+    if (!port1.shouldLink(port1, port2) || !port2.shouldLink(port1, port2)) return false;
 
     if (Link.canLink(port1, port2))
     {
@@ -844,6 +825,7 @@ Patch.prototype.getSubPatchOp = function (patchId, objName)
     for (const i in this.ops)
         if (this.ops[i].uiAttribs && this.ops[i].uiAttribs.subPatch == patchId && this.ops[i].objName == objName)
             return this.ops[i];
+
     return false;
 };
 
