c0c621c7fe2999dc4eacd9bd861d9d1bb33b0ee4
pandrr <tom@tomkom.net>
2024-09-06 16:56:11 +0200

    standalone directory ops

---
+++ Ops.Extension.Standalone/Ops.Extension.Standalone.Files.OpenFileManager/Ops.Extension.Standalone.Files.OpenFileManager.js
@@ -0,0 +1,10 @@
+
+const
+    inPath=op.inString("Path",""),
+    exec = op.inTriggerButton("Open File Manager");
+
+exec.onTriggered = () =>
+{
+    CABLESUILOADER.talkerAPI.send("openDir",{dir:inPath.get()});
+};
+
