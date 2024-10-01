32b7fcbde68787988ca8645524a813232ab6399c
pandrr <tom@tomkom.net>
2024-10-01 14:46:06 +0200

    rgbToHsl and back

---
+++ src/ops/base/Ops.Color.HSLtoRGB/Ops.Color.HSLtoRGB.js
@@ -0,0 +1,20 @@
+const
+    inH = op.inFloat("Hue", 0.5),
+    inS = op.inFloat("Saturation", 1),
+    inL = op.inFloat("Lightness", 0.5),
+    outR = op.outNumber("R"),
+    outG = op.outNumber("G"),
+    outB = op.outNumber("B");
+
+inH.onChange =
+inS.onChange =
+inL.onChange = () =>
+{
+    const rgb = chroma(inH.get() * 360, inS.get(), inL.get(), "hsl").rgb();
+
+    outR.set(rgb[0] / 255);
+    outG.set(rgb[1] / 255);
+    outB.set(rgb[2] / 255);
+};
+
+console.log(chroma("orange").rgba());
