fdfe02a6b30b4a785fb4629212d72f2b7d61c121
pandrr <tom@tomkom.net>
2024-10-11 11:29:35 +0200

    vetexdisplacement fix

---
+++ src/ops/base/Ops.Gl.ShaderEffects.VertexDisplacementMap_v5/Ops.Gl.ShaderEffects.VertexDisplacementMap_v5.js
@@ -0,0 +1,145 @@
+const
+    render = op.inTrigger("Render"),
+    extrude = op.inValue("Extrude", 0.5),
+    meth = op.inSwitch("Mode", ["Norm", "Tang", "BiTang", "VertCol", "*", "+", "/"], "Norm"),
+    axis = op.inSwitch("Axis", ["XYZ", "XY", "X", "Y", "Z"], "XYZ"),
+    src = op.inSwitch("Coordinates", ["Tex Coords", "Mesh XY", "Mesh XZ"], "Tex Coords"),
+
+    texture = op.inTexture("Texture", null, "texture"),
+    channel = op.inSwitch("Channel", ["Luminance", "R", "G", "B", "A", "RGB"], "Luminance"),
+    flip = op.inSwitch("Flip", ["None", "X", "Y", "XY"], "None"),
+    range = op.inSwitch("Range", ["0-1", "1-0", "Normalized"], "0-1"),
+    offsetX = op.inValueFloat("Offset X"),
+    offsetY = op.inValueFloat("Offset Y"),
+    scale = op.inValueFloat("Scale", 1),
+
+    calcNormals = op.inValueBool("Calc Normals", false),
+    calcNormalsAxis = op.inSwitch("Normal Axis", ["X", "Y", "Z"], "Z"),
+    removeZero = op.inValueBool("Discard Zero Values"),
+    colorize = op.inValueBool("colorize", false),
+    colorizeMin = op.inValueSlider("Colorize Min", 0),
+    colorizeMax = op.inValueSlider("Colorize Max", 1),
+    next = op.outTrigger("trigger");
+
+const cgl = op.patch.cgl;
+
+op.setPortGroup("Input", [texture, flip, channel, range, offsetX, offsetY, scale]);
+op.setPortGroup("Colorize", [colorize, colorizeMin, colorizeMax, removeZero]);
+
+op.toWorkPortsNeedToBeLinked(texture, next, render);
+
+render.onTriggered = dorender;
+
+channel.onChange =
+    src.onChange =
+    axis.onChange =
+    flip.onChange =
+    meth.onChange =
+    range.onChange =
+    colorize.onChange =
+    removeZero.onChange =
+    calcNormals.onChange =
+    calcNormalsAxis.onChange = updateDefines;
+
+const srcHeadVert = attachments.vertdisplace_head_vert;
+const srcBodyVert = attachments.vertdisplace_body_vert;
+
+const srcHeadFrag = ""
+    .endl() + "IN vec3 MOD_displHeightMapColor;"
+    .endl() + "vec3 MOD_map(vec3 value, float inMin, float inMax, float outMin, float outMax) { return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);}"
+
+    .endl();
+
+const srcBodyFrag = ""
+    .endl() + "#ifdef MOD_HEIGHTMAP_COLORIZE"
+    .endl() + "   col.rgb*=MOD_map( MOD_displHeightMapColor, 0.0,1.0 , MOD_colorizeMin,MOD_colorizeMax);"
+    .endl() + "#endif"
+    .endl() + "#ifdef MOD_DISPLACE_REMOVE_ZERO"
+    .endl() + "   if(MOD_displHeightMapColor.r==0.0)discard;"
+    .endl() + "#endif"
+    .endl();
+
+const mod = new CGL.ShaderModifier(cgl, op.name, { "opId": op.id });
+mod.addModule({
+    "title": op.name,
+    "name": "MODULE_VERTEX_POSITION",
+    "srcHeadVert": srcHeadVert,
+    "srcBodyVert": srcBodyVert
+});
+
+mod.addModule({
+    "title": op.name,
+    "name": "MODULE_COLOR",
+    "srcHeadFrag": srcHeadFrag,
+    "srcBodyFrag": srcBodyFrag
+});
+
+mod.addUniformVert("t", "MOD_texture", 0);
+mod.addUniformVert("f", "MOD_extrude", extrude);
+mod.addUniformVert("f", "MOD_offsetX", offsetX);
+mod.addUniformVert("f", "MOD_offsetY", offsetY);
+mod.addUniformVert("f", "MOD_scale", scale);
+
+mod.addUniformFrag("f", "MOD_colorizeMin", colorizeMin);
+mod.addUniformFrag("f", "MOD_colorizeMax", colorizeMax);
+
+updateDefines();
+
+function updateDefines()
+{
+    mod.toggleDefine("MOD_HEIGHTMAP_COLORIZE", colorize.get());
+
+    mod.toggleDefine("MOD_HEIGHTMAP_INVERT", range.get() == "1-0");
+    mod.toggleDefine("MOD_HEIGHTMAP_NORMALIZE", range.get() == "Normalized");
+
+    mod.toggleDefine("MOD_DISPLACE_REMOVE_ZERO", removeZero.get());
+
+    mod.toggleDefine("MOD_INPUT_R", channel.get() == "R");
+    mod.toggleDefine("MOD_INPUT_G", channel.get() == "G");
+    mod.toggleDefine("MOD_INPUT_B", channel.get() == "B");
+    mod.toggleDefine("MOD_INPUT_A", channel.get() == "A");
+    mod.toggleDefine("MOD_INPUT_RGB", channel.get() == "RGB");
+    mod.toggleDefine("MOD_INPUT_LUMI", channel.get() == "Luminance");
+
+    mod.toggleDefine("MOD_FLIP_X", flip.get() == "X");
+    mod.toggleDefine("MOD_FLIP_Y", flip.get() == "Y");
+    mod.toggleDefine("MOD_FLIP_XY", flip.get() == "XY");
+
+    mod.toggleDefine("MOD_AXIS_X", axis.get() == "X");
+    mod.toggleDefine("MOD_AXIS_Y", axis.get() == "Y");
+    mod.toggleDefine("MOD_AXIS_Z", axis.get() == "Z");
+    mod.toggleDefine("MOD_AXIS_XYZ", axis.get() == "XYZ");
+    mod.toggleDefine("MOD_AXIS_XY", axis.get() == "XY");
+
+    mod.toggleDefine("MOD_MODE_BITANGENT", meth.get() == "BiTang");
+    mod.toggleDefine("MOD_MODE_TANGENT", meth.get() == "Tang");
+    mod.toggleDefine("MOD_MODE_NORMAL", meth.get() == "Norm");
+    mod.toggleDefine("MOD_MODE_VERTCOL", meth.get() == "VertCol");
+    mod.toggleDefine("MOD_MODE_MUL", meth.get() == "*");
+    mod.toggleDefine("MOD_MODE_ADD", meth.get() == "+");
+    mod.toggleDefine("MOD_MODE_DIV", meth.get() == "/");
+    mod.toggleDefine("MOD_SMOOTHSTEP", 0);
+
+    mod.toggleDefine("MOD_COORD_TC", src.get() == "Tex Coords");
+    mod.toggleDefine("MOD_COORD_MESHXY", src.get() == "Mesh XY");
+    mod.toggleDefine("MOD_COORD_MESHXZ", src.get() == "Mesh XZ");
+
+    mod.toggleDefine("MOD_CALC_NORMALS", calcNormals.get());
+    mod.toggleDefine("MOD_NORMALS_X", calcNormalsAxis.get() == "X");
+    mod.toggleDefine("MOD_NORMALS_Y", calcNormalsAxis.get() == "Y");
+    mod.toggleDefine("MOD_NORMALS_Z", calcNormalsAxis.get() == "Z");
+
+    calcNormalsAxis.setUiAttribs({ "greyout": !calcNormals.get() });
+}
+
+function dorender()
+{
+    mod.bind();
+
+    if (texture.get() && !texture.get().deleted) mod.pushTexture("MOD_texture", texture.get());
+    else mod.pushTexture("MOD_texture", CGL.Texture.getEmptyTexture(cgl));
+
+    next.trigger();
+
+    mod.unbind();
+}
