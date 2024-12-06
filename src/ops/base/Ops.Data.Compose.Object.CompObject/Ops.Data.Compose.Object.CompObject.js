2284b6ca1033123b800fbe07a9dea8646bb3ea26
pandrr <tom@tomkom.net>
2024-12-06 12:24:29 +0100

    compose array/object clear toggle

--- src/ops/base/Ops.Data.Compose.Object.CompObject/Ops.Data.Compose.Object.CompObject.js
+++ src/ops/base/Ops.Data.Compose.Object.CompObject/Ops.Data.Compose.Object.CompObject.js
@@ -1,13 +1,16 @@
 const
     update = op.inTrigger("Update"),
+    inClear = op.inBool("Clear", true),
     next = op.outTrigger("Next"),
     outArr = op.outObject("Result");
+
+let obj = {};
 
 update.onTriggered = () =>
 {
     op.patch.frameStore.compObject = op.patch.frameStore.compObject || [];
 
-    let obj = {};
+    if (inClear.get())obj = {};
     op.patch.frameStore.compObject.push(obj);
     next.trigger();
 
