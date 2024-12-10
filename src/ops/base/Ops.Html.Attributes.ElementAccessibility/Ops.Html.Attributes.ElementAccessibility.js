a9cf1910185e77771f609ca915457c53b9c4ad59
pandrr <tom@tomkom.net>
2024-12-10 16:24:11 +0100

    new op element accessibility

---
+++ src/ops/base/Ops.Html.Attributes.ElementAccessibility/Ops.Html.Attributes.ElementAccessibility.js
@@ -0,0 +1,45 @@
+const
+    inEle = op.inObject("Element"),
+    inAriaHidden = op.inBool("aria hidden", false),
+
+    outEle = op.outObject("HTML Element");
+
+op.setPortGroup("Element", [inEle]);
+
+inAriaHidden.onChange = update;
+let ele = null;
+
+inEle.onChange =
+    outEle.onLinkChanged =
+    inEle.onLinkChanged = removeProp;
+
+function removeProp()
+{
+    if (ele) ele.removeAttribute("aria-hidden");
+}
+
+function update()
+{
+    ele = inEle.get();
+    if (ele && ele.style)
+    {
+        if (inAriaHidden.get())
+        {
+            ele.setAttribute("aria-hidden", true);
+
+            op.setUiAttrib({ "extendTitle": "hidden" });
+        }
+        else
+        {
+            ele.removeAttribute("aria-hidden");
+
+            op.setUiAttrib({ "extendTitle": "" });
+        }
+        ele.setAttribute("tabindex", 0);
+
+        if (inValue.get())ele.setAttribute("alt", inValue.get());
+        else ele.removeAttribute("alt");
+    }
+
+    outEle.setRef(inEle.get());
+}
