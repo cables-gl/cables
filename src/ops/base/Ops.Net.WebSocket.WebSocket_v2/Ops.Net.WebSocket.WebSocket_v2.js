4adf5481097a33152034b21a3785d444d53f259a
Stephan Maihöfer <stephan@undev.de>
2024-12-11 15:28:48 +0100

    websocket

--- src/ops/base/Ops.Net.WebSocket.WebSocket_v2/Ops.Net.WebSocket.WebSocket_v2.js
+++ src/ops/base/Ops.Net.WebSocket.WebSocket_v2/Ops.Net.WebSocket.WebSocket_v2.js
@@ -8,13 +8,11 @@ const
     outRaw = op.outString("Raw Data");
 
 let connection = null;
-let timeout = null;
+let timeout = setTimeout(checkConnection, 2000);
 let connectedTo = "";
 
 inUrl.onChange = connect;
-timeout = setTimeout(checkConnection, 2000);
-
-inUrl.set();
+connect();
 
 let connecting = false;
 
@@ -22,58 +20,62 @@ function checkConnection()
 {
     if (!outConnected.get() && !connecting)
     {
-        // op.log("reconnect websocket...");
         connect();
     }
-
     timeout = setTimeout(checkConnection, 2000);
 }
 
 op.onDelete = function ()
 {
-    if (outConnected.get())connection.close();
+    if (outConnected.get()) connection.close();
     connecting = false;
     clearTimeout(timeout);
 };
 
 function connect()
 {
+    const url = inUrl.get();
+    if (!url) return;
+
+    outConnected.set(false);
     op.setUiError("connection", null);
     op.setUiError("jsonvalid", null);
+    op.setUiError("websocket", null);
 
     if (outConnected.get() && connectedTo == inUrl.get()) return;
 
-    if (inUrl.get() && inUrl.get().indexOf("ws://") == -1 && inUrl.get().indexOf("wss://") == -1)
+    if (inUrl.get() && !inUrl.get().startsWith("ws://") && !inUrl.get().startsWith("wss://"))
     {
         op.setUiError("wrongproto", "only valid protocols are ws:// or wss:// ");
         return;
     }
     else
+    {
         op.setUiError("wrongproto", null);
+    }
 
     if (!inUrl.get() || inUrl.get() === "")
     {
         op.logWarn("websocket: invalid url ");
-        outConnected.set(false);
         return;
     }
 
     window.WebSocket = window.WebSocket || window.MozWebSocket;
 
     if (!window.WebSocket)
+    {
         return op.logError("Sorry, but your browser doesn't support WebSockets.");
-
-    op.setUiError("websocket", null);
+    }
 
     try
     {
         connecting = true;
-        if (connection !== null)connection.close();
+        if (connection !== null) connection.close();
         connection = new WebSocket(inUrl.get());
     }
     catch (e)
     {
-        if (e && e.message)op.setUiError("websocket", e.message);
+        if (e && e.message) op.setUiError("websocket", e.message);
         op.logWarn("could not connect to", inUrl.get());
         connecting = false;
     }
@@ -83,10 +85,8 @@ function connect()
         connection.onerror = function (e)
         {
             connecting = false;
-
             outConnected.set(false);
             outConnection.set(null);
-            // op.setUiError("connection", "Error connecting to websocket server", 2);
         };
 
         connection.onclose = function (message)
@@ -107,6 +107,8 @@ function connect()
         connection.onmessage = function (message)
         {
             op.setUiError("jsonvalid", null);
+
+            outConnected.set(true);
             outRaw.set(message.data);
             try
             {
