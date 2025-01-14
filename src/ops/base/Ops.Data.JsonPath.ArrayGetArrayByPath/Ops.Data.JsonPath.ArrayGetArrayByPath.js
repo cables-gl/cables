be39dd12a436869d110ae970e405aac77259db40
Stephan Maihöfer <stephan@undev.de>
2025-01-14 11:31:36 +0100

    ByPath, toWorkPortsNeedsString, errorLevels, cleanup

--- src/ops/base/Ops.Data.JsonPath.ArrayGetArrayByPath/Ops.Data.JsonPath.ArrayGetArrayByPath.js
+++ src/ops/base/Ops.Data.JsonPath.ArrayGetArrayByPath/Ops.Data.JsonPath.ArrayGetArrayByPath.js
@@ -6,21 +6,24 @@ const foundOut = op.outBool("Found");
 objectIn.onChange = update;
 pathIn.onChange = update;
 
+op.toWorkPortsNeedsString(pathIn);
+
 function update()
 {
     const data = objectIn.get();
     const path = pathIn.get();
     op.setUiError("missing", null);
+    op.setUiError("notiterable", null);
+
     if (data && path)
     {
         if (!Array.isArray(data) && !(typeof data === "object"))
         {
             foundOut.set(false);
-            op.setUiError("notiterable", "input object of type " + (typeof data) + " is not travesable by path");
+            op.setUiError("notiterable", "input of type " + (typeof data) + " is not traversable by path");
         }
         else
         {
-            op.setUiError("notiterable", null);
             const parts = path.split(".");
             op.setUiAttrib({ "extendTitle": parts[parts.length - 1] + "" });
             let result = resolve(path, data);
@@ -29,14 +32,14 @@ function update()
                 const errorMsg = "could not find element at path " + path;
                 foundOut.set(false);
                 result = null;
-                op.setUiError("missing", errorMsg, 2);
+                op.setUiError("missing", errorMsg, 1);
             }
             else if (!Array.isArray(result))
             {
                 const errorMsg = "element at path " + path + " is not an array";
                 foundOut.set(false);
                 result = null;
-                op.setUiError("missing", errorMsg, 2);
+                op.setUiError("missing", errorMsg, 1);
             }
             else
             {
