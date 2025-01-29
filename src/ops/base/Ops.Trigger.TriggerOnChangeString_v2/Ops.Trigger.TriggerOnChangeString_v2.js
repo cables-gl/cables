faf55c19fb206f6b9130f37dfd2bec3822dae7aa
pandrr <tom@tomkom.net>
2025-01-29 16:00:26 +0100

    triggerchangestring trigger on patch load/init

---
+++ src/ops/base/Ops.Trigger.TriggerOnChangeString_v2/Ops.Trigger.TriggerOnChangeString_v2.js
@@ -0,0 +1,16 @@
+const
+    inval = op.inString("String"),
+    next = op.outTrigger("Changed"),
+    outStr = op.outString("Result");
+
+outStr.ignoreValueSerialize = true;
+
+inval.onChange = function ()
+{
+    outStr.set(inval.get());
+    next.trigger();
+};
+op.init = () =>
+{
+    if (inval.get() != "")next.trigger();
+};
