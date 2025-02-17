7352ee99227f8a597f5994391382c0370aa37c08
pandrr <tom@tomkom.net>
2025-02-17 09:47:18 +0100

    viztrigger

--- src/ops/base/Ops.Ui.VizTrigger/Ops.Ui.VizTrigger.js
+++ src/ops/base/Ops.Ui.VizTrigger/Ops.Ui.VizTrigger.js
@@ -1,25 +1,33 @@
 const
     inTrigger = op.inTriggerButton("Trigger"),
     inReset = op.inTriggerButton("Reset"),
+    inText = op.inBool("Count Overlay", true),
     outCount = op.outNumber("Count");
 
 op.setUiAttrib({ "height": 100, "width": 130, "resizable": true });
 
 let lastTime = 0;
 let count = 0;
+let lh = 0;
 
 inReset.onTriggered = () =>
 {
     count = 0;
-    op.setUiAttrib({ "extendTitle": "_____" + String(count) });
+    if (!inText.get()) op.setUiAttrib({ "extendTitle": "_____" + String(count) });
     outCount.set(count);
 };
 
 inTrigger.onTriggered = () =>
 {
     lastTime = performance.now();
-    op.setUiAttrib({ "extendTitle": String(++count) });
+    count++;
+    if (!inText.get()) op.setUiAttrib({ "extendTitle": String(count) });
     outCount.set(count);
+};
+
+inText.onChange = () =>
+{
+    op.setUiAttrib({ "extendTitle": null });
 };
 
 op.renderVizLayer = (ctx, layer) =>
@@ -43,9 +51,19 @@ op.renderVizLayer = (ctx, layer) =>
     circle.arc(
         layer.x + layer.width / 2,
         layer.y + layer.height / 2,
-        radius - (ctx.lineWidth / 2) + (sizeAnim * 2),
+        Math.abs(radius - (ctx.lineWidth / 2) + (sizeAnim * 2)),
         0, 2 * Math.PI, false);
     ctx.fill(circle);
 
-    ctx.globalAlpha = 1.0;
+    ctx.globalAlpha = 1;
+
+    if (inText.get())
+    {
+        const fntSize = Math.min(layer.height, layer.width) / 4;
+        ctx.font = fntSize + "px monospace";
+        const textDimensions = ctx.measureText(String(count));
+
+        ctx.fillStyle = "#000";
+        ctx.fillText(String(count), layer.x + layer.width / 2 - textDimensions.width / 2, layer.y + layer.height / 2 + fntSize / 3);
+    }
 };
