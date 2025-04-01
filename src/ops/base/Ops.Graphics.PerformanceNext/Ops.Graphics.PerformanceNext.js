6fb7c78a2da0057472110d6c6412f977e3451a8e
pandrr <tom@tomkom.net>
2025-04-01 14:04:10 +0200

    stuff

---
+++ src/ops/base/Ops.Graphics.PerformanceNext/Ops.Graphics.PerformanceNext.js
@@ -0,0 +1,561 @@
+const
+    exe = op.inTrigger("exe"),
+    inActive = op.inValueBool("Active", true),
+    inShow = op.inValueBool("Visible", true),
+    inDoGpu = op.inValueBool("Measure GPU", true),
+    next = op.outTrigger("childs"),
+    position = op.inSwitch("Position", ["top", "bottom"], "top"),
+    openDefault = op.inBool("Open", false),
+    smoothGraph = op.inBool("Smooth Graph", true),
+    inScaleGraph = op.inFloat("Scale", 3),
+    inSizeGraph = op.inFloat("Size", 128),
+    outCanv = op.outObject("Canvas"),
+    outFPS = op.outNumber("FPS");
+
+const element = document.createElement("div");
+
+let cgl = null;
+let elementMeasures = null;
+let ctx = null;
+let opened = false;
+let frameCount = 0;
+let fps = 0;
+let fpsStartTime = 0;
+let childsTime = 0;
+let avgMsChilds = 0;
+const queue = [];
+const timesMainloop = [];
+const timesOnFrame = [];
+const timesGPU = [];
+let avgMs = 0;
+let selfTime = 0;
+let canvas = null;
+let lastTime = 0;
+let loadingCounter = 0;
+const loadingChars = ["|", "/", "-", "\\"];
+let initMeasures = true;
+
+const colorRAFSlow = "#007f9c";
+const colorRAFVeruSlow = "#aaaaaa";
+
+const colorBg = "#222222";
+const colorRAF = "#003f5c"; // color: https://learnui.design/tools/data-color-picker.html
+const colorMainloop = "#7a5195";
+const colorOnFrame = "#ef5675";
+const colorGPU = "#ffa600";
+
+let startedQuery = false;
+
+let currentTimeGPU = 0;
+let currentTimeMainloop = 0;
+let currentTimeOnFrame = 0;
+
+op.toWorkPortsNeedToBeLinked(exe, next);
+
+const gl = op.patch.cgl.gl;
+const glQueryExt = gl.getExtension("EXT_disjoint_timer_query_webgl2");
+
+inActive.onChange =
+exe.onLinkChanged =
+    inShow.onChange = () =>
+    {
+        updateOpened();
+        updateVisibility();
+    };
+
+position.onChange = updatePos;
+inSizeGraph.onChange = updateSize;
+
+element.id = "performance";
+element.style.position = "absolute";
+element.style.left = "0px";
+element.style.opacity = "0.8";
+element.style.padding = "10px";
+element.style.cursor = "pointer";
+element.style.background = "#222";
+element.style.color = "white";
+element.style["font-family"] = "monospace";
+element.style["font-size"] = "12px";
+element.style["z-index"] = "99999";
+
+element.innerHTML = "&nbsp;";
+element.addEventListener("click", toggleOpened);
+
+let container = null;
+
+updateSize();
+updateOpened();
+updatePos();
+updateVisibility();
+
+op.onDelete = function ()
+{
+    if (canvas)canvas.remove();
+    if (element)element.remove();
+};
+
+function updatePos()
+{
+    canvas.style["pointer-events"] = "none";
+    if (position.get() == "top")
+    {
+        canvas.style.top = element.style.top = "0px";
+        canvas.style.bottom = element.style.bottom = "initial";
+    }
+    else
+    {
+        canvas.style.bottom = element.style.bottom = "0px";
+        canvas.style.top = element.style.top = "initial";
+    }
+}
+
+function updateVisibility()
+{
+    if (!inShow.get() || !exe.isLinked() || !inActive.get())
+    {
+        element.style.display = "none";
+        element.style.opacity = 0;
+        canvas.style.display = "none";
+    }
+    else
+    {
+        element.style.display = "block";
+        element.style.opacity = 1;
+        canvas.style.display = "block";
+    }
+}
+
+function updateSize()
+{
+    if (!canvas) return;
+
+    const num = Math.max(0, parseInt(inSizeGraph.get()));
+
+    canvas.width = num;
+    canvas.height = num;
+    element.style.left = num + "px";
+
+    queue.length = 0;
+    timesMainloop.length = 0;
+    timesOnFrame.length = 0;
+    timesGPU.length = 0;
+
+    for (let i = 0; i < num; i++)
+    {
+        queue[i] = -1;
+        timesMainloop[i] = -1;
+        timesOnFrame[i] = -1;
+        timesGPU[i] = -1;
+    }
+}
+
+openDefault.onChange = function ()
+{
+    opened = openDefault.get();
+    updateOpened();
+};
+
+function toggleOpened()
+{
+    if (!inShow.get()) return;
+    element.style.opacity = 1;
+    opened = !opened;
+    updateOpened();
+}
+
+function updateOpened()
+{
+    updateText();
+    if (!canvas)createCanvas();
+    if (opened)
+    {
+        canvas.style.display = "block";
+        element.style.left = inSizeGraph.get() + "px";
+        element.style["min-height"] = "56px";
+    }
+    else
+    {
+        canvas.style.display = "none";
+        element.style.left = "0px";
+        element.style["min-height"] = "auto";
+    }
+}
+
+function updateCanvas()
+{
+    const height = canvas.height;
+    const hmul = inScaleGraph.get();
+
+    ctx.fillStyle = colorBg;
+    ctx.fillRect(0, 0, canvas.width, height);
+
+    ctx.fillStyle = colorRAF;
+
+    let k = 0;
+    const numBars = Math.max(0, parseInt(inSizeGraph.get()));
+
+    for (k = numBars; k >= 0; k--)
+    {
+        if (queue[k] > 30) ctx.fillStyle = colorRAFSlow;
+        if (queue[k] > 60) ctx.fillStyle = colorRAFVeruSlow;
+
+        ctx.fillRect(numBars - k, height - queue[k] * hmul, 1, queue[k] * hmul);
+        if (queue[k] > 30)ctx.fillStyle = colorRAF;
+    }
+
+    for (k = numBars; k >= 0; k--)
+    {
+        let sum = 0;
+        ctx.fillStyle = colorMainloop;
+        sum = timesMainloop[k];
+        ctx.fillRect(numBars - k, height - sum * hmul, 1, timesMainloop[k] * hmul);
+
+        ctx.fillStyle = colorOnFrame;
+        sum += timesOnFrame[k];
+        ctx.fillRect(numBars - k, height - sum * hmul, 1, timesOnFrame[k] * hmul);
+
+        ctx.fillStyle = colorGPU;
+        sum += timesGPU[k];
+        ctx.fillRect(numBars - k, height - sum * hmul, 1, timesGPU[k] * hmul);
+    }
+
+    for (let i = 10; i < height; i += 10)
+    {
+        ctx.fillStyle = "#888";
+        const y = height - (i * hmul);
+        ctx.fillRect(canvas.width - 5, y, 5, 1);
+        ctx.font = "8px arial";
+
+        ctx.fillText(i + "ms", canvas.width - 27, y + 3);
+    }
+
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(canvas.width - 5, height - (1000 / fps * hmul), 5, 1);
+    ctx.fillText(Math.round(1000 / fps) + "ms", canvas.width - 27, height - (1000 / fps * hmul));
+}
+
+function createCanvas()
+{
+    canvas = document.createElement("canvas");
+    canvas.id = "performance_" + op.patch.config.glCanvasId;
+    canvas.width = inSizeGraph.get();
+    canvas.height = inSizeGraph.get();
+    canvas.style.display = "block";
+    canvas.style.opacity = 0.9;
+    canvas.style.position = "absolute";
+    canvas.style.left = "0px";
+    canvas.style.cursor = "pointer";
+    canvas.style.top = "-64px";
+    canvas.style["z-index"] = "99998";
+    container.appendChild(canvas);
+    ctx = canvas.getContext("2d");
+
+    canvas.addEventListener("click", toggleOpened);
+
+    updateSize();
+}
+
+function updateText()
+{
+    if (!inShow.get()) return;
+    cgl = op.patch.cg || op.patch.cgl;
+
+    if (!container)
+    {
+        container = cgl.canvas.parentElement;
+        container.appendChild(element);
+    }
+    let warn = "";
+
+    if (cgl.profileData)
+    {
+        if (cgl.profileData.profileShaderCompiles > 0)warn += "Shader compile (" + cgl.profileData.profileShaderCompileName + ") ";
+        if (cgl.profileData.profileShaderGetUniform > 0)warn += "Shader get uni loc! (" + cgl.profileData.profileShaderGetUniformName + ")";
+        if (cgl.profileData.profileTextureResize > 0)warn += "Texture resize! ";
+        if (cgl.profileData.profileFrameBuffercreate > 0)warn += "Framebuffer create! ";
+        if (cgl.profileData.profileEffectBuffercreate > 0)warn += "Effectbuffer create! ";
+        if (cgl.profileData.profileTextureDelete > 0)warn += "Texture delete! ";
+        if (cgl.profileData.profileNonTypedAttrib > 0)warn += "Not-Typed Buffer Attrib! " + cgl.profileData.profileNonTypedAttribNames;
+        if (cgl.profileData.profileTextureNew > 0)warn += "new texture created! ";
+        if (cgl.profileData.profileGenMipMap > 0)warn += "generating mip maps!";
+    }
+    if (warn.length > 0)
+    {
+        warn = "| <span style=\"color:#f80;\">WARNING: " + warn + "<span>";
+    }
+
+    let html = "";
+
+    if (opened)
+    {
+        html += "<span style=\"color:" + colorRAF + "\">■</span> " + fps + " fps ";
+        html += "<span style=\"color:" + colorMainloop + "\">■</span> " + Math.round(currentTimeMainloop * 100) / 100 + "ms mainloop ";
+        html += "<span style=\"color:" + colorOnFrame + "\">■</span> " + Math.round((currentTimeOnFrame) * 100) / 100 + "ms onframe ";
+        if (currentTimeGPU) html += "<span style=\"color:" + colorGPU + "\">■</span> " + Math.round(currentTimeGPU * 100) / 100 + "ms GPU";
+        html += warn;
+        element.innerHTML = html;
+    }
+    else
+    {
+        html += fps + " fps / ";
+        html += "CPU: " + Math.round((cgl.profileData.profileOnAnimFrameOps) * 100) / 100 + "ms / ";
+        if (currentTimeGPU)html += "GPU: " + Math.round(currentTimeGPU * 100) / 100 + "ms  ";
+        element.innerHTML = html;
+    }
+
+    if (op.patch.loading.getProgress() != 1.0)
+    {
+        element.innerHTML += "<br/>loading " + Math.round(op.patch.loading.getProgress() * 100) + "% " + loadingChars[(++loadingCounter) % loadingChars.length];
+    }
+
+    if (opened)
+    {
+        let count = 0;
+        avgMs = 0;
+        avgMsChilds = 0;
+        for (let i = queue.length; i > queue.length - queue.length / 3; i--)
+        {
+            if (queue[i] > -1)
+            {
+                avgMs += queue[i];
+                count++;
+            }
+
+            if (timesMainloop[i] > -1) avgMsChilds += timesMainloop[i];
+        }
+
+        avgMs /= count;
+        avgMsChilds /= count;
+
+        element.innerHTML += "<br/> " + cgl.canvasWidth + " x " + cgl.canvasHeight + " (x" + cgl.pixelDensity + ") ";
+        element.innerHTML += "<br/>frame avg: " + Math.round(avgMsChilds * 100) / 100 + " ms (" + Math.round(avgMsChilds / avgMs * 100) + "%) / " + Math.round(avgMs * 100) / 100 + " ms";
+        element.innerHTML += " (self: " + Math.round((selfTime) * 100) / 100 + " ms) ";
+
+        element.innerHTML += "<br/>shader binds: " + Math.ceil(cgl.profileData.profileShaderBinds / fps) +
+            " uniforms: " + Math.ceil(cgl.profileData.profileUniformCount / fps) +
+            " mvp_uni_mat4: " + Math.ceil(cgl.profileData.profileMVPMatrixCount / fps) +
+            " num glPrimitives: " + Math.ceil(cgl.profileData.profileMeshNumElements / (fps)) +
+
+            " fenced pixelread: " + Math.ceil(cgl.profileData.profileFencedPixelRead) +
+
+            " mesh.setGeom: " + cgl.profileData.profileMeshSetGeom +
+            " videos: " + cgl.profileData.profileVideosPlaying +
+            " tex preview: " + cgl.profileData.profileTexPreviews;
+
+        element.innerHTML +=
+        " draw meshes: " + Math.ceil(cgl.profileData.profileMeshDraw / fps) +
+        " framebuffer blit: " + Math.ceil(cgl.profileData.profileFramebuffer / fps) +
+        " texeffect blit: " + Math.ceil(cgl.profileData.profileTextureEffect / fps);
+
+        element.innerHTML += " all shader compiletime: " + Math.round(cgl.profileData.shaderCompileTime * 100) / 100;
+    }
+
+    cgl.profileData.clear();
+}
+
+function styleMeasureEle(ele)
+{
+    ele.style.padding = "0px";
+    ele.style.margin = "0px";
+}
+
+function addMeasureChild(m, parentEle, timeSum, level)
+{
+    const height = 20;
+    m.usedAvg = (m.usedAvg || m.used);
+
+    if (!m.ele || initMeasures)
+    {
+        const newEle = document.createElement("div");
+        m.ele = newEle;
+
+        if (m.childs && m.childs.length > 0) newEle.style.height = "500px";
+        else newEle.style.height = height + "px";
+
+        newEle.style.overflow = "hidden";
+        newEle.style.display = "inline-block";
+
+        if (!m.isRoot)
+        {
+            newEle.innerHTML = "<div style=\"min-height:" + height + "px;width:100%;overflow:hidden;color:black;position:relative\">&nbsp;" + m.name + "</div>";
+            newEle.style["background-color"] = "rgb(" + m.colR + "," + m.colG + "," + m.colB + ")";
+            newEle.style["border-left"] = "1px solid black";
+        }
+
+        parentEle.appendChild(newEle);
+    }
+
+    if (!m.isRoot)
+    {
+        if (performance.now() - m.lastTime > 200)
+        {
+            m.ele.style.display = "none";
+            m.hidden = true;
+        }
+        else
+        {
+            if (m.hidden)
+            {
+                m.ele.style.display = "inline-block";
+                m.hidden = false;
+            }
+        }
+
+        m.ele.style.float = "left";
+        m.ele.style.width = Math.floor((m.usedAvg / timeSum) * 98.0) + "%";
+    }
+    else
+    {
+        m.ele.style.width = "100%";
+        m.ele.style.clear = "both";
+        m.ele.style.float = "none";
+    }
+
+    if (m && m.childs && m.childs.length > 0)
+    {
+        let thisTimeSum = 0;
+        for (var i = 0; i < m.childs.length; i++)
+        {
+            m.childs[i].usedAvg = (m.childs[i].usedAvg || m.childs[i].used) * 0.95 + m.childs[i].used * 0.05;
+            thisTimeSum += m.childs[i].usedAvg;
+        }
+        for (var i = 0; i < m.childs.length; i++)
+        {
+            addMeasureChild(m.childs[i], m.ele, thisTimeSum, level + 1);
+        }
+    }
+}
+
+function clearMeasures(p)
+{
+    for (let i = 0; i < p.childs.length; i++) clearMeasures(p.childs[i]);
+    p.childs.length = 0;
+}
+
+function measures()
+{
+    if (!CGL.performanceMeasures) return;
+
+    if (!elementMeasures)
+    {
+        op.log("create measure ele");
+        elementMeasures = document.createElement("div");
+        elementMeasures.style.width = "100%";
+        elementMeasures.style["background-color"] = "#444";
+        elementMeasures.style.bottom = "10px";
+        elementMeasures.style.height = "100px";
+        elementMeasures.style.opacity = "1";
+        elementMeasures.style.position = "absolute";
+        elementMeasures.style["z-index"] = "99999";
+        elementMeasures.innerHTML = "";
+        container.appendChild(elementMeasures);
+    }
+
+    let timeSum = 0;
+    const root = CGL.performanceMeasures[0];
+
+    for (let i = 0; i < root.childs.length; i++) timeSum += root.childs[i].used;
+
+    addMeasureChild(CGL.performanceMeasures[0], elementMeasures, timeSum, 0);
+
+    root.childs.length = 0;
+
+    clearMeasures(CGL.performanceMeasures[0]);
+
+    CGL.performanceMeasures.length = 0;
+    initMeasures = false;
+}
+
+exe.onTriggered = render;
+
+function render()
+{
+    const selfTimeStart = performance.now();
+
+    if (inActive.get())
+    {
+        frameCount++;
+
+        if (cgl && glQueryExt && inDoGpu.get() && inShow.get())cgl.profileData.doProfileGlQuery = true;
+        else cgl.profileData.doProfileGlQuery = false;
+
+        if (fpsStartTime === 0)fpsStartTime = Date.now();
+        if (Date.now() - fpsStartTime >= 1000)
+        {
+        // query=null;
+            fps = frameCount;
+            frameCount = 0;
+            // frames = 0;
+            outFPS.set(fps);
+            if (inShow.get())updateText();
+
+            fpsStartTime = Date.now();
+        }
+
+        const glQueryData = cgl.profileData.glQueryData;
+        currentTimeGPU = 0;
+        if (glQueryData)
+        {
+            let count = 0;
+            for (let i in glQueryData)
+            {
+                count++;
+                if (glQueryData[i].time)
+                    currentTimeGPU += glQueryData[i].time;
+            }
+        }
+
+        if (inShow.get())
+        {
+            measures();
+
+            if (opened && !cgl.profileData.pause)
+            {
+            // const timeUsed = performance.now() - lastTime;
+                queue.push(cgl.profileData.profileFrameDelta);
+                queue.shift();
+
+                timesMainloop.push(childsTime);
+                timesMainloop.shift();
+
+                timesOnFrame.push(cgl.profileData.profileOnAnimFrameOps - cgl.profileData.profileMainloopMs);
+                timesOnFrame.shift();
+
+                timesGPU.push(currentTimeGPU);
+                timesGPU.shift();
+
+                updateCanvas();
+            }
+        }
+
+        lastTime = performance.now();
+        selfTime = performance.now() - selfTimeStart;
+
+        outCanv.setRef(canvas);
+    }
+    const startTimeChilds = performance.now();
+
+    next.trigger();
+
+    if (inActive.get())
+    {
+        const nChildsTime = performance.now() - startTimeChilds;
+        const nCurrentTimeMainloop = cgl.profileData.profileMainloopMs;
+        const nCurrentTimeOnFrame = cgl.profileData.profileOnAnimFrameOps - cgl.profileData.profileMainloopMs;
+
+        if (smoothGraph.get())
+        {
+            childsTime = childsTime * 0.9 + nChildsTime * 0.1;
+            currentTimeMainloop = currentTimeMainloop * 0.5 + nCurrentTimeMainloop * 0.5;
+            currentTimeOnFrame = currentTimeOnFrame * 0.5 + nCurrentTimeOnFrame * 0.5;
+        }
+        else
+        {
+            childsTime = nChildsTime;
+            currentTimeMainloop = nCurrentTimeMainloop;
+            currentTimeOnFrame = nCurrentTimeOnFrame;
+        }
+
+        cgl.profileData.clearGlQuery();
+    }
+}
---
+++ src/ops/base/Ops.Graphics.PerformanceNext/Ops.Graphics.PerformanceNext.md
@@ -0,0 +1,4 @@
+to get meaningful results it should be directly under mainloop and above everything else.
+
+guide on how to optimize the performance of your patch:
+https://cables.gl/docs/9_performance_optimization/performance
