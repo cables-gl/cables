e8f5c540183fac4252c1c2a3282e21655f284be1
pandrr <tom@tomkom.net>
2025-11-24 20:31:21 +0100

    gradient op to css

---
+++ src/ops/base/Ops.HTML.ElementGradientBg/Ops.HTML.ElementGradientBg.js
@@ -0,0 +1,53 @@
+const
+    inEle = op.inObject("Element", null, "element"),
+    inColors = op.inArray("Color Array"),
+    inColorPos = op.inArray("Color Pos Array"),
+    outEle = op.outObject("HTML Element", null, "element");
+
+inColors.onChange =
+inColorPos.onChange =
+inEle.onChange = update;
+
+op.onDelete = remove;
+
+let ele = null;
+update();
+
+function remove()
+{
+    if (ele)
+    {
+        // ele.style.removeProperty("color");
+        // ele.style.removeProperty("background-color");
+    }
+}
+
+function update()
+{
+    ele = inEle.get();
+    const arrCols = inColors.get();
+    const arrPos = inColorPos.get();
+    if (!ele) return;
+    remove();
+    let str = "linear-gradient(90deg,";
+
+    for (let i = 0; i < arrCols.length; i += 3)
+    {
+        let p = i / arrCols.length * 100;
+        if (arrPos) p = arrPos[i / 3] * 100;
+        str += "rgb(";
+        str += Math.round(arrCols[i + 0] * 255);
+        str += ",";
+        str += Math.round(arrCols[i + 1] * 255);
+        str += ",";
+        str += Math.round(arrCols[i + 2] * 255);
+        str += ",";
+        str += 1;// arrCols[i+3]
+        str += ") " + p + "% ";
+
+        if (i < arrCols.length - 4) str += ",";
+    }
+    str += ")";
+
+    ele.style.background = str;
+}
