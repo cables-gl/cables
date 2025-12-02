21959b370b69e72097565197d42305c554ac644a
pandrr <tom@tomkom.net>
2025-12-02 14:48:16 +0100

    directional light

--- Ops.Extension.Three/Ops.Extension.Three.Material.PhongMaterial/Ops.Extension.Three.Material.PhongMaterial.js
+++ Ops.Extension.Three/Ops.Extension.Three.Material.PhongMaterial/Ops.Extension.Three.Material.PhongMaterial.js
@@ -3,7 +3,22 @@ const
     inR = op.inFloatSlider("Color R", 0),
     inG = op.inFloatSlider("Color G", 0),
     inB = op.inFloatSlider("Color B", 0),
+
+    inWire = op.inBool("Wireframe", false),
+    inFlat = op.inBool("Flat Shading", false),
+    inShiny = op.inFloat("Shininess", 30),
+
+    inEmissiveR = op.inFloatSlider("Emissive R", 0),
+    inEmissiveG = op.inFloatSlider("Emissive G", 0),
+    inEmissiveB = op.inFloatSlider("Emissive B", 0),
+
+    inSpecularR = op.inFloatSlider("Specular R", 0),
+    inSpecularG = op.inFloatSlider("Specular G", 0),
+    inSpecularB = op.inFloatSlider("Specular B", 0),
+
     next = op.outTrigger("Next");
+
+inR.setUiAttribs({ "colorPick": true });
 
 const material = new THREE.MeshPhongMaterial({ "color": 0xffff00		 });
 
@@ -12,6 +27,34 @@ inG.onChange =
 inB.onChange = () =>
 {
     material.color.set(inR.get(), inG.get(), inB.get());
+};
+
+inEmissiveR.onChange =
+inEmissiveG.onChange =
+inEmissiveB.onChange = () =>
+{
+    material.emissive.set(inEmissiveR.get(), inEmissiveG.get(), inEmissiveB.get());
+};
+
+inSpecularR.onChange =
+inSpecularG.onChange =
+inSpecularB.onChange = () =>
+{
+    material.specular.set(inSpecularR.get(), inSpecularG.get(), inSpecularB.get());
+};
+
+inFlat.onChange = () =>
+{
+    material.flatShading = inFlat.get();
+};
+inWire.onChange = () =>
+{
+    material.wireframe = inWire.get();
+};
+
+inShiny.onChange = () =>
+{
+    material.shininess = inShiny.get();
 };
 
 exec.onTriggered = () =>
