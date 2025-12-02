3ea33c75075daa453d49884986d7af514c23d75b
pandrr <tom@tomkom.net>
2025-12-02 15:38:19 +0100

    s

--- Ops.Extension.Three/Ops.Extension.Three.Material.PhongMaterial/Ops.Extension.Three.Material.PhongMaterial.js
+++ Ops.Extension.Three/Ops.Extension.Three.Material.PhongMaterial/Ops.Extension.Three.Material.PhongMaterial.js
@@ -4,17 +4,13 @@ const
     inG = op.inFloatSlider("Color G", 0),
     inB = op.inFloatSlider("Color B", 0),
     inA = op.inFloatSlider("Color A", 1),
-
     inWire = op.inBool("Wireframe", false),
     inFlat = op.inBool("Flat Shading", false),
     inShiny = op.inFloat("Shininess", 30),
-
     inMap = op.inObject("Map", null, "three texture"),
-
     inEmissiveR = op.inFloatSlider("Emissive R", 0),
     inEmissiveG = op.inFloatSlider("Emissive G", 0),
     inEmissiveB = op.inFloatSlider("Emissive B", 0),
-
     inSpecularR = op.inFloatSlider("Specular R", 1),
     inSpecularG = op.inFloatSlider("Specular G", 1),
     inSpecularB = op.inFloatSlider("Specular B", 1),
@@ -25,45 +21,17 @@ inR.setUiAttribs({ "colorPick": true });
 
 let material = new THREE.MeshPhongMaterial({ "color": 0xffff00		 });
 
-function recreate()
-{
-    const params = { "color": 0xffff00		 };
-    console.log("recreate...");
-    if (inMap.get())params.map = inMap.get();
-    material = new THREE.MeshPhongMaterial(params);
-    material.flatShading = inFlat.get();
-    material.wireframe = inWire.get();
-    setColor();
-    setEmissiveColor();
-    setSpecularColor();
-}
-
 inR.onChange =
 inG.onChange =
 inB.onChange = setColor;
-
-function setColor()
-{
-    material.color.set(inR.get(), inG.get(), inB.get());
-}
 
 inEmissiveR.onChange =
 inEmissiveG.onChange =
 inEmissiveB.onChange = setEmissiveColor;
 
-function setEmissiveColor()
-{
-    material.emissive.set(inEmissiveR.get(), inEmissiveG.get(), inEmissiveB.get());
-}
-
 inSpecularR.onChange =
 inSpecularG.onChange =
 inSpecularB.onChange = setSpecularColor;
-
-function setSpecularColor()
-{
-    material.specular.set(inSpecularR.get(), inSpecularG.get(), inSpecularB.get());
-}
 
 inMap.onChange =
 inWire.onChange =
@@ -80,3 +48,31 @@ exec.onTriggered = () =>
     next.trigger();
     op.patch.cg.frameStore.three.renderer.popMaterial();
 };
+
+function recreate()
+{
+    const params = { "color": 0xffff00		 };
+    console.log("recreate...");
+    if (inMap.get())params.map = inMap.get();
+    material = new THREE.MeshPhongMaterial(params);
+    material.flatShading = inFlat.get();
+    material.wireframe = inWire.get();
+    setColor();
+    setEmissiveColor();
+    setSpecularColor();
+}
+
+function setColor()
+{
+    material.color.set(inR.get(), inG.get(), inB.get());
+}
+
+function setEmissiveColor()
+{
+    material.emissive.set(inEmissiveR.get(), inEmissiveG.get(), inEmissiveB.get());
+}
+
+function setSpecularColor()
+{
+    material.specular.set(inSpecularR.get(), inSpecularG.get(), inSpecularB.get());
+}
