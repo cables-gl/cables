be72df6e022b1e5789f1827ee2c38033fe2462d5
pandrr <tom@tomkom.net>
2025-12-17 12:07:40 +0100

    stepdebugger ops

---
+++ src/ops/base/Ops.Debug.StepDebugger.StepDebugTrigger/Ops.Debug.StepDebugger.StepDebugTrigger.js
@@ -0,0 +1,27 @@
+const
+    exec = op.inTrigger("Trigger"),
+    inTitle = op.inString("Title", ""),
+    inActive = op.inBool("Active", true),
+    next = op.outTrigger("Next");
+
+inTitle.onChange = () =>
+{
+    op.setUiAttrib({ "extendTitle": inTitle.get() });
+};
+
+exec.onTriggered = () =>
+{
+    if (!inActive.get()) return next.trigger();
+
+    gui.corePatch().startStepDebug();
+    op.patch.tempData.continueStepDebugLog = op.patch.tempData.continueStepDebugLog || [];
+
+    op.patch.tempData.continueStepDebugLog.push({
+        "time": performance.now(),
+        "port": exec,
+        "action": "Start debug " + inTitle.get()
+    });
+
+    next.trigger();
+    gui.corePatch().stopStepDebug();
+};
