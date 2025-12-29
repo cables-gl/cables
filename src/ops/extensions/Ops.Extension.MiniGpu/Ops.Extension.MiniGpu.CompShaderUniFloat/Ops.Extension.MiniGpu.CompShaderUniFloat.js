96c0e98dad3f4964fbcb4c55c221904daa5002da
pandrr <tom@tomkom.net>
2025-12-30 00:18:13 +0100

    uniforms

---
+++ src/ops/extensions/Ops.Extension.MiniGpu/Ops.Extension.MiniGpu.CompShaderUniFloat/Ops.Extension.MiniGpu.CompShaderUniFloat.js
@@ -0,0 +1,52 @@
+const
+    exec = op.inTrigger("Trigger"),
+    inName = op.inString("Name", ""),
+    inX = op.inFloat("X"),
+    inY = op.inFloat("Y"),
+    inZ = op.inFloat("Z"),
+    inW = op.inFloat("W", 1),
+    next = op.outTrigger("Next");
+
+let binding = null;
+let uniformBuffer;
+const uniformArray = new Float32Array([0, 0, 0, 0]);
+
+exec.onTriggered = () =>
+{
+    uniformArray[0] = inX.get();
+    uniformArray[1] = inY.get();
+    uniformArray[2] = inZ.get();
+    uniformArray[3] = inW.get();
+
+    const mgpu = op.patch.frameStore.mgpu;
+    if (!binding)
+    {
+        /* minimalcore:start */
+        op.setUiAttrib({ "extendTitle": inName.get() });
+        /* minimalcore:end */
+
+        const layout = {
+            "visibility": GPUShaderStage.VERTEX,
+            "buffer": {
+                "type": "uniform",
+            },
+        };
+        uniformBuffer = mgpu.device.createBuffer({
+            "size": uniformArray.byteLength,
+            "usage": GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+        });
+
+        const type = "vec4f";
+
+        binding = {
+            "header": "var<uniform> " + inName.get() + " : " + type + ";",
+            "resource": { "buffer": uniformBuffer },
+            "layout": layout
+        };
+    }
+    mgpu.device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
+    mgpu.bindings.push(binding);
+
+    next.trigger();
+};
