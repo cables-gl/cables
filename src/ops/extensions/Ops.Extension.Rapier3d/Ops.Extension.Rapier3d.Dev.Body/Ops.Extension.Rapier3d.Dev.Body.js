3054ac4f84f8a846227fed76d8bf3cd783a6930c
pandrr <tom@tomkom.net>
2026-01-14 09:21:10 +0100

    move rapier to extension

---
+++ src/ops/extensions/Ops.Extension.Rapier3d/Ops.Extension.Rapier3d.Dev.Body/Ops.Extension.Rapier3d.Dev.Body.js
@@ -0,0 +1,244 @@
+const
+    exec = op.inTrigger("Trigger"),
+    inName = op.inString("Name", "default"),
+    inCollShape = op.inDropDown("Body Shape", ["Cuboid", "Ball", "Cylinder", "Capsule", "Tri Mesh", "Convex Hull"], "Cuboid"),
+    inType = op.inDropDown("Type", ["Dynamic", "Fixed", "KinematicPositionBased", "KinematicVelocityBased"], "Dynamic"),
+    inGeom = op.inObject("Geometry", null, "geometry"),
+    inCollRadius = op.inFloat("Radius", 0.5),
+    inCollSizeX = op.inFloat("Size X", 0.5),
+    inCollSizeY = op.inFloat("Size Y", 0.5),
+    inCollSizeZ = op.inFloat("Size Z", 0.5),
+
+    inTranslX = op.inFloat("Translate X", 0.0),
+    inTranslY = op.inFloat("Translate Y", 0.0),
+    inTranslZ = op.inFloat("Translate Z", 0.0),
+
+    inPositions = op.inArray("Positions"),
+    next = op.outTrigger("Next"),
+    outSleeping = op.outBoolNum("Sleeping"),
+    outPos = op.outArray("Result Positions", [], 3),
+    outSize = op.outArray("Result Size", [], 3),
+    outRot = op.outArray("Result Rotations", [], 4);
+let needsSetup, lastWorld, oldShape;
+let rigidBodies = [];
+let tmpOrigin = vec3.create();
+
+exec.onLinkChanged = removeBodies;
+let setPosition = false;
+
+inGeom.onChange =
+inName.onChange =
+inPositions.onChange =
+inType.onChange =
+    inCollSizeX.onChange =
+    inCollSizeY.onChange =
+    inCollSizeZ.onChange =
+    inCollShape.onChange =
+    inCollRadius.onChange = () => { needsSetup = true; };
+
+inTranslX.onChange =
+    inTranslY.onChange =
+    inTranslZ.onChange = () =>
+    {
+        if (inType.get() == "KinematicPositionBased" || inType.get() == "Fixed")
+        {
+            setPosition = true;
+        }
+    };
+
+exec.onTriggered = () =>
+{
+    const world = op.patch.cgl.frameStore.rapierWorld;
+    if (!world) return;
+
+    if (world != lastWorld)needsSetup = true;
+    if (rigidBodies.length == 0) needsSetup = true;
+    if (needsSetup)setup(world);
+
+    const posArray = [];
+    const rotArray = [];
+    const sizeArray = [];
+
+    let sleepCount = 0;
+    for (let i = 0; i < rigidBodies.length; i++)
+        if (rigidBodies[i].isSleeping()) sleepCount++;
+
+    outSleeping.set(sleepCount != rigidBodies.length);
+    mat4.getTranslation(tmpOrigin, op.patch.cgl.mMatrix);
+
+    if (setPosition == true)
+    {
+        for (let i = 0; i < rigidBodies.length; i++)
+        {
+            rigidBodies[i].setTranslation({ "x": inTranslX.get(), "y": inTranslY.get(), "z": inTranslZ.get() }, true);
+
+            setPosition = false;
+        }
+    }
+
+    if (sleepCount != rigidBodies.length)
+    {
+        for (let i = 0; i < rigidBodies.length; i++)
+        {
+            const pos = rigidBodies[i].translation();
+            const rot = rigidBodies[i].rotation();
+
+            posArray.push(pos.x, pos.y, pos.z);
+
+            rotArray.push(rot.x, rot.y, rot.z, rot.w);
+
+            if (inCollShape.get() == "Ball") sizeArray.push(inCollRadius.get(), inCollRadius.get(), inCollRadius.get());
+            else sizeArray.push(inCollSizeX.get() * 2, inCollSizeY.get() * 2, inCollSizeZ.get() * 2);
+        }
+
+        outPos.setRef(posArray);
+        outRot.setRef(rotArray);
+        outSize.setRef(sizeArray);
+    }
+
+    next.trigger();
+};
+
+function getPositions()
+{
+    let pos = inPositions.get() || [0, 0, 0];
+
+    for (let i = 0; i < pos.length; i += 3)
+    {
+        pos[i] += tmpOrigin[0];
+        pos[i + 1] += tmpOrigin[1];
+        pos[i + 2] += tmpOrigin[2];
+    }
+
+    return pos;
+}
+
+function updateUi()
+{
+    if (!CABLES.UI) return;
+    if (oldShape == inCollShape.get()) return;
+    oldShape = inCollShape.get();
+
+    inCollRadius.setUiAttribs({ "greyout": false });
+    inCollSizeX.setUiAttribs({ "greyout": false });
+    inCollSizeY.setUiAttribs({ "greyout": false });
+    inCollSizeZ.setUiAttribs({ "greyout": false });
+
+    if (inCollShape.get() == "Capsule" || inCollShape.get() == "Cylinder")
+    {
+        inCollSizeZ.setUiAttribs({ "greyout": true });
+        inCollSizeX.setUiAttribs({ "greyout": true });
+    }
+    if (inCollShape.get() == "Cuboid")
+    {
+        inCollRadius.setUiAttribs({ "greyout": true });
+    }
+    if (inCollShape.get() == "Ball")
+    {
+        inCollSizeX.setUiAttribs({ "greyout": true });
+        inCollSizeY.setUiAttribs({ "greyout": true });
+        inCollSizeZ.setUiAttribs({ "greyout": true });
+    }
+}
+
+function setup(world)
+{
+    removeBodies();
+
+    const pos = getPositions();
+    let colliderDesc, collider;
+    if (inCollShape.get() == "Capsule") colliderDesc = RAPIER.ColliderDesc.capsule(inCollSizeY.get(), inCollRadius.get());
+    else if (inCollShape.get() == "Cylinder") colliderDesc = RAPIER.ColliderDesc.cylinder(inCollSizeY.get(), inCollRadius.get());
+    else if (inCollShape.get() == "Cuboid") colliderDesc = RAPIER.ColliderDesc.cuboid(inCollSizeX.get(), inCollSizeY.get(), inCollSizeZ.get());
+    else if (inCollShape.get() == "Ball") colliderDesc = RAPIER.ColliderDesc.ball(inCollRadius.get());
+    else if (inCollShape.get() == "Tri Mesh")
+    {
+        const geom = inGeom.get();
+        if (!geom || !geom.vertices || !geom.verticesIndices || geom.vertices.length == 0)
+        {
+            op.warn("no geom, or geom without faces, using cubes");
+            colliderDesc = RAPIER.ColliderDesc.cuboid(0.02, 0.02, 0.02);
+        }
+        else
+        {
+            let faces = geom.verticesIndices || [];
+            if (geom.verticesIndices.length == 0)
+            {
+                for (let i = 0; i < geom.vertices.length / 3; i++)
+                    faces.push(i);
+            }
+            colliderDesc = RAPIER.ColliderDesc.trimesh(geom.vertices, faces);
+        }
+    }
+    else if (inCollShape.get() == "Convex Hull")
+    {
+        const geom = inGeom.get();
+        if (!geom || !geom.vertices || !geom.verticesIndices || geom.vertices.length == 0)
+        {
+            op.warn("no geom, or geom without faces, using cubes");
+            colliderDesc = RAPIER.ColliderDesc.cuboid(0.02, 0.02, 0.02);
+        }
+        else
+        {
+            colliderDesc = RAPIER.ColliderDesc.convexHull(geom.vertices);
+        }
+    }
+    else
+    {
+        op.warn("unknown shape");
+        colliderDesc = RAPIER.ColliderDesc.cuboid(0.02, 0.02, 0.02);
+    }
+
+    for (let i = 0; i < pos.length; i += 3)
+    {
+        const rigidBodyDesc = RAPIER.RigidBodyDesc
+            .dynamic()
+            // .setAdditionalMass(0.5)
+            .setTranslation(pos[i + 0], pos[i + 1], pos[i + 2]);
+
+        colliderDesc
+            .setMass(10)
+            // .setDensity(1)
+            .setFriction(1);
+        // .setRotation({ w: 1.0, x: 0.0, y: 0.0, z: 0.0})
+        // .setGravityScale(0.5)
+        // .setCanSleep(true)
+        // .setCcdEnabled(false);
+
+        const rigidBody = world.createRigidBody(rigidBodyDesc);
+
+        rigidBody.userData = { "name": inName.get() + "." + i };
+        rigidBody.setBodyType(inType.indexPort.get());
+        rigidBodies.push(rigidBody);
+
+        collider = world.createCollider(colliderDesc, rigidBody);
+    }
+
+    needsSetup = false;
+    updateUi();
+    lastWorld = world;
+}
+
+function removeBodies()
+{
+    if (lastWorld)
+    {
+        for (let i = 0; i < rigidBodies.length; i++)
+        {
+            lastWorld.removeRigidBody(rigidBodies[i]);
+        }
+        rigidBodies.length = 0;
+    }
+}
+
+op.onDelete = () =>
+{
+    removeBodies();
+};
+
+op.on("onEnabledChange", () =>
+{
+    removeBodies();
+});
+
+/// ////////
---
+++ src/ops/extensions/Ops.Extension.Rapier3d/Ops.Extension.Rapier3d.Dev.RayCast/Ops.Extension.Rapier3d.Dev.RayCast.js
@@ -0,0 +1,197 @@
+const
+    inExec = op.inTrigger("Update"),
+    // inCoords = op.inSwitch("Ray Coordinates", ["Screen XY", "Points 3d"], "Screen XY"),
+
+    inX = op.inValueFloat("Screen X"),
+    inY = op.inValueFloat("Screen Y"),
+    // inRayPoints = op.inArray("Ray Points"),
+    active = op.inBool("Active", true),
+    next = op.outTrigger("next"),
+
+    outHasHit = op.outBoolNum("Has Hit", false),
+    outName = op.outString("Hit Body Name", ""),
+    outX = op.outNumber("Hit X"),
+    outY = op.outNumber("Hit Y"),
+    outZ = op.outNumber("Hit Z");
+
+inExec.onTriggered = update;
+
+const cgl = op.patch.cgl;
+let world = null;
+let didsetCursor = false;
+let mat = mat4.create();
+let isScreenCoords = true;
+
+// inCoords.onChange = updateCoordsType;
+// updateCoordsType();
+
+// function updateCoordsType()
+// {
+//     isScreenCoords = inCoords.get() == "Screen XY";
+//     inX.setUiAttribs({ "greyout": !isScreenCoords });
+//     inY.setUiAttribs({ "greyout": !isScreenCoords });
+//     inRayPoints.setUiAttribs({ "greyout": isScreenCoords });
+// }
+
+function update()
+{
+    world = cgl.frameStore.rapierWorld;
+
+    if (!world) return;
+
+    if (active.get())
+    {
+        let afrom;
+        let ato;
+
+        // if (isScreenCoords)
+        {
+            const x = inX.get();
+            const y = inY.get();
+
+            const origin = vec3.fromValues(x, y, -1);
+            mat4.mul(mat, cgl.pMatrix, cgl.vMatrix);
+            mat4.invert(mat, mat);
+
+            vec3.transformMat4(origin, origin, mat);
+
+            // -----------
+
+            const to = vec3.fromValues(x, y, 1);
+            mat4.mul(mat, cgl.pMatrix, cgl.vMatrix);
+            mat4.invert(mat, mat);
+
+            vec3.transformMat4(to, to, mat);
+
+            const dir = vec3.fromValues(
+                to[0] - origin[0],
+                to[1] - origin[1],
+                to[2] - origin[2]);
+
+            vec3.normalize(dir, dir);
+
+            const ray = new RAPIER.Ray(new RAPIER.Vector3(origin[0], origin[1], origin[2]), new RAPIER.Vector3(dir[0], dir[1], dir[2]));
+
+            const result = world.castRay(ray, 999, true);
+
+            if (result && result.collider)
+            {
+                // console.log("cast result.collider",result.collider.parent().userData.name)
+
+                const parent = result.collider.parent();
+
+                if (parent && parent.userData)
+                {
+                    outName.set(result.collider.parent().userData.name);
+                }
+                else
+                    outName.set("unknown");
+
+                outX.set(origin[0] + dir[0] * result.timeOfImpact);
+                outY.set(origin[1] + dir[1] * result.timeOfImpact);
+                outZ.set(origin[2] + dir[2] * result.timeOfImpact);
+
+                // console.log(result)
+                outHasHit.set(true);
+            }
+            else
+            {
+                outHasHit.set(false);
+            }
+        }
+        // else
+        // {
+
+        //     const points=inRayPoints.get();
+        //     if(points)
+        //     {
+        //         const ray=new RAPIER.Ray(new RAPIER.Vector3(points[0],points[1],points[2]),new RAPIER.Vector3(points[3]-points[0],points[4]-points[1],points[5]-points[2]));
+
+        //         const result=world.castRay(ray);
+
+        //         if(result && result.collider )
+        //         {
+
+        //             outName.set(result.collider.parent().userData.name);
+        //         }
+
+        //     }
+        // }
+
+        // if (isScreenCoords)
+        // {
+        //     const x = inX.get();
+        //     const y = inY.get();
+
+        //     const origin = vec3.fromValues(x, y, -1);
+        //     mat4.mul(mat, cgl.pMatrix, cgl.vMatrix);
+        //     mat4.invert(mat, mat);
+
+        //     vec3.transformMat4(origin, origin, mat);
+
+        //     // -----------
+
+        //     const to = vec3.fromValues(x, y, 1);
+        //     mat4.mul(mat, cgl.pMatrix, cgl.vMatrix);
+        //     mat4.invert(mat, mat);
+
+        //     vec3.transformMat4(to, to, mat);
+
+        //     afrom = new Ammo.btVector3(origin[0], origin[1], origin[2]);
+        //     ato = new Ammo.btVector3(to[0], to[1], to[2]);
+        // }
+        // else
+        // {
+        //     const arr = inRayPoints.get() || [0, 0, 0, 0, 0, 0];
+
+        //     afrom = new Ammo.btVector3(arr[0], arr[1], arr[2]);
+        //     ato = new Ammo.btVector3(arr[3], arr[4], arr[5]);
+        // }
+
+        // const rayCallback = new Ammo.ClosestRayResultCallback(afrom, ato);
+        // world.world.rayTest(afrom, ato, rayCallback);
+
+        // if (rayCallback.hasHit())
+        // {
+        //     const meta = world.getBodyMeta(rayCallback.get_m_collisionObject());
+
+        //     if (meta)
+        //     {
+        //         world.emitEvent("rayCastHit", meta.name);
+        //         outName.set(meta.name);
+        //     }
+
+        //     outX.set(rayCallback.m_hitPointWorld.x());
+        //     outY.set(rayCallback.m_hitPointWorld.y());
+        //     outZ.set(rayCallback.m_hitPointWorld.z());
+        // }
+        // else
+        // {
+        //     outX.set(0);
+        //     outX.set(0);
+        //     outX.set(0);
+
+        //     world.emitEvent("rayCastHit", null);
+
+        //     outName.set("");
+        // }
+        // outHasHit.set(rayCallback.hasHit());
+
+        // if (rayCallback.hasHit() && inCursor.get())
+        // {
+        //     op.patch.cgl.setCursor("pointer");
+        //     didsetCursor = true;
+        // }
+        // else if (didsetCursor)
+        // {
+        //     op.patch.cgl.setCursor("auto");
+        //     didsetCursor = false;
+        // }
+
+        // Ammo.destroy(rayCallback);
+        // if (afrom)Ammo.destroy(afrom);
+        // if (ato)Ammo.destroy(ato);
+    }
+
+    next.trigger();
+}
