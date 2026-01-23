95ee9fb483e12e303ace66c2cbf1d5f293492e1a
pandrr <tom@tomkom.net>
2026-01-23 14:40:17 +0100

    rapier impulse

---
+++ src/ops/extensions/Ops.Extension.Rapier3d/Ops.Extension.Rapier3d.BodyApplyImpulse/Ops.Extension.Rapier3d.BodyApplyImpulse.js
@@ -0,0 +1,37 @@
+const
+    exec = op.inTrigger("Trigger"),
+    inBodies = op.inArray("Bodies"),
+    inType = op.inSwitch("Type", ["Impulse", "Torque Impulse"], "Impulse"),
+    myVecX = op.inFloat("Vector X"),
+    myVecY = op.inFloat("Vector Y"),
+    myVecZ = op.inFloat("Vector Z"),
+
+    impulse = op.inTriggerButton("Apply"),
+
+    next = op.outTrigger("Next"),
+    result = op.outNumber("Result");
+
+let apply = false;
+
+impulse.onTriggered = () =>
+{
+    apply = true;
+};
+
+exec.onTriggered = () =>
+{
+    const bodies = inBodies.get();
+
+    if (!bodies || !bodies.length) return;
+
+    if (apply)
+    {
+        const t = inType.get() == "Impulse";
+        for (let i = 0; i < bodies.length; i++)
+        {
+            if (t)bodies[i].applyImpulse({ "x": myVecX.get(), "y": myVecY.get(), "z": myVecZ.get() }, true);
+            else bodies[i].applyTorqueImpulse({ "x": myVecX.get(), "y": myVecY.get(), "z": myVecZ.get() }, true);
+        }
+        apply = false;
+    }
+};
