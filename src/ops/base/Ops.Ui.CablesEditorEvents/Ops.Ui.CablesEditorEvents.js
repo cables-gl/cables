fa5d8c206a5eeb991f1b299f6941405668483505
pandrr <tom@tomkom.net>
2026-02-22 08:48:22 +0100

    evends and standalone write file

--- src/ops/base/Ops.Ui.CablesEditorEvents/Ops.Ui.CablesEditorEvents.js
+++ src/ops/base/Ops.Ui.CablesEditorEvents/Ops.Ui.CablesEditorEvents.js
@@ -1,6 +1,9 @@
 const
     outSaving = op.outTrigger("Saving Patch"),
     outEdit = op.outTrigger("PortValueEdited"),
+    outOpAdd = op.outTrigger("Op Add"),
+    outOpDelete = op.outTrigger("Op Delete"),
+    outLink = op.outTrigger("Port Link"),
     inTriggerChanged = op.inTriggerButton("Set Changed Patch");
 
 op.patch.on("uiSavePatch", () =>
@@ -11,6 +14,24 @@ op.patch.on("uiSavePatch", () =>
 op.patch.on("uiSavePatch", () =>
 {
     outSaving.trigger();
+});
+
+op.patch.on("onOpAdd", () =>
+{
+    outOpAdd.trigger();
+});
+
+op.patch.on("onOpDelete", () =>
+{
+    outOpDelete.trigger();
+});
+op.patch.on("onLink", () =>
+{
+    outLink.trigger();
+});
+op.patch.on("onUnLink", () =>
+{
+    outLink.trigger();
 });
 
 if (window.gui)
