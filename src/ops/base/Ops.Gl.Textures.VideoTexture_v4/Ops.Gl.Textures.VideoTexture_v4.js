b9bc1fc1b4cb9d98afa835679c6af6c6323fbb08
pandrr <tom@tomkom.net>
2026-02-26 13:43:55 +0100

    video ops update

---
+++ src/ops/base/Ops.Gl.Textures.VideoTexture_v4/Ops.Gl.Textures.VideoTexture_v4.js
@@ -0,0 +1,380 @@
+const
+    inExec = op.inTrigger("Update"),
+    filename = op.inUrl("file", "video"),
+    play = op.inValueBool("play"),
+    loop = op.inValueBool("loop", true),
+
+    volume = op.inValueSlider("Volume", 1),
+    muted = op.inValueBool("mute", true),
+
+    tfilter = op.inSwitch("Filter", ["nearest", "linear"], "linear"),
+    wrap = op.inValueSelect("Wrap", ["repeat", "mirrored repeat", "clamp to edge"], "clamp to edge"),
+    flip = op.inValueBool("flip", true),
+
+    speed = op.inValueFloat("speed", 1),
+    time = op.inValueFloat("set time"),
+    rewind = op.inTriggerButton("Rewind"),
+
+    inPreload = op.inValueBool("Preload", true),
+    inShowSusp = op.inBool("Show Interaction needed Button", true),
+
+    outNext = op.outTrigger("Next"),
+    textureOut = op.outTexture("texture", null, "texture"),
+    outDuration = op.outNumber("duration"),
+    outProgress = op.outNumber("progress"),
+    outInteractionNeeded = op.outBoolNum("Interaction Needed"),
+    outTime = op.outNumber("CurrentTime"),
+    loading = op.outBoolNum("Loading"),
+    outPlaying = op.outBoolNum("Playing"),
+    canPlayThrough = op.outBoolNum("Can Play Through", false),
+    outEnded = op.outTrigger("Ended"),
+    outUpdated = op.outTrigger("Texture Updated"),
+
+    outWidth = op.outNumber("Width"),
+    outHeight = op.outNumber("Height"),
+    outAspect = op.outNumber("Aspect Ratio"),
+    outHasError = op.outBoolNum("Has Error"),
+    outError = op.outString("Error Message");
+
+op.setPortGroup("Texture", [tfilter, wrap, flip]);
+op.setPortGroup("Audio", [muted, volume]);
+op.setPortGroup("Timing", [time, rewind, speed]);
+
+let videoElementPlaying = false;
+let embedded = false;
+let interActionNeededButton = false;
+let addedListeners = false;
+let cgl_filter = 0;
+let cgl_wrap = 0;
+let tex = null;
+let timeout = null;
+let firstTime = true;
+let needsUpdate = true;
+let lastTime = 0;
+
+const cgl = op.patch.cgl;
+const videoElement = document.createElement("video");
+videoElement.setAttribute("playsinline", "");
+videoElement.setAttribute("webkit-playsinline", "");
+videoElement.setAttribute("autoplay", "autoplay");
+
+const emptyTexture = CGL.Texture.getEmptyTexture(cgl);
+op.toWorkPortsNeedToBeLinked(textureOut);
+
+textureOut.setRef(CGL.Texture.getEmptyTexture(cgl));
+play.onChange = updatePlayState;
+filename.onChange = reload;
+
+volume.onChange =
+    op.onMasterVolumeChanged = updateVolume;
+
+tfilter.onChange = wrap.onChange = () =>
+{
+    if (tex)tex.delete();
+    tex = null;
+};
+
+op.onDelete = () =>
+{
+    if (tex)tex.delete();
+    videoElement.remove();
+};
+
+inExec.onTriggered = () =>
+{
+    if (needsUpdate)
+    {
+        updateTexture();
+    }
+
+    outPlaying.set(!videoElement.paused);
+
+    if (interActionNeededButton && !videoElement.paused && play.get())
+    {
+        // remove button after player says no but plays anyhow after some time...
+        interActionNeededButton = false;
+        CABLES.interActionNeededButton.remove("videoplayer");
+    }
+    outInteractionNeeded.set(interActionNeededButton);
+
+    outNext.trigger();
+};
+
+function reInitTexture()
+{
+    if (tex)tex.delete();
+
+    cgl_filter = CGL.Texture.FILTER_NEAREST;
+    if (tfilter.get() == "linear") cgl_filter = CGL.Texture.FILTER_LINEAR;
+
+    if (wrap.get() == "repeat") cgl_wrap = CGL.Texture.WRAP_REPEAT;
+    if (wrap.get() == "mirrored repeat") cgl_wrap = CGL.Texture.WRAP_MIRRORED_REPEAT;
+    if (wrap.get() == "clamp to edge") cgl_wrap = CGL.Texture.WRAP_CLAMP_TO_EDGE;
+
+    tex = new CGL.Texture(cgl,
+        {
+            "wrap": cgl_wrap,
+            "filter": cgl_filter
+        });
+}
+
+rewind.onTriggered = function ()
+{
+    videoElement.currentTime = 0;
+    textureOut.setRef(emptyTexture);
+    needsUpdate = true;
+};
+
+time.onChange = function ()
+{
+    videoElement.currentTime = time.get() || 0;
+    needsUpdate = true;
+};
+
+function doPlay()
+{
+    videoElement.playbackRate = speed.get();
+}
+
+function updatePlayState()
+{
+    op.setUiError("playvideo", null);
+    embedVideo(true);
+
+    if (play.get())
+    {
+        videoElement.currentTime = time.get() || 0;
+
+        const promise = videoElement.play();
+
+        if (promise)
+            promise.then(function ()
+            {
+                doPlay();
+            }).catch(function (error)
+            {
+                op.setUiError("playvideo", error.message);
+                op.logWarn("exc", error, filename.get());
+                // op.log(error);
+                // op.log(videoElement);
+
+                if (videoElement.paused && inShowSusp.get())
+                {
+                    op.setUiError("playvideo", null);
+                    interActionNeededButton = true;
+                    CABLES.interActionNeededButton.add(op.patch, "videoplayer", () =>
+                    {
+                        interActionNeededButton = false;
+                        videoElement.play().then(() =>
+                        {
+                            doPlay();
+                            CABLES.interActionNeededButton.remove("videoplayer");
+                        }).catch((e) =>
+                        {
+                            op.setUiError("playvideo", e.message);
+                            op.logWarn("playvideo", e);
+                        });
+                    });
+                }
+                // Automatic playback failed.
+                // Show a UI element to let the user manually start playback.
+            });
+    }
+    else videoElement.pause();
+}
+
+speed.onChange = function ()
+{
+    try
+    {
+        op.setUiError("playbackRate", null);
+        videoElement.playbackRate = speed.get();
+    }
+    catch (e)
+    {
+        op.setUiError("playbackRate", "value for 'speed' not supported by browser", 1);
+    }
+};
+
+loop.onChange = function ()
+{
+    videoElement.loop = loop.get();
+};
+
+muted.onChange = function ()
+{
+    videoElement.muted = muted.get();
+};
+
+function updateTexture()
+{
+    const force = needsUpdate;
+    lastTime = performance.now();
+
+    if (!filename.get())
+    {
+        tex = null;
+        textureOut.set(emptyTexture);
+        return;
+    }
+
+    if (!videoElementPlaying) return;
+
+    if (!tex)reInitTexture();
+    if (tex.width != videoElement.videoWidth || tex.height != videoElement.videoHeight)
+    {
+        // op.log("video size", videoElement.videoWidth, videoElement.videoHeight);
+        tex.setSize(videoElement.videoWidth, videoElement.videoHeight);
+    }
+
+    outWidth.set(tex.width);
+    outHeight.set(tex.height);
+    outAspect.set(tex.width / tex.height);
+
+    if (!canPlayThrough.get()) return;
+    if (!videoElementPlaying) return;
+    if (!videoElement) return;
+    if (videoElement.videoHeight <= 0)
+    {
+        op.setUiError("videosize", "video width is 0!");
+        // op.log(videoElement);
+        return;
+    }
+    if (videoElement.videoWidth <= 0)
+    {
+        op.setUiError("videosize", "video height is 0!");
+        // op.log(videoElement);
+        return;
+    }
+
+    const perc = (videoElement.currentTime) / videoElement.duration;
+    if (!isNaN(perc)) outProgress.set(perc);
+
+    outTime.set(videoElement.currentTime);
+
+    cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, tex.tex);
+
+    // if (firstTime)
+    // {
+    cgl.gl.pixelStorei(cgl.gl.UNPACK_FLIP_Y_WEBGL, flip.get());
+    cgl.gl.texImage2D(cgl.gl.TEXTURE_2D, 0, cgl.gl.RGBA, cgl.gl.RGBA, cgl.gl.UNSIGNED_BYTE, videoElement);
+    tex._setFilter();
+    // }
+    // else
+    // {
+    // cgl.gl.pixelStorei(cgl.gl.UNPACK_FLIP_Y_WEBGL, flip.get());
+    // cgl.gl.texSubImage2D(cgl.gl.TEXTURE_2D, 0, 0, 0, cgl.gl.RGBA, cgl.gl.UNSIGNED_BYTE, videoElement);
+    // }
+
+    if (flip.get()) cgl.gl.pixelStorei(cgl.gl.UNPACK_FLIP_Y_WEBGL, false);
+
+    firstTime = false;
+
+    textureOut.setRef(tex);
+    needsUpdate = false;
+    outUpdated.trigger();
+
+    op.patch.cgl.profileData.count("videoPlaying");
+
+    if (videoElement.readyState == 4) loading.set(false);
+    else loading.set(false);
+
+    if (videoElement.requestVideoFrameCallback)
+        videoElement.requestVideoFrameCallback(
+            () =>
+            {
+                needsUpdate = true;
+            }
+        );
+}
+
+function initVideo()
+{
+    videoElement.controls = false;
+    videoElement.muted = muted.get();
+    videoElement.loop = loop.get();
+
+    needsUpdate = true;
+    canPlayThrough.set(true);
+}
+
+function updateVolume()
+{
+    videoElement.volume = Math.min(1, Math.max(0, (volume.get() || 0) * op.patch.config.masterVolume));
+}
+
+function loadedMetaData()
+{
+    outDuration.set(videoElement.duration);
+    updatePlayState();
+}
+
+function embedVideo(force)
+{
+    if (embedded) return;
+
+    outHasError.set(false);
+    outError.set("");
+    canPlayThrough.set(false);
+    if (filename.get() && String(filename.get()).length > 1) firstTime = true;
+
+    if (!filename.get())
+    {
+        outError.set(true);
+    }
+
+    if (inPreload.get() || force)
+    {
+        clearTimeout(timeout);
+        loading.set(true);
+        videoElement.preload = "true";
+
+        let url = op.patch.getFilePath(filename.get());
+        if (String(filename.get()).indexOf("data:") == 0) url = filename.get();
+        if (!url) return;
+
+        op.setUiError("onerror", null);
+        videoElement.style.display = "none";
+        videoElement.setAttribute("src", url);
+        videoElement.setAttribute("crossOrigin", "anonymous");
+        try
+        {
+            op.setUiError("playbackRate", null);
+            videoElement.playbackRate = speed.get();
+        }
+        catch (e)
+        {
+            op.setUiError("playbackRate", "value for 'speed' not supported by browser", 1);
+        }
+        if (!addedListeners)
+        {
+            addedListeners = true;
+            videoElement.addEventListener("canplaythrough", initVideo, true);
+            videoElement.addEventListener("loadedmetadata", loadedMetaData);
+            videoElement.addEventListener("ended", () => { outEnded.trigger(); }, true);
+            videoElement.addEventListener("playing", function () { videoElementPlaying = true; }, true);
+            videoElement.onerror = function ()
+            {
+                outHasError.set(true);
+                if (videoElement)
+                {
+                    outError.set("Error " + videoElement.error.code + "/" + videoElement.error.message);
+                    op.setUiError("onerror", "Could not load video / " + videoElement.error.message, 2);
+                }
+            };
+        }
+        embedded = true;
+    }
+}
+
+function loadVideo()
+{
+    setTimeout(embedVideo, 100);
+}
+
+function reload()
+{
+    if (!filename.get()) return;
+    embedded = false;
+    loadVideo();
+}
---
+++ src/ops/base/Ops.Gl.Textures.WebcamTexture_v4/Ops.Gl.Textures.WebcamTexture_v4.js
@@ -0,0 +1,349 @@
+const
+    inTrigger = op.inTrigger("Render"),
+    inActive = op.inBool("Active", true),
+    inGenTex = op.inValueBool("Generate Texture", true),
+    inInputDevices = op.inDropDown("Webcam Input", ["Default"], "Default"),
+    inWidth = op.inValueInt("Requested Width", 1280),
+    inHeight = op.inValueInt("Requested Height", 720),
+
+    flipX = op.inValueBool("Flip X", false),
+    flipY = op.inValueBool("Flip Y", false),
+
+    inAsDOM = op.inValueBool("Show HTML Element", false),
+    inCss = op.inStringEditor("CSS", "z-index:99999;\nposition:absolute;\n", "inline-css"),
+    htmlFlipX = op.inValueBool("Element Flip X", false),
+    htmlFlipY = op.inValueBool("Element Flip Y", false),
+
+    next = op.outTrigger("Next"),
+    textureOut = op.outTexture("Texture"),
+
+    outRatio = op.outNumber("Ratio"),
+    available = op.outBoolNum("Available"),
+    outWidth = op.outNumber("Size Width"),
+    outHeight = op.outNumber("Size Height"),
+    outError = op.outString("Error"),
+    outElement = op.outObject("HTML Element", null, "element"),
+    outDevices = op.outArray("Available devices"),
+    outSelectedDevice = op.outString("Active device"),
+    outUpdate = op.outTrigger("Texture updated");
+
+op.setPortGroup("Camera", [inInputDevices, inWidth, inHeight]);
+op.setPortGroup("Texture", [flipX, flipY]);
+op.setPortGroup("Video Element", [inAsDOM, inCss, htmlFlipX, htmlFlipY]);
+
+op.toWorkPortsNeedToBeLinked(inTrigger);
+
+let tries = 0;
+const cgl = op.patch.cgl;
+const emptyTexture = CGL.Texture.getEmptyTexture(cgl);
+const videoElement = document.createElement("video");
+const eleId = "webcam" + op.id;
+
+videoElement.setAttribute("id", eleId);
+videoElement.setAttribute("autoplay", "");
+videoElement.setAttribute("muted", "");
+videoElement.setAttribute("playsinline", "");
+videoElement.setAttribute("style", inCss.get());
+op.patch.cgl.canvas.parentElement.parentElement.appendChild(videoElement);
+// let oldCanvasParent = op.patch.cgl.canvas.parentElement;
+
+let tex = null;
+let initingDevices = false;
+let restarting = false;
+let started = false;
+let camsLoaded = false;
+let loadingId = null;
+let currentStream = null;
+let camInputDevices = null;
+let active = false;
+let alreadyRetried = false;
+let constraints = null;
+let tc = null;
+let needsUpdate = true;
+
+textureOut.setRef(emptyTexture);
+
+flipX.onChange =
+flipY.onChange = initCopyShader;
+
+inInputDevices.onChange =
+    inWidth.onChange =
+    inHeight.onChange = restartWebcam;
+htmlFlipX.onChange = htmlFlipY.onChange = flipVideoElement;
+op.onDelete = removeElement;
+inAsDOM.onChange = inCss.onChange = updateStyle;
+
+initTexture();
+updateStyle();
+
+op.on("loadedValueSet", delayedInitDevices);
+inActive.onChange = delayedInitDevices;
+
+delayedInitDevices();
+function delayedInitDevices()
+{
+    setTimeout(() =>
+    {
+        if (inActive.get()) initDevices();
+    }, 50);
+}
+
+function initCopyShader()
+{
+    if (!tc)tc = new CGL.CopyTexture(cgl, "webcamFlippedTexture", { "shader": attachments.texcopy_frag });
+    tc.bgShader.toggleDefine("FLIPX", flipX.get());
+    tc.bgShader.toggleDefine("FLIPY", !flipY.get());
+}
+
+function initTexture()
+{
+    if (tex)tex.delete();
+    tex = new CGL.Texture(cgl, { "name": "webcam" });
+    if (videoElement) tex.setSize(videoElement.videoWidth, videoElement.videoHeight);
+}
+
+function removeElement()
+{
+    if (videoElement) videoElement.remove();
+}
+
+function updateStyle()
+{
+    if (!inAsDOM.get()) videoElement.setAttribute("style", "display:none;");
+    else videoElement.setAttribute("style", inCss.get());
+
+    inCss.setUiAttribs({ "greyout": !inAsDOM.get() });
+    htmlFlipX.setUiAttribs({ "greyout": !inAsDOM.get() });
+    htmlFlipY.setUiAttribs({ "greyout": !inAsDOM.get() });
+}
+
+function flipVideoElement()
+{
+    if (htmlFlipX.get() && !htmlFlipY.get()) videoElement.style.transform = "scaleX(-1)";
+    else if (!htmlFlipX.get() && htmlFlipY.get()) videoElement.style.transform = "scaleY(-1)";
+    else if (htmlFlipX.get() && htmlFlipY.get()) videoElement.style.transform = "scale(-1, -1)";
+    else videoElement.style.transform = "unset";
+}
+
+function playCam(shouldPlay)
+{
+    if (started && camsLoaded)
+    {
+        if (shouldPlay)
+        {
+            active = true;
+            videoElement.play();
+        }
+        else
+        {
+            active = false;
+            videoElement.pause();
+        }
+    }
+}
+
+inGenTex.onChange = () =>
+{
+    playCam(inGenTex.get());
+};
+
+function updateTexture()
+{
+    cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, tex.tex);
+
+    cgl.gl.texImage2D(cgl.gl.TEXTURE_2D, 0, cgl.gl.RGBA, cgl.gl.RGBA, cgl.gl.UNSIGNED_BYTE, videoElement);
+    cgl.gl.bindTexture(cgl.gl.TEXTURE_2D, null);
+    // textureOut.set(emptyTexture);
+
+    if (!tc)initCopyShader();
+    if (tc)textureOut.setRef(tc.copy(tex));
+}
+
+function stopStream()
+{
+    if (!currentStream) return;
+
+    playCam(false);
+    available.set(false);
+
+    currentStream.getTracks().forEach((track) =>
+    {
+        track.stop();
+    });
+    currentStream = null;
+}
+
+function camInitComplete(stream)
+{
+    currentStream = stream;
+    videoElement.srcObject = stream;
+    videoElement.onloadedmetadata = (e) =>
+    {
+        outSelectedDevice.set(stream.getTracks()[0].label);
+        if (inInputDevices.get() != "Default" && stream.getTracks()[0].label != inInputDevices.get() && tries < 3)
+        {
+            tries++;
+            return restartWebcam();
+        }
+
+        const settings = stream.getTracks()[0].getSettings();
+        restarting = false;
+
+        const w = settings.width || inWidth.get();
+        const h = settings.height || inHeight.get();
+
+        outHeight.set(h);
+        outWidth.set(w);
+        outRatio.set(settings.aspectRatio || w / h);
+        outError.set("");
+
+        videoElement.setAttribute("width", settings.width);
+        videoElement.setAttribute("height", settings.height);
+
+        outElement.setRef(videoElement);
+
+        tex.setSize(w, h);
+
+        available.set(true);
+        playCam(inGenTex.get());
+    };
+}
+
+function isCorrectSize()
+{
+    const constraints = getCamConstraints();
+    const check = constraints.video.width == videoElement.videoWidth && constraints.video.height == videoElement.videoHeight;
+    return check;
+}
+
+function getCamConstraints()
+{
+    let constr = { "audio": false, "video": {} };
+
+    if (camsLoaded)
+    {
+        let deviceLabel = inInputDevices.get();
+        let deviceInfo = null;
+
+        if (!deviceLabel || deviceLabel === "Default" || deviceLabel === "...")
+        {
+            deviceInfo = Object.values(camInputDevices)[0];
+        }
+        else
+        {
+            deviceInfo = camInputDevices.filter((d) => { return d.label === deviceLabel; });
+            if (deviceInfo)
+            {
+                deviceInfo = deviceInfo[0];
+            }
+            else
+            { // otherwise get by number
+                deviceInfo = Object.values(camInputDevices)[deviceLabel];
+            }
+
+            if (!deviceInfo)
+            {
+                deviceInfo = Object.values(camInputDevices)[0];
+            }
+        }
+        constr.video = { "deviceId": { "exact": deviceInfo.deviceId } };
+    }
+
+    // constr.video.facingMode = { "exact": inFacing.get() };
+
+    const w = inWidth.get();
+    const h = inHeight.get();
+    let width = { "min": 640 };
+    let height = { "min": 480 };
+
+    if (w)
+        width.ideal = w;
+
+    if (h)
+        height.ideal = h;
+
+    constr.video.width = width;
+    constr.video.height = height;
+
+    return constr;
+}
+
+function restartWebcam()
+{
+    if (!inActive.get()) return;
+    stopStream();
+    restarting = true;
+
+    const constr = getCamConstraints();
+
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+    {
+        navigator.mediaDevices.getUserMedia(constr)
+            .then(camInitComplete)
+            .catch((error) =>
+            {
+                restarting = false;
+                op.logWarn(error.name + ": " + error.message, error);
+                outError.set(error.name + ": " + error.message);
+            });
+    }
+    else if (navigator.getUserMedia)
+    {
+        restarting = false;
+        navigator.getUserMedia(constr, camInitComplete, () => { return available.set(false); });
+    }
+}
+
+function initDevices()
+{
+    if (!inActive.get()) return;
+    initingDevices = true;
+    if (loadingId)cgl.patch.loading.finished(loadingId);
+    loadingId = cgl.patch.loading.start("Webcam inputs", "", op);
+    const constraints = getCamConstraints();
+
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then((res) => { return navigator.mediaDevices.enumerateDevices(); })
+        .then((devices) =>
+        {
+            camInputDevices = devices.filter((device) => { return device.kind === "videoinput"; });
+            initingDevices = false;
+            inInputDevices.uiAttribs.values = camInputDevices.map((d, idx) => { return d.label || idx; });
+            inInputDevices.uiAttribs.values.unshift("Default");
+            outDevices.set(inInputDevices.uiAttribs.values);
+            cgl.patch.loading.finished(loadingId);
+            camsLoaded = true;
+            restartWebcam();
+            started = true;
+            op.refreshParams();
+        }).catch((e) =>
+        {
+            initingDevices = false;
+            // op.error("error", e);
+            outError.set(e.name + ": " + e.message);
+            cgl.patch.loading.finished(loadingId);
+            camsLoaded = false;
+            op.refreshParams();
+        });
+}
+
+inTrigger.onTriggered = () =>
+{
+    if (!initingDevices && inActive.get())
+    {
+        if (started && camsLoaded && active && needsUpdate)
+        {
+            updateTexture();
+            outUpdate.trigger();
+        }
+
+        if (!started && camsLoaded)
+        {
+            restartWebcam();
+        }
+    }
+
+    next.trigger();
+};
