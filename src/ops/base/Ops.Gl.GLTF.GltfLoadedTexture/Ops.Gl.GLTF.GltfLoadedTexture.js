70e08c10b60b8663d4887aaae39ba0e8fd97ad52
pandrr <tom@tomkom.net>
2026-04-30 09:54:08 +0200

    new op gltfloadedtexture

---
+++ src/ops/base/Ops.Gl.GLTF.GltfLoadedTexture/Ops.Gl.GLTF.GltfLoadedTexture.js
@@ -0,0 +1,39 @@
+const
+    inExec = op.inTrigger("Render"),
+    imgName = op.inString("Name", ""),
+    outTex = op.outTexture("Texture"),
+    outFound = op.outBool("Found");
+
+const cgl = op.patch.cgl;
+let tex = null;
+
+let oldScene = null;
+imgName.onChange = () => { reloadSoon(); };
+
+function reloadSoon()
+{
+    tex = null;
+}
+
+inExec.onTriggered = function ()
+{
+    const gltf = cgl.tempData.currentScene;
+    if (gltf != oldScene)tex = null;
+    if (tex) return;
+
+    if (!gltf || !gltf.json || !gltf.chunks) return;
+
+    for (let index = 0; index < gltf.textures.length; index++)
+    {
+        let name = gltf.json.images[index].name;
+        if (name == imgName.get())
+        {
+            outTex.setRef(gltf.textures[index].tex);
+            return;
+        }
+    }
+    outTex.setRef(CGL.Texture.getEmptyTexture(cgl));
+
+// console.log("text",cgl.tempData.currentScene)
+    // outTex.setRef(tex);
+};
