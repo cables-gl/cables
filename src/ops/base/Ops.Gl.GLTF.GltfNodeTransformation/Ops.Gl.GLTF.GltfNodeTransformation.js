57f51cf07f6814b76d8d054228869ba9a90f318b
pandrr <tom@tomkom.net>
2026-05-19 17:35:22 +0200

    ops

---
+++ src/ops/base/Ops.Gl.GLTF.GltfNodeTransformation/Ops.Gl.GLTF.GltfNodeTransformation.js
@@ -0,0 +1,69 @@
+const
+    inExec = op.inTrigger("Render"),
+    inNodeName = op.inString("Node Name"),
+    inSpace = inSwitch("Space", ["World", "Model"], "World"),
+    next = op.outTrigger("Next"),
+    inApply = op.inBool("Set Matrix", true),
+    outFound = op.outBool("Found"),
+    outMat = op.outArray("Matrix");
+
+const cgl = op.patch.cgl;
+const translate = vec3.create();
+let node = null;
+let currentSceneLoaded = null;
+const m = mat4.create();
+
+inNodeName.onChange = function ()
+{
+    node = null;
+    outFound.set(false);
+    op.setUiAttrib({ "extendTitle": inNodeName.get() });
+};
+
+inExec.onTriggered = function ()
+{
+    if (!cgl.tempData.currentScene) return;
+    if (currentSceneLoaded != cgl.tempData.currentScene.loaded) node = null;
+    let found = false;
+
+    if (!node)
+    {
+        const name = inNodeName.get();
+
+        if (!cgl.tempData || !cgl.tempData.currentScene || !cgl.tempData.currentScene.nodes) return;
+
+        currentSceneLoaded = cgl.tempData.currentScene.loaded;
+
+        for (let i = 0; i < cgl.tempData.currentScene.nodes.length; i++)
+        {
+            if (cgl.tempData.currentScene.nodes[i].name == name)
+            {
+                node = cgl.tempData.currentScene.nodes[i];
+                found = true;
+                break;
+            }
+        }
+    }
+    else
+    {
+        found = true;
+    }
+
+    outFound.set(found);
+    cgl.pushModelMatrix();
+
+    if (node)
+    {
+        // node.transform(cgl, node._lastTimeTrans);
+
+        if (inSpace.get()) mat4.copy(m, node.modelMatAbs());
+        else mat4.copy(m, node.mat);
+
+        mat4.multiply(cgl.mMatrix, cgl.mMatrix, m);
+
+        outMat.setRef(m);
+    }
+
+    next.trigger();
+    cgl.popModelMatrix();
+};
