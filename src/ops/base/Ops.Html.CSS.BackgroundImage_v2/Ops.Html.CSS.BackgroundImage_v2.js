812c2126ab95741fd2c5b7ff013487c75aad41c2
steam0r <stephan@undev.de>
2026-07-17 17:02:28 +0200

    set background-position, ... of element without setting image

--- src/ops/base/Ops.Html.CSS.BackgroundImage_v2/Ops.Html.CSS.BackgroundImage_v2.js
+++ src/ops/base/Ops.Html.CSS.BackgroundImage_v2/Ops.Html.CSS.BackgroundImage_v2.js
@@ -10,13 +10,14 @@ const
     outEle = op.outObject("HTML Element");
 
 op.onLoadedValueSet =
-op.onLoaded =
-inPosition.onChange =
-inSize.onChange =
-inEle.onChange =
-inRepeat.onChange =
-active.onChange =
-filename.onChange = update;
+    op.onLoaded =
+    inPosition.onChange =
+    inSize.onChange =
+    inEle.onChange =
+    inEle.onLinkChanged =
+    inRepeat.onChange =
+    active.onChange =
+    filename.onChange = update;
 
 inMode.onChange = () =>
 {
@@ -28,13 +29,14 @@ inMode.onChange = () =>
 
 let ele = null;
 let cacheBust = null;
+let ownBackground = false;
 
 op.onFileChanged = function (fn)
 {
     if (filename.get() && filename.get().indexOf(fn) > -1)
     {
         const mode = inMode.get();
-        if (ele && ele.style)ele.style[mode + "-image"] = "none";
+        if (ele && ele.style) ele.style[mode + "-image"] = "none";
         cacheBust = CABLES.uuid();
         update();
     }
@@ -44,10 +46,10 @@ function remove(mode)
 {
     if (ele)
     {
-        ele.style[mode + "-image"] = "none";
-        ele.style[mode + "-size"] = "initial";
-        ele.style[mode + "-position"] = "initial";
-        ele.style[mode + "-repeat"] = "initial";
+        ele.style.removeProperty(mode + "-image");
+        ele.style.removeProperty(mode + "-size");
+        ele.style.removeProperty(mode + "-position");
+        ele.style.removeProperty(mode + "-repeat");
     }
 }
 
@@ -63,20 +65,27 @@ function update()
 
     ele = inEle.get();
 
-    if (ele && ele.style && filename.get())
+    if (ele && ele.style)
     {
         const mode = inMode.get();
         if (!active.get())
         {
-            if (!inNotUnset.get()) ele.style[mode + "-image"] = "none";
+            if (!inNotUnset.get()) remove(inMode.get());
         }
         else
         {
             let cb = "";
-            let url = op.patch.getFilePath(String(filename.get()));
-            if (cacheBust)url = CABLES.cacheBust(url);
-
-            ele.style[mode + "-image"] = "url(" + url + ")";
+            if (filename.get())
+            {
+                let url = op.patch.getFilePath(String(filename.get()));
+                if (cacheBust) url = CABLES.cacheBust(url);
+                ele.style[mode + "-image"] = "url(" + url + ")";
+                ownBackground = true;
+            }
+            else if (ownBackground)
+            {
+                if (!inNotUnset.get()) ele.style.removeProperty(mode + "-image");
+            }
             ele.style[mode + "-size"] = inSize.get();
             ele.style[mode + "-position"] = inPosition.get();
             ele.style[mode + "-repeat"] = inRepeat.get();
