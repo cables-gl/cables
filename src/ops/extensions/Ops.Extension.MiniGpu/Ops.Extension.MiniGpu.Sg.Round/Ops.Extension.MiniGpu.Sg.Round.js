8e02422d3230d0f7a3dbe74d96a0d3cbc8d2468a
pandrr <tom@tomkom.net>
2026-08-12 11:36:02 +0200

    more shadernodes

---
+++ src/ops/extensions/Ops.Extension.MiniGpu/Ops.Extension.MiniGpu.Sg.Round/Ops.Extension.MiniGpu.Sg.Round.js
@@ -0,0 +1,10 @@
+new CGL.ShaderGraphOp(this,
+    {
+        "type": "function",
+        "name": "round",
+        "params": [
+            { "type": "f32", "port": op.inObject("number", null, "sg_float") }
+        ],
+        "result": { "type": "f32", "port": op.outObject("result", null, "sg_float") }
+
+    });
