64335317c3224e0239368f72e3e01a0caedd1423
pandrr <tom@tomkom.net>
2026-08-14 14:17:28 +0200

    mix op

---
+++ src/ops/extensions/Ops.Extension.ShaderGraph/Ops.Extension.ShaderGraph.Lang.Mix/Ops.Extension.ShaderGraph.Lang.Mix.js
@@ -0,0 +1,12 @@
+new CABLES.ShaderGraphOp(this,
+    {
+        "type": "function",
+        "name": "mix",
+        "params": [
+            { "type": "gen", "port": op.inObject("value 0") },
+            { "type": "gen", "port": op.inObject("value 1") },
+            { "type": "float", "port": op.inObject("fade") }
+        ],
+        "result": { "type": "gen", "port": op.outObject("result") }
+
+    });
