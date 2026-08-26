0b3851c5a61fbd30923845ecb5dd051beedc8bb5
pandrr <tom@tomkom.net>
2026-08-26 10:13:47 +0200

    fragoutput

---
+++ src/ops/extensions/Ops.Extension.ShaderGraph/Ops.Extension.ShaderGraph.Lang.FragOutColor/Ops.Extension.ShaderGraph.Lang.FragOutColor.js
@@ -0,0 +1,20 @@
+new CABLES.ShaderGraphOp(this,
+    {
+        "type": "var",
+        "name": "color",
+        "title": "name",
+        "params": [
+            { "type": "vec4", "port": op.inObject("value") }
+        ],
+        "result": { "type": "vec4", "port": op.outObject("result") }
+    });
+
+const value = op.inString("var name", "color");
+
+op.init =
+    value.onChange =
+    () =>
+    {
+        op.shaderNode.name = value.get();
+        op.shaderNode.result.port.setRef({});
+    };
