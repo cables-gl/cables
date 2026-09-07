24ae2c746b94cc5c0425cf7daee9f08f4a826b6f
pandrr <tom@undev.de>
2026-09-07 13:56:20 -0500

    mapfix

--- src/corelibs/minigpu/target.js
+++ src/corelibs/minigpu/target.js
@@ -151,16 +151,19 @@ export class RenderTarget
             mgpu.commandEncoder.resolveQuerySet(t.querySet, 0, 2, t.resolveBuffer, 0);
             mgpu.commandEncoder.copyBufferToBuffer(t.resolveBuffer, 0, t.resultBuffer, 0, 16);
 
-            t.resultBuffer.mapAsync(GPUMapMode.READ).then(() =>
+            queueMicrotask(() =>
             {
-                const times = new BigInt64Array(t.resultBuffer.getMappedRange());
-                const ns = times[1] - times[0];
-                t.resultBuffer.unmap();
-                if (ns > 0n) self.gpuTimeMs = Number(ns) / 1000000;
-                self._gpuTimerPending = false;
-            }).catch(() =>
-            {
-                self._gpuTimerPending = false;
+                t.resultBuffer.mapAsync(GPUMapMode.READ).then(() =>
+                {
+                    const times = new BigInt64Array(t.resultBuffer.getMappedRange());
+                    const ns = times[1] - times[0];
+                    t.resultBuffer.unmap();
+                    if (ns > 0n) self.gpuTimeMs = Number(ns) / 1000000;
+                    self._gpuTimerPending = false;
+                }).catch(() =>
+                {
+                    self._gpuTimerPending = false;
+                });
             });
         }
     }
