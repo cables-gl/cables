ce2b35b17edd5bda9712cbf9f62601b3b8a0e32a
steam0r <stephan@undev.de>
2026-09-10 16:54:57 +0200

    add upload asset to other patches

--- src/ops/base/Ops.Cables.UploadAsset/Ops.Cables.UploadAsset.js
+++ src/ops/base/Ops.Cables.UploadAsset/Ops.Cables.UploadAsset.js
@@ -1,35 +1,51 @@
 const
     inFilename = op.inString("Filename", "file.bin"),
     inStr = op.inString("Base64 String", ""),
+    inPatchId = op.inString("Patch ID", ""),
     inExec = op.inTriggerButton("Upload"),
     outResult = op.outString("Result"),
     outError = op.outBoolNum("Error"),
     outFinished = op.outTrigger("Finished");
 
+inFilename.onChange = () =>
+{
+    this.setUiError("pathurl", null);
+    if (inFilename.get() && inFilename.get().includes("/"))
+    {
+        this.setUiError("pathurl", "Upload will only use basename of given file.", 1);
+    }
+};
+
+inPatchId.onChange = () =>
+{
+    this.setUiError("patchid", null);
+    const patchId = inPatchId.get();
+    if (patchId && (patchId.length < 6 || (patchId.length > 6 && patchId.length < 24) || patchId.length > 24))
+    {
+        this.setUiError("patchid", "Invalid patchid format", 1);
+    }
+};
+
 inExec.onTriggered = () =>
 {
+
     outError.set(false);
 
     let str = inStr.get();
-    if (str.indexOf("data") != 0)str = "data:;base64," + str;
+    if (str.indexOf("data") != 0) str = "data:;base64," + str;
 
     if (CABLES.UI)
         gui.getFileManager().uploadFile(inFilename.get(), str, (err, res) =>
         {
-            // console.log(err, res);
-
             if (err)
             {
                 outResult.set(err.msg);
                 outError.set(true);
             }
-
-            // if (CABLESUILOADER && CABLESUILOADER.talkerAPI)
-            // {
-            //     CABLESUILOADER.talkerAPI.emitEvent("fileUpdated", { "filename": inFilename.get() });
-            //     console.log("send file updated event");
-            // }
-
+            else
+            {
+                outResult.set(res.url);
+            }
             outFinished.trigger();
-        });
+        }, inPatchId.get());
 };
