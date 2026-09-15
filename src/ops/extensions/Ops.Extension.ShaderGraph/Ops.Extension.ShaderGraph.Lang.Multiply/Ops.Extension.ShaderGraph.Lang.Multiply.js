e672676977b5784127a6681fb2faf18bfe1d3188
pandrr <tom@tomkom.net>
2026-09-15 13:04:09 +0200

    value

--- src/ops/extensions/Ops.Extension.ShaderGraph/Ops.Extension.ShaderGraph.Lang.Multiply/Ops.Extension.ShaderGraph.Lang.Multiply.js
+++ src/ops/extensions/Ops.Extension.ShaderGraph/Ops.Extension.ShaderGraph.Lang.Multiply/Ops.Extension.ShaderGraph.Lang.Multiply.js
@@ -3,8 +3,8 @@ new CABLES.ShaderGraphOp(this,
         "type": "operator",
         "name": "*",
         "params": [
-            { "type": "gen", "name": "number 1" },
-            { "type": "gen", "name": "number 2" }
+            { "type": "gen", "name": "number 1", "value": 1 },
+            { "type": "gen", "name": "number 2", "value": 1 }
         ],
         "results": [{ "type": "gen", "port": op.outObject("result") }]
 
