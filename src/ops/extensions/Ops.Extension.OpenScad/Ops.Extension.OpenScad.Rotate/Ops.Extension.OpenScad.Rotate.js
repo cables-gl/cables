83f0eed25ea371467f814043aa547248929494c9
pandrr <tom@tomkom.net>
2026-09-24 15:52:57 +0200

    openscad

---
+++ src/ops/extensions/Ops.Extension.OpenScad/Ops.Extension.OpenScad.Rotate/Ops.Extension.OpenScad.Rotate.js
@@ -0,0 +1,22 @@
+const
+    update = op.inTrigger("Update"),
+    inX=op.inFloat("X",0),
+    inY=op.inFloat("Y",0),
+    inZ=op.inFloat("Z",0),
+    next = op.outTrigger("Next");
+
+update.onTriggered = () =>
+{
+    const os=op.patch.tempData.compScad;
+    os.op(op);
+    os.addLine( "rotate(["+os.portValue(inX)+","+os.portValue(inY)+","+os.portValue(inZ)+"])");
+
+    os.addLine( "{");
+    os.indentStart();
+
+    next.trigger();
+    os.indentEnd();
+
+    os.addLine( "}");
+    os.addLine( );
+};
